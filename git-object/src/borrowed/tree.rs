9694fcbeb7bea6ebf814119ba5757110ae33bc55
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-06-18 17:21:58 +0800

    first tree implementation, which seems to work well

--- git-object/src/borrowed/tree.rs
+++ git-object/src/borrowed/tree.rs
@@ -1,14 +1,36 @@
+use crate::borrowed::util::SPACE;
 use crate::borrowed::Error;
-use bstr::BStr;
+use bstr::{BStr, ByteSlice};
+use nom::bytes::complete::{tag, take, take_while1, take_while_m_n};
+use nom::character::is_digit;
+use nom::multi::many1;
+use nom::sequence::terminated;
 use nom::IResult;
+use std::convert::TryFrom;
 
-#[derive(PartialEq, Eq, Debug, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
+#[repr(u16)]
 pub enum Mode {
-    Tree = 0o040000,
+    Tree = 0o040000u16,
     Blob = 0o100644,
     BlobExecutable = 0o100755,
     Link = 0o120000,
     Commit = 0o160000,
+}
+
+impl TryFrom<&[u8]> for Mode {
+    type Error = Error;
+
+    fn try_from(mode: &[u8]) -> Result<Self, Self::Error> {
+        Ok(match mode {
+            b"40000" => Mode::Tree,
+            b"100644" => Mode::Blob,
+            b"100755" => Mode::BlobExecutable,
+            b"120000" => Mode::Link,
+            b"160000" => Mode::Commit,
+            _ => return Err(Error::NomDetail(mode.into(), "unknown tree mode")),
+        })
+    }
 }
 
 #[derive(PartialEq, Eq, Debug, Hash)]
@@ -22,6 +44,25 @@ pub struct Entry<'data> {
 #[derive(PartialEq, Eq, Debug, Hash)]
 pub struct Tree<'data>(pub Vec<Entry<'data>>);
 
+const NULL: &[u8] = b"\0";
+fn parse_entry(i: &[u8]) -> IResult<&[u8], Entry, Error> {
+    let (i, mode) = terminated(take_while_m_n(5, 6, is_digit), tag(SPACE))(i)?;
+    let mode = Mode::try_from(mode).map_err(nom::Err::Error)?;
+    let (i, filename) = terminated(take_while1(|b| b != NULL[0]), tag(NULL))(i)?;
+    let (i, oid) = take(20u8)(i)?;
+
+    Ok((
+        i,
+        Entry {
+            mode,
+            filename: filename.as_bstr(),
+            oid,
+        },
+    ))
+}
+
 pub(crate) fn parse(i: &[u8]) -> IResult<&[u8], Tree, Error> {
-    unimplemented!("todo tree parse")
+    let (i, mut entries) = many1(parse_entry)(i)?;
+    entries.shrink_to_fit();
+    Ok((i, Tree(entries)))
 }
