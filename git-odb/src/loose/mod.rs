ff350323e4a424df8c17a9dca53cc8967e45e960
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-06-21 11:20:02 +0800

    Allow skipping the header when decompressing files (streaming)

--- git-odb/src/loose/mod.rs
+++ git-odb/src/loose/mod.rs
@@ -6,23 +6,44 @@ mod object;
 
 pub mod io {
     use crate::zlib::stream::InflateReader;
-    use std::io::{BufReader, Bytes, Read};
-    use std::iter::Skip;
+    use std::io::BufReader;
 
     pub enum ObjectReader {
-        File(InflateReader<BufReader<std::fs::File>>),
+        File(usize, InflateReader<BufReader<std::fs::File>>),
     }
 
     impl ObjectReader {
         pub fn from_read(header_size: usize, file: std::fs::File) -> ObjectReader {
-            ObjectReader::File(InflateReader::new(file))
+            ObjectReader::File(header_size, InflateReader::new(file))
         }
     }
 
     impl std::io::Read for ObjectReader {
         fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
             match self {
-                ObjectReader::File(r) => r.read(buf),
+                ObjectReader::File(header_size_left, r) => {
+                    if *header_size_left == 0 {
+                        r.read(buf)
+                    } else {
+                        // We must assure we return at least one byte - otherwise it's considered EOF, thus '>='
+                        if *header_size_left >= buf.len() {
+                            {
+                                let mut tmp = [0u8; 32];
+                                assert!(
+                                    *header_size_left <= tmp.len(),
+                                    "encountered unusually large header"
+                                );
+                                r.read_exact(&mut tmp[..*header_size_left])?;
+                                *header_size_left = 0;
+                            }
+                            r.read(buf)
+                        } else {
+                            r.read_exact(&mut buf[..*header_size_left])?;
+                            *header_size_left = 0;
+                            r.read(buf)
+                        }
+                    }
+                }
             }
         }
     }
