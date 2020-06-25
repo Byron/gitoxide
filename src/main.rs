8c0e0b5bb79c8c337eed03d37cbf818d8bb9c924
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-06-25 11:39:29 +0800

    Add simple pack verification to gio

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use anyhow::{Context, Result};
 use structopt::StructOpt;
 
 mod options {
+    use std::path::PathBuf;
     use structopt::clap::AppSettings;
     use structopt::StructOpt;
 
@@ -16,12 +17,31 @@ mod options {
         pub cmd: Subcommands,
     }
 
+    /// Low-level commands that are not used every day
+    #[derive(Debug, StructOpt)]
+    pub enum Plumbing {
+        /// Verify the integrity of a pack or index file
+        #[structopt(setting = AppSettings::ColoredHelp)]
+        VerifyPack {
+            /// The '.pack' file whose checksum to validate.
+            ///
+            /// '.pack' files have a 20 byte trailer representing the Sha1 over all the bytes that
+            /// preceded it.
+            #[structopt(parse(from_os_str))]
+            path: PathBuf,
+        },
+    }
+
     #[derive(Debug, StructOpt)]
     pub enum Subcommands {
         /// Initialize the repository in the current directory.
         #[structopt(alias = "initialize")]
         #[structopt(setting = AppSettings::ColoredHelp)]
         Init,
+
+        #[structopt(alias = "p")]
+        #[structopt(setting = AppSettings::ColoredHelp)]
+        Plumbing(Plumbing),
     }
 }
 
@@ -31,6 +51,15 @@ fn main() -> Result<()> {
         options::Subcommands::Init => {
             git_repository::init::repository().with_context(|| "Repository initialization failed")
         }
+        options::Subcommands::Plumbing(cmd) => match cmd {
+            options::Plumbing::VerifyPack { path } => {
+                let pack =
+                    git_odb::pack::File::at(path).with_context(|| "Could not open pack file")?;
+                pack.verify_checksum().with_context(|| "Failed")?;
+                println!("OK");
+                Ok(())
+            }
+        },
     }?;
     Ok(())
 }
