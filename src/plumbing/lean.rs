aaf482584d1ee080c0a6c091c4675736c4c8d6a7
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-06-30 16:08:02 +0800

    --verbose flag for lean plumbing binary

--- src/plumbing/lean.rs
+++ src/plumbing/lean.rs
@@ -19,6 +19,9 @@ mod options {
     #[derive(FromArgs, PartialEq, Debug)]
     #[argh(subcommand, name = "verify-pack")]
     pub struct VerifyPack {
+        /// if set, verbose progress messages are printed line by line
+        #[argh(switch, short = 'v')]
+        pub verbose: bool,
         /// the '.pack' or '.idx' file whose checksum to validate.
         #[argh(positional)]
         pub path: PathBuf,
@@ -30,16 +33,26 @@ use git_features::progress;
 use gitoxide_core as core;
 use std::io::{stderr, stdout};
 
+fn init_log(verbose: bool) {
+    if verbose {
+        env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+    } else {
+        env_logger::init();
+    }
+}
+
 pub fn main() -> Result<()> {
     pub use options::*;
     let cli: Args = argh::from_env();
-    env_logger::init();
     match cli.subcommand {
-        SubCommands::VerifyPack(VerifyPack { path }) => core::verify_pack_or_pack_index(
-            path,
-            progress::Log::new("verify").into(),
-            stdout(),
-            stderr(),
-        ),
+        SubCommands::VerifyPack(VerifyPack { path, verbose }) => {
+            init_log(verbose);
+            core::verify_pack_or_pack_index(
+                path,
+                progress::Log::new("verify").into(),
+                stdout(),
+                stderr(),
+            )
+        }
     }
 }
