0fbba9fe7597af03912f956c251c88472b48c3eb
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-06-30 10:07:11 +0800

    refactor

--- src/porcelain-cli.rs
+++ src/porcelain-cli.rs
@@ -1,19 +1,15 @@
 #![forbid(unsafe_code)]
 
-#[cfg(feature = "pretty-cli")]
-mod pretty;
-
-#[cfg(all(feature = "lean-cli", not(feature = "pretty-cli")))]
-mod lean;
+mod porcelain;
 
 use anyhow::Result;
 
 #[cfg(feature = "pretty-cli")]
 fn main() -> Result<()> {
-    pretty::main()
+    porcelain::pretty::main()
 }
 
 #[cfg(all(feature = "lean-cli", not(feature = "pretty-cli")))]
 fn main() -> Result<()> {
-    lean::main()
+    porcelain::lean::main()
 }
