22880881d5e442acdeb8dd0cfb5ecc4f62783951
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-06 07:18:52 +0800

    Fixup text file tests on windows

--- git-object/tests/borrowed/mod.rs
+++ git-object/tests/borrowed/mod.rs
@@ -5,8 +5,21 @@ mod commit;
 mod tag;
 mod tree;
 
+#[cfg(not(windows))]
+fn fixup(v: Vec<u8>) -> Vec<u8> {
+    v
+}
+
+#[cfg(windows)]
+fn fixup(v: Vec<u8>) -> Vec<u8> {
+    // Git checks out text files with line ending conversions, git itself will of course not put '\r\n' anywhere,
+    // so that wouldn't be expected in an object and doesn't have to be parsed.
+    use bstr::{ByteSlice, ByteVec};
+    v.replace(b"\r\n", "\n")
+}
+
 fn fixture_bytes(kind: &str, path: &str) -> Vec<u8> {
-    super::fixture_bytes(PathBuf::from(kind).join(path).to_str().unwrap())
+    fixup(super::fixture_bytes(PathBuf::from(kind).join(path).to_str().unwrap()))
 }
 
 fn signature(time: u32) -> Signature<'static> {
