5688a3427ff3673e1422d43106f4d685fa837aed
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-12 12:21:56 +0800

    Make crates publishable

--- etc/release.sh
+++ etc/release.sh
@@ -3,5 +3,5 @@
 set -eu -o pipefail
 
 for crate in git-features git-object git-odb git-repository git-transport gitoxide-core .; do
-  (cd $crate && cargo release)
+  (cd $crate && cargo release "$@")
 done
