9f825b849d14494a2d58a09eb6499fd86fd05af3
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-13 22:07:30 +0800

    refactor

--- git-odb/src/loose/db/mod.rs
+++ git-odb/src/loose/db/mod.rs
@@ -8,7 +8,6 @@ use crate::{
 };
 use smallvec::SmallVec;
 use std::{fs, io::Cursor, io::Read, path::PathBuf};
-use walkdir::WalkDir;
 
 pub(crate) mod serde;
 
@@ -19,9 +18,6 @@ pub struct Db {
 quick_error! {
     #[derive(Debug)]
     pub enum Error {
-        WalkDir(err: walkdir::Error) {
-            cause(err)
-        }
         DecompressFile(err: zlib::Error, path: PathBuf) {
             display("decompression of loose object at '{}' failed", path.display())
             cause(err)
@@ -55,47 +51,66 @@ fn sha1_path(id: &object::Id, mut root: PathBuf) -> PathBuf {
     root
 }
 
-impl Db {
-    pub fn at(path: impl Into<PathBuf>) -> Db {
-        Db { path: path.into() }
+pub mod iter {
+    use crate::loose::Db;
+    use git_object as object;
+    use quick_error::quick_error;
+    use walkdir::WalkDir;
+
+    quick_error! {
+        #[derive(Debug)]
+        pub enum Error {
+            WalkDir(err: walkdir::Error) {
+                cause(err)
+            }
+        }
     }
-    pub fn iter(&self) -> impl Iterator<Item = Result<object::Id, Error>> {
-        use std::path::Component::Normal;
-        // TODO: Put this behind a feature flag in git-features and allow iterting with jwalk
-        WalkDir::new(&self.path)
-            .min_depth(2)
-            .max_depth(3)
-            .follow_links(false)
-            .into_iter()
-            .filter_map(|res| {
-                let mut is_valid_path = false;
-                let e = res.map_err(Error::WalkDir).map(|e| {
-                    let p = e.path();
-                    let (c1, c2) = p.components().fold((None, None), |(_c1, c2), cn| (c2, Some(cn)));
-                    if let (Some(Normal(c1)), Some(Normal(c2))) = (c1, c2) {
-                        if c1.len() == 2 && c2.len() == 38 {
-                            if let (Some(c1), Some(c2)) = (c1.to_str(), c2.to_str()) {
-                                let mut buf = [0u8; 40];
-                                {
-                                    let (first_byte, rest) = buf.split_at_mut(2);
-                                    first_byte.copy_from_slice(c1.as_bytes());
-                                    rest.copy_from_slice(c2.as_bytes());
-                                }
-                                if let Ok(b) = object::Id::from_hex(&buf[..]) {
-                                    is_valid_path = true;
-                                    return b;
+
+    impl Db {
+        pub fn iter(&self) -> impl Iterator<Item = Result<object::Id, Error>> {
+            use std::path::Component::Normal;
+            // TODO: Put this behind a feature flag in git-features and allow iterting with jwalk
+            WalkDir::new(&self.path)
+                .min_depth(2)
+                .max_depth(3)
+                .follow_links(false)
+                .into_iter()
+                .filter_map(|res| {
+                    let mut is_valid_path = false;
+                    let e = res.map_err(Error::WalkDir).map(|e| {
+                        let p = e.path();
+                        let (c1, c2) = p.components().fold((None, None), |(_c1, c2), cn| (c2, Some(cn)));
+                        if let (Some(Normal(c1)), Some(Normal(c2))) = (c1, c2) {
+                            if c1.len() == 2 && c2.len() == 38 {
+                                if let (Some(c1), Some(c2)) = (c1.to_str(), c2.to_str()) {
+                                    let mut buf = [0u8; 40];
+                                    {
+                                        let (first_byte, rest) = buf.split_at_mut(2);
+                                        first_byte.copy_from_slice(c1.as_bytes());
+                                        rest.copy_from_slice(c2.as_bytes());
+                                    }
+                                    if let Ok(b) = object::Id::from_hex(&buf[..]) {
+                                        is_valid_path = true;
+                                        return b;
+                                    }
                                 }
                             }
                         }
+                        object::Id::null()
+                    });
+                    if is_valid_path {
+                        Some(e)
+                    } else {
+                        None
                     }
-                    object::Id::null()
-                });
-                if is_valid_path {
-                    Some(e)
-                } else {
-                    None
-                }
-            })
+                })
+        }
+    }
+}
+
+impl Db {
+    pub fn at(path: impl Into<PathBuf>) -> Db {
+        Db { path: path.into() }
     }
 
     const OPEN_ACTION: &'static str = "open";
--- git-odb/src/loose/mod.rs
+++ git-odb/src/loose/mod.rs
@@ -1,7 +1,7 @@
 const HEADER_READ_COMPRESSED_BYTES: usize = 256;
 const HEADER_READ_UNCOMPRESSED_BYTES: usize = 512;
 
-pub(crate) mod db;
+pub mod db;
 
 pub mod object;
 pub use object::Object;
