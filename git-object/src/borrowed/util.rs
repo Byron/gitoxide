256581bad6692a458b331b712d16ce2d5143cb75
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-15 08:48:10 +0800

    refactor

--- git-object/src/borrowed/util.rs
+++ git-object/src/borrowed/util.rs
@@ -92,4 +92,63 @@ pub(crate) fn parse_signature(i: &[u8]) -> IResult<&[u8], Signature, Error> {
 }
 
 #[cfg(test)]
-mod tests;
+mod tests {
+    mod parse_signature {
+        use crate::borrowed::util::parse_signature;
+        use crate::borrowed::Signature;
+        use crate::ByteSlice;
+        use crate::{Sign, Time};
+
+        fn signature(
+            name: &'static str,
+            email: &'static str,
+            time: u32,
+            sign: Sign,
+            offset: i32,
+        ) -> Signature<'static> {
+            Signature {
+                name: name.as_bytes().as_bstr(),
+                email: email.as_bytes().as_bstr(),
+                time: Time { time, offset, sign },
+            }
+        }
+
+        #[test]
+        fn tz_minus() {
+            assert_eq!(
+                parse_signature(b"Sebastian Thiel <byronimo@gmail.com> 1528473343 -0230")
+                    .unwrap()
+                    .1,
+                signature("Sebastian Thiel", "byronimo@gmail.com", 1528473343, Sign::Minus, -9000)
+            );
+        }
+
+        #[test]
+        fn tz_plus() {
+            assert_eq!(
+                parse_signature(b"Sebastian Thiel <byronimo@gmail.com> 1528473343 +0230")
+                    .unwrap()
+                    .1,
+                signature("Sebastian Thiel", "byronimo@gmail.com", 1528473343, Sign::Plus, 9000)
+            );
+        }
+
+        #[test]
+        fn negative_offset_0000() {
+            assert_eq!(
+                parse_signature(b"Sebastian Thiel <byronimo@gmail.com> 1528473343 -0000")
+                    .unwrap()
+                    .1,
+                signature("Sebastian Thiel", "byronimo@gmail.com", 1528473343, Sign::Minus, 0)
+            );
+        }
+
+        #[test]
+        fn empty_name_and_email() {
+            assert_eq!(
+                parse_signature(b" <> 12345 -1215").unwrap().1,
+                signature("", "", 12345, Sign::Minus, -44100)
+            );
+        }
+    }
+}
