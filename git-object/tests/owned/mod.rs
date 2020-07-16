3b9d66c932075feb08cdf2967f7698daef9fd3ff
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-16 09:33:11 +0800

    refactor: make reusing round-trip code easier

--- git-object/tests/owned/mod.rs
+++ git-object/tests/owned/mod.rs
@@ -1,2 +1,35 @@
+macro_rules! round_trip {
+    ($owned:ty, $borrowed:ty, $( $files:literal ), +) => {
+        // Git checks out text files with different line feeds, which causes parsing failure.
+        // No way to configure this in the checkout action :/
+        #[cfg_attr(windows, ignore)]
+        #[test]
+        fn round_trip() {
+            use crate::fixture_bytes;
+            use bstr::ByteSlice;
+            for input in &[
+                $( $files ),*
+            ] {
+                let input = fixture_bytes(input);
+                let item: $owned = <$borrowed>::from_bytes(&input).unwrap().into();
+                let mut output = Vec::new();
+                item.write_to(&mut output).unwrap();
+                assert_eq!(input.as_bstr(), output.as_bstr());
+            }
+        }
+    };
+}
+
 mod object;
-mod tag;
+mod tag {
+    use git_object::{borrowed, owned};
+
+    round_trip!(
+        owned::Tag,
+        borrowed::Tag,
+        "tag/empty.txt",
+        "tag/whitespace.txt",
+        "tag/with-newlines.txt",
+        "tag/signed.txt"
+    );
+}
