b1f9ae825e65eeb64dec41c1345b85287912b3e3
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-16 14:26:33 +0800

    enforce checking the package size before releases

--- etc/release.sh
+++ etc/release.sh
@@ -2,6 +2,8 @@
 
 set -eu -o pipefail
 
+./etc/check-package-size.sh
+
 for crate in git-features git-ref git-object git-odb git-repository git-transport gitoxide-core .; do
   (cd $crate && cargo release "$@")
 done
