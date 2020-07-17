7447340bad095c580d3252e559650a798b77308b
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-17 11:01:07 +0800

    more space for git-object crates

--- etc/check-package-size.sh
+++ etc/check-package-size.sh
@@ -18,7 +18,7 @@ echo "in root: gitoxide CLI"
 indent cargo diet -n --package-size-limit 20KB
 (enter git-features && indent cargo diet -n --package-size-limit 6KB)
 (enter git-ref && indent cargo diet -n --package-size-limit 4KB)
-(enter git-object && indent cargo diet -n --package-size-limit 10KB)
+(enter git-object && indent cargo diet -n --package-size-limit 15KB)
 (enter git-odb && indent cargo diet -n --package-size-limit 25KB)
 (enter git-repository && indent cargo diet -n --package-size-limit 10KB)
 (enter git-transport && indent cargo diet -n --package-size-limit 4KB)
