7c2f60a7fe08f7cc5e4bbc8228470de75511ecf1
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-20 10:48:09 +0800

    refactor

--- tests/stateless-journey.sh
+++ tests/stateless-journey.sh
@@ -38,61 +38,60 @@ title "CLI ${kind}"
       )
     )
   )
+)
 
-  (when "running 'plumbing verify pack"
-    (with "a valid pack file"
-      PACK_FILE="$fixtures/packs/pack-11fdfa9e156ab73caae3b6da867192221f2089c2.pack"
-      it "verifies the pack successfully and with desired output" && {
-        WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-success" \
-        expect_run $SUCCESSFULLY "$exe_plumbing" verify-pack "$PACK_FILE"
+(when "running 'plumbing verify-pack"
+  (with "a valid pack file"
+    PACK_FILE="$fixtures/packs/pack-11fdfa9e156ab73caae3b6da867192221f2089c2.pack"
+    it "verifies the pack successfully and with desired output" && {
+      WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-success" \
+      expect_run $SUCCESSFULLY "$exe_plumbing" verify-pack "$PACK_FILE"
+    }
+  )
+  (with "a valid pack INDEX file"
+    PACK_INDEX_FILE="$fixtures/packs/pack-11fdfa9e156ab73caae3b6da867192221f2089c2.idx"
+    (with "no statistics"
+      it "verifies the pack index successfully and with desired output" && {
+        WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-success" \
+        expect_run $SUCCESSFULLY "$exe_plumbing" verify-pack "$PACK_INDEX_FILE"
       }
     )
-    (with "a valid pack INDEX file"
-      PACK_INDEX_FILE="$fixtures/packs/pack-11fdfa9e156ab73caae3b6da867192221f2089c2.idx"
-      (with "no statistics"
-        it "verifies the pack index successfully and with desired output" && {
-          WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-success" \
-          expect_run $SUCCESSFULLY "$exe_plumbing" verify-pack "$PACK_INDEX_FILE"
-        }
-      )
-      (with "statistics"
-        it "verifies the pack index successfully and with desired output" && {
-          WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-with-statistics-success" \
-          expect_run $SUCCESSFULLY "$exe_plumbing" verify-pack --statistics "$PACK_INDEX_FILE"
-        }
-      )
-      (with "decode"
-        it "verifies the pack index successfully and with desired output, and decodes all objects" && {
-          WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-success" \
-          expect_run $SUCCESSFULLY "$exe_plumbing" verify-pack --decode "$PACK_INDEX_FILE"
-        }
-      )
-      (with "decode"
-        it "verifies the pack index successfully and with desired output, and re-encodes all objects" && {
-          WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-success" \
-          expect_run $SUCCESSFULLY "$exe_plumbing" verify-pack --re-encode "$PACK_INDEX_FILE"
-        }
-      )
-      if test "$kind" = "max"; then
-      (with "statistics (JSON)"
-        it "verifies the pack index successfully and with desired output" && {
-          WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-with-statistics-json-success" \
-          expect_run $SUCCESSFULLY "$exe_plumbing" --threads 1 verify-pack --statistics --format json "$PACK_INDEX_FILE"
-        }
-      )
-      fi
+    (with "statistics"
+      it "verifies the pack index successfully and with desired output" && {
+        WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-with-statistics-success" \
+        expect_run $SUCCESSFULLY "$exe_plumbing" verify-pack --statistics "$PACK_INDEX_FILE"
+      }
     )
-    (sandbox
-      (with "an INvalid pack INDEX file"
-        PACK_INDEX_FILE="$fixtures/packs/pack-11fdfa9e156ab73caae3b6da867192221f2089c2.idx"
-        cp $PACK_INDEX_FILE index.idx
-        echo $'\0' >> index.idx
-        it "fails to verify the pack index and with desired output" && {
-          WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-failure" \
-          expect_run $WITH_FAILURE "$exe_plumbing" verify-pack index.idx
-        }
-      )
+    (with "decode"
+      it "verifies the pack index successfully and with desired output, and decodes all objects" && {
+        WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-success" \
+        expect_run $SUCCESSFULLY "$exe_plumbing" verify-pack --decode "$PACK_INDEX_FILE"
+      }
+    )
+    (with "decode"
+      it "verifies the pack index successfully and with desired output, and re-encodes all objects" && {
+        WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-success" \
+        expect_run $SUCCESSFULLY "$exe_plumbing" verify-pack --re-encode "$PACK_INDEX_FILE"
+      }
+    )
+    if test "$kind" = "max"; then
+    (with "statistics (JSON)"
+      it "verifies the pack index successfully and with desired output" && {
+        WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-with-statistics-json-success" \
+        expect_run $SUCCESSFULLY "$exe_plumbing" --threads 1 verify-pack --statistics --format json "$PACK_INDEX_FILE"
+      }
+    )
+    fi
+  )
+  (sandbox
+    (with "an INvalid pack INDEX file"
+      PACK_INDEX_FILE="$fixtures/packs/pack-11fdfa9e156ab73caae3b6da867192221f2089c2.idx"
+      cp $PACK_INDEX_FILE index.idx
+      echo $'\0' >> index.idx
+      it "fails to verify the pack index and with desired output" && {
+        WITH_SNAPSHOT="$snapshot/plumbing-verify-pack-index-failure" \
+        expect_run $WITH_FAILURE "$exe_plumbing" verify-pack index.idx
+      }
     )
   )
 )
-
