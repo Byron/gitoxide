6225f36cc4735dd41b0c01d7c7ce6ed61f384e9a
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-21 13:45:14 +0800

    Make chunk statistics independent of traversal method

--- git-odb/src/pack/index/verify/reduce.rs
+++ git-odb/src/pack/index/verify/reduce.rs
@@ -22,7 +22,6 @@ pub struct Reducer<'a, P> {
     pub progress: &'a std::sync::Mutex<P>,
     pub then: Instant,
     pub entries_seen: u32,
-    pub chunks_seen: usize,
     pub stats: verify::Outcome,
 }
 
@@ -35,7 +34,6 @@ where
             progress: &progress,
             then: Instant::now(),
             entries_seen: 0,
-            chunks_seen: 0,
             stats: verify::Outcome {
                 average: decode::Outcome::default_from_kind(git_object::Kind::Tree),
                 objects_per_chain_length: Default::default(),
@@ -58,33 +56,33 @@ where
 
     fn feed(&mut self, input: Self::Input) -> Result<(), Self::Error> {
         let chunk_stats: Vec<_> = input?;
-        let num_entries_in_chunk = chunk_stats.len();
-        self.entries_seen += num_entries_in_chunk as u32;
-        self.chunks_seen += 1;
+        self.entries_seen += chunk_stats.len() as u32;
 
-        let mut chunk_average = chunk_stats.into_iter().fold(
+        let chunk_total = chunk_stats.into_iter().fold(
             decode::Outcome::default_from_kind(git_object::Kind::Tree),
-            |mut average, stats| {
+            |mut total, stats| {
                 *self.stats.objects_per_chain_length.entry(stats.num_deltas).or_insert(0) += 1;
                 self.stats.total_decompressed_entries_size += stats.decompressed_size;
                 self.stats.total_compressed_entries_size += stats.compressed_size as u64;
                 self.stats.total_object_size += stats.object_size as u64;
-                add_decode_result(&mut average, stats);
-                average
+                add_decode_result(&mut total, stats);
+                total
             },
         );
-        div_decode_result(&mut chunk_average, num_entries_in_chunk);
-        add_decode_result(&mut self.stats.average, chunk_average);
 
+        add_decode_result(&mut self.stats.average, chunk_total);
         self.progress.lock().unwrap().set(self.entries_seen);
         Ok(())
     }
 
     fn finalize(mut self) -> Result<Self::Output, Self::Error> {
         self.progress.lock().unwrap().done("finished");
-        div_decode_result(&mut self.stats.average, self.chunks_seen);
-        let elapsed_s = Instant::now().duration_since(self.then).as_secs_f32();
+
+        div_decode_result(&mut self.stats.average, self.entries_seen as usize);
+
+        let elapsed_s = self.then.elapsed().as_secs_f32();
         let objects_per_second = (self.entries_seen as f32 / elapsed_s) as u32;
+
         self.progress.lock().unwrap().info(format!(
             "Verified {} objects in {:.2}s ({} objects/s, ~{}/s)",
             self.entries_seen,
