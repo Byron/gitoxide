97eb524ffa4cbf04113d3a622aca3a76606f0d96
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-07-31 08:59:20 +0800

    refactor

--- git-odb/src/pack/index/write/mod.rs
+++ git-odb/src/pack/index/write/mod.rs
@@ -2,43 +2,11 @@ use crate::{hash, pack, pack::index::util::Chunks, pack::index::V2_SIGNATURE};
 use byteorder::{BigEndian, WriteBytesExt};
 use git_features::{parallel, parallel::in_parallel_if, progress::Progress};
 use git_object::owned;
-use quick_error::quick_error;
 use smallvec::alloc::collections::BTreeMap;
 use std::io;
 
-quick_error! {
-    #[derive(Debug)]
-    pub enum Error {
-        Io(err: io::Error) {
-            display("An IO error occurred when reading the pack or creating a temporary file")
-            from()
-            source(err)
-        }
-        PackEntryDecode(err: pack::data::iter::Error) {
-            display("A pack entry could not be extracted")
-            from()
-            source(err)
-        }
-        Unsupported(kind: pack::index::Kind) {
-            display("Indices of type {} cannot be written, only {} are supported", *kind as usize, pack::index::Kind::default() as usize)
-        }
-        RefDelta {
-            display("Ref delta objects are not supported as there is no way to look them up. Resolve them beforehand.")
-        }
-        IteratorInvariantTrailer {
-            display("The iterator failed to set a trailing hash over all prior pack entries in the last provided entry")
-        }
-        IteratorInvariantNonEmpty {
-            display("Is there ever a need to create empty indices? If so, please post a PR.")
-        }
-        IteratorInvariantBasesPresent {
-            display("Did not find a single base")
-        }
-        IteratorInvariantBasesBeforeDeltasNeedThem(delta_pack_offset: u64, base_pack_offset: u64) {
-            display("The delta at pack offset {} could not find its base at {} - it should have been seen already", delta_pack_offset, base_pack_offset)
-        }
-    }
-}
+mod error;
+pub use error::Error;
 
 #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
