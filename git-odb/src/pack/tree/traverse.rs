6629e3043786e5caf7d2b6fedc9350cd9e7bc6fb
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-08-05 11:14:04 +0800

    make traversal part of the tree for greater ease of use

--- git-odb/src/pack/tree/traverse.rs
+++ git-odb/src/pack/tree/traverse.rs
@@ -1,35 +1,47 @@
-use crate::{pack::index::write::EntrySlice, pack::tree::Node};
+use crate::{
+    pack::index::write::EntrySlice,
+    pack::tree::{Item, Tree},
+};
 use git_features::{parallel, parallel::in_parallel_if, progress::Progress};
 
-pub fn chunks<'a, T: 'a + Send, F, P>(
-    it: impl Iterator<Item = Vec<Node<'a, T>>> + Send,
-    num_objects: u32,
-    should_run_in_parallel: impl FnOnce() -> bool,
-    resolve: F,
-    mut progress: P,
-    thread_limit: Option<usize>,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
+impl<T> Tree<T>
 where
-    F: for<'r> Fn(EntrySlice, &'r mut Vec<u8>) -> Option<()> + Send + Sync,
-    P: Progress,
-    <P as Progress>::SubProgress: Send,
+    T: Default + Send,
 {
-    let (chunk_size, thread_limit, _) = parallel::optimize_chunk_size_and_thread_limit(1, None, thread_limit, None);
-    let reduce_progress = parking_lot::Mutex::new(progress.add_child("Resolving"));
-    in_parallel_if(
-        should_run_in_parallel,
-        it,
-        thread_limit,
-        |thread_index| {
-            (
-                Vec::<u8>::with_capacity(4096),
-                reduce_progress.lock().add_child(format!("thread {}", thread_index)),
-            )
-        },
-        // |root_nodes, state| apply_deltas(root_nodes, state, &resolver, kind.hash()),
-        |_root_nodes, _state| Ok(0),
-        Reducer::new(num_objects, &reduce_progress),
-    )
+    pub fn traverse<F, P>(
+        mut self,
+        should_run_in_parallel: impl FnOnce() -> bool,
+        resolve: F,
+        mut progress: P,
+        thread_limit: Option<usize>,
+    ) -> Result<Vec<Item<T>>, Box<dyn std::error::Error + Send + Sync>>
+    where
+        F: for<'r> Fn(EntrySlice, &'r mut Vec<u8>) -> Option<()> + Send + Sync,
+        P: Progress,
+        <P as Progress>::SubProgress: Send,
+    {
+        let (chunk_size, thread_limit, _) = parallel::optimize_chunk_size_and_thread_limit(1, None, thread_limit, None);
+        let reduce_progress = parking_lot::Mutex::new(progress.add_child("Resolving"));
+
+        // SAFETY: We are owning 'self', and it's the UnsafeCell which we are supposed to use requiring unsafe on every access now.
+        #[allow(unsafe_code)]
+        let num_objects = unsafe { (*self.items.get()).len() } as u32;
+        in_parallel_if(
+            should_run_in_parallel,
+            self.iter_root_chunks(chunk_size),
+            thread_limit,
+            |thread_index| {
+                (
+                    Vec::<u8>::with_capacity(4096),
+                    reduce_progress.lock().add_child(format!("thread {}", thread_index)),
+                )
+            },
+            // |root_nodes, state| apply_deltas(root_nodes, state, &resolver, kind.hash()),
+            |_root_nodes, _state| Ok(0),
+            Reducer::new(num_objects, &reduce_progress),
+        )?;
+        Ok(self.into_items())
+    }
 }
 
 pub(crate) struct Reducer<'a, P> {
