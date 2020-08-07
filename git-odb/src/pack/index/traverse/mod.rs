0e27763995e135fc1bca56e6084b5c81825dba22
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-08-07 08:56:40 +0800

    refactor

--- git-odb/src/pack/index/traverse/mod.rs
+++ git-odb/src/pack/index/traverse/mod.rs
@@ -4,7 +4,6 @@ use git_features::{
     progress::{self, Progress},
 };
 use git_object::owned;
-use quick_error::quick_error;
 use std::collections::BTreeMap;
 
 mod indexed;
@@ -12,52 +11,8 @@ mod lookup;
 mod reduce;
 pub(crate) use reduce::Reducer;
 
-quick_error! {
-    #[derive(Debug)]
-    pub enum Error {
-        Processor(err: Box<dyn std::error::Error + Send + Sync>) {
-            display("One of the traversal processors failed")
-            source(&**err)
-            from()
-        }
-        Verify(err: index::verify::Error) {
-            display("Index file, pack file or object verification failed")
-            source(err)
-            from()
-        }
-        Tree(err: pack::tree::from_offsets::Error) {
-            display("The pack delta tree index could not be built")
-            from()
-            source(err)
-        }
-        TreeTraversal(err: pack::tree::traverse::Error) {
-            display("The tree traversal failed")
-            from()
-            source(err)
-        }
-        PackChecksum(err: pack::data::verify::Error) {
-            display("The pack of this index file failed to verify its checksums")
-            from()
-            source(err)
-        }
-        PackDecode(err: pack::data::decode::Error, id: owned::Id, offset: u64) {
-            display("Object {} at offset {} could not be decoded", id, offset)
-            source(err)
-        }
-        PackMismatch { expected: owned::Id, actual: owned::Id } {
-            display("The packfiles checksum didn't match the index file checksum: expected {}, got {}", expected, actual)
-        }
-        PackObjectMismatch { expected: owned::Id, actual: owned::Id, offset: u64, kind: git_object::Kind} {
-            display("The SHA1 of {} object at offset {} didn't match the checksum in the index file: expected {}, got {}", kind, offset, expected, actual)
-        }
-        Crc32Mismatch { expected: u32, actual: u32, offset: u64, kind: git_object::Kind} {
-            display("The CRC32 of {} object at offset {} didn't match the checksum in the index file: expected {}, got {}", kind, offset, expected, actual)
-        }
-        Interrupted {
-            display("Interrupted by user")
-        }
-    }
-}
+mod error;
+pub use error::Error;
 
 #[derive(Debug, PartialEq, Eq, Hash, Ord, PartialOrd, Clone)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
