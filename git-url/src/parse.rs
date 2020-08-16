a501bc19c0a2ad1b4ee576379841fea2af6db6cc
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-08-16 22:36:43 +0800

    [url] the first green tests

--- git-url/src/parse.rs
+++ git-url/src/parse.rs
@@ -1,12 +1,32 @@
-use crate::borrowed;
-use nom::error::ParseError;
+use crate::{borrowed, bstr::ByteSlice, Protocol};
+use bstr::BStr;
+use nom::{
+    bytes::complete::{tag, take_till1, take_while1},
+    character::complete::alphanumeric1,
+    combinator::recognize,
+    error::ParseError,
+    sequence::tuple,
+    IResult, Parser,
+};
 use quick_error::quick_error;
 
 quick_error! {
     #[derive(Debug)]
     pub enum Error {
+        Nom(err_msg: String) {
+            display("{}", err_msg)
+        }
         NomDetail(input: bstr::BString, msg: &'static str) {
             display("{}: '{}' could not be parsed", msg, input)
+        }
+    }
+}
+
+impl From<nom::Err<Error>> for Error {
+    fn from(e: nom::Err<Error>) -> Self {
+        match e {
+            nom::Err::Error(err) | nom::Err::Failure(err) => Error::Nom(err.to_string()),
+            nom::Err::Incomplete(_) => unreachable!("we do not implement streaming parsers"),
         }
     }
 }
@@ -34,6 +54,41 @@ impl ParseError<&[u8]> for Error {
     }
 }
 
-pub fn parse(_url: &[u8]) -> Result<borrowed::Url, Error> {
-    unimplemented!("parse")
+fn protocol(i: &[u8]) -> IResult<&[u8], Protocol, Error> {
+    tag(b"ssh://")
+        .map(|_| Protocol::Ssh)
+        .parse(i)
+        .map_err(Error::context("protocol parsing failed"))
+}
+
+fn host(i: &[u8]) -> IResult<&[u8], &BStr, Error> {
+    recognize(tuple((take_till1(|c| c == b'.'), tag(b"."), alphanumeric1)))
+        .map(|host: &[u8]| host.as_bstr())
+        .parse(i)
+}
+
+fn path(i: &[u8]) -> IResult<&[u8], &BStr, Error> {
+    // TODO: be much less permissive
+    take_while1(|_| true).map(|path: &[u8]| path.as_bstr()).parse(i)
+}
+
+fn full_url(i: &[u8]) -> IResult<&[u8], borrowed::Url, Error> {
+    tuple((protocol, host, path))
+        .map(|(proto, host, path)| borrowed::Url {
+            protocol: proto,
+            user: None,
+            host: Some(host),
+            port: None,
+            path: path,
+            expand_user: None,
+        })
+        .parse(i)
+}
+
+fn any(i: &[u8]) -> IResult<&[u8], borrowed::Url, Error> {
+    full_url(i)
+}
+
+pub fn parse(url: &[u8]) -> Result<borrowed::Url, Error> {
+    any(url).map(|(_, url)| url).map_err(Into::into)
 }
--- git-url/tests/parse/mod.rs
+++ git-url/tests/parse/mod.rs
@@ -3,6 +3,22 @@ fn assert_url(url: &[u8], expected: git_url::Borrowed) -> crate::Result {
     Ok(())
 }
 
+fn assert_failure(url: &[u8], expected_err: &str) {
+    assert_eq!(git_url::parse(url).unwrap_err().to_string(), expected_err);
+}
+
+mod invalid {
+    use crate::parse::assert_failure;
+
+    #[test]
+    fn unknown_protocol() {
+        assert_failure(
+            b"foo://host.xz/path/to/repo.git/",
+            "protocol parsing failed: 'foo://host.xz/path/to/repo.git/' could not be parsed",
+        )
+    }
+}
+
 mod ssh {
     use crate::parse::assert_url;
     use bstr::ByteSlice;
@@ -17,7 +33,7 @@ mod ssh {
                 user: None,
                 host: Some(b"host.xz".as_bstr()),
                 port: None,
-                path: b"path/to/repo.git/".as_bstr(),
+                path: b"/path/to/repo.git/".as_bstr(),
                 expand_user: None,
             },
         )
