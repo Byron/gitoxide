0ae38edcfd2c7b9c6793c0bc21e88e9d4d19a6b1
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-08-16 21:42:24 +0800

    [url] infrastructure for nom errors, taken from git-object

--- git-url/src/lib.rs
+++ git-url/src/lib.rs
@@ -1,5 +1,8 @@
 #![forbid(unsafe_code)]
 
+/// For convenience to allow using `bstr` without adding it to own cargo manifest
+pub use bstr;
+
 #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone, Copy)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
 pub enum Protocol {
@@ -35,12 +38,38 @@ pub use borrowed::Url as Borrowed;
 
 pub mod parse {
     use crate::borrowed;
+    use nom::error::ParseError;
     use quick_error::quick_error;
 
     quick_error! {
         #[derive(Debug)]
         pub enum Error {
-            TBD
+            NomDetail(input: bstr::BString, msg: &'static str) {
+                display("{}: '{}' could not be parsed", msg, input)
+            }
+        }
+    }
+
+    impl Error {
+        fn set_parse_context(mut self, ctx: &'static str) -> Self {
+            if let Error::NomDetail(_, ref mut message) = self {
+                *message = ctx
+            }
+            self
+        }
+
+        pub(crate) fn context(msg: &'static str) -> impl Fn(nom::Err<Self>) -> nom::Err<Self> {
+            move |e: nom::Err<Self>| e.map(|e| e.set_parse_context(msg))
+        }
+    }
+
+    impl ParseError<&[u8]> for Error {
+        fn from_error_kind(input: &[u8], _kind: nom::error::ErrorKind) -> Self {
+            Error::NomDetail(input.into(), "parse error")
+        }
+
+        fn append(_: &[u8], _: nom::error::ErrorKind, other: Self) -> Self {
+            other
         }
     }
 
