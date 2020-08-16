c3d0e7a490cfa4d114bf8c13b5b3803eb6187290
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-08-16 12:10:55 +0800

    [protocol] first steps towards parsing remote progress

--- git-protocol/src/progress.rs
+++ git-protocol/src/progress.rs
@@ -1,17 +1,58 @@
-use bstr::ByteSlice;
+use bstr::BStr;
+use nom::{
+    bytes::complete::{take_till, take_till1},
+    combinator::{map_res, opt},
+    sequence::preceded,
+};
 
 #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone, Copy)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
 pub struct Remote<'a> {
-    pub action: &'a [u8],
+    pub action: &'a BStr,
     pub percent: Option<u32>,
     pub step: Option<usize>,
     pub max: Option<usize>,
 }
 
-pub fn decode(line: &[u8]) -> Remote {
-    line.find_byte(b':')
-        .map(|pos| (&line[..pos], pos))
-        .and_then(|(action, pos)| line.get(pos..).map(|last_part| (action, pos, last_part)));
-    unimplemented!("progress decode")
+impl<'a> Remote<'a> {
+    pub fn from_bytes(line: &'a [u8]) -> Self {
+        parse_progress(line)
+            .ok()
+            .and_then(|(_, r)| {
+                if r.percent.is_none() && r.step.is_none() && r.max.is_none() {
+                    None
+                } else {
+                    Some(r)
+                }
+            })
+            .unwrap_or_else(|| Remote {
+                action: line.into(),
+                percent: None,
+                step: None,
+                max: None,
+            })
+    }
+}
+
+fn parse_number(i: &[u8]) -> nom::IResult<&[u8], usize> {
+    map_res(take_till(|c: u8| !c.is_ascii_digit()), btoi::btoi)(i)
+}
+
+fn next_optional_number(i: &[u8]) -> nom::IResult<&[u8], Option<usize>> {
+    opt(preceded(take_till(|c: u8| c.is_ascii_digit()), parse_number))(i)
+}
+
+fn parse_progress(line: &[u8]) -> nom::IResult<&[u8], Remote> {
+    let (i, action) = take_till1(|c| c == b':')(line)?;
+    let (i, step) = next_optional_number(i)?;
+    let (i, max) = next_optional_number(i)?;
+    Ok((
+        i,
+        Remote {
+            action: action.into(),
+            percent: None,
+            step,
+            max,
+        },
+    ))
 }
--- git-protocol/tests/progress.rs
+++ git-protocol/tests/progress.rs
@@ -1,12 +1,13 @@
 mod decode {
+    use bstr::ByteSlice;
     use git_protocol::progress;
 
     #[test]
     fn a_message_we_dont_understand() {
         assert_eq!(
-            progress::decode(b"something that might be progress: but is not."),
+            progress::Remote::from_bytes(b"something that might be progress: but is not."),
             progress::Remote {
-                action: b"something that might be progress: but is not.",
+                action: b"something that might be progress: but is not.".as_bstr(),
                 percent: None,
                 step: None,
                 max: None
@@ -17,9 +18,9 @@ mod decode {
     #[test]
     fn enumerating_just_with_count() {
         assert_eq!(
-            progress::decode(b"Enumerating objects: 10, done."),
+            progress::Remote::from_bytes(b"Enumerating objects: 10, done."),
             progress::Remote {
-                action: b"Enumerating objects",
+                action: b"Enumerating objects".as_bstr(),
                 percent: None,
                 step: Some(10),
                 max: None
