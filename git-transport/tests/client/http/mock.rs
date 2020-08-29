22cb37d26f18653b81f52e23f58b5797c763f883
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-08-29 12:33:42 +0800

    refactor

---
+++ git-transport/tests/client/http/mock.rs
@@ -0,0 +1,114 @@
+use crate::fixture_bytes;
+use bstr::ByteVec;
+use git_transport::{client::http, Protocol};
+use std::{
+    io::{Read, Write},
+    net::SocketAddr,
+    time::Duration,
+};
+
+enum Command {
+    ReadAndRespond(Vec<u8>),
+}
+
+enum CommandResult {
+    ReadAndRespond(Vec<u8>),
+}
+
+pub struct MockServer {
+    pub addr: SocketAddr,
+    send_command: std::sync::mpsc::SyncSender<Command>,
+    recv_result: std::sync::mpsc::Receiver<CommandResult>,
+}
+
+impl MockServer {
+    pub fn new(fixture: Vec<u8>) -> Self {
+        let ports = (15411..).take(10);
+        let listener = std::net::TcpListener::bind(
+            ports
+                .map(|port| SocketAddr::from(([127, 0, 0, 1], port)))
+                .collect::<Vec<_>>()
+                .as_slice(),
+        )
+        .expect("one of these ports to be free");
+        let addr = listener.local_addr().expect("a local address");
+        let (send_result, recv_result) = std::sync::mpsc::sync_channel(0);
+        let (send_command, recv_commands) = std::sync::mpsc::sync_channel(0);
+        std::thread::spawn(move || {
+            for command in recv_commands {
+                match command {
+                    Command::ReadAndRespond(response) => {
+                        let (mut stream, _) = listener.accept().expect("accept to always work");
+                        stream
+                            .set_read_timeout(Some(Duration::from_millis(50)))
+                            .expect("timeout to always work");
+                        stream
+                            .set_write_timeout(Some(Duration::from_millis(50)))
+                            .expect("timeout to always work");
+                        let mut out = Vec::new();
+                        stream.read_to_end(&mut out).ok();
+                        stream.write_all(&response).expect("write to always work");
+                        stream.flush().expect("flush to work");
+                        if send_result.send(CommandResult::ReadAndRespond(out)).is_err() {
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        send_command
+            .send(Command::ReadAndRespond(fixture))
+            .expect("send to go through when thread is up");
+        MockServer {
+            addr,
+            send_command,
+            recv_result,
+        }
+    }
+
+    pub fn next_read_and_respond_with(&self, fixture: Vec<u8>) {
+        self.send_command
+            .send(Command::ReadAndRespond(fixture))
+            .expect("thread to be waiting");
+    }
+
+    pub fn ignore_this_result(&self) {
+        self.recv_result.recv().ok();
+    }
+
+    pub fn received(&mut self) -> Vec<u8> {
+        match self.recv_result.recv().expect("thread to be up") {
+            CommandResult::ReadAndRespond(received) => received,
+        }
+    }
+
+    pub fn addr(&self) -> &SocketAddr {
+        &self.addr
+    }
+
+    pub fn received_as_string(&mut self) -> String {
+        self.received().into_string().expect("utf8 only")
+    }
+}
+
+pub fn serve_once(name: &str) -> MockServer {
+    MockServer::new(fixture_bytes(name))
+}
+
+pub fn serve_and_connect(
+    name: &str,
+    path: &str,
+    version: Protocol,
+) -> Result<(MockServer, http::Transport<http::Impl>), crate::Error> {
+    let server = serve_once(name);
+    let client = git_transport::client::http::connect(
+        &format!(
+            "http://{}:{}/{}",
+            &server.addr().ip().to_string(),
+            &server.addr().port(),
+            path
+        ),
+        version,
+    )?;
+    Ok((server, client))
+}
--- git-transport/tests/client/http/mod.rs
+++ git-transport/tests/client/http/mod.rs
@@ -1,126 +1,22 @@
 use crate::fixture_bytes;
-use bstr::{ByteSlice, ByteVec};
-use git_transport::client::TransportV2Ext;
-use git_transport::{client, client::http, client::SetServiceResponse, client::Transport, Protocol, Service};
+use bstr::ByteSlice;
+use git_transport::{
+    client::{self, SetServiceResponse, Transport, TransportV2Ext},
+    Protocol, Service,
+};
 use std::{
     cell::RefCell,
     error::Error,
     io::{self, BufRead, Read, Write},
-    net::SocketAddr,
     ops::Deref,
     rc::Rc,
-    time::Duration,
 };
 
-enum Command {
-    ReadAndRespond(Vec<u8>),
-}
-
-enum CommandResult {
-    ReadAndRespond(Vec<u8>),
-}
-
-struct MockServer {
-    addr: SocketAddr,
-    send_command: std::sync::mpsc::SyncSender<Command>,
-    recv_result: std::sync::mpsc::Receiver<CommandResult>,
-}
-
-impl MockServer {
-    pub fn new(fixture: Vec<u8>) -> Self {
-        let ports = (15411..).take(10);
-        let listener = std::net::TcpListener::bind(
-            ports
-                .map(|port| SocketAddr::from(([127, 0, 0, 1], port)))
-                .collect::<Vec<_>>()
-                .as_slice(),
-        )
-        .expect("one of these ports to be free");
-        let addr = listener.local_addr().expect("a local address");
-        let (send_result, recv_result) = std::sync::mpsc::sync_channel(0);
-        let (send_command, recv_commands) = std::sync::mpsc::sync_channel(0);
-        std::thread::spawn(move || {
-            for command in recv_commands {
-                match command {
-                    Command::ReadAndRespond(response) => {
-                        let (mut stream, _) = listener.accept().expect("accept to always work");
-                        stream
-                            .set_read_timeout(Some(Duration::from_millis(50)))
-                            .expect("timeout to always work");
-                        stream
-                            .set_write_timeout(Some(Duration::from_millis(50)))
-                            .expect("timeout to always work");
-                        let mut out = Vec::new();
-                        stream.read_to_end(&mut out).ok();
-                        stream.write_all(&response).expect("write to always work");
-                        stream.flush().expect("flush to work");
-                        if send_result.send(CommandResult::ReadAndRespond(out)).is_err() {
-                            break;
-                        }
-                    }
-                }
-            }
-        });
-        send_command
-            .send(Command::ReadAndRespond(fixture))
-            .expect("send to go through when thread is up");
-        MockServer {
-            addr,
-            send_command,
-            recv_result,
-        }
-    }
-
-    pub fn next_read_and_respond_with(&self, fixture: Vec<u8>) {
-        self.send_command
-            .send(Command::ReadAndRespond(fixture))
-            .expect("thread to be waiting");
-    }
-
-    pub fn ignore_this_result(&self) {
-        self.recv_result.recv().ok();
-    }
-
-    pub fn received(&mut self) -> Vec<u8> {
-        match self.recv_result.recv().expect("thread to be up") {
-            CommandResult::ReadAndRespond(received) => received,
-        }
-    }
-
-    pub fn addr(&self) -> &SocketAddr {
-        &self.addr
-    }
-
-    pub fn received_as_string(&mut self) -> String {
-        self.received().into_string().expect("utf8 only")
-    }
-}
-
-fn serve_once(name: &str) -> MockServer {
-    MockServer::new(fixture_bytes(name))
-}
-
-fn serve_and_connect(
-    name: &str,
-    path: &str,
-    version: Protocol,
-) -> Result<(MockServer, http::Transport<http::Impl>), crate::Error> {
-    let server = serve_once(name);
-    let client = git_transport::client::http::connect(
-        &format!(
-            "http://{}:{}/{}",
-            &server.addr().ip().to_string(),
-            &server.addr().port(),
-            path
-        ),
-        version,
-    )?;
-    Ok((server, client))
-}
+mod mock;
 
 fn assert_error_status(status: usize, kind: std::io::ErrorKind) -> crate::Result {
     let (_server, mut client) =
-        serve_and_connect(&format!("http-{}.response", status), "path/not-important", Protocol::V1)?;
+        mock::serve_and_connect(&format!("http-{}.response", status), "path/not-important", Protocol::V1)?;
     let error = client
         .handshake(Service::UploadPack)
         .err()
@@ -147,7 +43,7 @@ fn http_error_results_in_observable_error() -> crate::Result {
 
 #[test]
 fn handshake_v1() -> crate::Result {
-    let (mut server, mut c) = serve_and_connect(
+    let (mut server, mut c) = mock::serve_and_connect(
         "v1/http-handshake.response",
         "path/not/important/due/to/mock",
         Protocol::V1,
@@ -246,7 +142,7 @@ User-Agent: git/oxide-{}
 
 #[test]
 fn clone_v1() -> crate::Result {
-    let (mut server, mut c) = serve_and_connect(
+    let (mut server, mut c) = mock::serve_and_connect(
         "v1/http-handshake.response",
         "path/not/important/due/to/mock",
         Protocol::V1,
@@ -312,7 +208,7 @@ Accept: application/x-git-upload-pack-result
 
 #[test]
 fn handshake_and_lsrefs_and_fetch_v2() -> crate::Result {
-    let (mut server, mut c) = serve_and_connect(
+    let (mut server, mut c) = mock::serve_and_connect(
         "v2/http-handshake.response",
         "path/not/important/due/to/mock",
         Protocol::V2,
