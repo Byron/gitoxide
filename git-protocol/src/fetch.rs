2da70f688da95434e256ba1f355dbb809100604a
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-08-30 21:02:20 +0800

    [clone] frame for first 'fetch' tests

--- git-protocol/src/fetch.rs
+++ git-protocol/src/fetch.rs
@@ -1,12 +1,24 @@
 use crate::credentials;
 use git_transport::client;
+use quick_error::quick_error;
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum Error {
+        Credentials(err: credentials::Error) {
+            display("Failed to obtain, approve or reject credentials")
+            from()
+            source(err)
+        }
+    }
+}
 
 pub trait Delegate {}
 
 pub fn fetch(
     _transport: impl client::Transport,
     _delegate: impl Delegate,
-    _authenticate: impl FnMut(credentials::Action, &str) -> credentials::Result,
-) {
+    _authenticate: impl FnMut(credentials::Action) -> credentials::Result,
+) -> Result<(), Error> {
     unimplemented!("fetch")
 }
--- git-protocol/tests/protocol.rs
+++ git-protocol/tests/protocol.rs
@@ -1,4 +1,37 @@
 type Result = std::result::Result<(), Box<dyn std::error::Error>>;
+use std::path::PathBuf;
+
+pub fn fixture_bytes(path: &str) -> Vec<u8> {
+    std::fs::read(PathBuf::from("tests").join("fixtures").join(path)).expect("fixture to be present and readable")
+}
 
 mod credentials;
 mod remote_progress;
+mod fetch {
+    struct CloneDelegate;
+    use git_protocol::fetch;
+
+    impl fetch::Delegate for CloneDelegate {}
+
+    mod v1 {
+        use crate::{fetch::CloneDelegate, fixture_bytes};
+        use bstr::ByteSlice;
+
+        #[test]
+        #[ignore]
+        fn clone() -> crate::Result {
+            let mut out = Vec::new();
+            let response = fixture_bytes("v1/clone.response");
+            let transport = git_transport::client::git::Connection::new(
+                response.as_slice(),
+                &mut out,
+                git_transport::Protocol::V1,
+                b"does/not/matter".as_bstr().to_owned(),
+                None::<(&str, _)>,
+                git_transport::client::git::ConnectMode::Process,
+            );
+            git_protocol::fetch(transport, CloneDelegate, git_protocol::credentials::helper)?;
+            Ok(())
+        }
+    }
+}
