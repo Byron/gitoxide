58ddd2955a407efb6a46249810b916c6035eb5ff
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-09-08 12:44:06 +0800

    [ref-ls] don't leak the PacketLine error type in Transport interface

--- git-transport/src/client/http/mod.rs
+++ git-transport/src/client/http/mod.rs
@@ -239,8 +239,13 @@ impl<H: Http, B: ExtendedBufRead> ExtendedBufRead for HeadersThenBody<H, B> {
         self.body.set_progress_handler(handle_progress)
     }
 
-    fn peek_data_line(&mut self) -> Option<io::Result<Result<&[u8], git_packetline::decode::Error>>> {
-        self.body.peek_data_line()
+    fn peek_data_line(&mut self) -> Option<io::Result<Result<&[u8], client::Error>>> {
+        match self.body.peek_data_line() {
+            Some(Ok(Ok(line))) => Some(Ok(Ok(line))),
+            Some(Ok(Err(err))) => Some(Ok(Err(err.into()))),
+            Some(Err(err)) => Some(Err(err)),
+            None => None,
+        }
     }
 }
 
--- git-transport/src/client/mod.rs
+++ git-transport/src/client/mod.rs
@@ -142,15 +142,20 @@ impl<'a> RequestWriter<'a> {
 
 pub trait ExtendedBufRead: io::BufRead {
     fn set_progress_handler(&mut self, handle_progress: Option<HandleProgress>);
-    fn peek_data_line(&mut self) -> Option<io::Result<Result<&[u8], git_packetline::decode::Error>>>;
+    fn peek_data_line(&mut self) -> Option<io::Result<Result<&[u8], Error>>>;
 }
 
 impl<'a, T: io::Read> ExtendedBufRead for git_packetline::provider::ReadWithSidebands<'a, T, HandleProgress> {
     fn set_progress_handler(&mut self, handle_progress: Option<HandleProgress>) {
         self.set_progress_handler(handle_progress)
     }
-    fn peek_data_line(&mut self) -> Option<io::Result<Result<&[u8], git_packetline::decode::Error>>> {
-        self.peek_data_line()
+    fn peek_data_line(&mut self) -> Option<io::Result<Result<&[u8], Error>>> {
+        match self.peek_data_line() {
+            Some(Ok(Ok(line))) => Some(Ok(Ok(line))),
+            Some(Ok(Err(err))) => Some(Ok(Err(err.into()))),
+            Some(Err(err)) => Some(Err(err)),
+            None => None,
+        }
     }
 }
 
