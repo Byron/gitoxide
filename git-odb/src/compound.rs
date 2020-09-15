9e68c6bcd1d07ea73730ce5ff59d7883152f894d
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-09-15 22:23:54 +0800

    refactor

--- git-odb/src/compound.rs
+++ git-odb/src/compound.rs
@@ -86,22 +86,23 @@ mod init {
     impl compound::Db {
         pub fn at(objects_directory: impl Into<PathBuf>) -> Result<compound::Db, Error> {
             let loose_objects = objects_directory.into();
-            let mut packs_and_sizes = if let Ok(entries) = std::fs::read_dir(loose_objects.join("packs")) {
-                entries
+            let packs = if let Ok(entries) = std::fs::read_dir(loose_objects.join("packs")) {
+                let mut packs_and_sizes = entries
                     .filter_map(Result::ok)
                     .filter_map(|e| e.metadata().map(|md| (e.path().to_owned(), md)).ok())
                     .filter(|(_, md)| md.file_type().is_file())
                     .filter(|(p, _)| p.extension().unwrap_or_default() == "idx" && p.starts_with("pack-"))
                     .map(|(p, md)| pack::Bundle::at(p).map(|b| (b, md.len())))
-                    .collect::<Result<Vec<_>, _>>()?
+                    .collect::<Result<Vec<_>, _>>()?;
+                packs_and_sizes.sort_by_key(|e| e.1);
+                packs_and_sizes.into_iter().rev().map(|(b, _)| b).collect()
             } else {
                 Vec::new()
             };
-            packs_and_sizes.sort_by_key(|e| e.1);
 
             Ok(compound::Db {
                 loose: loose::Db::at(loose_objects),
-                packs: packs_and_sizes.into_iter().rev().map(|(b, _)| b).collect(),
+                packs,
             })
         }
     }
