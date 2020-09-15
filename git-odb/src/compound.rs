02b88c28304ff6d8c1fbad6fdcfa36f3b1f9dafc
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-09-15 21:47:55 +0800

    Implement Write in terms of writing to the loose object DB
    This is pretty much the default way of doing it, too.

--- git-odb/src/compound.rs
+++ git-odb/src/compound.rs
@@ -5,6 +5,34 @@ pub struct Db {
     pub packs: Vec<pack::Bundle>,
 }
 
+mod write {
+    use crate::{compound, loose};
+    use git_object::{owned, HashKind, Kind};
+    use std::io::Read;
+
+    impl crate::Write for compound::Db {
+        type Error = loose::db::write::Error;
+
+        fn write(&self, object: &owned::Object, hash: HashKind) -> Result<owned::Id, Self::Error> {
+            self.loose.write(object, hash)
+        }
+
+        fn write_buf(&self, object: Kind, from: &[u8], hash: HashKind) -> Result<owned::Id, Self::Error> {
+            self.loose.write_buf(object, from, hash)
+        }
+
+        fn write_stream(
+            &self,
+            kind: Kind,
+            size: u64,
+            from: impl Read,
+            hash: HashKind,
+        ) -> Result<owned::Id, Self::Error> {
+            self.loose.write_stream(kind, size, from, hash)
+        }
+    }
+}
+
 mod init {
     use crate::{compound, loose, pack};
     use quick_error::quick_error;
