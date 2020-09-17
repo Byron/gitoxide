91d9f78a9af04b44b2cead30d6e1cbaaeb76a522
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-09-17 11:48:06 +0800

    refactor

--- git-odb/src/loose/db/write.rs
+++ git-odb/src/loose/db/write.rs
@@ -1,27 +1,24 @@
 use super::Db;
 use crate::{hash, loose, zlib::stream::DeflateWriter};
 use git_object::{owned, HashKind};
-use quick_error::quick_error;
 use std::{fs, io, io::Write, path::PathBuf};
 use tempfile::NamedTempFile;
 
-quick_error! {
-    #[derive(Debug)]
-    pub enum Error {
-        Io(err: io::Error, msg: &'static str, path: PathBuf) {
-            display("Could not {} '{}'", msg, path.display())
-            source(err)
-        }
-        IoRaw(err: io::Error) {
-            display("An IO error occurred while writing an object")
-            source(err)
-            from()
-        }
-        Persist(err: tempfile::PersistError, target: PathBuf) {
-            display("Could not turn temporary file into persisted file at '{}'", target.display())
-            source(err)
-       }
-    }
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Could not {message} '{path}'")]
+    Io {
+        source: io::Error,
+        message: &'static str,
+        path: PathBuf,
+    },
+    #[error("An IO error occurred while writing an object")]
+    IoRaw(#[from] io::Error),
+    #[error("Could not turn temporary file into persisted file at '{target}'")]
+    Persist {
+        source: tempfile::PersistError,
+        target: PathBuf,
+    },
 }
 
 impl crate::Write for Db {
@@ -31,8 +28,11 @@ impl crate::Write for Db {
         match hash {
             HashKind::Sha1 => {
                 let mut to = self.write_header(kind, from.len() as u64, hash)?;
-                to.write_all(from)
-                    .map_err(|err| Error::Io(err, "stream all data into tempfile in", self.path.to_owned()))?;
+                to.write_all(from).map_err(|err| Error::Io {
+                    source: err,
+                    message: "stream all data into tempfile in",
+                    path: self.path.to_owned(),
+                })?;
                 to.flush()?;
                 self.finalize_object(to)
             }
@@ -49,8 +49,11 @@ impl crate::Write for Db {
         match hash {
             HashKind::Sha1 => {
                 let mut to = self.write_header(kind, size, hash)?;
-                io::copy(&mut from, &mut to)
-                    .map_err(|err| Error::Io(err, "stream all data into tempfile in", self.path.to_owned()))?;
+                io::copy(&mut from, &mut to).map_err(|err| Error::Io {
+                    source: err,
+                    message: "stream all data into tempfile in",
+                    path: self.path.to_owned(),
+                })?;
                 to.flush()?;
                 self.finalize_object(to)
             }
@@ -68,15 +71,19 @@ impl Db {
         hash: HashKind,
     ) -> Result<hash::Write<HashAndTempFile>, Error> {
         let mut to = hash::Write::new(
-            DeflateWriter::new(
-                NamedTempFile::new_in(&self.path)
-                    .map_err(|err| Error::Io(err, "create named temp file in", self.path.to_owned()))?,
-            ),
+            DeflateWriter::new(NamedTempFile::new_in(&self.path).map_err(|err| Error::Io {
+                source: err,
+                message: "create named temp file in",
+                path: self.path.to_owned(),
+            })?),
             hash,
         );
 
-        loose::object::header::encode(kind, size, &mut to)
-            .map_err(|err| Error::Io(err, "write header to tempfile in", self.path.to_owned()))?;
+        loose::object::header::encode(kind, size, &mut to).map_err(|err| Error::Io {
+            source: err,
+            message: "write header to tempfile in",
+            path: self.path.to_owned(),
+        })?;
         Ok(to)
     }
 
@@ -96,8 +103,10 @@ impl Db {
             }
         }
         let file = file.into_inner();
-        file.persist(&object_path)
-            .map_err(|err| Error::Persist(err, object_path))?;
+        file.persist(&object_path).map_err(|err| Error::Persist {
+            source: err,
+            target: object_path,
+        })?;
         Ok(id)
     }
 }
