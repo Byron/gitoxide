3ec99dc7360c01b4f3c4593ff5049361e7043254
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-09-21 23:10:29 +0800

    refactor

--- git-odb/src/loose/object/verify.rs
+++ git-odb/src/loose/object/verify.rs
@@ -1,25 +1,15 @@
 use crate::{hash::Write as HashWrite, loose};
 use git_object::{borrowed, owned};
-use quick_error::quick_error;
 use std::io;
 
-quick_error! {
-    #[derive(Debug)]
-    pub enum Error {
-        Io(err: io::Error) {
-            display("reading of object failed")
-            from()
-            source(err)
-        }
-        Decode(err: super::decode::Error) {
-            display("Decoding of object failed")
-            from()
-            source(err)
-        }
-        ChecksumMismatch(desired: owned::Id, actual: owned::Id) {
-            display("Object expected to have id {}, but actual id was {}", desired, actual)
-        }
-    }
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("reading of object failed")]
+    Io(#[from] io::Error),
+    #[error("Decoding of object failed")]
+    Decode(#[from] super::decode::Error),
+    #[error("Object expected to have id {desired}, but actual id was {actual}")]
+    ChecksumMismatch { desired: owned::Id, actual: owned::Id },
 }
 
 impl loose::Object {
@@ -31,9 +21,12 @@ impl loose::Object {
         loose::object::header::encode(kind, size as u64, &mut sink).expect("hash to always work");
         io::copy(&mut reader, &mut sink)?;
 
-        let actual_id = owned::Id::from(sink.hash.digest());
-        if desired != actual_id.to_borrowed() {
-            return Err(Error::ChecksumMismatch(desired.into(), actual_id));
+        let actual = owned::Id::from(sink.hash.digest());
+        if desired != actual.to_borrowed() {
+            return Err(Error::ChecksumMismatch {
+                desired: desired.into(),
+                actual,
+            });
         }
         Ok(())
     }
