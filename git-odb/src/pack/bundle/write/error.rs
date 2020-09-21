519dd12f2bf58dd3048bc12e5b058236ad727853
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-09-21 23:07:24 +0800

    refactor

--- git-odb/src/pack/bundle/write/error.rs
+++ git-odb/src/pack/bundle/write/error.rs
@@ -1,29 +1,14 @@
 use crate::pack;
-use quick_error::quick_error;
 use std::io;
 
-quick_error! {
-    #[derive(Debug)]
-    pub enum Error {
-        Io(err: io::Error) {
-            display("An IO error occurred when reading the pack or creating a temporary file")
-            from()
-            source(err)
-        }
-        PackIter(err: pack::data::iter::Error) {
-            display("Pack iteration failed")
-            from()
-            source(err)
-        }
-        PeristError(err: tempfile::PersistError) {
-            display("Could not move a temporary file into its desired place")
-            from()
-            source(err)
-        }
-        IndexWrite(err: pack::index::write::Error) {
-            display("The index file could not be written")
-            from()
-            source(err)
-        }
-    }
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("An IO error occurred when reading the pack or creating a temporary file")]
+    Io(#[from] io::Error),
+    #[error(transparent)]
+    PackIter(#[from] pack::data::iter::Error),
+    #[error("Could not move a temporary file into its desired place")]
+    PeristError(#[from] tempfile::PersistError),
+    #[error(transparent)]
+    IndexWrite(#[from] pack::index::write::Error),
 }
