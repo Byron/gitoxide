a45c5941cf426537710842917c0e715cf4c74863
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-09-27 23:32:56 +0800

    First bunch of simple unescapes

--- git-odb/src/alternate.rs
+++ git-odb/src/alternate.rs
@@ -21,6 +21,10 @@ pub mod unquote {
     pub enum Error {
         #[error("{message}: {:?}", String::from_utf8_lossy(&.input))]
         InvalidInput { message: &'static str, input: Vec<u8> },
+        #[error("Unexpected end of input when fetching the next {0} bytes")]
+        UnexpectedEndOfInput(usize),
+        #[error("Invalid escaped value {byte} in input {:?}", String::from_utf8_lossy(&.input))]
+        UnsupportedEscapeByte { byte: u8, input: Vec<u8> },
     }
 
     impl Error {
@@ -39,18 +43,41 @@ pub mod unquote {
         if input.len() < 2 {
             return Err(Error::new("Input must be surrounded by double quotes", input));
         }
-        let input = &input[1..];
+        let original = input.as_bstr();
+        let mut input = &input[1..];
         let mut out = BString::default();
+        fn consume_one_past(input: &mut &BStr, position: usize) -> Result<u8, Error> {
+            *input = input
+                .get(position + 1..)
+                .ok_or_else(|| Error::new("Unexpected end of input", input))?
+                .as_bstr();
+            let next = input[0];
+            *input = input.get(1..).unwrap_or_default().as_bstr();
+            Ok(next)
+        }
         loop {
             match input.find_byteset(b"\"\\") {
-                Some(position) => match input[position] {
-                    b'"' => {
-                        out.extend_from_slice(&input[..position]);
-                        break;
+                Some(position) => {
+                    out.extend_from_slice(&input[..position]);
+                    match input[position] {
+                        b'"' => break,
+                        b'\\' => {
+                            let next = consume_one_past(&mut input, position)?;
+                            match next {
+                                b'n' => out.push(b'\n'),
+                                b'r' => out.push(b'\r'),
+                                b't' => out.push(b'\t'),
+                                _ => {
+                                    return Err(Error::UnsupportedEscapeByte {
+                                        byte: next,
+                                        input: original.to_vec(),
+                                    })
+                                }
+                            }
+                        }
+                        _ => unreachable!("cannot find character that we didn't search for"),
                     }
-                    b'\\' => unimplemented!("actual unescaping"),
-                    _ => unreachable!("cannot find character that we didn't search for"),
-                },
+                }
                 None => {
                     out.extend_from_slice(input);
                     break;
@@ -80,6 +107,7 @@ pub mod unquote {
         test!(unquoted_remains_unchanged, "hello", "hello");
         test!(empty_surrounded_by_quotes, "\"\"", "");
         test!(surrounded_only_by_quotes, "\"hello\"", "hello");
+        test!(typical_escapes, r#""\n\r\t""#, b"\n\r\t");
     }
 }
 
