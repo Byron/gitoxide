1a8141c2c4a8bcc79d68049a35bd8aba5ab822a3
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-11-19 15:46:03 +0800

    first sketch of filesystem docs for git-features

--- git-features/src/fs.rs
+++ git-features/src/fs.rs
@@ -1,3 +1,11 @@
+//! Filesystem utilities
+//!
+//! These are will be parallel if the `parallel` feature is enabled, at the expense of compiling additional dependencies
+//! along with runtime costs for maintaining a global [`rayon`](https://docs.rs/rayon) thread pool.
+//!
+//! For information on how to use the [`WalkDir`] type, have a look at
+//! * [`jwalk::WalkDir`](https://docs.rs/jwalk/0.5.1/jwalk/type.WalkDir.html) if `parallel` feature is enabled
+//! * [walkdir::WalkDir](https://docs.rs/walkdir/2.3.1/walkdir/struct.WalkDir.html) otherwise
 #[cfg(feature = "parallel")]
 pub mod walkdir {
     pub use jwalk::{Error, WalkDir};
--- git-features/src/lib.rs
+++ git-features/src/lib.rs
@@ -1,7 +1,9 @@
 #![forbid(unsafe_code, rust_2018_idioms)]
 
 pub mod fs;
+/// Hash functions and hash utilities
 pub mod hash;
+/// Process-global interrupt handling
 pub mod interrupt;
 pub mod parallel;
 #[cfg(feature = "pipe")]
