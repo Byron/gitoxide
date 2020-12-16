6b255c2422acd2b40f082101e1ded402849c020e
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-12-16 18:00:54 +0800

    add git-hash to list of size-checked crates

--- etc/check-package-size.sh
+++ etc/check-package-size.sh
@@ -16,6 +16,7 @@ function indent () {
 
 echo "in root: gitoxide CLI"
 indent cargo diet -n --package-size-limit 25KB
+(enter git-hash && indent cargo diet -n --package-size-limit 5KB)
 (enter git-features && indent cargo diet -n --package-size-limit 15KB)
 (enter git-ref && indent cargo diet -n --package-size-limit 4KB)
 (enter git-url && indent cargo diet -n --package-size-limit 6KB)
