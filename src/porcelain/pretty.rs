3f01742d4240a272fd8aff81d5d2604132df0267
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-01-13 17:22:35 +0800

    Port 'ogranize' options to pretty version of porcelain command

--- src/porcelain/pretty.rs
+++ src/porcelain/pretty.rs
@@ -4,6 +4,7 @@ use gitoxide_core as core;
 
 mod options {
     use clap::{AppSettings, Clap};
+    use std::path::PathBuf;
 
     #[derive(Debug, Clap)]
     #[clap(about = "The rusty git", version = clap::crate_version!())]
@@ -21,6 +22,26 @@ mod options {
         #[clap(setting = AppSettings::ColoredHelp)]
         #[clap(setting = AppSettings::DisableVersion)]
         Init,
+        /// Move all repositories found in a directory into a structure matching their clone URLs.
+        #[clap(setting = AppSettings::ColoredHelp)]
+        #[clap(setting = AppSettings::DisableVersion)]
+        Organize {
+            #[clap(long)]
+            /// The operation will be in dry-run mode unless this flag is set.
+            execute: bool,
+
+            #[clap(long, short = 'f')]
+            /// The directory to use when finding input repositories to move into position.
+            ///
+            /// Defaults to the current working directory.
+            repository_source: Option<PathBuf>,
+
+            #[clap(long, short = 't')]
+            /// The directory to which to move repositories found in the repository-source.
+            ///
+            /// Defaults to the current working directory.
+            destination_directory: Option<PathBuf>,
+        },
     }
 }
 
@@ -30,6 +51,11 @@ pub fn main() -> Result<()> {
     git_features::interrupt::init_handler(std::io::stderr());
     match args.cmd {
         Subcommands::Init => core::repository::init(),
+        Subcommands::Organize {
+            execute,
+            repository_source,
+            destination_directory,
+        } => unimplemented!("organize"),
     }?;
     Ok(())
 }
