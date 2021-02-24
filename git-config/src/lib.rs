79dc19f7a7517f0953a1b14b180e54cfe110bab8
Edward Shen <code@eddie.sh>
2021-02-24 12:30:49 -0500

    select nom features

--- git-config/src/lib.rs
+++ git-config/src/lib.rs
@@ -1,5 +1,9 @@
 #![forbid(unsafe_code)]
 
+// Cargo.toml cannot have self-referential dependencies, so you can't just
+// specify the actual serde crate when you define a feature called serde. We
+// instead call the serde crate as serde_crate and then rename the crate to
+// serde, to get around this in an intuitive manner.
 #[cfg(feature = "serde")]
 extern crate serde_crate as serde;
 
