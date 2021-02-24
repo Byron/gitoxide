c3cb04a5750cde70c2aa73b0fd35c2712cb9b4ca
Edward Shen <code@eddie.sh>
2021-02-24 17:55:58 -0500

    Don't immediately drop fuzzer values

--- git-config/fuzz/fuzz_targets/parser.rs
+++ git-config/fuzz/fuzz_targets/parser.rs
@@ -4,5 +4,6 @@ use git_config::parser::Parser;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    Parser::from_bytes(data);
+    // Don't name this _; Rust may optimize it out.
+    let _a = Parser::from_bytes(data);
 });
