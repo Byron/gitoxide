bec647e5b9463f40180302f3fe883ab939f874f6
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-04-07 16:47:03 +0800

    More usage examples and performance tests (#63)
    Learnings: see https://github.com/Byron/gitoxide/issues/63#issuecomment-814728406

--- experiments/hash-owned-borrowed/benches/usage.rs
+++ experiments/hash-owned-borrowed/benches/usage.rs
@@ -1,36 +1,55 @@
 use criterion::*;
+use std::borrow::Borrow;
 
 fn use_owned_by_ref(id: &hash::Owned) {
     black_box(id);
 }
 
+fn use_by_ref_impl_borrow(id: impl Borrow<hash::Owned>) {
+    black_box(id.borrow());
+}
+
 fn usage(c: &mut Criterion) {
     c.benchmark_group("Owned::cloned")
-        .throughput(Throughput::Bytes(std::mem::size_of::<hash::Owned>() as u64))
+        .throughput(Throughput::Elements(1))
         .bench_function("Sha1", |b| {
-            let source = hash::Owned::Sha1([0; 20]);
+            let source = hash::Owned::sha1();
             b.iter(|| {
                 black_box(source.clone());
             });
         })
         .bench_function("Sha256", |b| {
-            let source = hash::Owned::Sha256([0; 32]);
+            let source = hash::Owned::sha256();
             b.iter(|| {
                 black_box(source.clone());
             });
         });
     c.benchmark_group("Owned::by_ref")
-        .throughput(Throughput::Bytes(std::mem::size_of::<hash::Owned>() as u64))
+        .throughput(Throughput::Elements(1))
         .bench_function("Sha1", |b| {
-            let source = hash::Owned::Sha1([0; 20]);
+            let source = hash::Owned::sha1();
             b.iter(|| {
                 use_owned_by_ref(&source);
             });
         })
         .bench_function("Sha256", |b| {
-            let source = hash::Owned::Sha256([0; 32]);
+            let source = hash::Owned::sha256();
             b.iter(|| {
                 use_owned_by_ref(&source);
+            });
+        });
+    c.benchmark_group("Owned::by_ref_via_impl_borrow")
+        .throughput(Throughput::Elements(1))
+        .bench_function("Sha1", |b| {
+            let source = hash::Owned::sha1();
+            b.iter(|| {
+                use_by_ref_impl_borrow(&source);
+            });
+        })
+        .bench_function("Sha256", |b| {
+            let source = hash::Owned::sha256();
+            b.iter(|| {
+                use_by_ref_impl_borrow(&source);
             });
         });
 }
--- experiments/hash-owned-borrowed/src/lib.rs
+++ experiments/hash-owned-borrowed/src/lib.rs
@@ -1,5 +1,51 @@
+#![allow(unused)]
+use std::borrow::Borrow;
+
 #[derive(Clone)]
 pub enum Owned {
     Sha1([u8; 20]),
     Sha256([u8; 32]),
+}
+
+impl Owned {
+    pub fn sha1() -> Self {
+        Owned::Sha1([0; 20])
+    }
+    pub fn sha256() -> Self {
+        Owned::Sha256([0; 32])
+    }
+}
+
+fn use_owned(_id: Owned) {}
+fn use_by_ref(_id: &Owned) {}
+
+fn use_by_ref_impl_as_ref(id: impl AsRef<Owned>) {
+    use_by_ref(id.as_ref())
+}
+
+pub fn use_by_ref_impl_borrow(id: impl Borrow<Owned>) {
+    use_by_ref(id.borrow());
+}
+
+fn call_use_owned() {
+    use_owned(Owned::sha1())
+}
+fn call_by_ref() {
+    use_by_ref(&Owned::sha1())
+}
+
+fn call_by_ref_impl_as_ref_with_owned() {
+    // use_by_ref_impl_as_ref(Owned::sha1()) // does not work as it requires types like String/str, Vec/slice
+}
+
+fn call_by_ref_impl_as_ref_with_reference() {
+    // use_by_ref_impl_as_ref(&Owned::sha1()) // does not work as it requires types like String/str, Vec/slice
+}
+
+fn call_by_ref_impl_borrow_with_owned() {
+    use_by_ref_impl_borrow(Owned::sha1())
+}
+
+fn call_by_ref_impl_borrow_with_reference() {
+    use_by_ref_impl_borrow(&Owned::sha1())
 }
