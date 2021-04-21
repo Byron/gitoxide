942791c0887bcf36c13fbf5c4c8f3e07b2adc4b1
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-04-21 20:02:22 +0800

    object-access experiment now obtains object ids with gitoxide #(67)

--- experiments/object-access/src/main.rs
+++ experiments/object-access/src/main.rs
@@ -4,8 +4,8 @@
 //! Let's see if a unit test can reproduce this too, right now this functionality is entirely untested
 //! I think.
 use anyhow::anyhow;
+use git_hash::ObjectId;
 use std::{
-    io::BufRead,
     path::{Path, PathBuf},
     time::Instant,
 };
@@ -14,9 +14,6 @@ const GITOXIDE_STATIC_CACHE_SIZE: usize = 64;
 const GITOXIDE_CACHED_OBJECT_DATA_PER_THREAD_IN_BYTES: usize = 60_000_000;
 
 fn main() -> anyhow::Result<()> {
-    if atty::is(atty::Stream::Stdin) {
-        anyhow::bail!("Need object hashes on stdin, one per line");
-    }
     let repo_git_dir = std::env::args()
         .nth(1)
         .ok_or_else(|| anyhow!("First argument is the .git directory to work in"))
@@ -28,20 +25,22 @@ fn main() -> anyhow::Result<()> {
                 Err(anyhow!("Path '{}' needs to be a .git directory", p.display()))
             }
         })?;
-
-    let start = Instant::now();
-    let stdin = std::io::stdin();
-    let stdin = stdin.lock();
-    let hashes = stdin.lines().collect::<Result<Vec<String>, _>>()?;
-    let elapsed = start.elapsed();
-    println!("{} objects (collected in {:?}", hashes.len(), elapsed);
-
-    let start = Instant::now();
     let repo_objects_dir = {
         let mut d = repo_git_dir.clone();
         d.push("objects");
         d
     };
+
+    let hashes = {
+        let db = git_odb::linked::Db::at(&repo_objects_dir)?;
+        let start = Instant::now();
+        let hashes = db.iter().collect::<Result<Vec<_>, _>>()?;
+        let elapsed = start.elapsed();
+        println!("gitoxide: {} objects (collected in {:?}", hashes.len(), elapsed);
+        hashes
+    };
+
+    let start = Instant::now();
     let bytes = do_gitoxide(&hashes, &repo_objects_dir, || {
         git_odb::pack::cache::lru::MemoryCappedHashmap::new(GITOXIDE_CACHED_OBJECT_DATA_PER_THREAD_IN_BYTES)
     })?;
@@ -141,20 +140,20 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn do_git2(hashes: &[String], git_dir: &Path) -> anyhow::Result<u64> {
+fn do_git2(hashes: &[ObjectId], git_dir: &Path) -> anyhow::Result<u64> {
     git2::opts::strict_hash_verification(false);
     let repo = git2::Repository::open(git_dir)?;
     let odb = repo.odb()?;
     let mut bytes = 0u64;
     for hash in hashes {
-        let hash = git2::Oid::from_str(&hash)?;
+        let hash = git2::Oid::from_bytes(hash.as_slice())?;
         let obj = odb.read(hash)?;
         bytes += obj.len() as u64;
     }
     Ok(bytes)
 }
 
-fn do_parallel_git2(hashes: &[String], git_dir: &Path) -> anyhow::Result<u64> {
+fn do_parallel_git2(hashes: &[ObjectId], git_dir: &Path) -> anyhow::Result<u64> {
     use rayon::prelude::*;
     git2::opts::strict_hash_verification(false);
     let bytes = std::sync::atomic::AtomicU64::default();
@@ -162,7 +161,7 @@ fn do_parallel_git2(hashes: &[String], git_dir: &Path) -> anyhow::Result<u64> {
         || git2::Repository::open(git_dir).expect("git directory is valid"),
         |repo, hash| {
             let odb = repo.odb()?;
-            let hash = git2::Oid::from_str(&hash)?;
+            let hash = git2::Oid::from_bytes(hash.as_slice())?;
             let obj = odb.read(hash)?;
             bytes.fetch_add(obj.len() as u64, std::sync::atomic::Ordering::Relaxed);
             Ok(())
@@ -172,7 +171,7 @@ fn do_parallel_git2(hashes: &[String], git_dir: &Path) -> anyhow::Result<u64> {
     Ok(bytes.load(std::sync::atomic::Ordering::Acquire))
 }
 
-fn do_gitoxide<C>(hashes: &[String], objects_dir: &Path, new_cache: impl FnOnce() -> C) -> anyhow::Result<u64>
+fn do_gitoxide<C>(hashes: &[ObjectId], objects_dir: &Path, new_cache: impl FnOnce() -> C) -> anyhow::Result<u64>
 where
     C: git_odb::pack::cache::DecodeEntry,
 {
@@ -181,7 +180,6 @@ where
     let mut bytes = 0u64;
     let mut cache = new_cache();
     for hash in hashes {
-        let hash: git_hash::ObjectId = hash.parse()?;
         let obj = odb.locate(hash, &mut buf, &mut cache)?.expect("object must exist");
         bytes += obj.data.len() as u64;
     }
@@ -189,7 +187,7 @@ where
 }
 
 fn do_gitoxide_in_parallel<C>(
-    hashes: &[String],
+    hashes: &[ObjectId],
     objects_dir: &Path,
     new_cache: impl Fn() -> C + Sync + Send,
 ) -> anyhow::Result<u64>
@@ -202,7 +200,6 @@ where
     hashes.par_iter().try_for_each_init::<_, _, _, anyhow::Result<_>>(
         || (Vec::new(), new_cache()),
         |(buf, cache), hash| {
-            let hash: git_hash::ObjectId = hash.parse()?;
             let obj = odb.locate(hash, buf, cache)?.expect("object must exist");
             bytes.fetch_add(obj.data.len() as u64, std::sync::atomic::Ordering::Relaxed);
             Ok(())
