8945f95364b489e7a639d74dd0f28b17e82e70f3
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-04-22 16:20:18 +0800

    git-odb without cargo warnings due to using the same test twice

--- git-odb/tests/odb/mod.rs
+++ git-odb/tests/odb/mod.rs
@@ -1,14 +1,14 @@
 pub use test_tools::{fixture_path, scripted_fixture_repo_read_only};
 
-type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error>>;
+pub type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[cfg(not(windows))]
-fn fixup(v: Vec<u8>) -> Vec<u8> {
+pub fn fixup(v: Vec<u8>) -> Vec<u8> {
     v
 }
 
 #[cfg(windows)]
-fn fixup(v: Vec<u8>) -> Vec<u8> {
+pub fn fixup(v: Vec<u8>) -> Vec<u8> {
     // Git checks out text files with line ending conversions, git itself will of course not put '\r\n' anywhere,
     // so that wouldn't be expected in an object and doesn't have to be parsed.
     use bstr::ByteSlice;
@@ -19,10 +19,10 @@ pub fn hex_to_id(hex: &str) -> git_hash::ObjectId {
     git_hash::ObjectId::from_hex(hex.as_bytes()).expect("40 bytes hex")
 }
 
-mod alternate;
-mod compound;
-mod linked;
-mod loose;
-mod pack;
-mod sink;
-mod traverse;
+pub mod alternate;
+pub mod compound;
+pub mod linked;
+pub mod loose;
+pub mod pack;
+pub mod sink;
+pub mod traverse;
