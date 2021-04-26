42fdd8d94b6fb65c1900cfef4f44dad619f7f09d
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-04-26 10:10:17 +0800

    Add git-diff crate

--- etc/release.sh
+++ etc/release.sh
@@ -4,6 +4,6 @@ set -eu -o pipefail
 
 ./etc/check-package-size.sh
 
-for crate in git-features git-ref git-object git-odb git-packetline git-url git-transport git-protocol git-repository gitoxide-core .; do
+for crate in git-features git-ref git-object git-diff git-odb git-packetline git-url git-transport git-protocol git-repository gitoxide-core .; do
   (cd $crate && cargo release "$@")
 done
