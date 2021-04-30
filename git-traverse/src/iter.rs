fcd36c21f9cce1ec88871f1b1c506104b962eebc
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-04-30 13:14:32 +0800

    [traversal] use State to allow reuse

--- git-traverse/src/iter.rs
+++ git-traverse/src/iter.rs
@@ -21,15 +21,29 @@ pub mod ancestors {
         }
     }
 
-    /// An iterator over the ancestors one or more starting commits
-    pub struct Ancestors<Find> {
-        find: Find,
+    /// The state used and potentially shared by multiple graph traversals.
+    #[derive(Default, Clone)]
+    pub struct State {
         next: VecDeque<ObjectId>,
         buf: Vec<u8>,
         seen: BTreeSet<ObjectId>,
     }
 
-    impl<Find> Ancestors<Find>
+    impl State {
+        fn clear(&mut self) {
+            self.next.clear();
+            self.buf.clear();
+            self.seen.clear();
+        }
+    }
+
+    /// An iterator over the ancestors one or more starting commits
+    pub struct Ancestors<'s, Find> {
+        find: Find,
+        state: &'s mut State,
+    }
+
+    impl<'s, Find> Ancestors<'s, Find>
     where
         Find: for<'a> FnMut(&oid, &'a mut Vec<u8>) -> Option<immutable::CommitIter<'a>>,
     {
@@ -38,31 +52,29 @@ pub mod ancestors {
         /// * `find` - a way to lookup new object data during traversal by their ObjectId, writing their data into buffer and returning
         ///    an iterator over commit tokens if the object is present and is a commit. Caching should be implemented within this function
         ///    as needed.
+        /// * `state` - all state used for the traversal. If multiple traversals are performed, allocations can be minimized by reusing
+        ///   this state.
         /// * `tips`
         ///   * the starting points of the iteration, usually commits
         ///   * each commit they lead to will only be returned once, including the tip that started it
-        pub fn new(find: Find, tips: impl IntoIterator<Item = impl Into<ObjectId>>) -> Self {
-            let next: VecDeque<_> = tips.into_iter().map(Into::into).collect();
-            let seen = next.iter().cloned().collect();
-            Ancestors {
-                find,
-                next,
-                buf: Vec::with_capacity(4096),
-                seen,
-            }
+        pub fn new(tips: impl IntoIterator<Item = impl Into<ObjectId>>, state: &'s mut State, find: Find) -> Self {
+            state.clear();
+            state.next.extend(tips.into_iter().map(Into::into));
+            state.seen.extend(state.next.iter().cloned());
+            Ancestors { find, state }
         }
     }
 
-    impl<Find> Iterator for Ancestors<Find>
+    impl<'s, Find> Iterator for Ancestors<'s, Find>
     where
         Find: for<'a> FnMut(&oid, &'a mut Vec<u8>) -> Option<immutable::CommitIter<'a>>,
     {
         type Item = Result<ObjectId, Error>;
 
         fn next(&mut self) -> Option<Self::Item> {
-            let res = self.next.pop_front();
+            let res = self.state.next.pop_front();
             if let Some(oid) = res {
-                match (self.find)(&oid, &mut self.buf) {
+                match (self.find)(&oid, &mut self.state.buf) {
                     Some(mut commit_iter) => {
                         if let Some(Err(decode_tree_err)) = commit_iter.next() {
                             return Some(Err(decode_tree_err.into()));
@@ -70,9 +82,9 @@ pub mod ancestors {
                         for token in commit_iter {
                             match token {
                                 Ok(immutable::commit::iter::Token::Parent { id }) => {
-                                    let was_inserted = self.seen.insert(id);
+                                    let was_inserted = self.state.seen.insert(id);
                                     if was_inserted {
-                                        self.next.push_back(id);
+                                        self.state.next.push_back(id);
                                     }
                                 }
                                 Ok(_a_token_past_the_parents) => break,
