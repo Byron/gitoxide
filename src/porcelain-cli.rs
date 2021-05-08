4382802378dfc0cf565b5610ff0e5320dcf4f474
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-05-08 10:00:41 +0800

    Remove 'argh' implementation in porcelain, opening it up to sub-sub commands…
    …and otherwise more fancy CLIs

--- src/porcelain-cli.rs
+++ src/porcelain-cli.rs
@@ -1,17 +1,20 @@
 #![forbid(unsafe_code)]
 #![deny(rust_2018_idioms)]
 
+#[cfg(all(not(feature = "lean-cli"), feature = "pretty-cli"))]
 mod porcelain;
+#[cfg(all(not(feature = "lean-cli"), feature = "pretty-cli"))]
 mod shared;
 
 use anyhow::Result;
 
-#[cfg(feature = "pretty-cli")]
+#[cfg(all(not(feature = "lean-cli"), feature = "pretty-cli"))]
 fn main() -> Result<()> {
-    porcelain::pretty::main()
+    porcelain::options::main()
 }
 
-#[cfg(all(feature = "lean-cli", not(feature = "pretty-cli")))]
+#[cfg(feature = "lean-cli")]
 fn main() -> Result<()> {
-    porcelain::lean::main()
+    eprintln!("There is no lean version of the porcelain CLI. Please build with the 'pretty-cli' feature flag.");
+    Ok(())
 }
--- src/porcelain/mod.rs
+++ src/porcelain/mod.rs
@@ -1,5 +1,1 @@
-#[cfg(feature = "pretty-cli")]
-pub mod pretty;
-
-#[cfg(all(feature = "lean-cli", not(feature = "pretty-cli")))]
-pub mod lean;
+pub mod options;
