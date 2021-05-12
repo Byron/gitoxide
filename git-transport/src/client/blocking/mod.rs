9d62ca338927139708246ce0934f1cb317f14784
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-05-12 16:04:41 +0800

    Make the blocking client the default…
    …and if UNSET, everything is non-blocking, but nothing is actually
    pulled in right now.

--- git-transport/src/client/blocking/mod.rs
+++ git-transport/src/client/blocking/mod.rs
@@ -1,7 +1,6 @@
 ///
 pub mod capabilities;
-///
-pub mod connect;
+pub(crate) mod connect;
 ///
 pub mod file;
 ///
@@ -9,7 +8,193 @@ pub mod git;
 ///
 #[cfg(feature = "http-client-curl")]
 pub mod http;
-///
-pub mod request;
+pub(crate) mod request;
 ///
 pub mod ssh;
+
+use crate::{Protocol, Service};
+use bstr::BString;
+use std::{io, io::Write};
+
+use capabilities::Capabilities;
+use request::{ExtendedBufRead, RequestWriter};
+
+#[cfg(feature = "http-client-curl")]
+type HttpError = http::Error;
+#[cfg(not(feature = "http-client-curl"))]
+type HttpError = std::convert::Infallible;
+
+/// The error used in most methods of the [`client`][crate::client] module
+#[derive(thiserror::Error, Debug)]
+#[allow(missing_docs)]
+pub enum Error {
+    #[error("An IO error occurred when talking to the server")]
+    Io {
+        #[from]
+        err: io::Error,
+    },
+    #[error("Capabilities could not be parsed")]
+    Capabilities {
+        #[from]
+        err: capabilities::Error,
+    },
+    #[error("A packet line could not be decoded")]
+    LineDecode {
+        #[from]
+        err: git_packetline::decode::Error,
+    },
+    #[error("A {0} line was expected, but there was none")]
+    ExpectedLine(&'static str),
+    #[error("Expected a data line, but got a delimiter")]
+    ExpectedDataLine,
+    #[error("The transport layer does not support authentication")]
+    AuthenticationUnsupported,
+    #[error("The transport layer refuses to use a given identity: {0}")]
+    AuthenticationRefused(&'static str),
+    #[error(transparent)]
+    Http(#[from] HttpError),
+}
+
+/// The response of the [`handshake()`][Transport::handshake()] method.
+pub struct SetServiceResponse<'a> {
+    /// The protocol the service can provide. May be different from the requested one
+    pub actual_protocol: Protocol,
+    /// The capabilities parsed from the server response.
+    pub capabilities: Capabilities,
+    /// In protocol version one, this is set to a list of refs and their peeled counterparts.
+    pub refs: Option<Box<dyn io::BufRead + 'a>>,
+}
+
+/// Configure how the [`RequestWriter`] behaves when writing bytes.
+#[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone, Copy)]
+#[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
+pub enum WriteMode {
+    /// Each [write()][Write::write()] call writes the bytes verbatim as one or more packet lines.
+    Binary,
+    /// Each [write()][Write::write()] call assumes text in the input, assures a trailing newline and writes it as single packet line.
+    OneLfTerminatedLinePerWriteCall,
+}
+
+impl Default for WriteMode {
+    fn default() -> Self {
+        WriteMode::OneLfTerminatedLinePerWriteCall
+    }
+}
+
+/// The kind of packet line to write when transforming a [`RequestWriter`] into an [`ExtendedBufRead`].
+#[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone, Copy)]
+#[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
+pub enum MessageKind {
+    /// A `flush` packet.
+    Flush,
+    /// A V2 delimiter.
+    Delimiter,
+    /// The end of a response.
+    ResponseEnd,
+    /// The given text.
+    Text(&'static [u8]),
+}
+
+#[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
+#[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
+/// An identity for use when authenticating the transport layer.
+pub enum Identity {
+    /// An account based identity
+    Account {
+        /// The user's name
+        username: String,
+        /// The user's password
+        password: String,
+    },
+}
+
+/// All methods provided here must be called in the correct order according to the [communication protocol][Protocol]
+/// used to connect to them.
+/// It does, however, know just enough to be able to provide a higher-level interface than would otherwise be possible.
+/// Thus the consumer of this trait will not have to deal with packet lines at all.
+/// **Note that**  whenever a `Read` trait or `Write` trait is produced, it must be exhausted.
+pub trait Transport {
+    /// Initiate connection to the given service.
+    /// Returns the service capabilities according according to the actual [Protocol] it supports,
+    /// and possibly a list of refs to be obtained.
+    /// This means that asking for an unsupported protocol will result in a protocol downgrade to the given one.
+    /// using the `read_line(…)` function of the given [BufReader][SetServiceResponse::refs].
+    /// It must be exhausted, that is, read to the end before the next method can be invoked.
+    fn handshake(&mut self, service: Service) -> Result<SetServiceResponse<'_>, Error>;
+
+    /// If the handshake or subsequent reads failed with [io::ErrorKind::PermissionDenied], use this method to
+    /// inform the transport layer about the identity to use for subsequent calls.
+    /// If authentication continues to fail even with an identity set, consider communicating this to the provider
+    /// of the identity in order to mark it as invalid. Otherwise the user might have difficulty updating obsolete
+    /// credentials.
+    /// Please note that most transport layers are unauthenticated and thus return [an error][Error::AuthenticationUnsupported] here.
+    fn set_identity(&mut self, _identity: Identity) -> Result<(), Error> {
+        Err(Error::AuthenticationUnsupported)
+    }
+    /// Get a writer for sending data and obtaining the response. It can be configured in various ways
+    /// to support the task at hand.
+    /// `write_mode` determines how calls to the `write(…)` method are interpreted, and `on_into_read` determines
+    /// which message to write when the writer is turned into the response reader using [`into_read()`][RequestWriter::into_read()].
+    fn request(&mut self, write_mode: WriteMode, on_into_read: MessageKind) -> Result<RequestWriter<'_>, Error>;
+
+    /// Closes the connection to indicate no further requests will be made.
+    fn close(&mut self) -> Result<(), Error>;
+
+    /// Returns the canonical URL pointing to the destination of this transport.
+    /// Please note that local paths may not be represented correctly, as they will go through a potentially lossy
+    /// unicode conversion.
+    fn to_url(&self) -> String;
+
+    /// Returns the protocol version that was initially desired upon connection
+    /// Please note that the actual protocol might differ after the handshake was conducted in case the server
+    /// did not support it.
+    fn desired_protocol_version(&self) -> Protocol;
+
+    /// Returns true if the transport is inherently stateful, or false otherwise.
+    /// Not being stateful implies that certain information has to be resent on each 'turn'
+    /// of the fetch negotiation.
+    ///
+    /// # Implementation Details
+    ///
+    /// This answer should not be based on the [Protocol] itself, which might enforce stateless
+    /// interactions despite the connections staying intact which might imply statefulness.
+    fn is_stateful(&self) -> bool;
+}
+
+/// An extension trait to add more methods to everything implementing [`Transport`].
+pub trait TransportV2Ext {
+    /// Invoke a protocol V2 style `command` with given `capabilities` and optional command specific `arguments`.
+    /// The `capabilities` were communicated during the handshake.
+    /// _Note:_ panics if [handshake][Transport::handshake()] wasn't performed beforehand.
+    fn invoke<'a>(
+        &mut self,
+        command: &str,
+        capabilities: impl IntoIterator<Item = (&'a str, Option<&'a str>)>,
+        arguments: Option<impl IntoIterator<Item = bstr::BString>>,
+    ) -> Result<Box<dyn ExtendedBufRead + '_>, Error>;
+}
+
+impl<T: Transport> TransportV2Ext for T {
+    fn invoke<'a>(
+        &mut self,
+        command: &str,
+        capabilities: impl IntoIterator<Item = (&'a str, Option<&'a str>)>,
+        arguments: Option<impl IntoIterator<Item = BString>>,
+    ) -> Result<Box<dyn ExtendedBufRead + '_>, Error> {
+        let mut writer = self.request(WriteMode::OneLfTerminatedLinePerWriteCall, MessageKind::Flush)?;
+        writer.write_all(format!("command={}", command).as_bytes())?;
+        for (name, value) in capabilities {
+            match value {
+                Some(value) => writer.write_all(format!("{}={}", name, value).as_bytes()),
+                None => writer.write_all(name.as_bytes()),
+            }?;
+        }
+        if let Some(arguments) = arguments {
+            writer.write_message(MessageKind::Delimiter)?;
+            for argument in arguments {
+                writer.write_all(argument.as_ref())?;
+            }
+        }
+        Ok(writer.into_read()?)
+    }
+}
