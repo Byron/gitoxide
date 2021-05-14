7d792887d743cc649ae20010a3686a14f65cd3ad
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-05-14 14:40:21 +0800

    [git-packetline] refactor

--- git-packetline/tests/blocking/encode.rs
+++ git-packetline/tests/blocking/encode.rs
@@ -1,19 +1,11 @@
 mod data_to_write {
     use crate::assert_err_display;
     use bstr::ByteSlice;
-    use git_packetline::encode::{
-        data_to_write, delim_to_write, error_to_write, flush_to_write, response_end_to_write, text_to_write,
-    };
+    use git_packetline::encode::data_to_write;
     use std::io;
 
-    fn vec_sized(size: usize) -> Vec<u8> {
-        let mut v = Vec::new();
-        v.resize(size, 0);
-        v
-    }
-
     #[test]
-    fn success_binary_and_non_binary() -> crate::Result {
+    fn binary_and_non_binary() -> crate::Result {
         let mut out = Vec::new();
         assert_eq!(data_to_write(b"\0", &mut out)?, 5);
         assert_eq!(out.as_bstr(), b"0005\0".as_bstr());
@@ -25,7 +17,30 @@ mod data_to_write {
     }
 
     #[test]
-    fn success_text_to_write_always_appends_a_newline() -> crate::Result {
+    fn error_if_data_exceeds_limit() {
+        fn vec_sized(size: usize) -> Vec<u8> {
+            let mut v = Vec::new();
+            v.resize(size, 0);
+            v
+        }
+
+        assert_err_display(
+            data_to_write(&vec_sized(65516 + 1), io::sink()),
+            "Cannot encode more than 65516 bytes, got 65517",
+        );
+    }
+    #[test]
+    fn error_if_data_is_empty() {
+        assert_err_display(data_to_write(&[], io::sink()), "Empty lines are invalid");
+    }
+}
+
+mod text_to_write {
+    use bstr::ByteSlice;
+    use git_packetline::encode::text_to_write;
+
+    #[test]
+    fn always_appends_a_newline() -> crate::Result {
         let mut out = Vec::new();
         assert_eq!(text_to_write(b"a", &mut out)?, 6);
         assert_eq!(out.as_bstr(), b"0006a\n".as_bstr());
@@ -39,6 +54,11 @@ mod data_to_write {
         );
         Ok(())
     }
+}
+
+mod flush_delim_response_end {
+    use bstr::ByteSlice;
+    use git_packetline::encode::{delim_to_write, flush_to_write, response_end_to_write};
 
     #[test]
     fn success_flush_delim_response_end() -> crate::Result {
@@ -55,24 +75,17 @@ mod data_to_write {
         assert_eq!(out.as_bstr(), b"0002".as_bstr());
         Ok(())
     }
+}
+
+mod error {
+    use bstr::ByteSlice;
+    use git_packetline::encode::error_to_write;
 
     #[test]
-    fn successful_write_of_error_line() -> crate::Result {
+    fn write_line() -> crate::Result {
         let mut out = Vec::new();
         assert_eq!(error_to_write(b"hello error", &mut out)?, 19);
         assert_eq!(out.as_bstr(), b"0013ERR hello error".as_bstr());
         Ok(())
-    }
-
-    #[test]
-    fn error_if_data_exceeds_limit() {
-        assert_err_display(
-            data_to_write(&vec_sized(65516 + 1), io::sink()),
-            "Cannot encode more than 65516 bytes, got 65517",
-        );
-    }
-    #[test]
-    fn error_if_data_is_empty() {
-        assert_err_display(data_to_write(&[], io::sink()), "Empty lines are invalid");
     }
 }
