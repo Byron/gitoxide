eef8c9f0b320cea89e900cfd7b5eed54d3bc7a8f
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-05-15 13:32:40 +0800

    [git-packetline] All tests for high-level writer pass

--- git-packetline/tests/async_io/write.rs
+++ git-packetline/tests/async_io/write.rs
@@ -2,46 +2,50 @@ use bstr::ByteSlice;
 use futures_lite::{future, prelude::*};
 use git_packetline::Writer;
 
-//
-// const MAX_DATA_LEN: usize = 65516;
-// const MAX_LINE_LEN: usize = 4 + MAX_DATA_LEN;
-//
+const MAX_DATA_LEN: usize = 65516;
+const MAX_LINE_LEN: usize = 4 + MAX_DATA_LEN;
+
 #[test]
-fn each_write_results_in_one_line() {
+fn each_write_results_in_one_line() -> crate::Result {
     let buf = future::block_on(async {
         let mut w = Writer::new(Vec::new());
         w.write_all(b"hello").await?;
         w.write(b"world!").await?;
         Ok::<_, std::io::Error>(w.into_inner())
-    })
-    .unwrap();
+    })?;
     assert_eq!(buf.as_bstr(), b"0009hello000aworld!".as_bstr());
-    // Ok(())
+    Ok(())
 }
-//
-// #[test]
-// fn write_text_and_write_binary() -> crate::Result {
-//     let mut w = Writer::new(Vec::new()).text_mode();
-//     w.write_all(b"hello")?;
-//     w = w.binary_mode();
-//     w.write(b"world")?;
-//     assert_eq!(w.inner.as_bstr(), b"000ahello\n0009world".as_bstr());
-//     Ok(())
-// }
-//
-// #[test]
-// fn huge_writes_are_split_into_lines() -> crate::Result {
-//     let data = {
-//         let mut v = Vec::new();
-//         v.resize(MAX_DATA_LEN * 2, 0);
-//         v
-//     };
-//     let mut w = Writer::new(Vec::new());
-//     w.write(&data)?;
-//     assert_eq!(w.inner.len(), MAX_LINE_LEN * 2);
-//     Ok(())
-// }
-//
+
+#[test]
+fn write_text_and_write_binary() -> crate::Result {
+    let buf = future::block_on(async {
+        let mut w = Writer::new(Vec::new()).text_mode();
+        w.write_all(b"hello").await?;
+        w = w.binary_mode();
+        w.write(b"world").await?;
+        Ok::<_, std::io::Error>(w.into_inner())
+    })?;
+    assert_eq!(buf.as_bstr(), b"000ahello\n0009world".as_bstr());
+    Ok(())
+}
+
+#[test]
+fn huge_writes_are_split_into_lines() -> crate::Result {
+    let buf = future::block_on(async {
+        let data = {
+            let mut v = Vec::new();
+            v.resize(MAX_DATA_LEN * 2, 0);
+            v
+        };
+        let mut w = Writer::new(Vec::new());
+        w.write(&data).await?;
+        Ok::<_, std::io::Error>(w.into_inner())
+    })?;
+    assert_eq!(buf.len(), MAX_LINE_LEN * 2);
+    Ok(())
+}
+
 #[test]
 fn empty_writes_fail_with_error() {
     assert_eq!(
