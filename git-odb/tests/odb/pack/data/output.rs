67b1512db8c3bdb2ea946d0de96190146be9ed18
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-05-21 11:18:45 +0800

    [pack-gen] the first test for object counts
    Lets keep it implemented as iterator to allow interruptions more easily.
    The caller will have to see how to build a vector without wasting too
    much.

--- git-odb/tests/odb/pack/data/output.rs
+++ git-odb/tests/odb/pack/data/output.rs
@@ -1,4 +1,6 @@
-use git_odb::pack::data::output;
+use crate::odb::fixture_path;
+use git_odb::{linked, pack::data::output};
+use std::{path::PathBuf, sync::Arc};
 
 #[test]
 fn size_of_entry() {
@@ -18,31 +20,122 @@ fn size_of_count() {
     )
 }
 
+enum DbKind {
+    AbunchOfRandomObjects,
+    DeterministicGeneratedContent,
+}
+
+fn db(kind: DbKind) -> crate::Result<Arc<linked::Db>> {
+    use DbKind::*;
+    let path: PathBuf = match kind {
+        AbunchOfRandomObjects => fixture_path("objects"),
+        DeterministicGeneratedContent => git_testtools::scripted_fixture_repo_read_only("make_pack_gen_repo.sh")?
+            .join(".git")
+            .join("objects"),
+    };
+    linked::Db::at(path).map_err(Into::into).map(Into::into)
+}
+
+mod count {
+    use crate::odb::{
+        hex_to_id,
+        pack::data::output::{db, DbKind},
+    };
+    use git_features::progress;
+    use git_odb::{pack, pack::data::output, FindExt};
+    use git_traverse::commit;
+    use std::sync::Arc;
+
+    #[test]
+    fn traversals() -> crate::Result {
+        let db = db(DbKind::DeterministicGeneratedContent)?;
+        #[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+        struct Count {
+            trees: usize,
+            commits: usize,
+            blobs: usize,
+            tags: usize,
+        }
+        impl Count {
+            fn total(&self) -> usize {
+                self.tags + self.trees + self.commits + self.blobs
+            }
+            fn add(&mut self, kind: git_object::Kind) {
+                use git_object::Kind::*;
+                match kind {
+                    Tree => self.trees += 1,
+                    Commit => self.commits += 1,
+                    Blob => self.blobs += 1,
+                    Tag => self.tags += 1,
+                }
+            }
+        }
+        let whole_pack = Count {
+            trees: 40,
+            commits: 16,
+            blobs: 811,
+            tags: 1,
+        };
+        for (expansion_mode, expected_count) in [
+            (
+                output::count_objects::ObjectExpansion::AsIs,
+                Count {
+                    trees: 0,
+                    commits: 15,
+                    blobs: 0,
+                    tags: 1,
+                },
+            ),
+            (output::count_objects::ObjectExpansion::TreeContents, whole_pack),
+            (
+                output::count_objects::ObjectExpansion::TreeAdditionsComparedToAncestor,
+                whole_pack,
+            ),
+        ]
+        .iter()
+        .copied()
+        {
+            let head = hex_to_id("dfcb5e39ac6eb30179808bbab721e8a28ce1b52e");
+            let commits = commit::Ancestors::new(Some(head), commit::ancestors::State::default(), {
+                let db = Arc::clone(&db);
+                move |oid, buf| db.find_existing_commit_iter(oid, buf, &mut pack::cache::Never).ok()
+            })
+            .map(Result::unwrap);
+            let entries: Vec<_> = output::count_objects_iter(
+                db.clone(),
+                || pack::cache::Never,
+                commits.chain(std::iter::once(hex_to_id("e3fb53cbb4c346d48732a24f09cf445e49bc63d6"))),
+                progress::Discard,
+                output::count_objects::Options {
+                    input_object_expansion: expansion_mode,
+                    ..Default::default()
+                },
+            )
+            .collect::<Result<Vec<_>, _>>()?
+            .into_iter()
+            .flatten()
+            .collect();
+            let actual_count = entries.iter().fold(Count::default(), |mut c, e| {
+                c.add(e.object_kind);
+                c
+            });
+            assert_eq!(actual_count, expected_count);
+            assert_eq!(entries.len(), expected_count.total());
+        }
+        Ok(())
+    }
+}
+
 mod entries {
     mod simple_compression {
-        use crate::{fixture_path, odb::hex_to_id};
+        use crate::{
+            odb::hex_to_id,
+            odb::pack::data::output::{db, DbKind},
+        };
         use git_features::progress;
-        use git_odb::{compound, linked, pack, pack::data::output, FindExt};
+        use git_odb::{compound, pack, pack::data::output, FindExt};
         use git_traverse::commit;
-        use std::{path::PathBuf, sync::Arc};
-
-        enum DbKind {
-            AbunchOfRandomObjects,
-            DeterministicGeneratedContent,
-        }
-
-        fn db(kind: DbKind) -> crate::Result<Arc<linked::Db>> {
-            use DbKind::*;
-            let path: PathBuf = match kind {
-                AbunchOfRandomObjects => fixture_path("objects"),
-                DeterministicGeneratedContent => {
-                    git_testtools::scripted_fixture_repo_read_only("make_pack_gen_repo.sh")?
-                        .join(".git")
-                        .join("objects")
-                }
-            };
-            linked::Db::at(path).map_err(Into::into).map(Into::into)
-        }
+        use std::sync::Arc;
 
         #[test]
         fn all_input_objects_as_is() -> crate::Result {
