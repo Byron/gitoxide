77d0cc088d6de8c37fec9ae0136c9f85bfdbc643
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-05-26 11:16:41 +0800

    [git-ref] refactor

--- git-ref/tests/loose/mod.rs
+++ git-ref/tests/loose/mod.rs
@@ -12,27 +12,34 @@ mod reference {
             use git_ref::loose::Reference;
             use git_testtools::hex_to_id;
 
-            #[test]
-            fn peeled() {
-                let store = store();
-                let reference =
-                    Reference::from_path(&store, "name", b"c5241b835b93af497cda80ce0dceb8f49800df1c\n").unwrap();
-                assert_eq!(reference.kind(), git_ref::Kind::Peeled);
-                assert_eq!(
-                    reference.target().as_id(),
-                    Some(hex_to_id("c5241b835b93af497cda80ce0dceb8f49800df1c").as_ref())
-                );
-                assert_eq!(reference.target().as_ref(), None);
+            macro_rules! mktest {
+                ($name:ident, $input:literal, $kind:path, $id:expr, $ref:expr) => {
+                    #[test]
+                    fn $name() {
+                        let store = store();
+                        let reference = Reference::from_path(&store, "name", $input).unwrap();
+                        assert_eq!(reference.kind(), $kind);
+                        assert_eq!(reference.target().as_id(), $id);
+                        assert_eq!(reference.target().as_ref(), $ref);
+                    }
+                };
             }
 
-            #[test]
-            fn symbolic() {
-                let store = store();
-                let reference = Reference::from_path(&store, "name", b"ref: refs/heads/main\n").unwrap();
-                assert_eq!(reference.kind(), git_ref::Kind::Symbolic);
-                assert_eq!(reference.target().as_id(), None);
-                assert_eq!(reference.target().as_ref(), Some(b"refs/heads/main".as_bstr()));
-            }
+            mktest!(
+                peeled,
+                b"c5241b835b93af497cda80ce0dceb8f49800df1c\n",
+                git_ref::Kind::Peeled,
+                Some(hex_to_id("c5241b835b93af497cda80ce0dceb8f49800df1c").as_ref()),
+                None
+            );
+
+            mktest!(
+                symbolic,
+                b"ref: refs/heads/main\n",
+                git_ref::Kind::Symbolic,
+                None,
+                Some(b"refs/heads/main".as_bstr())
+            );
         }
     }
 }
