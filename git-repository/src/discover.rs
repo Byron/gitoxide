a2d58c100ca696bceaaa0788347bba41f29ab0b8
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-05-27 23:38:12 +0800

    [git-repository] refactor

--- git-repository/src/discover.rs
+++ git-repository/src/discover.rs
@@ -4,26 +4,6 @@ pub mod existing {
     use quick_error::quick_error;
     use std::path::PathBuf;
 
-    mod repository {
-        use crate::{discover, Repository};
-        use std::path::Path;
-
-        impl Repository {
-            pub fn discover(directory: impl AsRef<Path>) -> Result<Self, discover::existing::Error> {
-                let path = discover::existing(directory)?;
-                let (git_dir, working_tree) = match path {
-                    crate::Path::WorkingTree(working_tree) => (working_tree.join(".git"), Some(working_tree)),
-                    crate::Path::Repository(repository) => (repository, None),
-                };
-                Ok(Repository {
-                    odb: git_odb::linked::Store::at(git_dir.join("objects")).unwrap(), // TODO: proper error
-                    refs: git_ref::file::Store::at(git_dir),
-                    working_tree,
-                })
-            }
-        }
-    }
-
     quick_error! {
         #[derive(Debug)]
         pub enum Error {
