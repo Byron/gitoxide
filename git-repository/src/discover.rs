c8323e484f08d5ea59400636cb26334d6976e4c0
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-05-27 22:24:44 +0800

    [git-repository] refactor

--- git-repository/src/discover.rs
+++ git-repository/src/discover.rs
@@ -1,4 +1,4 @@
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 mod path {
     use quick_error::quick_error;
@@ -26,11 +26,11 @@ pub fn existing(directory: impl AsRef<Path>) -> Result<crate::Path, path::Error>
 
     let mut cursor = directory;
     loop {
-        if let Ok(kind) = is_git(cursor) {
+        if let Ok(kind) = crate::is_git(cursor) {
             break Ok(crate::Path::from_dot_git_dir(cursor, kind));
         }
         let git_dir = cursor.join(".git");
-        if let Ok(kind) = is_git(&git_dir) {
+        if let Ok(kind) = crate::is_git(&git_dir) {
             break Ok(crate::Path::from_dot_git_dir(git_dir, kind));
         }
         match cursor.parent() {
@@ -38,70 +38,4 @@ pub fn existing(directory: impl AsRef<Path>) -> Result<crate::Path, path::Error>
             None => break Err(path::Error::NoGitRepository(directory.to_owned())),
         }
     }
-}
-
-pub mod is_git {
-    use quick_error::quick_error;
-    use std::path::PathBuf;
-
-    quick_error! {
-        #[derive(Debug)]
-        pub enum Error {
-            FindHeadRef(err: git_ref::file::find_one::existing::Error) {
-                display("Could not find a valid HEAD reference")
-                from()
-                source(err)
-            }
-            MisplacedHead(relative_path: PathBuf) {
-                display("Expected HEAD at '.git/HEAD', got '.git/{}'", relative_path.display())
-            }
-            MissingObjectsDirectory(missing: PathBuf) {
-                display("Expected an objects directory at '{}'", missing.display())
-            }
-            MissingRefsDirectory(missing: PathBuf) {
-                display("Expected a refs directory at '{}'", missing.display())
-            }
-        }
-    }
-}
-
-/// What constitutes a valid git repository, and what's yet to be implemented.
-///
-/// * [x] a valid head
-/// * [ ] git common directory
-///   * [ ] respect GIT_COMMON_DIR
-/// * [x] an objects directory
-///   * [x] respect GIT_OBJECT_DIRECTORY
-/// * [x] a refs directory
-pub fn is_git(git_dir: impl AsRef<Path>) -> Result<crate::Kind, is_git::Error> {
-    let dot_git = git_dir.as_ref();
-
-    {
-        let refs = git_ref::file::Store::at(&dot_git);
-        let head = refs.find_one_existing("HEAD")?;
-        if head.relative_path != Path::new("HEAD") {
-            return Err(is_git::Error::MisplacedHead(head.relative_path));
-        }
-    }
-
-    {
-        let objects_path = std::env::var("GIT_OBJECT_DIRECTORY")
-            .map(PathBuf::from)
-            .unwrap_or_else(|_| dot_git.join("objects"));
-        if !objects_path.is_dir() {
-            return Err(is_git::Error::MissingObjectsDirectory(objects_path));
-        }
-    }
-    {
-        let refs_path = dot_git.join("refs");
-        if !refs_path.is_dir() {
-            return Err(is_git::Error::MissingRefsDirectory(refs_path));
-        }
-    }
-
-    Ok(if dot_git.join("index").is_file() {
-        crate::Kind::WorkingTree
-    } else {
-        crate::Kind::Bare
-    })
 }
--- git-repository/src/lib.rs
+++ git-repository/src/lib.rs
@@ -53,3 +53,70 @@ mod path {
     }
 }
 pub use path::Path;
+
+pub mod is_git {
+    use quick_error::quick_error;
+    use std::path::{Path, PathBuf};
+
+    quick_error! {
+        #[derive(Debug)]
+        pub enum Error {
+            FindHeadRef(err: git_ref::file::find_one::existing::Error) {
+                display("Could not find a valid HEAD reference")
+                from()
+                source(err)
+            }
+            MisplacedHead(relative_path: PathBuf) {
+                display("Expected HEAD at '.git/HEAD', got '.git/{}'", relative_path.display())
+            }
+            MissingObjectsDirectory(missing: PathBuf) {
+                display("Expected an objects directory at '{}'", missing.display())
+            }
+            MissingRefsDirectory(missing: PathBuf) {
+                display("Expected a refs directory at '{}'", missing.display())
+            }
+        }
+    }
+
+    /// What constitutes a valid git repository, and what's yet to be implemented.
+    ///
+    /// * [x] a valid head
+    /// * [ ] git common directory
+    ///   * [ ] respect GIT_COMMON_DIR
+    /// * [x] an objects directory
+    ///   * [x] respect GIT_OBJECT_DIRECTORY
+    /// * [x] a refs directory
+    pub fn is_git(git_dir: impl AsRef<Path>) -> Result<crate::Kind, Error> {
+        let dot_git = git_dir.as_ref();
+
+        {
+            let refs = git_ref::file::Store::at(&dot_git);
+            let head = refs.find_one_existing("HEAD")?;
+            if head.relative_path != Path::new("HEAD") {
+                return Err(Error::MisplacedHead(head.relative_path));
+            }
+        }
+
+        {
+            let objects_path = std::env::var("GIT_OBJECT_DIRECTORY")
+                .map(PathBuf::from)
+                .unwrap_or_else(|_| dot_git.join("objects"));
+            if !objects_path.is_dir() {
+                return Err(Error::MissingObjectsDirectory(objects_path));
+            }
+        }
+        {
+            let refs_path = dot_git.join("refs");
+            if !refs_path.is_dir() {
+                return Err(Error::MissingRefsDirectory(refs_path));
+            }
+        }
+
+        Ok(if dot_git.join("index").is_file() {
+            crate::Kind::WorkingTree
+        } else {
+            crate::Kind::Bare
+        })
+    }
+}
+pub use is_git::is_git;
