51135291b60d38bdf50d24569596c421bcb4f0b9
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-05-28 16:13:23 +0800

    [git-refs] a way to build a big packed-refs file
    150k refs consume about 10MB, git handles resolving them very quickly,
    too.

---
+++ git-ref/tests/fixtures/make_repository_with_lots_of_refs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eu -o pipefail
+
+git init -q
+git config commit.gpgsign false
+
+git checkout -q -b main
+git commit -q --allow-empty -m c1
+
+current_commit="$(git rev-parse HEAD)"
+
+for level in $(seq -w 1000); do
+  mkdir -p .git/refs/heads/"$level"
+  for refname in $(seq 150); do
+    echo "$current_commit" > .git/refs/heads/"$level"/$refname
+  done
+done
+
+git tag t1
+git tag dt1
+
+git pack-refs --all
