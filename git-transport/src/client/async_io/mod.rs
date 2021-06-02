d94fbf83d3e57c54dead3fb849e63b1d37343cb2
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-06-02 21:20:29 +0800

    [git-transport] the first part of async transport for git connections

--- git-transport/src/client/async_io/mod.rs
+++ git-transport/src/client/async_io/mod.rs
@@ -10,6 +10,91 @@ pub use bufread_ext::{ExtendedBufRead, HandleProgress};
 mod request;
 pub use request::RequestWriter;
 
+mod git {
+    use crate::{
+        client::{self, capabilities, git, Capabilities, SetServiceResponse},
+        Protocol, Service,
+    };
+    use async_trait::async_trait;
+    use bstr::BString;
+    use futures_io::{AsyncRead, AsyncWrite};
+    use futures_lite::AsyncWriteExt;
+    use git_packetline::PacketLine;
+    use std::io;
+
+    #[async_trait]
+    impl<R, W> client::Transport for git::Connection<R, W>
+    where
+        R: AsyncRead + Unpin + Send,
+        W: AsyncWrite + Unpin + Send,
+    {
+        async fn handshake(&mut self, service: Service) -> Result<SetServiceResponse<'_>, client::Error> {
+            if self.mode == git::ConnectMode::Daemon {
+                let mut line_writer = git_packetline::Writer::new(&mut self.writer).binary_mode();
+                line_writer
+                    .write_all(&git::message::connect(
+                        service,
+                        self.desired_version,
+                        &self.path,
+                        self.virtual_host.as_ref(),
+                    ))
+                    .await?;
+                line_writer.flush().await?;
+            }
+
+            // let capabilities::recv::Outcome {
+            //     capabilities,
+            //     refs,
+            //     protocol: actual_protocol,
+            // } = Capabilities::from_lines_with_version_detection(&mut self.line_provider)?;
+            // Ok(SetServiceResponse {
+            //     actual_protocol,
+            //     capabilities,
+            //     refs,
+            // })
+            todo!("Capabilities::from_lines_with_version_detection")
+        }
+
+        fn request(
+            &mut self,
+            write_mode: client::WriteMode,
+            on_into_read: client::MessageKind,
+        ) -> Result<client::RequestWriter<'_>, client::Error> {
+            Ok(client::RequestWriter::new_from_bufread(
+                &mut self.writer,
+                Box::new(self.line_provider.as_read_without_sidebands()),
+                write_mode,
+                on_into_read,
+            ))
+        }
+
+        async fn close(&mut self) -> Result<(), client::Error> {
+            git_packetline::encode::flush_to_write(&mut self.writer).await?;
+            self.writer.flush().await?;
+            Ok(())
+        }
+
+        fn to_url(&self) -> String {
+            git_url::Url {
+                scheme: git_url::Scheme::File,
+                user: None,
+                host: None,
+                port: None,
+                path: self.path.clone(),
+            }
+            .to_string()
+        }
+
+        fn desired_protocol_version(&self) -> Protocol {
+            self.desired_version
+        }
+
+        fn is_stateful(&self) -> bool {
+            true
+        }
+    }
+}
+
 /// The response of the [`handshake()`][Transport::handshake()] method.
 pub struct SetServiceResponse<'a> {
     /// The protocol the service can provide. May be different from the requested one
@@ -48,7 +133,7 @@ pub trait Transport {
     /// to support the task at hand.
     /// `write_mode` determines how calls to the `write(…)` method are interpreted, and `on_into_read` determines
     /// which message to write when the writer is turned into the response reader using [`into_read()`][RequestWriter::into_read()].
-    async fn request(&mut self, write_mode: WriteMode, on_into_read: MessageKind) -> Result<RequestWriter<'_>, Error>;
+    fn request(&mut self, write_mode: WriteMode, on_into_read: MessageKind) -> Result<RequestWriter<'_>, Error>;
 
     /// Closes the connection to indicate no further requests will be made.
     async fn close(&mut self) -> Result<(), Error>;
@@ -74,7 +159,7 @@ pub trait Transport {
     fn is_stateful(&self) -> bool;
 }
 
-// TODO: needs fixes
+// TODO: needs fixes, important for easy V2
 // mod trait_ext;
 // pub use trait_ext::TransportV2Ext;
 
@@ -97,12 +182,8 @@ mod box_impl {
             self.deref_mut().set_identity(identity)
         }
 
-        async fn request(
-            &mut self,
-            write_mode: WriteMode,
-            on_into_read: MessageKind,
-        ) -> Result<RequestWriter<'_>, Error> {
-            self.deref_mut().request(write_mode, on_into_read).await
+        fn request(&mut self, write_mode: WriteMode, on_into_read: MessageKind) -> Result<RequestWriter<'_>, Error> {
+            self.deref_mut().request(write_mode, on_into_read)
         }
 
         async fn close(&mut self) -> Result<(), Error> {
