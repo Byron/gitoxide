71379ac25c44bc744ed0e93d2b126d4959bc4469
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-06-02 16:36:34 +0800

    [git-transport] ARGH: PIN!!!
    This is what happens if you forget that Pins are needed to call
    AsyncWrite methods - these are only implemented if the receiver is
    Pin. Makes sense, very much known, but stupid me sometimes forgets
    and the compiler in this case really wasn't helpful.
    
    Maybe one day it can propose trat a trait method requires a different
    receiver.

--- git-transport/src/client/request.rs
+++ git-transport/src/client/request.rs
@@ -1,25 +1,16 @@
-use crate::client::{ExtendedBufRead, MessageKind};
-use std::io;
-
-#[cfg(all(not(feature = "blocking-client"), feature = "async-client"))]
-use futures_io::AsyncWrite;
-
-/// A [`Write`][io::Write] implementation optimized for writing packet lines.
-/// A type implementing `Write` for packet lines, which when done can be transformed into a `Read` for
-/// obtaining the response.
-pub struct RequestWriter<'a> {
-    on_into_read: MessageKind,
-    #[cfg(feature = "blocking-client")]
-    pub(crate) writer: git_packetline::Writer<Box<dyn io::Write + 'a>>,
-    #[cfg(all(not(feature = "blocking-client"), feature = "async-client"))]
-    pub(crate) writer: git_packetline::Writer<Box<dyn AsyncWrite + 'a>>,
-    pub(crate) reader: Box<dyn ExtendedBufRead + 'a>,
-}
-
 #[cfg(feature = "blocking-client")]
 mod blocking_io {
-    use crate::client::{ExtendedBufRead, MessageKind, RequestWriter, WriteMode};
+    use crate::client::{ExtendedBufRead, MessageKind, WriteMode};
     use std::io;
+
+    /// A [`Write`][io::Write] implementation optimized for writing packet lines.
+    /// A type implementing `Write` for packet lines, which when done can be transformed into a `Read` for
+    /// obtaining the response.
+    pub struct RequestWriter<'a> {
+        on_into_read: MessageKind,
+        pub(crate) writer: git_packetline::Writer<Box<dyn io::Write + 'a>>,
+        pub(crate) reader: Box<dyn ExtendedBufRead + 'a>,
+    }
 
     impl<'a> io::Write for RequestWriter<'a> {
         fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
@@ -64,76 +55,93 @@ mod blocking_io {
             }
             .map(|_| ())
         }
+
+        /// Discard the ability to write and turn this instance into the reader for obtaining the other side's response.
+        pub fn into_read(mut self) -> std::io::Result<Box<dyn ExtendedBufRead + 'a>> {
+            self.write_message(self.on_into_read)?;
+            Ok(self.reader)
+        }
     }
 }
+#[cfg(feature = "blocking-client")]
+pub use blocking_io::RequestWriter;
 
 #[cfg(all(not(feature = "blocking-client"), feature = "async-client"))]
 mod async_io {
-    use crate::client::{ExtendedBufRead, MessageKind, RequestWriter, WriteMode};
-
+    use crate::client::{ExtendedBufRead, MessageKind};
     use futures_io::AsyncWrite;
+    use pin_project_lite::pin_project;
     use std::{
         io,
         pin::Pin,
         task::{Context, Poll},
     };
 
+    pin_project! {
+        /// A [`Write`][io::Write] implementation optimized for writing packet lines.
+        /// A type implementing `Write` for packet lines, which when done can be transformed into a `Read` for
+        /// obtaining the response.
+        pub struct RequestWriter<'a> {
+            on_into_read: MessageKind,
+            #[pin]
+            pub(crate) writer: git_packetline::Writer<Box<dyn AsyncWrite + Unpin + 'a>>,
+            pub(crate) reader: Box<dyn ExtendedBufRead + 'a>,
+        }
+    }
     impl<'a> futures_io::AsyncWrite for RequestWriter<'a> {
         fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<io::Result<usize>> {
-            self.writer.poll_write(cx, buf)
+            self.project().writer.poll_write(cx, buf)
         }
 
         fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-            self.writer.poll_flush(cx)
+            self.project().writer.poll_flush(cx)
         }
 
         fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-            self.writer.poll_close(cx)
+            self.project().writer.poll_close(cx)
         }
     }
 
-    /// methods with bonds to IO
-    impl<'a> RequestWriter<'a> {
-        /// Create a new instance from a `writer` (commonly a socket), a `reader` into which to transform once the
-        /// writes are finished, along with configuration for the `write_mode` and information about which message to write
-        /// when this instance is converted into a `reader` to read the request's response.
-        pub fn new_from_bufread<W: AsyncWrite + 'a>(
-            writer: W,
-            reader: Box<dyn ExtendedBufRead + 'a>,
-            write_mode: WriteMode,
-            on_into_read: MessageKind,
-        ) -> Self {
-            let mut writer = git_packetline::Writer::new(Box::new(writer) as Box<dyn io::Write>);
-            match write_mode {
-                WriteMode::Binary => writer.enable_binary_mode(),
-                WriteMode::OneLfTerminatedLinePerWriteCall => writer.enable_text_mode(),
-            }
-            RequestWriter {
-                on_into_read,
-                writer,
-                reader,
-            }
-        }
-
-        /// Write the given message as packet line.
-        pub async fn write_message(&mut self, message: MessageKind) -> io::Result<()> {
-            match message {
-                MessageKind::Flush => git_packetline::PacketLine::Flush.to_write(self.writer.inner_mut()),
-                MessageKind::Delimiter => git_packetline::PacketLine::Delimiter.to_write(self.writer.inner_mut()),
-                MessageKind::ResponseEnd => git_packetline::PacketLine::ResponseEnd.to_write(self.writer.inner_mut()),
-                MessageKind::Text(t) => git_packetline::immutable::Text::from(t).to_write(self.writer.inner_mut()),
-            }
-            .map(|_| ())
-            .await
-        }
-    }
+    // /// methods with bonds to IO
+    // impl<'a> RequestWriter<'a> {
+    //     /// Create a new instance from a `writer` (commonly a socket), a `reader` into which to transform once the
+    //     /// writes are finished, along with configuration for the `write_mode` and information about which message to write
+    //     /// when this instance is converted into a `reader` to read the request's response.
+    //     pub fn new_from_bufread<W: AsyncWrite + 'a>(
+    //         writer: W,
+    //         reader: Box<dyn ExtendedBufRead + 'a>,
+    //         write_mode: WriteMode,
+    //         on_into_read: MessageKind,
+    //     ) -> Self {
+    //         let mut writer = git_packetline::Writer::new(Box::new(writer) as Box<dyn io::Write>);
+    //         match write_mode {
+    //             WriteMode::Binary => writer.enable_binary_mode(),
+    //             WriteMode::OneLfTerminatedLinePerWriteCall => writer.enable_text_mode(),
+    //         }
+    //         RequestWriter {
+    //             on_into_read,
+    //             writer,
+    //             reader,
+    //         }
+    //     }
+    //
+    //     /// Write the given message as packet line.
+    //     pub async fn write_message(&mut self, message: MessageKind) -> io::Result<()> {
+    //         match message {
+    //             MessageKind::Flush => git_packetline::PacketLine::Flush.to_write(self.writer.inner_mut()),
+    //             MessageKind::Delimiter => git_packetline::PacketLine::Delimiter.to_write(self.writer.inner_mut()),
+    //             MessageKind::ResponseEnd => git_packetline::PacketLine::ResponseEnd.to_write(self.writer.inner_mut()),
+    //             MessageKind::Text(t) => git_packetline::immutable::Text::from(t).to_write(self.writer.inner_mut()),
+    //         }
+    //         .map(|_| ())
+    //         .await
+    //     }
+    // /// Discard the ability to write and turn this instance into the reader for obtaining the other side's response.
+    // pub fn into_read(mut self) -> std::io::Result<Box<dyn ExtendedBufRead + 'a>> {
+    //     self.write_message(self.on_into_read)?;
+    //     Ok(self.reader)
+    // }
+    // }
 }
-
-/// General
-impl<'a> RequestWriter<'a> {
-    /// Discard the ability to write and turn this instance into the reader for obtaining the other side's response.
-    pub fn into_read(mut self) -> io::Result<Box<dyn ExtendedBufRead + 'a>> {
-        self.write_message(self.on_into_read)?;
-        Ok(self.reader)
-    }
-}
+#[cfg(all(not(feature = "blocking-client"), feature = "async-client"))]
+pub use async_io::RequestWriter;
