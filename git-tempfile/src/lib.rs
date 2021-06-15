9dffc2b918178650a3b40adfcc35730e48f46951
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-06-15 11:51:32 +0800

    [tempfile] initial docs as there is a lot to consider

--- git-tempfile/src/lib.rs
+++ git-tempfile/src/lib.rs
@@ -1,3 +1,22 @@
-//! Various functionality related to git references
+//! git-style registered tempfiles that are removed upon typical termination signals.
+//!
+//! This crate installs signal handlers powered by [`signal-hook`] to get notified when the application is told to shut down
+//! using signals to assure these are deleted.
+//!
+//! As typical handlers for `TERM`ination are installed on first use and effectively overriding the defaults, we install
+//! default handlers to restore this behaviour.
+//!
+//! # Why tempfiles might remain on disk nonetheless
+//!
+//! * Uninterruptible signals are received like `SIGKILL`
+//! * The application is performing a write operation on the tempfile when a signal arrives, preventing this tempfile to be removed,
+//!   but not others.
+//!
+//! # Limitations
+//!
+//! * The amount of temporary files open at a time are not only limited by the amount of open file handles, but also
+//!   the amount of items storable in the concurrent slab serving as backend.
+//!
+//! [signal-hook]: https://docs.rs/signal-hook
 #![deny(unsafe_code, rust_2018_idioms)]
 #![allow(missing_docs)]
