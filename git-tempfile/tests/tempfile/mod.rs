0cc5b33f0e421ed761e5c350fb4d3ad85ef3e884
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-06-15 11:56:32 +0800

    [tempfile] show that slabs can store a lot actually

--- git-tempfile/tests/tempfile/mod.rs
+++ git-tempfile/tests/tempfile/mod.rs
@@ -1,1 +1,20 @@
+mod slab_assumptions {
+    use sharded_slab::Slab;
 
+    #[test]
+    fn the_capacity_is_limited_or_this_hangs_or_runs_out_of_memory() {
+        let s = Slab::new();
+        let mut item = 0;
+        const MAX: usize = 10_000;
+        loop {
+            if s.insert(item).is_none() || item == MAX {
+                break;
+            }
+            item += 1;
+        }
+        eprintln!(
+            "Could store {} items in slab with default configuration, gave up after {}",
+            item, MAX
+        );
+    }
+}
