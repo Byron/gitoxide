220eb9945b2006f79b82d80eaabd09b5e291953b
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-06-21 14:16:30 +0800

    [lock] support for randomization

--- git-lock/src/acquire.rs
+++ git-lock/src/acquire.rs
@@ -115,26 +115,51 @@ fn lock_with_mode<T>(
 mod backoff {
     use std::time::Duration;
 
-    struct Exponential {
+    pub fn randomize(backoff_ms: usize) -> usize {
+        let new_value = (fastrand::usize(750..=1250) * backoff_ms) / 1000;
+        if new_value == 0 {
+            backoff_ms
+        } else {
+            new_value
+        }
+    }
+
+    struct Exponential<Fn> {
         multiplier: usize,
         max_multiplier: usize,
         exponent: usize,
+        transform: Fn,
     }
-    impl Default for Exponential {
+    impl Default for Exponential<fn(usize) -> usize> {
         fn default() -> Self {
             Exponential {
                 multiplier: 1,
                 max_multiplier: 1000,
                 exponent: 1,
+                transform: std::convert::identity,
             }
         }
     }
 
-    impl Iterator for Exponential {
+    impl Exponential<fn(usize) -> usize> {
+        fn default_with_random() -> Self {
+            Exponential {
+                multiplier: 1,
+                max_multiplier: 1000,
+                exponent: 1,
+                transform: randomize,
+            }
+        }
+    }
+
+    impl<Transform> Iterator for Exponential<Transform>
+    where
+        Transform: Fn(usize) -> usize,
+    {
         type Item = Duration;
 
         fn next(&mut self) -> Option<Self::Item> {
-            let wait = Duration::from_millis(self.multiplier as u64);
+            let wait = Duration::from_millis((self.transform)(self.multiplier) as u64);
 
             self.multiplier += 2 * self.exponent + 1;
             if self.multiplier > self.max_multiplier {
@@ -149,6 +174,41 @@ mod backoff {
     #[cfg(test)]
     mod tests {
         use super::*;
+        use std::convert::TryInto;
+
+        const EXPECTED_TILL_SECOND: &[usize] = &[
+            1usize, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, 441, 484, 529,
+            576, 625, 676, 729, 784, 841, 900, 961, 1000, 1000,
+        ];
+
+        #[test]
+        fn random_exponential_produces_values_in_the_correct_range() {
+            let mut num_identities = 0;
+            for (actual, expected) in Exponential::default_with_random().zip(EXPECTED_TILL_SECOND) {
+                let actual: usize = actual.as_millis().try_into().unwrap();
+                if actual == *expected {
+                    num_identities += 1;
+                }
+                assert!(
+                    actual * 1000 >= expected * 750,
+                    "value too small: {} < {}",
+                    actual,
+                    expected
+                );
+                assert!(
+                    actual * 1000 <= expected * 1250,
+                    "value too big: {} > {}",
+                    actual,
+                    expected
+                );
+            }
+            assert!(
+                num_identities < EXPECTED_TILL_SECOND.len(),
+                "too many untransformed values: {}",
+                num_identities
+            );
+        }
+
         #[test]
         fn how_many_iterations_for_a_second_of_waittime() {
             let mut total = Duration::default();
@@ -169,13 +229,10 @@ mod backoff {
         fn output_with_default_settings() {
             assert_eq!(
                 Exponential::default().take(33).collect::<Vec<_>>(),
-                vec![
-                    1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, 441, 484,
-                    529, 576, 625, 676, 729, 784, 841, 900, 961, 1000, 1000
-                ]
-                .into_iter()
-                .map(Duration::from_millis)
-                .collect::<Vec<_>>()
+                EXPECTED_TILL_SECOND
+                    .iter()
+                    .map(|n| Duration::from_millis(*n as u64))
+                    .collect::<Vec<_>>()
             );
         }
     }
