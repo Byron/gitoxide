d9a5d328f575dfd86e414091688a545f931059e3
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-06-23 15:01:11 +0800

    [refs] sketch some parts of a transaction based on git source

--- git-ref/src/lib.rs
+++ git-ref/src/lib.rs
@@ -24,6 +24,44 @@ use git_hash::oid;
 mod store;
 pub use store::*;
 
+pub mod transaction {
+    #![allow(missing_docs, dead_code)]
+
+    use git_hash::ObjectId;
+    use std::marker::PhantomData;
+
+    /// Still to handle:
+    ///     RefLogOnly
+    ///     REF_FORCE_CREATE_REFLOG
+    ///     REF_NO_DEREF
+    ///     Be able to delete broken refs (those with invalid content)
+    pub enum Update {
+        CreateNew(ObjectId),
+        Delete(),
+    }
+
+    pub struct Transaction<T> {
+        updates: Vec<usize>,
+        _state: PhantomData<T>,
+    }
+}
+
+mod mutable {
+    #![allow(dead_code)]
+    use bstr::BString;
+    use git_hash::ObjectId;
+
+    /// Denotes a ref target, equivalent to [`Kind`], but with mutable data.
+    #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
+    #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
+    pub enum Target {
+        /// A ref that points to an object id
+        Peeled(ObjectId),
+        /// A ref that points to another reference by its non-validated name, adding a level of indirection.
+        UnvalidatedSymbolic(BString),
+    }
+}
+
 /// A validated and potentially partial reference name - it can safely be used for common operations.
 pub struct SafePartialName<'a>(&'a BStr);
 mod safe_name;
@@ -40,13 +78,13 @@ pub enum Kind {
     Symbolic,
 }
 
-/// Denotes a ref target, equivalent to [`Kind`], but with data.
+/// Denotes a ref target, equivalent to [`Kind`], but with immutable data.
 #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone, Copy)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
 pub enum Target<'a> {
     /// A ref that points to an object id
     Peeled(&'a oid),
-    /// A ref that points to another reference by its name, adding a level of indirection.
+    /// A ref that points to another reference by its validated name, adding a level of indirection.
     Symbolic(&'a BStr),
 }
 
