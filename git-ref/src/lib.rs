114fce8368fe858bc64696b4d7253c425367560a
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-06-25 11:36:03 +0800

    [refs] allow writing any valid ref value instead of limiting ourselves to object ids
    This is more consistent as an interface for changing values and allows
    HEAD references to be detached and reattched using a single mechanism
    which is more likely to get all the looking right.
    
    Especially when ref-table shows up there should be no special handling
    of HEAD as compared to other references.

--- git-ref/src/lib.rs
+++ git-ref/src/lib.rs
@@ -26,49 +26,52 @@ pub use store::*;
 
 pub mod transaction {
     #![allow(missing_docs, dead_code)]
+    //! Research
+    //!
+    //!   * `RefLogOnly`
+    //!      - symbolic references don't actually change but one might still want to record the HEAD changes for convenience.
+    //!      - Judging by how `reflog-transaction` hooks one transaction is scheduled for each ref, so the deref part is
+    //!         actually not done automatically.
+    //!   * `REF_FORCE_CREATE_REFLOG`
+    //!      - required only for tags which otherwise wouldn't have a reflog. Otherwise it's up to the implementation
+    //!                       which seems to have an automation on where to create a reflog or not.
+    //!                       Reflogs are basic features of all stores.
+    //!   * REF_NO_DEREF - Apparently dereffing is the default so detaching HEAD would need this flag to write HEAD directly
+    //!                  opposedly this might mean that a change to HEAD will change the branch it points to and affect two reflogs
+    //!                  automaticaly.
+    //!   * Be able to delete broken refs (those with invalid content) - this is part of the ref iteration
+    //!   * How to handle initial_ref_transaction_commit (to be a version that assumes no other writers)? It uses packed-refs essentially
+    //!     and it does validate certain invariants, too, but doesn't have to check for file refs.
+    //!     - **it's probably a standard transaction passed to `store.apply_exclusive(…)` as opposed to `store.apply(…)`.**
+    //!
+    //! |                         |Update        |Kind    |Data       |Reflog Mode |Deref|ref itself|reflog|referent|referent reflog|
+    //! |-------------------------|--------------|--------|-----------|------------|-----|----------|------|--------|---------------|
+    //! |HEAD                     |CreateOrUpdate|symbolic|oid        |only-reflog |✔    |          |✔     |✔       |✔              |
+    //! |HEAD to detached HEAD    |CreateOrUpdate|symbolic|oid        |auto        |     |✔         |✔     |        |               |
+    //! |detached HEAD to HEAD|CreateOrUpdate|peeled      |refpath    |auto        |     |✔         |✔     |        |               |
+    //! |HEAD                     |Delete        |any     |oid        |only-reflog |✔    |          |      |        |               |
+    //! |HEAD                     |Delete        |any     |oid        |auto        |✔    |✔         |✔     |        |               |
+    //! |refs/heads/main          |CreateOrUpdate|peeled  |oid        |auto        |     |✔         |✔     |        |               |
+    //! |refs/tags/0.1.0          |CreateOrUpdate|peeled  |oid        |auto        |     |✔         |      |        |               |
+    //! |refs/tags/0.1.0          |CreateOrUpdate|peeled  |oid        |force-reflog|     |✔         |✔     |        |               |
 
-    use git_hash::ObjectId;
+    use crate::mutable;
     use std::marker::PhantomData;
 
-    /// Research
-    ///
-    ///   * `RefLogOnly`
-    ///      - symbolic references don't actually change but one might still want to record the HEAD changes for convenience.
-    ///      - Judging by how `reflog-transaction` hooks one transaction is scheduled for each ref, so the deref part is
-    ///         actually not done automatically.
-    ///   * `REF_FORCE_CREATE_REFLOG`
-    ///      - required only for tags which otherwise wouldn't have a reflog. Otherwise it's up to the implementation
-    ///                       which seems to have an automation on where to create a reflog or not.
-    ///                       Reflogs are basic features of all stores.
-    ///   * REF_NO_DEREF - Apparently dereffing is the default so detaching HEAD would need this flag to write HEAD directly
-    ///                  opposedly this might mean that a change to HEAD will change the branch it points to and affect two reflogs
-    ///                  automaticaly.
-    ///   * Be able to delete broken refs (those with invalid content) - this is part of the ref iteration
-    ///   * How to handle initial_ref_transaction_commit (to be a version that assumes no other writers)? It uses packed-refs essentially
-    ///     and it does validate certain invariants, too, but doesn't have to check for file refs.
-    ///     - **it's probably a standard transaction passed to `store.apply_exclusive(…)` as opposed to `store.apply(…)`.**
-    ///
-    /// |                         |Update        |Kind    |Data       |Reflog Mode |Deref|ref itself|reflog|referent|referent reflog|
-    /// |-------------------------|--------------|--------|-----------|------------|-----|----------|------|--------|---------------|
-    /// |HEAD                     |CreateOrUpdate|symbolic|oid        |only-reflog |✔    |          |✔     |✔       |✔              |
-    /// |HEAD to detached HEAD    |CreateOrUpdate|symbolic|oid        |auto        |     |✔         |✔     |        |               |
-    /// |~~detached HEAD to HEAD~~|CreateOrUpdate|peeled  |~~refpath~~|auto        |     |✔         |✔     |        |               |
-    /// |HEAD                     |Delete        |any     |oid        |only-reflog |✔    |          |      |        |               |
-    /// |HEAD                     |Delete        |any     |oid        |auto        |✔    |✔         |✔     |        |               |
-    /// |refs/heads/main          |CreateOrUpdate|peeled  |oid        |auto        |     |✔         |✔     |        |               |
-    /// |refs/tags/0.1.0          |CreateOrUpdate|peeled  |oid        |auto        |     |✔         |      |        |               |
-    /// |refs/tags/0.1.0          |CreateOrUpdate|peeled  |oid        |force-reflog|     |✔         |✔     |        |               |
     pub enum Update {
         /// If previous is not `None`, the ref must exist and its `oid` must agree with the `previous`, and
         /// we function like `update`.
         /// Otherwise it functions as `create-or-update`.
         Update {
             mode: Reflog,
-            previous: Option<ObjectId>,
-            new: ObjectId,
+            previous: Option<mutable::Target>,
+            new: mutable::Target,
+            /// Set if this update is coming from a symbolic reference and used to make it appear like it is the one that is handled,
+            /// instead of the referent reference.
+            parent_index: Option<usize>,
         },
         Delete {
-            previous: Option<ObjectId>,
+            previous: Option<mutable::Target>,
         },
     }
 
@@ -89,19 +92,27 @@ pub mod transaction {
     }
 }
 
-mod mutable {
+/// Indicate that the given BString is a validate reference name that can be used as path on disk or written as target
+/// of a symbolic reference
+#[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
+pub struct ValidatedRefname(bstr::BString);
+
+pub mod mutable {
+    //!
     #![allow(dead_code)]
-    use bstr::BString;
+    use crate::ValidatedRefname;
     use git_hash::ObjectId;
 
-    /// Denotes a ref target, equivalent to [`Kind`], but with mutable data.
+    /// Denotes a ref target, equivalent to [`Kind`][super::Kind], but with mutable data.
     #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
     #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
     pub enum Target {
         /// A ref that points to an object id
         Peeled(ObjectId),
-        /// A ref that points to another reference by its non-validated name, adding a level of indirection.
-        UnvalidatedSymbolic(BString),
+        /// A ref that points to another reference by its validated name, adding a level of indirection.
+        ///
+        /// Note that this is an extension of gitoxide which will be helpful in logging all reference changes.
+        Symbolic(ValidatedRefname),
     }
 }
 
