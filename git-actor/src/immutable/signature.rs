e6c7fc18954a5a5ad12b3da6c290f8cb9a74c19c
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-06-29 15:01:29 +0800

    [actor] don't leak btoi errors…
    …and the impl is not nice

--- git-actor/src/immutable/signature.rs
+++ git-actor/src/immutable/signature.rs
@@ -7,7 +7,6 @@ mod decode {
         branch::alt,
         bytes::complete::{tag, take, take_until, take_while_m_n},
         character::is_digit,
-        combinator::map_res,
         error::{context, ContextError, ParseError},
         sequence::{terminated, tuple},
         IResult,
@@ -19,27 +18,32 @@ mod decode {
     pub fn signature<'a, E: ParseError<&'a [u8]> + ContextError<&'a [u8]> + std::fmt::Debug>(
         i: &'a [u8],
     ) -> IResult<&'a [u8], Signature<'a>, E> {
-        fn conv<'a, E, T>(i: &'a [u8], res: Result<T, btoi::ParseIntegerError>) -> IResult<&'a [u8], T, nom::Err<E>>
-        where
-            E: ParseError<&'a [u8]> + ContextError<&'a [u8]> + std::fmt::Debug,
-        {
-            res.map(|v| (i, v))
-                .map_err(|err| nom::Err::Error(E::from_error_kind(i, nom::error::ErrorKind::MapRes)))
-        };
         let (i, (name, email, time, tzsign, hours, minutes)) = context(
             "<name> <<email>> <timestamp> <+|-><HHMM>",
             tuple((
                 context("<name>", terminated(take_until(&b" <"[..]), take(2usize))),
                 context("<email>", terminated(take_until(&b"> "[..]), take(2usize))),
                 context("<timestamp>", |i| {
-                    terminated(take_until(SPACE), take(1usize))(i).and_then(|(i, v)| conv(i, btoi::<u32>(v)))
+                    terminated(take_until(SPACE), take(1usize))(i).and_then(|(i, v)| {
+                        btoi::<u32>(v)
+                            .map(|v| (i, v))
+                            .map_err(|_| nom::Err::Error(E::from_error_kind(i, nom::error::ErrorKind::MapRes)))
+                    })
                 }),
                 context("+|-", alt((tag(b"-"), tag(b"+")))),
                 context("HH", |i| {
-                    take_while_m_n(2usize, 2, is_digit)(i).and_then(|(i, v)| conv(i, btoi::<i32>(v)))
+                    take_while_m_n(2usize, 2, is_digit)(i).and_then(|(i, v)| {
+                        btoi::<i32>(v)
+                            .map(|v| (i, v))
+                            .map_err(|_| nom::Err::Error(E::from_error_kind(i, nom::error::ErrorKind::MapRes)))
+                    })
                 }),
                 context("MM", |i| {
-                    take_while_m_n(2usize, 2, is_digit)(i).and_then(|(i, v)| conv(i, btoi::<i32>(v)))
+                    take_while_m_n(2usize, 2, is_digit)(i).and_then(|(i, v)| {
+                        btoi::<i32>(v)
+                            .map(|v| (i, v))
+                            .map_err(|_| nom::Err::Error(E::from_error_kind(i, nom::error::ErrorKind::MapRes)))
+                    })
                 }),
             )),
         )(i)?;
@@ -149,7 +153,7 @@ mod decode {
                             .map_err(to_bstr_err)
                             .expect_err("parse fails as > is missing")
                             .to_string(),
-                        "Parse error:\nMapRes at: abc -1215\nin section '<timestamp>', at: abc -1215\nin section '<name> <<email>> <timestamp> <+|-><HHMM>', at: hello <> abc -1215\n"
+                        "Parse error:\nMapRes at: -1215\nin section '<timestamp>', at: abc -1215\nin section '<name> <<email>> <timestamp> <+|-><HHMM>', at: hello <> abc -1215\n"
                     );
             }
         }
