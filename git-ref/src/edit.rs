6a84790b13e445d5a1b85fd3cae2ec0feed4ff02
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-07-02 08:12:31 +0800

    [ref] refactor

---
+++ git-ref/src/edit.rs
@@ -0,0 +1,77 @@
+#![allow(missing_docs, dead_code)]
+//! Research
+//!
+//!   * `RefLogOnly`
+//!      - symbolic references don't actually change but one might still want to record the HEAD changes for convenience.
+//!      - Judging by how `reflog-transaction` hooks one transaction is scheduled for each ref, so the deref part is
+//!         actually not done automatically.
+//!   * `REF_FORCE_CREATE_REFLOG`
+//!      - required only for tags which otherwise wouldn't have a reflog. Otherwise it's up to the implementation
+//!                       which seems to have an automation on where to create a reflog or not.
+//!                       Reflogs are basic features of all stores.
+//!   * REF_NO_DEREF - Apparently dereffing is the default so detaching HEAD would need this flag to write HEAD directly
+//!                  opposedly this might mean that a change to HEAD will change the branch it points to and affect two reflogs
+//!                  automaticaly.
+//!   * Be able to delete broken refs (those with invalid content) - this is part of the ref iteration
+//!   * How to handle initial_ref_transaction_commit (to be a version that assumes no other writers)? It uses packed-refs essentially
+//!     and it does validate certain invariants, too, but doesn't have to check for file refs.
+//!     - **it's probably a standard transaction passed to `store.apply_exclusive(…)` as opposed to `store.apply(…)`.**
+//!
+//! |                         |Update        |Kind    |Data       |Reflog Mode |Deref|ref itself|reflog|referent|referent reflog|
+//! |-------------------------|--------------|--------|-----------|------------|-----|----------|------|--------|---------------|
+//! |HEAD                     |CreateOrUpdate|symbolic|oid        |only-reflog |✔    |          |✔     |✔       |✔              |
+//! |HEAD to detached HEAD    |CreateOrUpdate|symbolic|oid        |auto        |     |✔         |✔     |        |               |
+//! |detached HEAD to HEAD|CreateOrUpdate|peeled      |refpath    |auto        |     |✔         |✔     |        |               |
+//! |HEAD                     |Delete        |any     |oid        |only-reflog |✔    |          |      |        |               |
+//! |HEAD                     |Delete        |any     |oid        |auto        |✔    |✔         |✔     |        |               |
+//! |refs/heads/main          |CreateOrUpdate|peeled  |oid        |auto        |     |✔         |✔     |        |               |
+//! |refs/tags/0.1.0          |CreateOrUpdate|peeled  |oid        |auto        |     |✔         |      |        |               |
+//! |refs/tags/0.1.0          |CreateOrUpdate|peeled  |oid        |force-reflog|     |✔         |✔     |        |               |
+
+use crate::{mutable, ValidName};
+use std::marker::PhantomData;
+
+pub struct Update {
+    /// How to treat the reference log.
+    pub mode: Reflog,
+    /// The previous value of the ref, which will be used to assure the ref is still in the known `previous` state before
+    /// updating it.
+    pub previous: Option<mutable::Target>,
+    /// The new state of the reference
+    pub new: mutable::Target,
+    /// Set if this update is coming from a symbolic reference and used to make it appear like it is the one that is handled,
+    /// instead of the referent reference.
+    parent_index: Option<usize>,
+}
+
+/// A description of an edit to perform on a reference
+pub enum Edit {
+    /// If previous is not `None`, the ref must exist and its `oid` must agree with the `previous`, and
+    /// we function like `update`.
+    /// Otherwise it functions as `create-or-update`.
+    Update(Update),
+    Delete {
+        previous: Option<mutable::Target>,
+    },
+}
+
+pub struct RefEdit {
+    edit: Edit,
+    refpath: ValidName,
+}
+
+pub enum Reflog {
+    /// As symbolic references only ever see this when you want to detach them, we won't try to dereference them
+    /// in this case and apply the change to it directly.
+    AutoAndNoDeref,
+    /// Only update the reflog but require this to be a symbolic ref so the actual update can be performed on the
+    /// referent.
+    OnlyAndDeref,
+    /// Create a reflog even if it otherwise wouldn't be created, as is the case for tags. Otherwise it acts like `AutoNoDeref`.
+    CreateUnconditionally,
+}
+
+pub struct Transaction<T> {
+    updates: Vec<RefEdit>,
+    _state: PhantomData<T>,
+}
