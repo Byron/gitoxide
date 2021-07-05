58a5653a6647931bf90f88ff2d83c6b0322ad9b1
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-07-05 14:22:05 +0800

    [ref] refactor

--- git-ref/tests/file/transaction.rs
+++ git-ref/tests/file/transaction.rs
@@ -1,19 +1,31 @@
-mod prepare {
+mod prepare_and_commit {
+    use git_ref::file;
+
+    fn empty_store(log_mode: git_ref::file::WriteReflog) -> crate::Result<file::Store> {
+        let dir = tempfile::TempDir::new().unwrap();
+        let mut store: file::Store = dir.path().to_owned().into();
+        store.write_reflog = log_mode;
+        Ok(store)
+    }
+
     mod create {
+        use crate::file::transaction::prepare_and_commit::empty_store;
         use bstr::ByteSlice;
-        use git_ref::{file, mutable, mutable::Target};
+        use git_ref::{mutable, mutable::Target};
         use std::{convert::TryInto, path::Path};
 
-        #[test]
-        #[should_panic]
-        fn reference_with_equally_named_empty_directory_already_in_place() {
-            todo!("lock file renaming of a.lock to a but a is an empty directory")
-        }
+        mod reference_with_equally_named {
+            #[test]
+            #[should_panic]
+            fn empty_directory_already_in_place() {
+                todo!("lock file renaming of a.lock to a but a is an empty directory")
+            }
 
-        #[test]
-        #[should_panic]
-        fn reference_with_equally_named_non_empty_directory_already_in_place() {
-            todo!("lock file renaming of a.lock to a but a is a non-empty directory")
+            #[test]
+            #[should_panic]
+            fn non_empty_directory_already_in_place() {
+                todo!("lock file renaming of a.lock to a but a is a non-empty directory")
+            }
         }
 
         #[test]
@@ -23,35 +35,31 @@ mod prepare {
         }
 
         #[test]
-        fn symbolic_missing_referent() {
+        fn symbolic_head_missing_referent() -> crate::Result {
             for reflog_writemode in &[git_ref::file::WriteReflog::Normal, git_ref::file::WriteReflog::Disable] {
-                let dir = tempfile::TempDir::new().unwrap();
-                let mut store: file::Store = dir.path().to_owned().into();
-                store.write_reflog = *reflog_writemode;
+                let store = empty_store(*reflog_writemode)?;
                 let referent = "refs/heads/alt-main";
-                assert!(
-                    store.find_one(referent).unwrap().is_none(),
-                    "the reference does not exist"
-                );
+                assert!(store.find_one(referent)?.is_none(), "the reference does not exist");
                 let t = store.transaction(
                     Some(mutable::RefEdit {
                         edit: mutable::Change::Update(mutable::Update {
                             mode: mutable::Reflog::AutoAndNoDeref,
-                            new: Target::Symbolic(referent.try_into().unwrap()),
+                            new: Target::Symbolic(referent.try_into()?),
                             previous: None, // TODO: check failure if it doesn't exist
                         }),
-                        name: "HEAD".try_into().unwrap(),
+                        name: "HEAD".try_into()?,
                     }),
                     git_lock::acquire::Fail::Immediately,
                 );
-                let edits = t.commit().unwrap();
+                let edits = t.commit()?;
                 assert_eq!(edits.len(), 1, "no split was performed");
-                let written = store.find_one_existing(edits[0].name.to_partial()).unwrap();
+                let written = store.find_one_existing(edits[0].name.to_partial())?;
                 assert_eq!(written.relative_path, Path::new("HEAD"));
                 assert_eq!(written.kind(), git_ref::Kind::Symbolic);
                 assert_eq!(written.target().as_name(), Some(referent.as_bytes().as_bstr()));
-                assert!(!written.log_exists().unwrap(), "no revlog is written for symbolic ref");
+                assert!(!written.log_exists()?, "no revlog is written for symbolic ref");
             }
+            Ok(())
         }
 
         #[test]
