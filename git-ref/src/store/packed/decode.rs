7af27c5676c986b05953995d216b78389e986ee0
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-07-12 15:31:47 +0800

    [ref] first test for line (and peeled ref) parsin

--- git-ref/src/store/packed/decode.rs
+++ git-ref/src/store/packed/decode.rs
@@ -1,6 +1,6 @@
 use crate::{
     parse::newline,
-    store::{packed::Header, packed::Peeled},
+    store::{packed, packed::Peeled},
 };
 use bstr::ByteSlice;
 use nom::{
@@ -11,7 +11,7 @@ use nom::{
     IResult,
 };
 
-fn header<'a, E>(input: &'a [u8]) -> IResult<&'a [u8], Header, E>
+fn header<'a, E>(input: &'a [u8]) -> IResult<&'a [u8], packed::Header, E>
 where
     E: ParseError<&'a [u8]>,
 {
@@ -36,22 +36,62 @@ where
         }
     }
 
-    Ok((rest, Header { peeled, sorted }))
+    Ok((rest, packed::Header { peeled, sorted }))
+}
+
+fn reference<'a, E: ParseError<&'a [u8]>>(input: &[u8]) -> IResult<&[u8], packed::Reference<'a>, E> {
+    todo!("line parsing")
 }
 
 #[cfg(test)]
 mod tests {
+    type Result = std::result::Result<(), Box<dyn std::error::Error>>;
+
+    mod reference {
+        use crate::store::packed::decode;
+        use bstr::ByteSlice;
+        use git_testtools::hex_to_id;
+        use nom::error::VerboseError;
+
+        #[test]
+        #[ignore]
+        fn two_refs_in_a_row() {
+            let input: &[u8] = b"d53c4b0f91f1b29769c9430f2d1c0bcab1170c75 refs/heads/alternates-after-packs-and-loose\n^e9cdc958e7ce2290e2d7958cdb5aa9323ef35d37\neaae9c1bc723209d793eb93f5587fa2604d5cd92 refs/heads/avoid-double-lookup\n";
+            let (input, parsed) = decode::reference::<VerboseError<_>>(input).unwrap();
+            assert_eq!(parsed.full_name, "refs/heads/alternates-after-packs-and-loose");
+            assert_eq!(parsed.target(), hex_to_id("d53c4b0f91f1b29769c9430f2d1c0bcab1170c75"));
+            assert_eq!(
+                parsed.object,
+                Some(b"e9cdc958e7ce2290e2d7958cdb5aa9323ef35d37".as_bstr())
+            );
+            assert_eq!(parsed.object(), hex_to_id("e9cdc958e7ce2290e2d7958cdb5aa9323ef35d37"));
+
+            let (input, parsed) = decode::reference::<VerboseError<_>>(input).unwrap();
+            assert!(input.is_empty(), "exhausted");
+            assert_eq!(parsed.full_name, "refs/heads/avoid-double-lookup");
+            assert_eq!(parsed.target, "e9cdc958e7ce2290e2d7958cdb5aa9323ef35d37");
+            assert!(parsed.object.is_none());
+        }
+    }
     mod header {
+        use super::Result;
         use crate::store::packed::{decode, Header, Peeled};
         use bstr::ByteSlice;
         use git_testtools::to_bstr_err;
 
         #[test]
-        fn valid_fully_peeled_stored() {
+        fn invalid() {
+            assert!(
+                decode::header::<()>(b"# some user comment").is_err(),
+                "something the user put there"
+            );
+            assert!(decode::header::<()>(b"# pack-refs: ").is_err(), "looks right but isn't");
+        }
+
+        #[test]
+        fn valid_fully_peeled_stored_with_leading_spaces() -> Result {
             let input: &[u8] = b"  # pack-refs with: peeled fully-peeled sorted  \nsomething else";
-            let (rest, header) = decode::header::<nom::error::VerboseError<_>>(input)
-                .map_err(to_bstr_err)
-                .unwrap();
+            let (rest, header) = decode::header::<nom::error::VerboseError<_>>(input).map_err(to_bstr_err)?;
             assert_eq!(rest.as_bstr(), "something else", "remainder starts after newline");
             assert_eq!(
                 header,
@@ -60,12 +100,13 @@ mod tests {
                     sorted: true
                 }
             );
+            Ok(())
         }
 
         #[test]
-        fn valid_peeled_unsorted() {
+        fn valid_peeled_unsorted_with_leading_newlines() -> Result {
             let input: &[u8] = b"\n\n# pack-refs with: peeled\n";
-            let (rest, header) = decode::header::<()>(input).unwrap();
+            let (rest, header) = decode::header::<()>(input)?;
             assert!(rest.is_empty());
             assert_eq!(
                 header,
@@ -74,12 +115,13 @@ mod tests {
                     sorted: false
                 }
             );
+            Ok(())
         }
 
         #[test]
-        fn valid_empty() {
-            let input: &[u8] = b"\n\n# pack-refs with: \n";
-            let (rest, header) = decode::header::<()>(input).unwrap();
+        fn valid_empty_without_leading_bytes() -> Result {
+            let input: &[u8] = b"# pack-refs with: \n";
+            let (rest, header) = decode::header::<()>(input)?;
             assert!(rest.is_empty());
             assert_eq!(
                 header,
@@ -88,6 +130,7 @@ mod tests {
                     sorted: false
                 }
             );
+            Ok(())
         }
     }
 }
--- git-ref/src/store/packed/mod.rs
+++ git-ref/src/store/packed/mod.rs
@@ -1,5 +1,8 @@
 #![allow(missing_docs, dead_code)]
 
+use bstr::BStr;
+use git_hash::ObjectId;
+
 pub mod iter {}
 
 #[derive(Debug, PartialEq, Eq)]
@@ -14,6 +17,34 @@ enum Peeled {
 struct Header {
     peeled: Peeled,
     sorted: bool,
+}
+
+#[derive(Debug)]
+#[non_exhaustive]
+struct Reference<'a> {
+    /// The unvalidated full name of the reference.
+    pub full_name: &'a BStr,
+    /// The target object id of the reference, hex encoded.
+    pub target: &'a BStr,
+    /// The fully peeled object id, hex encoded, that the ref is ultimately pointing to
+    /// i.e. when all indirections are removed.
+    pub object: Option<&'a BStr>,
+}
+
+impl<'a> Reference<'a> {
+    /// Decode the target as object
+    pub fn target(&self) -> ObjectId {
+        git_hash::ObjectId::from_hex(self.target).expect("parser validation")
+    }
+
+    /// Decode the object this reference is ultimately pointing to. Note that this is
+    /// the [`target()`] if this is not a fully peeled reference like a tag.
+    pub fn object(&self) -> ObjectId {
+        self.object.clone().map_or_else(
+            || self.target(),
+            |id| ObjectId::from_hex(id).expect("parser validation"),
+        )
+    }
 }
 
 mod decode;
