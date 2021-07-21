2ccefb2832b326966a24d0cbcfd79ca5309f91aa
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-07-21 21:37:01 +0800

    [pack] refactor

--- git-pack/tests/pack/data/output/count_and_entries.rs
+++ git-pack/tests/pack/data/output/count_and_entries.rs
@@ -78,155 +78,192 @@ fn traversals() -> crate::Result {
         tags: 1,
     };
     let db = db(DbKind::DeterministicGeneratedContent)?;
-    for allow_thin_pack in &[false, true] {
-        for (
-            expansion_mode,
-            expected_count,
-            expected_obj_count,
-            expected_counts_outcome,
-            expected_entries_outcome,
-            expected_pack_hash,
-            expects_thin_pack,
-        ) in [
-            (
-                count::iter_from_objects::ObjectExpansion::AsIs,
-                Count {
-                    trees: 0,
-                    commits: 15,
-                    blobs: 0,
-                    tags: 1,
-                    delta_ref: 0,
-                    delta_oid: 0,
-                },
-                ObjectCount {
-                    trees: 0,
-                    commits: 15,
-                    blobs: 0,
-                    tags: 1,
-                },
-                output::count::iter_from_objects::Outcome {
-                    input_objects: 16,
-                    expanded_objects: 0,
-                    decoded_objects: 16,
-                    total_objects: 16,
-                },
-                output::entry::iter_from_counts::Outcome {
-                    decoded_and_recompressed_objects: 0,
-                    objects_copied_from_pack: 16,
-                },
-                hex_to_id("b920bbb055e1efb9080592a409d3975738b6efb3"),
-                true,
-            ),
-            (
-                count::iter_from_objects::ObjectExpansion::TreeContents,
-                whole_pack,
-                whole_pack_obj_count,
-                output::count::iter_from_objects::Outcome {
-                    input_objects: 16,
-                    expanded_objects: 852,
-                    decoded_objects: 57,
-                    total_objects: 868,
-                },
-                output::entry::iter_from_counts::Outcome {
-                    decoded_and_recompressed_objects: 0,
-                    objects_copied_from_pack: 868,
-                },
-                hex_to_id("d2ea809066bec3f5f2a38ef4adba7ebd4f2eda22"),
-                false,
-            ),
-            (
-                count::iter_from_objects::ObjectExpansion::TreeAdditionsComparedToAncestor,
-                whole_pack,
-                whole_pack_obj_count,
-                output::count::iter_from_objects::Outcome {
-                    input_objects: 16,
-                    expanded_objects: 866,
-                    decoded_objects: 208,
-                    total_objects: 868,
-                },
-                output::entry::iter_from_counts::Outcome {
-                    decoded_and_recompressed_objects: 0,
-                    objects_copied_from_pack: 868,
-                },
-                hex_to_id("d2ea809066bec3f5f2a38ef4adba7ebd4f2eda22"),
-                false,
-            ),
-        ]
-        .iter()
-        .copied()
-        {
-            let head = hex_to_id("dfcb5e39ac6eb30179808bbab721e8a28ce1b52e");
-            let commits = commit::Ancestors::new(Some(head), commit::ancestors::State::default(), {
-                let db = Arc::clone(&db);
-                move |oid, buf| db.find_existing_commit_iter(oid, buf, &mut pack::cache::Never).ok()
-            })
-            .map(Result::unwrap);
-
-            let deterministic_count_needs_single_thread = Some(1);
-            let mut counts_iter = output::count::iter_from_objects(
-                db.clone(),
-                || pack::cache::Never,
-                commits.chain(std::iter::once(hex_to_id("e3fb53cbb4c346d48732a24f09cf445e49bc63d6"))),
-                progress::Discard,
-                count::iter_from_objects::Options {
-                    input_object_expansion: expansion_mode,
-                    thread_limit: deterministic_count_needs_single_thread,
-                    ..Default::default()
-                },
-            );
-            let counts: Vec<_> = counts_iter
-                .by_ref()
-                .collect::<Result<Vec<_>, _>>()?
-                .into_iter()
-                .flatten()
-                .collect();
-            let actual_count = counts.iter().fold(ObjectCount::default(), |mut c, e| {
-                let mut buf = Vec::new();
-                if let Some(obj) = db.find_existing(e.id, &mut buf, &mut pack::cache::Never).ok() {
-                    c.add(obj.kind);
-                }
-                c
-            });
-            assert_eq!(actual_count, expected_obj_count);
-            let counts_len = counts.len();
-            assert_eq!(counts_len, expected_obj_count.total());
-
-            let stats = counts_iter.finalize()?;
-            assert_eq!(stats, expected_counts_outcome);
-            assert_eq!(stats.total_objects, expected_obj_count.total());
-            assert_eq!(
-                expected_obj_count.total(),
-                expected_count.total(),
-                "two different ways of counting, still the same in the end"
-            );
-
-            let mut entries_iter = output::entry::iter_from_counts(
-                counts,
-                db.clone(),
-                || pack::cache::Never,
-                progress::Discard,
-                output::entry::iter_from_counts::Options {
-                    allow_thin_pack: *allow_thin_pack,
-                    ..Default::default()
-                },
-            );
-            let entries: Vec<_> = output::InOrderIter::from(entries_iter.by_ref())
-                .collect::<Result<Vec<_>, _>>()?
-                .into_iter()
-                .flatten()
-                .collect();
-            let actual_count = entries.iter().fold(Count::default(), |mut c, e| {
-                c.add(e.kind);
-                c
-            });
-            assert_eq!(actual_count, expected_count);
-            assert_eq!(counts_len, expected_count.total());
-            let stats = entries_iter.finalize()?;
-            assert_eq!(stats, expected_entries_outcome);
-
-            write_and_verify(entries, expected_pack_hash, expects_thin_pack)?;
+    let some_input_objects = 1;
+    for (
+        expansion_mode,
+        expected_count,
+        expected_obj_count,
+        expected_counts_outcome,
+        expected_entries_outcome,
+        expected_pack_hash,
+        allow_thin_pack,
+    ) in [
+        (
+            count::iter_from_objects::ObjectExpansion::AsIs,
+            Count {
+                trees: 0,
+                commits: 15,
+                blobs: 0,
+                tags: 1,
+                delta_ref: 0,
+                delta_oid: 0,
+            },
+            ObjectCount {
+                trees: 0,
+                commits: 15,
+                blobs: 0,
+                tags: 1,
+            },
+            output::count::iter_from_objects::Outcome {
+                input_objects: 16,
+                expanded_objects: 0,
+                decoded_objects: 16,
+                total_objects: 16,
+            },
+            output::entry::iter_from_counts::Outcome {
+                decoded_and_recompressed_objects: 0,
+                objects_copied_from_pack: 16,
+            },
+            hex_to_id("b920bbb055e1efb9080592a409d3975738b6efb3"),
+            false,
+        ),
+        (
+            // TODO: trigger thin pack code
+            count::iter_from_objects::ObjectExpansion::TreeContents,
+            Count {
+                trees: 8,
+                commits: 2, // todo: why more?
+                blobs: 158,
+                tags: 1,
+                delta_ref: 444,
+                delta_oid: 0,
+            },
+            ObjectCount {
+                trees: 9,
+                commits: 2,
+                blobs: 601,
+                tags: 1,
+            },
+            output::count::iter_from_objects::Outcome {
+                input_objects: 2, // todo: figure out why its more than expected
+                expanded_objects: 611,
+                decoded_objects: 12,
+                total_objects: 613,
+            },
+            output::entry::iter_from_counts::Outcome {
+                decoded_and_recompressed_objects: 0,
+                objects_copied_from_pack: 613,
+            },
+            hex_to_id("d2ea809066bec3f5f2a38ef4adba7ebd4f2eda22"),
+            true,
+        ),
+        (
+            count::iter_from_objects::ObjectExpansion::TreeContents,
+            whole_pack,
+            whole_pack_obj_count,
+            output::count::iter_from_objects::Outcome {
+                input_objects: 16,
+                expanded_objects: 852,
+                decoded_objects: 57,
+                total_objects: 868,
+            },
+            output::entry::iter_from_counts::Outcome {
+                decoded_and_recompressed_objects: 0,
+                objects_copied_from_pack: 868,
+            },
+            hex_to_id("d2ea809066bec3f5f2a38ef4adba7ebd4f2eda22"),
+            false,
+        ),
+        (
+            count::iter_from_objects::ObjectExpansion::TreeAdditionsComparedToAncestor,
+            whole_pack,
+            whole_pack_obj_count,
+            output::count::iter_from_objects::Outcome {
+                input_objects: 16,
+                expanded_objects: 866,
+                decoded_objects: 208,
+                total_objects: 868,
+            },
+            output::entry::iter_from_counts::Outcome {
+                decoded_and_recompressed_objects: 0,
+                objects_copied_from_pack: 868,
+            },
+            hex_to_id("d2ea809066bec3f5f2a38ef4adba7ebd4f2eda22"),
+            false,
+        ),
+    ]
+    .iter()
+    .copied()
+    {
+        let head = hex_to_id("dfcb5e39ac6eb30179808bbab721e8a28ce1b52e");
+        let mut commits = commit::Ancestors::new(Some(head), commit::ancestors::State::default(), {
+            let db = Arc::clone(&db);
+            move |oid, buf| db.find_existing_commit_iter(oid, buf, &mut pack::cache::Never).ok()
+        })
+        .map(Result::unwrap)
+        .collect::<Vec<_>>();
+        if allow_thin_pack {
+            commits.resize(some_input_objects, git_hash::ObjectId::null_sha1());
         }
+
+        let deterministic_count_needs_single_thread = Some(1);
+        let mut counts_iter = output::count::iter_from_objects(
+            db.clone(),
+            || pack::cache::Never,
+            commits
+                .into_iter()
+                .chain(std::iter::once(hex_to_id("e3fb53cbb4c346d48732a24f09cf445e49bc63d6"))),
+            progress::Discard,
+            count::iter_from_objects::Options {
+                input_object_expansion: expansion_mode,
+                thread_limit: deterministic_count_needs_single_thread,
+                ..Default::default()
+            },
+        );
+        let counts: Vec<_> = counts_iter
+            .by_ref()
+            .collect::<Result<Vec<_>, _>>()?
+            .into_iter()
+            .flatten()
+            .collect();
+        let actual_count = counts.iter().fold(ObjectCount::default(), |mut c, e| {
+            let mut buf = Vec::new();
+            if let Some(obj) = db.find_existing(e.id, &mut buf, &mut pack::cache::Never).ok() {
+                c.add(obj.kind);
+            }
+            c
+        });
+        assert_eq!(actual_count, expected_obj_count);
+        let counts_len = counts.len();
+        assert_eq!(counts_len, expected_obj_count.total());
+
+        let stats = counts_iter.finalize()?;
+        assert_eq!(stats, expected_counts_outcome);
+        assert_eq!(stats.total_objects, expected_obj_count.total());
+
+        let mut entries_iter = output::entry::iter_from_counts(
+            counts,
+            db.clone(),
+            || pack::cache::Never,
+            progress::Discard,
+            output::entry::iter_from_counts::Options {
+                allow_thin_pack,
+                ..Default::default()
+            },
+        );
+        let entries: Vec<_> = output::InOrderIter::from(entries_iter.by_ref())
+            .collect::<Result<Vec<_>, _>>()?
+            .into_iter()
+            .flatten()
+            .collect();
+        let actual_count = entries.iter().fold(Count::default(), |mut c, e| {
+            c.add(e.kind);
+            c
+        });
+        assert_eq!(actual_count, expected_count);
+        assert_eq!(counts_len, expected_count.total());
+        let stats = entries_iter.finalize()?;
+        assert_eq!(stats, expected_entries_outcome);
+
+        assert_eq!(
+            expected_obj_count.total(),
+            expected_count.total(),
+            "two different ways of counting, still the same in the end"
+        );
+
+        write_and_verify(entries, expected_pack_hash, allow_thin_pack)?;
     }
+
     Ok(())
 }
 
