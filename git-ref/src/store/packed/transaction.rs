169a39d72106c24dac78af2198e54ca6e09b743e
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-07-27 22:14:38 +0800

    thanks clippy

--- git-ref/src/store/packed/transaction.rs
+++ git-ref/src/store/packed/transaction.rs
@@ -189,7 +189,7 @@ fn write_packed_ref(file: &mut git_lock::File, pref: packed::Reference<'_>) -> s
         out.write_all(pref.name.as_bstr())?;
         out.write_all(b"\n")?;
         if let Some(object) = pref.object {
-            write!(out, "^{}\n", object)?;
+            writeln!(out, "^{}", object)?;
         }
         Ok(())
     })
