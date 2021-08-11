05d309670fbfd342db5f80492082ef7b7a956c3b
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-08-11 11:00:04 +0800

    [utils #154] depth-first traversal over workspace members works

--- etc/utils/src/command/release.rs
+++ etc/utils/src/command/release.rs
@@ -1,26 +1,45 @@
-#![allow(unused)]
-use anyhow::{anyhow, Context};
+use anyhow::{anyhow, bail};
+use cargo_metadata::{Metadata, PackageId};
+use std::collections::BTreeSet;
 
 pub fn release(dry_run: bool, version_bump_spec: String, crates: Vec<String>) -> anyhow::Result<()> {
-    todo!("impl command")
+    let meta = cargo_metadata::MetadataCommand::new().exec()?;
+    let mut seen = BTreeSet::new();
+    for crate_name in crates {
+        if !meta.workspace_members.iter().any(|p| to_name(p) == crate_name) {
+            bail!("Package to release must be a workspace member: '{}'", crate_name);
+        }
+        release_depth_first(dry_run, &meta, &crate_name, &version_bump_spec, &mut seen)?;
+    }
+    Ok(())
 }
 
-fn crate_version(name: &str) -> anyhow::Result<String> {
-    let mut args = std::env::args().skip(1);
-    let cmd = args
-        .next()
-        .ok_or_else(|| anyhow!("The first argument is the subcommand"))?;
+fn release_depth_first(
+    dry_run: bool,
+    meta: &Metadata,
+    crate_name: &str,
+    bump_spec: &str,
+    seen: &mut BTreeSet<String>,
+) -> anyhow::Result<()> {
+    let package = meta
+        .packages
+        .iter()
+        .find(|p| p.name == crate_name)
+        .ok_or_else(|| anyhow!("workspace member must be a listed package: '{}'", crate_name))?;
+    for dependency in &package.dependencies {
+        if seen.contains(&dependency.name) || !meta.workspace_members.iter().any(|p| to_name(p) == dependency.name) {
+            continue;
+        }
+        seen.insert(dependency.name.clone());
+        release_depth_first(dry_run, meta, &dependency.name, bump_spec, seen)?;
+    }
+    log::info!("ready to release {}", crate_name);
+    Ok(())
+}
 
-    let crate_name = args
+fn to_name(p: &PackageId) -> &str {
+    p.repr
+        .splitn(2, " ")
         .next()
-        .ok_or_else(|| anyhow!("The first argument is the name of the crate whose path "))?;
-    let crate_dir = std::env::current_dir()?.join(&crate_name);
-    let cargo_toml_path = crate_dir.join("Cargo.toml");
-    let manifest = cargo_toml::Manifest::from_path(&cargo_toml_path)
-        .with_context(|| format!("Couldn't read Cargo manifest at '{}'", cargo_toml_path.display()))?;
-    let version = manifest
-        .package
-        .ok_or_else(|| anyhow!("Need package information"))
-        .map(|p| p.version)?;
-    Ok(version)
+        .expect("crate-name <additional data we don't need>")
 }
