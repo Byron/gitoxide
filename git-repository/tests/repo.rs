b496d9952924afdb67e9ba8ea0b9b61c8c8fb1f2
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-08-20 14:04:21 +0800

    thanks clippy

--- git-repository/tests/repo.rs
+++ git-repository/tests/repo.rs
@@ -4,7 +4,7 @@ type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn repo(name: &str) -> crate::Result<Repository> {
     let repo_path = git_testtools::scripted_fixture_repo_read_only(name)?;
-    Ok(Repository::discover(repo_path)?.into())
+    Ok(Repository::discover(repo_path)?)
 }
 
 mod access;
