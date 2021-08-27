c8d2e72d272243da7d853f78463552bfc58ed9d6
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-08-27 09:02:58 +0800

    [packetline #178] fix compile warnings

--- git-repository/src/ext/object_id.rs
+++ git-repository/src/ext/object_id.rs
@@ -1,6 +1,5 @@
 #![allow(missing_docs)]
 use git_hash::ObjectId;
-use git_object::CommitRefIter;
 #[cfg(feature = "git-traverse")]
 use git_traverse::commit::ancestors::{Ancestors, State};
 
@@ -12,7 +11,7 @@ pub trait ObjectIdExt: Sealed {
     #[cfg(feature = "git-traverse")]
     fn ancestors_iter<Find>(self, find: Find) -> Ancestors<Find, fn(&git_hash::oid) -> bool, State>
     where
-        Find: for<'a> FnMut(&git_hash::oid, &'a mut Vec<u8>) -> Option<CommitRefIter<'a>>;
+        Find: for<'a> FnMut(&git_hash::oid, &'a mut Vec<u8>) -> Option<git_object::CommitRefIter<'a>>;
 
     fn attach<A: easy::Access + Sized>(self, access: &A) -> easy::Oid<'_, A>;
 }
@@ -23,7 +22,7 @@ impl ObjectIdExt for ObjectId {
     #[cfg(feature = "git-traverse")]
     fn ancestors_iter<Find>(self, find: Find) -> Ancestors<Find, fn(&git_hash::oid) -> bool, State>
     where
-        Find: for<'a> FnMut(&git_hash::oid, &'a mut Vec<u8>) -> Option<CommitRefIter<'a>>,
+        Find: for<'a> FnMut(&git_hash::oid, &'a mut Vec<u8>) -> Option<git_object::CommitRefIter<'a>>,
     {
         Ancestors::new(Some(self), State::default(), find)
     }
