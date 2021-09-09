fd5020868c7141e377a604c0d34cbc527d4959f9
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-09-09 14:54:30 +0800

    [smart-release #195] a test that in theory should trigger the desired behaviour

--- cargo-smart-release/tests/journey.sh
+++ cargo-smart-release/tests/journey.sh
@@ -14,17 +14,44 @@ fixtures="$root/fixtures"
 SUCCESSFULLY=0
 # WITH_FAILURE=1
 
+function set-static-git-environment() {
+  set -a
+  export GIT_AUTHOR_DATE="2021-09-09 09:06:03 +0200"
+  export GIT_COMMITTER_DATE="${GIT_AUTHOR_DATE}"
+  export GIT_AUTHOR_NAME="Sebastian Thiel"
+  export GIT_COMMITTER_NAME="${GIT_AUTHOR_NAME}"
+  export GIT_AUTHOR_EMAIL="git@example.com"
+  export GIT_COMMITTER_EMAIL="${GIT_AUTHOR_EMAIL}"
+  set +a
+}
+
 title "smart-release"
 (sandbox
+  set-static-git-environment
+
   snapshot="$snapshot/triple-depth-workspace"
   cp -R $fixtures/tri-depth-workspace/* .
   { echo 'target/' > .gitignore && git init . && git add . && git commit -q -m "initial"; } &>/dev/null
 
-  (with "'c' as argument and minor-bumping dependencies"
-    it "succeeds" && {
-      WITH_SNAPSHOT="$snapshot/no-args-success" \
-      expect_run $SUCCESSFULLY "$exe" smart-release c -d minor
-    }
+  (with "'c'"
+    (with '-d minor to bump minor dependencies'
+      it "succeeds" && {
+        expect_run $SUCCESSFULLY "$exe" smart-release c -d minor
+      }
+    )
+  )
+  (with "'a'"
+    (with '--execute (but without side-effects'
+      it "succeeds" && {
+        expect_run $SUCCESSFULLY "$exe" smart-release a --skip-push --skip-publish --execute
+      }
+    )
+    (with ".git and target/ directories removed"
+      rm -Rf .git/ target/
+      it "managed to bump B's minor but left C alone as it's not pre-release anymore" && {
+        expect_snapshot "$snapshot/crate-a-released" .
+      }
+    )
   )
 )
 
