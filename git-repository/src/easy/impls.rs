4111d22ebe4cc9ddd726cce566e5872708067440
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-09-11 21:56:33 +0800

    [repository #164]

--- git-repository/src/easy/impls.rs
+++ git-repository/src/easy/impls.rs
@@ -1,9 +1,8 @@
-#![allow(missing_docs)]
 use std::{rc::Rc, sync::Arc};
 
 use parking_lot::lock_api::{ArcRwLockReadGuard, ArcRwLockWriteGuard};
 
-use crate::{easy, Easy, EasyArc, EasyShared, Repository};
+use crate::{easy, Easy, EasyArc, EasyArcExclusive, EasyShared, Repository};
 
 impl From<Repository> for Easy {
     fn from(repo: Repository) -> Self {
@@ -23,31 +22,49 @@ impl From<Repository> for EasyArc {
     }
 }
 
-impl From<Repository> for crate::EasyArcExclusive {
+impl From<Repository> for EasyArcExclusive {
     fn from(repo: Repository) -> Self {
-        crate::EasyArcExclusive {
+        EasyArcExclusive {
             repo: Arc::new(parking_lot::RwLock::new(repo)),
             state: Default::default(),
         }
     }
 }
 
+/// # Which `Easy` is for me?
+///
+/// For one-off commands and single-threaded applications, use [`EasyShared`] or [`Easy`] as they don't allow mutable repository
+/// access which won't be needed.
+///
+/// When multiple threads are involved, use [`EasyArc`] instead.
+///
+/// Finally, if there is the need for adapting to changed object packs on disk or working with namespaces, mutable `Repository` access
+/// is needed, as provided by `EasyArcExclusive`. Currently mutable shared access is only available in thread-save versions, but that
+/// shall be fixed in the future as Rust's support for GATs becomes stable.
+// TODO: Update this once there is `EasyExclusive` as well.
 impl Repository {
+    /// Transform this instance into an [`EasyShared`] with borrowed `Repository`.
+    ///
+    /// Since a standard reference is used, the repository can never be mutated, which is required for the
+    /// fewest operations.
     pub fn to_easy(&self) -> EasyShared<'_> {
         EasyShared {
             repo: self,
             state: Default::default(),
         }
     }
+    /// Transform this instance into an [`Easy`], offering shared immutable access to the repository, for the current thread.
     pub fn into_easy(self) -> Easy {
         self.into()
     }
 
+    /// Transform this instance into an [`EasyArc`], offering shared immutable access to the repository for use across threads.
     pub fn into_easy_arc(self) -> EasyArc {
         self.into()
     }
 
-    pub fn into_easy_arc_exclusive(self) -> crate::EasyArcExclusive {
+    /// Transform this instance into an [`EasyArcExclusive`], offering shared immutable access to the repository for use across threads.
+    pub fn into_easy_arc_exclusive(self) -> EasyArcExclusive {
         self.into()
     }
 }
@@ -102,7 +119,7 @@ impl easy::Access for EasyArc {
     }
 }
 
-impl easy::Access for crate::EasyArcExclusive {
+impl easy::Access for EasyArcExclusive {
     type RepoRef = ArcRwLockReadGuard<parking_lot::RawRwLock, Repository>;
     type RepoRefMut = ArcRwLockWriteGuard<parking_lot::RawRwLock, Repository>;
 
