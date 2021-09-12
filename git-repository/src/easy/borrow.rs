3e612f441e1e837d7ba3d3ddd40b4a8c2ba05c61
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-09-12 21:34:36 +0800

    [repository #164] Documentation for `easy::borrow`

--- git-repository/src/easy/borrow.rs
+++ git-repository/src/easy/borrow.rs
@@ -1,6 +1,11 @@
-#![allow(missing_docs)]
+//!
+///
 pub mod state {
+    /// An error to indicate that `Easy*` state could not be borrowed mutable or immutably.
+    ///
+    /// With `Easy*`borrowing rules are validated at runtime, resulting in additional failure modes.
     #[derive(Debug, thiserror::Error)]
+    #[allow(missing_docs)]
     pub enum Error {
         #[error("A state member could not be borrowed")]
         Borrow(#[from] std::cell::BorrowError),
@@ -8,15 +13,22 @@ pub mod state {
         BorrowMut(#[from] std::cell::BorrowMutError),
     }
 
+    /// A utility type to help with state accessors that may fail due to runtime borrow checking.
     pub type Result<T> = std::result::Result<T, Error>;
 }
 
+///
 pub mod repo {
     use std::{
         cell::{BorrowError, BorrowMutError},
         fmt::{Display, Formatter},
     };
 
+    /// An error to indicate that the [`Repository`][crate::Repository] could not be borrowed immutably or mutably as
+    /// another mutable borrowed is already present.
+    ///
+    /// Note that mutable borrows are uncommon and used only in rare cases, and with [`EasyArcExclusive`][crate::EasyArcExclusive]
+    /// errors of this kind can't happen as a lock would block instead.
     #[derive(Debug)]
     pub struct Error;
 
@@ -40,5 +52,6 @@ pub mod repo {
         }
     }
 
+    /// A utility type to help with repository accessors that may fail due to runtime borrow checking.
     pub type Result<T> = std::result::Result<T, Error>;
 }
