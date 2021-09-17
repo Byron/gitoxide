cd3fc99968baa827302aa9c4f5d181bc9c4f9084
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-09-17 14:57:26 +0800

    feat(commit): A summary for commit messages suitable for logs (#198)

--- git-object/tests/immutable/commit/message.rs
+++ git-object/tests/immutable/commit/message.rs
@@ -91,3 +91,50 @@ fn title_with_windows_separator_and_empty_body() {
         }
     );
 }
+
+pub mod summary {
+    use std::borrow::Cow;
+
+    use git_object::bstr::{BStr, ByteSlice};
+    use git_object::commit::MessageRef;
+
+    fn summary(input: &[u8]) -> Cow<'_, BStr> {
+        MessageRef::from_bytes(input).summary()
+    }
+
+    #[test]
+    fn no_newline_yields_the_message_itself() {
+        let input = b"hello world".as_bstr();
+        assert_eq!(summary(input), Cow::Borrowed(input));
+    }
+
+    #[test]
+    fn trailing_newlines_and_whitespace_are_trimmed() {
+        let input = b"hello world \t\r\n \n";
+        assert_eq!(summary(input), Cow::Borrowed(b"hello world".as_bstr()));
+    }
+
+    #[test]
+    fn prefixed_newlines_and_whitespace_are_trimmed() {
+        let input = b" \t\r\n \nhello world";
+        assert_eq!(summary(input), Cow::Borrowed(b"hello world".as_bstr()));
+    }
+
+    #[test]
+    fn whitespace_up_to_a_newline_is_collapsed_into_a_space() {
+        let input = b" \t\r\n \nhello\r\nworld \t\r\n \n";
+        assert_eq!(summary(input), Cow::Borrowed(b"hello world".as_bstr()));
+    }
+
+    #[test]
+    fn whitespace_without_newlines_is_ignored_except_for_leading_and_trailing_whitespace() {
+        let input = b" \t\r\n \nhello \t \rworld \t\r\n \n";
+        assert_eq!(summary(input), Cow::Borrowed(b"hello \t \rworld".as_bstr()));
+    }
+
+    #[test]
+    fn lines_separated_by_double_newlines_are_subjects() {
+        let input = b" \t\r\n \nhello\t \r\nworld \t\r \nfoo\n\nsomething else we ignore";
+        assert_eq!(summary(input), Cow::Borrowed(b"hello world foo".as_bstr()));
+    }
+}
