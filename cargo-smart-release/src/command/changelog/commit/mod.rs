0c355ed24eb230e9834e797d5c8dc72ae21f0c46
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-09-17 08:02:05 +0800

    smart-release: add git-conventional (#198)

--- cargo-smart-release/src/command/changelog/commit/mod.rs
+++ cargo-smart-release/src/command/changelog/commit/mod.rs
@@ -1,23 +1,6 @@
 pub mod history;
 
-mod message {
-
-    pub struct MessageRef<'a> {
-        category
-    }
-
-    mod decode {
-        use nom::error::{ContextError, ParseError};
-        use nom::IResult;
-
-        /// Parse a signature from the bytes input `i` using `nom`.
-        pub fn decode<'a, E: ParseError<&'a [u8]> + ContextError<&'a [u8]>>(
-            i: &'a [u8],
-        ) -> IResult<&'a [u8], (), E> {
-            todo!("probably not to be done")
-        }
-    }
-}
+mod message {}
 
 pub struct History {
     pub head: git_repository::refs::Reference,
