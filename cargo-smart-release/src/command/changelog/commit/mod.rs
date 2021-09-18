b167d39ecf0cd306dcf4d2c00413251cbfd02ed6
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-09-18 19:36:37 +0800

    Sketch Message fields from which change logs can be built (#198)

--- cargo-smart-release/src/command/changelog/commit/mod.rs
+++ cargo-smart-release/src/command/changelog/commit/mod.rs
@@ -1,17 +1,33 @@
 #![allow(unused)]
+
+use std::borrow::Cow;
+
 pub mod history;
 
 pub struct Message<'a> {
-    conventional: Option<git_conventional::Commit<'a>>,
-    unconventional: &'a str,
-    additions: message::Additions<'a>,
+    /// The cleared, plain title with any `additions` removed.
+    title: Cow<'a, str>,
+    /// More detailed information about the changes.
+    body: &'a str,
+    /// If set, this is a breaking change as indicated git-conventional.
+    breaking: bool,
+    /// If set, this commit message body contains a specific description of the breaking change.
+    breaking_description: Option<&'a str>,
+    /// all dditional information parsed from the title.
+    additions: message::Addition<'a>,
+}
+
+pub struct History {
+    pub head: git_repository::refs::Reference,
+    pub items: Vec<history::Item>,
 }
 
 mod message {
     use git_repository::bstr::BStr;
 
-    pub struct Additions<'a> {
-        issue_id: Option<&'a BStr>,
+    pub enum Addition<'a> {
+        /// The plain issue ID, like "123".
+        IssueId(Option<&'a BStr>),
     }
 
     mod decode {
@@ -25,13 +41,8 @@ mod message {
         /// Parse a signature from the bytes input `i` using `nom`.
         pub fn _decode_description<'a, E: ParseError<&'a [u8]> + ContextError<&'a [u8]>>(
             _i: &'a [u8],
-        ) -> IResult<&'a [u8], message::Additions<'a>, E> {
+        ) -> IResult<&'a [u8], message::Addition<'a>, E> {
             todo!("probably not to be done")
         }
     }
-}
-
-pub struct History {
-    pub head: git_repository::refs::Reference,
-    pub items: Vec<history::Item>,
 }
