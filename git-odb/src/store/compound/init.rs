0509b4fb5a78a3e4bfcacbeb661d262f8592884a
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-09-21 15:35:37 +0800

    Assure pack-ids are actually unique, the simple way…(#67)
    …we could also double-check the produced crc32 values, but
    assigning consecutive numbers seems like it's doing the job.

--- git-odb/src/store/compound/init.rs
+++ git-odb/src/store/compound/init.rs
@@ -37,7 +37,19 @@ impl compound::Store {
                     .filter(|(p, _)| p.extension().unwrap_or_default() == "idx")
                     // TODO: make this configurable, git for instance sorts by modification date
                     //       https://github.com/libgit2/libgit2/blob/main/src/odb_pack.c#L41-L158
-                    .map(|(p, md)| pack::Bundle::at(p).map(|b| (b, md.len())))
+                    .enumerate()
+                    .map(|(idx, (p, md))| {
+                        pack::Bundle::at(p).map(|mut b| {
+                            (
+                                {
+                                    // don't rely on crc32 for producing non-clashing ids. It's the kind of bug we don't want
+                                    b.pack.id = idx as u32;
+                                    b
+                                },
+                                md.len(),
+                            )
+                        })
+                    })
                     .collect::<Result<Vec<_>, _>>()?;
                 packs_and_sizes.sort_by_key(|e| e.1);
                 packs_and_sizes.into_iter().rev().map(|(b, _)| b).collect()
