ec4e3ca4c7211655549a76cae252742633da1083
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-10-11 16:45:36 +0800

    Fix git-url re-export to respect feature flags (#198)

--- git-repository/src/lib.rs
+++ git-repository/src/lib.rs
@@ -134,6 +134,7 @@ pub use git_traverse as traverse;
 #[cfg(all(feature = "unstable", feature = "git-url"))]
 pub use git_url as url;
 #[doc(inline)]
+#[cfg(all(feature = "unstable", feature = "git-url"))]
 pub use git_url::Url;
 
 pub mod interrupt;
