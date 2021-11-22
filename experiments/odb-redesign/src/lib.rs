cf17816640ce2418624de04c3979a2c03a1cc917
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-11-22 09:00:05 +0800

    A first stab at sketching types (#259)
    Already showing some issues but I think it can be done smartly
    nonetheless.

--- experiments/odb-redesign/src/lib.rs
+++ experiments/odb-redesign/src/lib.rs
@@ -1,8 +1,53 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+#![allow(dead_code)]
+
+use std::cell::RefCell;
+use std::ops::Deref;
+use std::rc::Rc;
+use std::sync::Arc;
+
+pub mod policy {
+    pub struct IndexMarker(u32);
+
+    pub mod next_indices {
+        use crate::policy::IndexMarker;
+
+        pub enum Outcome<IndexRef> {
+            Next {
+                indices: Vec<IndexRef>, // should probably be small vec to get around most allocations
+                mark: IndexMarker,      // use to show where you left off next time you call
+            },
+            /// No new indices to look at, caller should stop give up
+            NoMoreIndices,
+        }
     }
+}
+
+trait Policy {
+    type PackData: Deref<Target = git_pack::data::File>;
+    type PackIndex: Deref<Target = git_pack::index::File>;
+
+    fn next_indices(&mut self) -> std::io::Result<policy::next_indices::Outcome<Self::PackIndex>>;
+}
+
+struct EagerLocal {}
+
+type DynRcPolicy = dyn Policy<PackIndex = Rc<git_pack::data::File>, PackData = Rc<git_pack::data::File>>;
+type DynArcPolicy = dyn Policy<PackIndex = Arc<git_pack::data::File>, PackData = Arc<git_pack::data::File>>;
+
+/// Formerly RepositoryLocal
+struct SharedLocal {
+    pack_policy: Rc<RefCell<dyn Policy<PackIndex = Rc<git_pack::data::File>, PackData = Rc<git_pack::data::File>>>>,
+}
+
+/// Formerly Repository
+struct Shared {
+    pack_policy: Arc<parking_lot::Mutex<DynArcPolicy>>,
+}
+
+/// Using generics here would mean we need policy to handle its mutability itself, pushing it down might be easiest if generics
+/// should be a thing.
+/// Without generics, there would be a thread-safe and thread-local version of everything.
+/// Maybe this should be solved with a feature toggle instead? Aka thread-safe or not?
+struct SharedGeneric<PackPolicy: Policy> {
+    pack_policy: PackPolicy,
 }
