ab14f70202a5a80369a640eff809003925bccb58
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-12-02 16:49:13 +0800

    refactor (#266)

--- git-odb/src/store/linked/mod.rs
+++ git-odb/src/store/linked/mod.rs
@@ -13,7 +13,6 @@ pub struct Store {
 pub mod store {
     #![allow(missing_docs, dead_code, unreachable_code)]
 
-    use crate::linked::handle;
     use git_features::threading::OwnShared;
 
     /// Note that this type is only `Send` if `git-features/parallel` is toggled on.
@@ -25,38 +24,13 @@ pub mod store {
         object_cache: Box<handle::ObjectCache>,
     }
 
-    impl<S> Handle<S> {
-        pub fn with_pack_cache(mut self, create: impl Fn() -> Box<handle::PackCache> + 'static) -> Self {
-            self.new_pack_cache = OwnShared::new(create);
-            self.pack_cache = (self.new_pack_cache)();
-            self
-        }
-        pub fn with_object_cache(mut self, create: impl Fn() -> Box<handle::ObjectCache> + 'static) -> Self {
-            self.new_object_cache = OwnShared::new(create);
-            self.object_cache = (self.new_object_cache)();
-            self
-        }
-    }
-
-    impl<S: Clone> Clone for Handle<S> {
-        fn clone(&self) -> Self {
-            Handle {
-                store: self.store.clone(),
-                new_pack_cache: self.new_pack_cache.clone(),
-                new_object_cache: self.new_object_cache.clone(),
-                pack_cache: (self.new_pack_cache)(),
-                object_cache: (self.new_object_cache)(),
-            }
-        }
-    }
+    pub mod handle;
 }
 
 ///
 pub mod init;
 
 mod find;
-
-mod handle;
 
 ///
 mod write;
---
+++ git-odb/src/store/linked/store/handle.rs
@@ -0,0 +1,33 @@
+use crate::linked::store::Handle;
+use git_features::threading::OwnShared;
+
+pub type PackCache = dyn git_pack::cache::DecodeEntry + Send + 'static;
+pub type NewPackCacheFn = dyn Fn() -> Box<PackCache> + 'static;
+
+pub type ObjectCache = dyn git_pack::cache::Object + Send + 'static;
+pub type NewObjectCacheFn = dyn Fn() -> Box<ObjectCache> + 'static;
+
+impl<S> Handle<S> {
+    pub fn with_pack_cache(mut self, create: impl Fn() -> Box<PackCache> + 'static) -> Self {
+        self.new_pack_cache = OwnShared::new(create);
+        self.pack_cache = (self.new_pack_cache)();
+        self
+    }
+    pub fn with_object_cache(mut self, create: impl Fn() -> Box<ObjectCache> + 'static) -> Self {
+        self.new_object_cache = OwnShared::new(create);
+        self.object_cache = (self.new_object_cache)();
+        self
+    }
+}
+
+impl<S: Clone> Clone for Handle<S> {
+    fn clone(&self) -> Self {
+        Handle {
+            store: self.store.clone(),
+            new_pack_cache: self.new_pack_cache.clone(),
+            new_object_cache: self.new_object_cache.clone(),
+            pack_cache: (self.new_pack_cache)(),
+            object_cache: (self.new_object_cache)(),
+        }
+    }
+}
