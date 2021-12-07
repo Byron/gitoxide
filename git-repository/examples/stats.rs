e7e54a22fbc06c5e54216abc426d70d7bff0ac26
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-12-07 20:55:00 +0800

    episode 4

--- git-repository/examples/stats.rs
+++ git-repository/examples/stats.rs
@@ -1,11 +1,13 @@
+use git_repository as git;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let repo = git_repository::discover(".")?;
     println!(
         "Repo: {}",
         repo.work_tree.as_deref().unwrap_or(repo.git_dir()).display()
     );
-    let repo = repo.to_easy();
-    let commit_ids = repo
+    let handle = repo.to_easy();
+    let commit_ids = handle
         .head()?
         .into_fully_peeled_id()
         .ok_or_else(|| "There are no commits - nothing to do here.")??
@@ -17,7 +19,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let last_commit_id = &commit_ids[0];
     println!("Most recent commit message");
-    println!("{}", last_commit_id.object()?.to_commit().message);
+
+    let object_ref = last_commit_id.object()?;
+    let handle2 = handle.clone();
+    let commit = object_ref.to_commit();
+    println!("{}", commit.message);
+    let tree_ref = handle2.find_object(commit.tree())?.into_tree();
+    println!("{:?}", git::objs::TreeRefIter::from_bytes(&tree_ref.data).nth(1));
 
     Ok(())
 }
