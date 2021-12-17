af0cc5f27488df497bc929492bb13386aa88cc2a
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-12-17 17:39:31 +0800

    A more suitable iterator implementation for general store (#266)

--- git-odb/src/store/general/iter.rs
+++ git-odb/src/store/general/iter.rs
@@ -1,31 +1,32 @@
+use std::sync::Arc;
+use std::vec::IntoIter;
 use std::{ops::Deref, option::Option::None};
 
-use crate::general::load_index::Snapshot;
+use crate::general::handle;
 use crate::loose;
 use git_hash::ObjectId;
 
 use crate::store::general;
 
-#[allow(clippy::large_enum_variant)]
-enum DbState {
-    Pack { pack_index: usize, entry_index: u32 },
-    Loose { iter: loose::iter::Iter },
-}
-
-impl Default for DbState {
-    fn default() -> Self {
-        DbState::Pack {
-            pack_index: 0,
-            entry_index: 0,
-        }
-    }
+pub enum State {
+    Pack {
+        index_iter: IntoIter<handle::IndexLookup>,
+        index: handle::IndexLookup,
+        entry_index: u32,
+        num_objects: u32,
+    },
+    Loose {
+        iter: loose::iter::Iter,
+        index: usize,
+    },
+    Depleted,
 }
 
 /// An iterator over all objects of a linked database
 pub struct AllObjects {
-    snapshot: Snapshot,
-    loose_db_index: usize,
-    db_state: DbState,
+    state: State,
+    num_objects: usize,
+    loose_dbs: Arc<Vec<loose::Store>>,
 }
 
 impl AllObjects {
@@ -36,20 +37,34 @@ impl AllObjects {
             snapshot = new_snapshot
         }
 
-        let db_index = 0;
-        let db_state = {
-            if snapshot.indices.is_empty() {
-                DbState::Loose {
-                    iter: snapshot.loose_dbs.get(db_index).expect("at least one loose db").iter(),
+        let packed_objects = snapshot
+            .indices
+            .iter()
+            .fold(0usize, |dbc, index| dbc.saturating_add(index.num_objects() as usize));
+        let mut index_iter = snapshot.indices.into_iter();
+        let loose_dbs = snapshot.loose_dbs;
+        let state = match index_iter.next() {
+            Some(index) => {
+                let num_objects = index.num_objects();
+                State::Pack {
+                    index_iter,
+                    index,
+                    entry_index: 0,
+                    num_objects,
                 }
-            } else {
-                DbState::default()
+            }
+            None => {
+                let index = 0;
+                State::Loose {
+                    iter: loose_dbs.get(index).expect("at least one loose db").iter(),
+                    index,
+                }
             }
         };
         Ok(AllObjects {
-            snapshot,
-            loose_db_index: db_index,
-            db_state,
+            state,
+            loose_dbs,
+            num_objects: packed_objects,
         })
     }
 }
@@ -58,57 +73,57 @@ impl Iterator for AllObjects {
     type Item = Result<ObjectId, loose::iter::Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let snapshot = &self.snapshot;
-        if self.loose_db_index == snapshot.indices.len() {
-            return None;
-        }
-
-        match &mut self.db_state {
-            DbState::Pack {
-                pack_index,
+        match &mut self.state {
+            State::Depleted => None,
+            State::Pack {
+                index_iter,
+                index,
                 entry_index,
-            } => match snapshot.indices.get(*pack_index) {
-                Some(index) => {
-                    if *entry_index < index.num_objects() {
-                        let oid = index.oid_at_index(*entry_index).to_owned();
-                        *entry_index += 1;
-                        Some(Ok(oid))
-                    } else {
-                        *pack_index += 1;
-                        *entry_index = 0;
-                        self.next()
+                num_objects,
+            } => {
+                if *entry_index < *num_objects {
+                    let oid = index.oid_at_index(*entry_index).to_owned();
+                    *entry_index += 1;
+                    Some(Ok(oid))
+                } else {
+                    match index_iter.next() {
+                        Some(new_index) => {
+                            *index = new_index;
+                            *entry_index = 0;
+                            *num_objects = index.num_objects();
+                        }
+                        None => {
+                            let index = 0;
+                            self.state = State::Loose {
+                                iter: self.loose_dbs.get(index).expect("at least one loose odb").iter(),
+                                index,
+                            }
+                        }
                     }
-                }
-                None => {
-                    self.db_state = DbState::Loose {
-                        iter: snapshot
-                            .loose_dbs
-                            .get(self.loose_db_index)
-                            .expect("at least one loose db")
-                            .iter(),
-                    };
                     self.next()
                 }
-            },
-            DbState::Loose { iter } => match iter.next() {
+            }
+            State::Loose { iter, index } => match iter.next() {
                 Some(id) => Some(id),
                 None => {
-                    self.loose_db_index += 1;
-                    let iter = snapshot.loose_dbs.get(self.loose_db_index)?.iter();
-                    self.db_state = DbState::Loose { iter };
-                    self.next()
+                    *index += 1;
+                    match self.loose_dbs.get(*index).map(|ldb| ldb.iter()) {
+                        Some(new_iter) => {
+                            *iter = new_iter;
+                            self.next()
+                        }
+                        None => {
+                            self.state = State::Depleted;
+                            None
+                        }
+                    }
                 }
             },
         }
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let packed_objects = self
-            .snapshot
-            .indices
-            .iter()
-            .fold(0usize, |dbc, index| dbc.saturating_add(index.num_objects() as usize));
-        (packed_objects, None)
+        (self.num_objects, None)
     }
 }
 
