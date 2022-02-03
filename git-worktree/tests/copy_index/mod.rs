25a9dc16dbb26e9aa0f3379b2af53cc0baa96663
Aratrik Pal <aratrik.pal@gmail.com>
2022-02-03 17:39:39 +0530

    Refactored tests

--- git-worktree/tests/copy_index/mod.rs
+++ git-worktree/tests/copy_index/mod.rs
@@ -1,4 +1,4 @@
-use crate::{dir_structure, fixture_path, Result};
+use crate::{dir_structure, fixture_path};
 use git_object::bstr::ByteSlice;
 use git_odb::FindExt;
 use git_worktree::{copy_index, Options};
@@ -8,7 +8,7 @@ use std::fs;
 use std::os::unix::prelude::MetadataExt;
 
 #[test]
-fn test_copy_index() -> Result<()> {
+fn test_copy_index() -> crate::Result<()> {
     let path = fixture_path("make_repo");
     let path_git = path.join(".git");
     let mut file = git_index::File::at(path_git.join("index"), git_index::decode::Options::default())?;
@@ -28,14 +28,29 @@ fn test_copy_index() -> Result<()> {
 
     let srepo_files: Vec<_> = repo_files.iter().flat_map(|p| p.strip_prefix(&path)).collect();
     let scopy_files: Vec<_> = copy_files.iter().flat_map(|p| p.strip_prefix(output)).collect();
-    assert_eq!(srepo_files, scopy_files);
+    assert_eq!(
+        srepo_files,
+        scopy_files,
+        "Testing if {} and {} have the same structure",
+        path.display(),
+        output.display()
+    );
 
     for (file1, file2) in repo_files.iter().zip(copy_files.iter()) {
-        assert_eq!(fs::read(file1)?, fs::read(file2)?);
+        assert_eq!(
+            fs::read(file1)?,
+            fs::read(file2)?,
+            "Testing if the contents of {} and {} are the same",
+            file1.display(),
+            file2.display(),
+        );
         #[cfg(unix)]
         assert_eq!(
             fs::symlink_metadata(file1)?.mode() & 0b111 << 6,
-            fs::symlink_metadata(file2)?.mode() & 0b111 << 6
+            fs::symlink_metadata(file2)?.mode() & 0b111 << 6,
+            "Testing if the permissions of {} and {} are the same",
+            file1.display(),
+            file2.display(),
         );
     }
 
@@ -43,7 +58,7 @@ fn test_copy_index() -> Result<()> {
 }
 
 #[test]
-fn test_copy_index_without_symlinks() -> Result<()> {
+fn test_copy_index_without_symlinks() -> crate::Result<()> {
     let path = fixture_path("make_repo");
     let path_git = path.join(".git");
     let mut file = git_index::File::at(path_git.join("index"), git_index::decode::Options::default())?;
@@ -63,18 +78,39 @@ fn test_copy_index_without_symlinks() -> Result<()> {
 
     let srepo_files: Vec<_> = repo_files.iter().flat_map(|p| p.strip_prefix(&path)).collect();
     let scopy_files: Vec<_> = copy_files.iter().flat_map(|p| p.strip_prefix(output)).collect();
-    assert_eq!(srepo_files, scopy_files);
+    assert_eq!(
+        srepo_files,
+        scopy_files,
+        "Testing if {} and {} have the same structure",
+        path.display(),
+        output.display()
+    );
 
     for (file1, file2) in repo_files.iter().zip(copy_files.iter()) {
         if file1.is_symlink() {
-            assert!(!file2.is_symlink());
-            assert_eq!(fs::read(file2)?.to_path()?, fs::read_link(file1)?);
+            assert!(!file2.is_symlink(), "Testing if the new file is not a symlink");
+            assert_eq!(
+                fs::read(file2)?.to_path()?,
+                fs::read_link(file1)?,
+                "Testing if the {} contains the path the symlink {} is pointing to",
+                file2.display(),
+                file1.display(),
+            );
         } else {
-            assert_eq!(fs::read(file1)?, fs::read(file2)?);
+            assert_eq!(
+                fs::read(file1)?,
+                fs::read(file2)?,
+                "Testing if the contents of {} and {} are the same",
+                file1.display(),
+                file2.display(),
+            );
             #[cfg(unix)]
             assert_eq!(
                 fs::symlink_metadata(file1)?.mode() & 0b111 << 6,
-                fs::symlink_metadata(file2)?.mode() & 0b111 << 6
+                fs::symlink_metadata(file2)?.mode() & 0b111 << 6,
+                "Testing if the permissions of {} and {} are the same",
+                file1.display(),
+                file2.display(),
             );
         }
     }
