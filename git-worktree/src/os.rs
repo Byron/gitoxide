4b1650ba1988f52a7a91ce4f5327eca350f32520
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-03-02 18:34:54 +0800

    fix symlink creation on windows, hopefully (#301)

--- git-worktree/src/os.rs
+++ git-worktree/src/os.rs
@@ -23,6 +23,26 @@ pub fn create_symlink(original: &Path, link: &Path) -> io::Result<()> {
     if std::fs::metadata(link.parent().expect("dir for link").join(original))?.is_dir() {
         symlink_dir(original, link)
     } else {
-        symlink_file(original, original)
+        symlink_file(original, link)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn io_err_if_symlink_target_is_a_file() {
+        let dir = tempfile::tempdir().unwrap();
+        let a = dir.path().join("a");
+        let b = dir.path().join("b");
+
+        std::fs::write(&a, &[]).unwrap();
+        std::fs::write(&b, &[]).unwrap();
+
+        assert_eq!(
+            create_symlink(&a, &b).unwrap_err().kind(),
+            std::io::ErrorKind::AlreadyExists
+        );
     }
 }
