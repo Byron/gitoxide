cabe071dbf251db5e4f7740ca4a9aac4201bcb32
Svetlin Stefanov <s.m.stefanov@gmail.com>
2022-03-05 11:00:58 +0100

    Rename `String` to `Bytes`.

--- git-config/tests/integration_tests/file_integeration_test.rs
+++ git-config/tests/integration_tests/file_integeration_test.rs
@@ -73,8 +73,8 @@ fn get_value_for_all_provided_values() -> crate::Result {
     );
 
     assert_eq!(
-        file.value::<String>("core", None, "other")?,
-        String {
+        file.value::<Bytes>("core", None, "other")?,
+        Bytes {
             value: Cow::Borrowed(b"hello world")
         }
     );
