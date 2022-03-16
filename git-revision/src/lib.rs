eefa6c51da2bafb6a6bcfb1a2fdb785b73cf919c
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-03-16 20:32:44 +0800

    git-describe complete formatting (#298)

--- git-revision/src/lib.rs
+++ git-revision/src/lib.rs
@@ -5,6 +5,63 @@ use git_object::bstr::BStr;
 use std::borrow::Cow;
 use std::collections::HashSet;
 
-pub fn describe(_commit: &oid, _name_set: &HashSet<(ObjectId, Cow<'_, BStr>)>) {
+pub mod describe {
+    use git_object::bstr::BStr;
+    use std::borrow::Cow;
+    use std::fmt::{Display, Formatter};
+
+    #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
+    #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
+    pub struct Outcome<'a> {
+        pub name: Cow<'a, BStr>,
+        pub id: git_hash::ObjectId,
+        pub hex_len: usize,
+        pub depth: usize,
+        pub long: bool,
+        pub dirty_suffix: Option<String>,
+    }
+
+    impl<'a> Outcome<'a> {
+        pub fn is_exact_match(&self) -> bool {
+            self.depth == 0
+        }
+        pub fn long(&mut self) -> &mut Self {
+            self.long = true;
+            self
+        }
+        pub fn short(&mut self) -> &mut Self {
+            self.long = false;
+            self
+        }
+    }
+
+    impl<'a> Display for Outcome<'a> {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            if !self.long && self.is_exact_match() {
+                self.name.fmt(f)?;
+            } else {
+                write!(
+                    f,
+                    "{}-{}-g{}",
+                    self.name,
+                    self.depth,
+                    self.id.to_hex_with_len(self.hex_len)
+                )?;
+            }
+            if let Some(suffix) = &self.dirty_suffix {
+                write!(f, "-{}", suffix)?;
+            }
+            Ok(())
+        }
+    }
+
+    pub type Error = ();
+}
+
+pub fn describe<'a>(
+    _commit: &oid,
+    _hex_len: usize,
+    _name_set: &HashSet<(ObjectId, Cow<'a, BStr>)>,
+) -> Result<Option<describe::Outcome<'a>>, describe::Error> {
     todo!()
 }
--- git-revision/tests/describe/mod.rs
+++ git-revision/tests/describe/mod.rs
@@ -1,5 +1,40 @@
 use git_object::bstr::ByteSlice;
+use git_revision::describe;
 use std::borrow::Cow;
+
+mod outcome_format {
+    use git_object::bstr::ByteSlice;
+    use git_revision::describe;
+    use git_testtools::hex_to_id;
+    use std::borrow::Cow;
+
+    #[test]
+    fn exact_match_with_dirty_and_long() {
+        let mut outcome = describe::Outcome {
+            name: Cow::Borrowed(b"main".as_bstr()),
+            id: hex_to_id("b920bbb055e1efb9080592a409d3975738b6efb3"),
+            hex_len: 7,
+            depth: 0,
+            dirty_suffix: None,
+            long: false,
+        };
+        assert_eq!(outcome.is_exact_match(), true);
+        assert_eq!(outcome.to_string(), "main");
+        assert_eq!(outcome.long().to_string(), "main-0-gb920bbb");
+
+        outcome.dirty_suffix = Some("dirty".into());
+        assert_eq!(outcome.short().to_string(), "main-dirty");
+        assert_eq!(outcome.long().to_string(), "main-0-gb920bbb-dirty");
+
+        outcome.dirty_suffix = None;
+        outcome.depth = 42;
+        assert_eq!(outcome.short().to_string(), "main-42-gb920bbb");
+
+        outcome.dirty_suffix = Some("dirty".into());
+        assert_eq!(outcome.to_string(), "main-42-gb920bbb-dirty");
+        assert_eq!(outcome.long().to_string(), "main-42-gb920bbb-dirty");
+    }
+}
 
 #[test]
 #[ignore]
@@ -8,10 +43,24 @@ fn it_uses_the_ref_the_described_commit_is_on() {
     let repo = git_repository::open(dir).unwrap();
 
     let commit = repo.head().unwrap().peel_to_commit_in_place().unwrap();
-    git_revision::describe(
+    let name = Cow::Borrowed(b"main".as_bstr());
+    let hex_len = 7;
+    let res = git_revision::describe(
         &commit.id,
-        &vec![(commit.id, Cow::Borrowed(b"main".as_bstr()))]
-            .into_iter()
-            .collect(),
+        hex_len,
+        &vec![(commit.id, name.clone())].into_iter().collect(),
+    )
+    .unwrap()
+    .expect("find exact match");
+    assert_eq!(
+        res,
+        describe::Outcome {
+            name,
+            id: commit.id,
+            depth: 0,
+            hex_len,
+            long: false,
+            dirty_suffix: None
+        }
     );
 }
