9a5d089010c5f46dc0470a8611b88c532836f841
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-03-18 11:35:45 +0800

    Handle trailing whitespaces (#301)

--- git-attributes/src/lib.rs
+++ git-attributes/src/lib.rs
@@ -39,15 +39,60 @@ pub mod parse {
                     return None;
                 }
                 let mut lines = self.cursor.lines();
-                let res = None;
-                for line in lines.by_ref() {
+                let mut res = None;
+                for mut line in lines.by_ref() {
+                    let mut mode = ignore::pattern::Mode::empty();
+                    if line.is_empty() {
+                        continue;
+                    };
                     if line.starts_with(b"#") {
                         continue;
+                    } else if line.starts_with(b"\\#") {
+                        line = &line[1..];
+                    } else if line.starts_with(b"!") {
+                        mode |= ignore::pattern::Mode::NEGATIVE;
+                        line = &line[1..];
+                    } else if line.starts_with(b"\\!") {
+                        line = &line[1..];
                     }
-                    todo!("handle escapes and trim trailing non-escaped whitespace")
+                    res = Some((truncate_non_escaped_trailing_spaces(line), mode));
                 }
                 self.cursor = lines.next().unwrap_or(&[]).as_bstr();
                 res
+            }
+        }
+
+        /// We always copy just because that's ultimately needed anyway, not because we always have to.
+        fn truncate_non_escaped_trailing_spaces(buf: &[u8]) -> BString {
+            match buf.rfind_not_byteset(br"\ ") {
+                Some(pos) if pos + 1 == buf.len() => buf.into(), // does not end in (escaped) whitespace
+                None => buf.into(),
+                Some(start_of_non_space) => {
+                    // This seems a bit strange but attempts to recreate the git implementation while
+                    // actually removing the escape characters before spaces. We leave other backslashes
+                    // for escapes to be handled by `glob/globset`.
+                    let mut res: BString = buf[..start_of_non_space + 1].into();
+
+                    let mut trailing_bytes = buf[start_of_non_space + 1..].iter();
+                    let mut bare_spaces = 0;
+                    while let Some(b) = trailing_bytes.next() {
+                        match b {
+                            b' ' => {
+                                bare_spaces += 1;
+                            }
+                            b'\\' => {
+                                res.extend(std::iter::repeat(b' ').take(bare_spaces));
+                                bare_spaces = 0;
+                                // Skip what follows, like git does, but keep spaces if possible.
+                                if trailing_bytes.next() == Some(&b' ') {
+                                    res.push(b' ');
+                                }
+                            }
+                            _ => unreachable!("BUG: this must be either backslash or space"),
+                        }
+                    }
+                    res
+                }
             }
         }
     }
--- git-attributes/tests/attributes.rs
+++ git-attributes/tests/attributes.rs
@@ -1,8 +1,93 @@
 mod parse {
     mod ignore {
+        use git_attributes::ignore;
+
         #[test]
         fn comments_are_ignored() {
             assert!(git_attributes::parse::ignore(b"# hello world").next().is_none());
+        }
+
+        #[test]
+        fn backslashes_before_hashes_are_no_comments() {
+            assert_eq!(
+                git_attributes::parse::ignore(br"\#hello").next(),
+                Some((r"#hello".into(), ignore::pattern::Mode::empty()))
+            );
+        }
+
+        #[test]
+        fn backslashes_are_part_of_the_pattern_if_not_in_specific_positions() {
+            assert_eq!(
+                git_attributes::parse::ignore(br"\hello\world").next(),
+                Some((r"\hello\world".into(), ignore::pattern::Mode::empty()))
+            );
+        }
+
+        #[test]
+        fn leading_exclamation_mark_negates_pattern() {
+            assert_eq!(
+                git_attributes::parse::ignore(b"!hello").next(),
+                Some(("hello".into(), ignore::pattern::Mode::NEGATIVE))
+            );
+        }
+
+        #[test]
+        fn leading_exclamation_marks_can_be_escaped_with_backslash() {
+            assert_eq!(
+                git_attributes::parse::ignore(br"\!hello").next(),
+                Some(("!hello".into(), ignore::pattern::Mode::empty()))
+            );
+        }
+
+        #[test]
+        fn trailing_spaces_are_ignored() {
+            assert_eq!(
+                git_attributes::parse::ignore(br"a   ").next(),
+                Some(("a".into(), ignore::pattern::Mode::empty()))
+            );
+            assert_eq!(
+                git_attributes::parse::ignore(b"a\t\t  ").next(),
+                Some(("a\t\t".into(), ignore::pattern::Mode::empty()),),
+                "trailing tabs are not ignored"
+            );
+        }
+        #[test]
+        fn trailing_spaces_can_be_escaped_to_be_literal() {
+            assert_eq!(
+                git_attributes::parse::ignore(br"a  \ ").next(),
+                Some(("a   ".into(), ignore::pattern::Mode::empty())),
+                "a single escape in front of the last desired space is enough"
+            );
+            assert_eq!(
+                git_attributes::parse::ignore(br"a  b  c ").next(),
+                Some(("a  b  c".into(), ignore::pattern::Mode::empty())),
+                "spaces in the middle are fine"
+            );
+            assert_eq!(
+                git_attributes::parse::ignore(br"a\ \ \ ").next(),
+                Some(("a   ".into(), ignore::pattern::Mode::empty())),
+                "one can also escape every single one"
+            );
+            assert_eq!(
+                git_attributes::parse::ignore(br"a \  ").next(),
+                Some(("a  ".into(), ignore::pattern::Mode::empty())),
+                "or just the one in the middle, losing the last actual space"
+            );
+            assert_eq!(
+                git_attributes::parse::ignore(br"a   \").next(),
+                Some(("a   ".into(), ignore::pattern::Mode::empty())),
+                "escaping nothing also works as a whitespace protection"
+            );
+            assert_eq!(
+                git_attributes::parse::ignore(br"a   \\\ ").next(),
+                Some((r"a    ".into(), ignore::pattern::Mode::empty())),
+                "strange things like these work too"
+            );
+            assert_eq!(
+                git_attributes::parse::ignore(br"a   \\ ").next(),
+                Some((r"a   ".into(), ignore::pattern::Mode::empty())),
+                "strange things like these work as well"
+            );
         }
     }
 }
