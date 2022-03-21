a4de008b88f892e95bf6da36d09b27190e9c5ede
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-03-21 20:51:17 +0800

    INTERMEDIATE RESET ME

--- git-revision/src/describe.rs
+++ git-revision/src/describe.rs
@@ -59,7 +59,7 @@ pub(crate) mod function {
     #[allow(clippy::result_unit_err)]
     pub fn describe<'a, Find, E>(
         commit: &oid,
-        find: Find,
+        mut find: Find,
         hex_len: usize,
         name_set: &HashMap<ObjectId, Cow<'a, BStr>>,
     ) -> Result<Option<Outcome<'a>>, E>
@@ -77,21 +77,23 @@ pub(crate) mod function {
                 dirty_suffix: None,
             }));
         }
-        let mut queue = VecDeque::from_iter(Some(commit.to_owned()));
+        type Flags = usize;
+        let mut queue = VecDeque::from_iter(Some((commit.to_owned(), 0 as Flags)));
         let mut candidates = Vec::new();
         let mut seen_commits = 0;
         let mut gave_up_on_commit = None;
         let mut seen = hash_hasher::HashedSet::default();
+        let mut buf = Vec::new();
 
-        const MAX_CANDIDATES: usize = std::mem::size_of::<usize>() * 8;
-        while let Some(commit) = queue.pop_front() {
+        const MAX_CANDIDATES: usize = std::mem::size_of::<Flags>() * 8;
+        while let Some((commit, flags)) = queue.pop_front() {
             seen_commits += 1;
             assert!(seen.insert(commit), "BUG: shouldn't ever see dupes here");
             if let Some(name) = name_set.get(&commit) {
                 if candidates.len() < MAX_CANDIDATES {
                     candidates.push(Candidate {
                         name: name.clone(),
-                        depth: seen_commits - 1,
+                        commits_in_its_future: seen_commits - 1,
                         identity_bit: 1 << candidates.len(),
                         order: candidates.len(),
                     });
@@ -100,13 +102,40 @@ pub(crate) mod function {
                     break;
                 }
             }
+
+            for candidate in candidates
+                .iter_mut()
+                .filter(|c| !((flags & c.identity_bit) == c.identity_bit))
+            {
+                candidate.commits_in_its_future += 1;
+            }
+
+            let commit_iter = find(&commit, &mut buf)?;
+            for token in commit_iter {
+                match token {
+                    Ok(git_object::commit::ref_iter::Token::Tree { .. }) => continue,
+                    Ok(git_object::commit::ref_iter::Token::Parent { id }) => {
+                        let mut parent = find(id.as_ref(), &mut buf)?;
+
+                        // TODO: figure out if not having a date is a hard error.
+                        let parent_committer_date = parent
+                            .committer()
+                            .map(|committer| committer.time.time)
+                            .unwrap_or_default();
+
+                        // queue.binary_search_by_key()
+                    }
+                    Ok(_unused_token) => break,
+                    Err(err) => todo!("return a decode error"),
+                }
+            }
         }
         todo!("actually search for it")
     }
 
     struct Candidate<'a> {
         name: Cow<'a, BStr>,
-        depth: usize,
+        commits_in_its_future: usize,
         /// A single bit identifying this candidate uniquely in a bitset
         identity_bit: usize,
         /// The order at which we found the candidate, first one has order = 0
