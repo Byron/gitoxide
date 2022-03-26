f458817005b884e966bcc894a0cf7c9958882ba4
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-03-26 18:00:41 +0800

    high-level parsing to deal with allowed mailmap lines (#366)

--- git-mailmap/src/parse.rs
+++ git-mailmap/src/parse.rs
@@ -16,7 +16,7 @@ mod error {
 }
 
 use crate::Entry;
-use bstr::ByteSlice;
+use bstr::{BStr, ByteSlice};
 pub use error::Error;
 
 pub struct Lines<'a> {
@@ -40,11 +40,44 @@ impl<'a> Iterator for Lines<'a> {
         for line in self.lines.by_ref() {
             self.line_no += 1;
             match line.first() {
-                None => continue,
-                Some(b) if *b == b'#' => continue,
+                None => return None,
+                Some(b) if *b == b'#' => return None,
                 Some(_) => {}
             }
+
+            return parse_line(line.into(), self.line_no).into();
         }
         None
     }
+}
+
+fn parse_line(line: &BStr, line_number: usize) -> Result<Entry<'_>, Error> {
+    let (name1, email1, rest) = parse_name_and_email(line, line_number)?;
+    let (name2, email2, rest) = parse_name_and_email(rest, line_number)?;
+    if !rest.trim().is_empty() {
+        return Err(Error::UnconsumedInput {
+            line_number,
+            line: line.into(),
+        });
+    }
+    Ok(match (name1, email1, name2, email2) {
+        (Some(proper_name), Some(commit_email), None, None) => Entry::change_name_by_email(proper_name, commit_email),
+        (None, Some(proper_email), None, Some(commit_email)) => {
+            Entry::change_email_by_email(proper_email, commit_email)
+        }
+        (Some(proper_name), Some(proper_email), None, Some(commit_email)) => {
+            Entry::change_name_and_email_by_email(proper_name, proper_email, commit_email)
+        }
+        (Some(proper_name), Some(proper_email), Some(commit_name), Some(commit_email)) => {
+            Entry::change_name_and_email_by_name_and_email(proper_name, proper_email, commit_name, commit_email)
+        }
+        _ => todo!(),
+    })
+}
+
+fn parse_name_and_email(
+    line: &BStr,
+    line_number: usize,
+) -> Result<(Option<&'_ BStr>, Option<&'_ BStr>, &'_ BStr), Error> {
+    todo!()
 }
