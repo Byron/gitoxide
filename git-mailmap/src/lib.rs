fc47c497f992e0f3fbef2f55d0e3b0909cac8290
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-03-26 10:46:44 +0800

    the first empty test (#366)

--- git-mailmap/src/lib.rs
+++ git-mailmap/src/lib.rs
@@ -1,1 +1,24 @@
 #![forbid(unsafe_code, rust_2018_idioms)]
+
+pub mod parse {
+    mod error {
+        use bstr::BString;
+        use quick_error::quick_error;
+
+        quick_error! {
+            #[derive(Debug)]
+            pub enum Error {
+                UnconsumedInput { line_number: usize, line: BString } {
+                    display("Line {} has too many names or emails: {}", line_number, line)
+                }
+                Malformed { line_number: usize, line: BString } {
+                    display("Line {} is malformed, an email address lacks the closing '>' bracket: {}", line_number, line)
+                }
+            }
+        }
+    }
+    pub struct Lines<'a> {
+        lines: bstr::Lines<'a>,
+        line_no: usize,
+    }
+}
---
+++ git-mailmap/tests/parse/mod.rs
@@ -0,0 +1,3 @@
+#[test]
+#[ignore]
+fn empty_lines_and_comments_are_ignored() {}
