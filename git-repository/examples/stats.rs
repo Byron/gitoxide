5052a4e532fec63ccf49f6ce54df41707779e70b
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-03-31 09:06:25 +0800

    also inform about average and max commit sizes (#364)
    This helps to tune caches.
    
    Linux v5.16
    
    - Max commit Size: 74428
    - Avg commit Size: 833

--- git-repository/examples/stats.rs
+++ git-repository/examples/stats.rs
@@ -2,19 +2,35 @@ use git_repository as git;
 use git_repository::Reference;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let repo = git::discover(".")?.apply_environment();
+    let mut repo = git::discover(".")?.apply_environment();
     println!(
         "Repo: {}",
-        repo.work_dir().as_deref().unwrap_or(repo.git_dir()).display()
+        repo.work_dir()
+            .as_deref()
+            .unwrap_or(repo.git_dir())
+            .display()
     );
+    let mut max_commit_size = 0;
+    let mut avg_commit_size = 0;
+    repo.object_cache_size(32 * 1024);
     let commit_ids = repo
         .head()?
         .into_fully_peeled_id()
         .ok_or_else(|| "There are no commits - nothing to do here.")??
         .ancestors()
         .all()
+        .inspect(|id| {
+            if let Ok(Ok(object)) = id.as_ref().map(|id| id.object()) {
+                avg_commit_size += object.data.len();
+                if object.data.len() > max_commit_size {
+                    max_commit_size = object.data.len();
+                }
+            }
+        })
         .collect::<Result<Vec<_>, _>>()?;
     println!("Num Commits: {}", commit_ids.len());
+    println!("Max commit Size: {}", max_commit_size);
+    println!("Avg commit Size: {}", avg_commit_size / commit_ids.len());
     assert!(!commit_ids.is_empty(), "checked that before");
 
     let last_commit_id = &commit_ids[0];
