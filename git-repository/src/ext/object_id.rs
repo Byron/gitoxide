0f5a9439d6b1716345f0e122c23c1a566fdd3088
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-04-05 14:19:04 +0800

    thanks clippy

--- git-repository/src/ext/object_id.rs
+++ git-repository/src/ext/object_id.rs
@@ -3,13 +3,12 @@ use git_traverse::commit::{ancestors, Ancestors};
 
 pub trait Sealed {}
 
+pub type AncestorsIter<Find> = Ancestors<Find, fn(&git_hash::oid) -> bool, ancestors::State>;
+
 /// An extension trait to add functionality to [`ObjectId`]s.
 pub trait ObjectIdExt: Sealed {
     /// Create an iterator over the ancestry of the commits reachable from this id, which must be a commit.
-    fn ancestors<Find, E>(
-        self,
-        find: Find,
-    ) -> Result<Ancestors<Find, fn(&git_hash::oid) -> bool, ancestors::State>, git_traverse::commit::ancestors::Error>
+    fn ancestors<Find, E>(self, find: Find) -> Result<AncestorsIter<Find>, git_traverse::commit::ancestors::Error>
     where
         Find: for<'a> FnMut(&git_hash::oid, &'a mut Vec<u8>) -> Result<git_object::CommitRefIter<'a>, E>,
         E: std::error::Error + Send + Sync + 'static;
@@ -21,10 +20,7 @@ pub trait ObjectIdExt: Sealed {
 impl Sealed for ObjectId {}
 
 impl ObjectIdExt for ObjectId {
-    fn ancestors<Find, E>(
-        self,
-        find: Find,
-    ) -> Result<Ancestors<Find, fn(&git_hash::oid) -> bool, ancestors::State>, git_traverse::commit::ancestors::Error>
+    fn ancestors<Find, E>(self, find: Find) -> Result<AncestorsIter<Find>, git_traverse::commit::ancestors::Error>
     where
         Find: for<'a> FnMut(&git_hash::oid, &'a mut Vec<u8>) -> Result<git_object::CommitRefIter<'a>, E>,
         E: std::error::Error + Send + Sync + 'static,
