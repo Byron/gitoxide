3be3e325b1c538fa2638c4eb2986dc1c2a8476fa
Svetlin Stefanov <s.m.stefanov@gmail.com>
2022-04-09 22:16:55 +0200

    Add doc comment.

--- git-config/src/values.rs
+++ git-config/src/values.rs
@@ -599,6 +599,9 @@ impl Integer {
         self.into()
     }
 
+    /// Canonicalize values as simple decimal numbers.
+    /// An optional suffix of k, m, or g, upon creation, will cause the value to be multiplied by
+    /// 1024, 1048576, or 1073741824 respectively.
     pub fn as_decimal(&self) -> i64 {
         return match self.suffix {
             None => self.value,
