cdf3c3e42433a85e8b47b9dc5558f5c76df3c6ae
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-04-15 14:14:12 +0800

    feat: add `Identity` type (#386)

--- git-sec/src/lib.rs
+++ git-sec/src/lib.rs
@@ -1,1 +1,16 @@
-#![forbid(unsafe_code, rust_2018_idioms)]
+#![forbid(unsafe_code)]
+#![deny(rust_2018_idioms, missing_docs)]
+//! A shared trust model for `gitoxide` crates.
+
+#[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
+#[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
+/// An identity for use when authenticating the transport layer.
+pub enum Identity {
+    /// An account based identity
+    Account {
+        /// The user's name
+        username: String,
+        /// The user's password
+        password: String,
+    },
+}
