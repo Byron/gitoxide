37a607db7c09ab897f306e3bbd4e0ca4e4387bae
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-04-15 16:37:57 +0800

    change!: remove `Identity` in favor of `identity::Account` module; add `identity::UserId` (#386)
    As the fewest consumers will be able to deal with multiple identities,
    remove the enumeration approach in favor of individual type which deal
    with one specific way of identifying a user.

--- git-sec/src/lib.rs
+++ git-sec/src/lib.rs
@@ -2,15 +2,24 @@
 #![deny(rust_2018_idioms, missing_docs)]
 //! A shared trust model for `gitoxide` crates.
 
-#[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
-#[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
-/// An identity for use when authenticating the transport layer.
-pub enum Identity {
+/// Various types to identify entities.
+pub mod identity {
+    /// A unix user id as obtained from the file system.
+    #[cfg(not(windows))]
+    pub type UserId = u32;
+
+    /// A windows [security identifier](https://docs.microsoft.com/en-us/windows/security/identity-protection/access-control/security-identifiers)
+    /// in its stringified form.
+    #[cfg(windows)]
+    pub type UserId = String;
+
+    #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
+    #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
     /// An account based identity
-    Account {
+    pub struct Account {
         /// The user's name
-        username: String,
+        pub username: String,
         /// The user's password
-        password: String,
-    },
+        pub password: String,
+    }
 }
