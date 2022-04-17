f00f4a4a3a9149bf5cf925e931a8105aeb9b9db9
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-04-17 20:39:43 +0800

    change!: simplify `Permission` type radically `(#386)

--- git-sec/src/lib.rs
+++ git-sec/src/lib.rs
@@ -5,7 +5,8 @@ use std::marker::PhantomData;
 use std::ops::Deref;
 
 /// A way to specify how 'safe' we feel about a resource, typically about a git repository.
-#[derive(Copy, Clone, Ord, PartialOrd, PartialEq, Eq, Debug)]
+#[derive(Copy, Clone, Ord, PartialOrd, PartialEq, Eq, Debug, Hash)]
+#[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
 pub enum Trust {
     /// Caution is warranted when using the resource.
     Reduced,
@@ -112,6 +113,18 @@ pub mod permission {
     }
 }
 
+/// Allow, deny or forbid using a resource or performing an action.
+#[derive(Debug, Copy, Clone, PartialOrd, PartialEq, Ord, Eq, Hash)]
+#[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
+pub enum Permission {
+    /// Fail outright when trying to load a resource or performing an action.
+    Forbid,
+    /// Ignore resources or try to avoid performing an operation.
+    Deny,
+    /// Allow loading a reasource or performing an action.
+    Allow,
+}
+
 bitflags::bitflags! {
     /// Whether something can be read or written.
     #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
@@ -136,41 +149,6 @@ impl<T: permission::Tag, P> Deref for Access<T, P> {
     fn deref(&self) -> &Self::Target {
         &self.permission
     }
-}
-
-// TODO: this probably belongs to `git-config` and can be simplified then as AllowIfOwned and Deny can probably be merged.
-/// Permissions related to resources at _locations_, like configuration files, executables or destinations for operations.
-///
-/// Note that typically the permission refers to the place where the _location_ is configured, not to the _location_ itself.
-/// For example, we may trust an owned configuration file, and by relation all the _locations_ inside of it even though
-/// these are not owned by us. The exact place where a permission applies is identified.
-pub enum Permission {
-    /// The greatest permission level without any restrictions, all _locations_ are permitted.
-    ///
-    /// For _locations_ to executables, it can be found in the `PATH` or configured from any git config file.
-    ///
-    /// Note that, however, some executables still won't be picked up from repository-local configuration
-    /// for safety reasons.
-    Allow,
-    /// For _locations_ to executables, only run these if these have been configured by git config files
-    /// that are owned by the user executing the application, or if these are in the `PATH`.
-    /// Resources or write destinations adhere to the same rules.
-    AllowIfOwned {
-        /// If true, if a _location_ is not under user control, instead of failing, fallback to a configuration setting that
-        /// is or try to not fail by using suitable defaults. For executables this may mean to search for them in the `PATH`
-        /// or fall back to another configuration value from configuration files under user control.
-        allow_fallback: bool,
-    },
-    /// Do not use any _location_ unless it's required for git to function by using defaults.
-    ///
-    /// If such a resource is encountered, the operation may fail.
-    Deny {
-        /// If true, operations that would fail may proceed by ignoring the resource if possible or using
-        /// defaults that are deemed safe.
-        ///
-        /// For executables this means using those in the `PATH` only.
-        allow_fallback: bool,
-    },
 }
 
 /// Various types to identify entities.
