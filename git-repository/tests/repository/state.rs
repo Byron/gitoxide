6540869ec1a1492e3338ff2d33074be33890ee8f
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-04-18 20:00:11 +0800

    refactor (#393)

--- git-repository/tests/repository/state.rs
+++ git-repository/tests/repository/state.rs
@@ -5,10 +5,7 @@ use git_repository as git;
 fn apply_mailbox() -> Result {
     let repo = named_repo("make_am_repo.sh")?;
 
-    let head = repo.head()?;
-    let head_name = head.referent_name().expect("no detached head").shorten();
-    assert_eq!(head_name, "main");
-
+    assert_eq!(repo.head_name()?.unwrap().shorten(), "main");
     assert_eq!(repo.in_progress_operation(), Some(git::state::InProgress::ApplyMailbox));
     Ok(())
 }
@@ -17,10 +14,7 @@ fn apply_mailbox() -> Result {
 fn bisect() -> Result {
     let repo = named_repo("make_bisect_repo.sh")?;
 
-    let head = repo.head()?;
-    let head_name = head.referent_name().expect("no detached head").shorten();
-    assert_eq!(head_name, "main");
-
+    assert_eq!(repo.head_name()?.unwrap().shorten(), "main");
     assert_eq!(repo.in_progress_operation(), Some(git::state::InProgress::Bisect));
 
     Ok(())
@@ -30,10 +24,7 @@ fn bisect() -> Result {
 fn cherry_pick() -> Result {
     let repo = named_repo("make_cherry_pick_repo.sh")?;
 
-    let head = repo.head()?;
-    let head_name = head.referent_name().expect("no detached head").shorten();
-    assert_eq!(head_name, "main");
-
+    assert_eq!(repo.head_name()?.unwrap().shorten(), "main");
     assert_eq!(repo.in_progress_operation(), Some(git::state::InProgress::CherryPick));
     Ok(())
 }
@@ -42,10 +33,7 @@ fn cherry_pick() -> Result {
 fn cherry_pick_sequence() -> Result {
     let repo = named_repo("make_cherry_pick_sequence_repo.sh")?;
 
-    let head = repo.head()?;
-    let head_name = head.referent_name().expect("no detached head").shorten();
-    assert_eq!(head_name, "main");
-
+    assert_eq!(repo.head_name()?.unwrap().shorten(), "main");
     assert_eq!(
         repo.in_progress_operation(),
         Some(git::state::InProgress::CherryPickSequence)
@@ -58,10 +46,7 @@ fn cherry_pick_sequence() -> Result {
 fn merge() -> Result {
     let repo = named_repo("make_merge_repo.sh")?;
 
-    let head = repo.head()?;
-    let head_name = head.referent_name().expect("no detached head").shorten();
-    assert_eq!(head_name, "main");
-
+    assert_eq!(repo.head_name()?.unwrap().shorten(), "main");
     assert_eq!(repo.in_progress_operation(), Some(git::state::InProgress::Merge));
 
     Ok(())
@@ -71,8 +56,7 @@ fn merge() -> Result {
 fn rebase_interactive() -> Result {
     let repo = named_repo("make_rebase_i_repo.sh")?;
 
-    let head = repo.head()?;
-    assert!(head.is_detached());
+    assert!(repo.head()?.is_detached());
     assert_eq!(
         repo.in_progress_operation(),
         Some(git::state::InProgress::RebaseInteractive)
@@ -85,10 +69,7 @@ fn rebase_interactive() -> Result {
 fn revert() -> Result {
     let repo = named_repo("make_revert_repo.sh")?;
 
-    let head = repo.head()?;
-    let head_name = head.referent_name().expect("no detached head").shorten();
-    assert_eq!(head_name, "main");
-
+    assert_eq!(repo.head_name()?.unwrap().shorten(), "main");
     assert_eq!(repo.in_progress_operation(), Some(git::state::InProgress::Revert));
 
     Ok(())
@@ -98,10 +79,7 @@ fn revert() -> Result {
 fn revert_sequence() -> Result {
     let repo = named_repo("make_revert_sequence_repo.sh")?;
 
-    let head = repo.head()?;
-    let head_name = head.referent_name().expect("no detached head").shorten();
-    assert_eq!(head_name, "main");
-
+    assert_eq!(repo.head_name()?.unwrap().shorten(), "main");
     assert_eq!(
         repo.in_progress_operation(),
         Some(git::state::InProgress::RevertSequence)
