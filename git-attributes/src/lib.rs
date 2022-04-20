388a8cd55a31d309f5c683645fc18ace6ddf4af3
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-04-20 17:18:53 +0800

    A sketch of something that shouldn't be: a Description to instantiate patterns (#301)
    Instead, rather make it a matcher who helps with matching in various
    match groups.

--- git-attributes/src/lib.rs
+++ git-attributes/src/lib.rs
@@ -1,7 +1,8 @@
 #![forbid(unsafe_code)]
 #![deny(rust_2018_idioms)]
 
-use bstr::BStr;
+use bstr::{BStr, BString};
+use std::path::PathBuf;
 
 #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
@@ -17,6 +18,48 @@ pub enum State<'a> {
     /// The attribute isn't mentioned with a given path or is explicitly set to `Unspecified` using the `!` sign.
     Unspecified,
 }
+
+/// A way to describe the combination of files of a given kind one wants to instantiate, with support for additional
+/// overrides as passed by a command-line.
+pub struct Description<T: description::Kind> {
+    /// Paths to files whose patterns don't have a root directory, thus apply as if the file was at the root of the repository.
+    pub global_files: Vec<PathBuf>,
+    /// Paths to files whose patterns apply only to the directory they themselves are in.
+    /// They have higher priority than `global_files`.
+    pub per_directory_files: Vec<PathBuf>,
+    /// Additional pattern/data combinations that should have the highest priority and apply from the root of the repository.
+    /// These are unparsed as of yet and will be parsed later when preparing the files.
+    pub global_overrides: Vec<(BString, T::Value)>,
+}
+
+///
+pub mod description {
+    use crate::Description;
+
+    /// A marker trait to identify the type of a description.
+    pub trait Kind {
+        /// The value associated with a pattern.
+        type Value;
+    }
+
+    /// Identify ignore patterns.
+    pub struct Ignore;
+    impl Kind for Ignore {
+        type Value = ();
+    }
+
+    /// Identify patterns with attributes.
+    pub struct Attributes;
+    impl Kind for Attributes {
+        /// TODO: identify the actual value, should be name/State pairs, but there is the question of storage.
+        type Value = ();
+    }
+
+    impl Description<Ignore> {}
+}
+
+pub type Files = Description<description::Attributes>;
+pub type IgnoreFiles = Description<description::Ignore>;
 
 pub mod parse;
 
