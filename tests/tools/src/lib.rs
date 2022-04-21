eea3988462a61e8a64d646a15d062d13fdbfb615
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-04-21 11:04:42 +0800

    set the time to wait for lock to longest expected runtime of fixture scripts
    That way, we don't get timeouts unnecessarily.

--- tests/tools/src/lib.rs
+++ tests/tools/src/lib.rs
@@ -124,7 +124,7 @@ pub fn scripted_fixture_repo_read_only_with_args(
 
     let _marker = git_lock::Marker::acquire_to_hold_resource(
         script_basename,
-        git_lock::acquire::Fail::AfterDurationWithBackoff(Duration::from_secs(5)),
+        git_lock::acquire::Fail::AfterDurationWithBackoff(Duration::from_secs(if cfg!(windows) { 3 * 60 } else { 30 })),
         None,
     )?;
     let failure_marker = script_result_directory.join("_invalid_state_due_to_script_failure_");
