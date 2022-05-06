e1dbf85999f2b6a56467419687319b79b65899a9
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-05-06 16:15:59 +0800

    adjust to changes in git-repository (#301)

--- gitoxide-core/src/organize.rs
+++ gitoxide-core/src/organize.rs
@@ -3,6 +3,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use crate::pack::receive::git;
 use git_config::file::GitConfig;
 use git_repository::{objs::bstr::ByteSlice, progress, Progress};
 
@@ -40,7 +41,7 @@ where
 
         if path.is_dir() {
             if path.join("HEAD").is_file() && path.join("config").is_file() {
-                git_repository::path::is::git(path).ok()
+                git::discover::is_git(path).ok()
             } else {
                 None
             }
@@ -50,7 +51,7 @@ where
         }
     }
     fn into_workdir(git_dir: PathBuf) -> PathBuf {
-        if git_repository::path::is::bare(&git_dir) {
+        if git::discover::is_bare(&git_dir) {
             git_dir
         } else {
             git_dir.parent().expect("git is never in the root").to_owned()
--- gitoxide-core/src/repository/mod.rs
+++ gitoxide-core/src/repository/mod.rs
@@ -1,8 +1,9 @@
 use std::path::PathBuf;
 
 use anyhow::{Context as AnyhowContext, Result};
+use git_repository as git;
 
-pub fn init(directory: Option<PathBuf>) -> Result<git_repository::Path> {
+pub fn init(directory: Option<PathBuf>) -> Result<git::discover::repository::Path> {
     git_repository::path::create::into(directory.unwrap_or_default(), git_repository::Kind::WorkTree)
         .with_context(|| "Repository initialization failed")
 }
