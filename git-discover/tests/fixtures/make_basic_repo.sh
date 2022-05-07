a4cec4ab8ba80a8f439b283d369d48cdafbe7b74
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-05-07 21:38:12 +0800

    prepare test to provide worktrees of different kinds as well (#301)

--- git-discover/tests/fixtures/make_basic_repo.sh
+++ git-discover/tests/fixtures/make_basic_repo.sh
@@ -12,4 +12,14 @@ git commit -q -am c2
 
 mkdir -p some/very/deeply/nested/subdir
 
-git init --bare bare.git
+git clone --bare --shared . bare.git
+
+git worktree add worktrees/a
+git worktree add worktrees/b-deleted
+
+rm -Rf worktrees/b-deleted
+
+cd bare.git
+git worktree add ../worktrees/from-bare/c
+git worktree add ../worktrees/from-bare/d-deleted
+rm -Rf -v ../worktrees/from-bare/d-deleted
