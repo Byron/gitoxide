a25b0bccfa001273057089fecca637c2d1fc591a
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-05-13 16:32:34 +0800

    first draft of fixture  setup for worktrees (#301)

---
+++ git-ref/tests/fixtures/make_worktree_repo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -eu -o pipefail
+
+mkdir repo && cd repo
+git init -q
+
+git checkout -q -b main
+git commit -q --allow-empty -m c1
+git commit -q --allow-empty -m c2
+git branch dt1
+git branch d1
+
+mkdir -p .git/refs/remotes/origin
+
+cp .git/refs/heads/main .git/refs/remotes/origin/
+cp .git/refs/heads/main .git/refs/d1
+
+echo "ref: refs/remotes/origin/main" > .git/refs/remotes/origin/HEAD
+echo "notahexsha" > .git/refs/broken
+
+echo "ref: refs/heads/multi-link-target1" > .git/refs/multi-link
+echo "ref: refs/tags/multi-link-target2" > .git/refs/heads/multi-link-target1
+echo "ref: refs/remotes/origin/multi-link-target3" > .git/refs/tags/multi-link-target2
+git rev-parse HEAD > .git/refs/remotes/origin/multi-link-target3
+
+
+echo "ref: refs/loop-b" > .git/refs/loop-a
+echo "ref: refs/loop-a" > .git/refs/loop-b
+
+git tag t1
+git tag -m "tag object" dt1
+
+git worktree add ../w1 HEAD~1
+git bisect start && git bisect bad HEAD
+
+(
+  cd ../w1
+  git bisect start
+  git bisect bad main-worktree/HEAD
+)
+
+if [ "${1:-}"  = "packed" ]; then
+  git pack-refs --all --prune
+fi
