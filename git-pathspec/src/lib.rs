c04d4be29c7a0cf72500d30432215513f6066338
Sidney Douw <s.douw@gmx.de>
2022-05-20 14:23:05 +0200

    added some documentation (#415)

--- git-pathspec/src/lib.rs
+++ git-pathspec/src/lib.rs
@@ -5,6 +5,7 @@ use bstr::BString;
 
 pub mod parse;
 
+/// The output of a pathspec parsing operaion. It can be used to matched against a path.
 #[derive(PartialEq, Eq, Debug, Hash, Ord, PartialOrd, Clone)]
 pub struct Pattern {
     pub path: BString,
@@ -13,15 +14,22 @@ pub struct Pattern {
 
 bitflags! {
     pub struct MagicSignature: u32 {
+        /// Matches patterns from the root of the repository
         const TOP = 1 << 0;
+        /// Special characters in the pattern, like '*' or '?', are treated literally
         const LITERAL = 1 << 1;
+        /// Matches patterns in case insensitive mode
         const ICASE = 1 << 2;
+        /// A single '*' will not match a '/' in the pattern, but a '**' will
         const GLOB = 1 << 3;
+        /// Specifies a list of attribute requirements that the matches should meet
         const ATTR = 1 << 4;
+        /// Excludes the matching patterns from the previous results
         const EXCLUDE = 1 << 5;
     }
 }
 
+/// Parse a git-style pathspec into a `Pattern`
 pub fn parse(input: &[u8]) -> Result<Pattern, parse::Error> {
     Pattern::from_bytes(input)
 }
