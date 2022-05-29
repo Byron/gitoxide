26680c48951a82d5119f54c57b4e7045d2c20649
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-05-29 10:31:38 +0800

    Finalize onbranch tests; remove mixed ones in favor of specific cases

--- git-config/tests/file/from_paths/includes/conditional/onbranch.rs
+++ git-config/tests/file/from_paths/includes/conditional/onbranch.rs
@@ -134,6 +134,41 @@ fn patterns_ending_with_slash_match_subdirectories_recursively() {
 }
 
 #[test]
+fn simple_glob_patterns() {
+    assert_section_value(Options {
+        condition: "prefix*",
+        branch_name: "refs/heads/prefixsuffix",
+        expect: Value::OverrideByInclude,
+    });
+    assert_section_value_msg(
+        Options {
+            condition: "prefix*",
+            branch_name: "refs/heads/prefix/suffix",
+            expect: Value::Base,
+        },
+        "single-stars do not cross component boundaries".into(),
+    );
+    assert_section_value(Options {
+        condition: "*suffix",
+        branch_name: "refs/heads/prefixsuffix",
+        expect: Value::OverrideByInclude,
+    });
+    assert_section_value(Options {
+        condition: "*/suffix",
+        branch_name: "refs/heads/prefix/suffix",
+        expect: Value::OverrideByInclude,
+    });
+    assert_section_value_msg(
+        Options {
+            condition: "*suffix",
+            branch_name: "refs/heads/prefix/suffix",
+            expect: Value::Base,
+        },
+        "single-stars do not cross component boundaries".into(),
+    );
+}
+
+#[test]
 fn simple_globs_do_not_cross_component_boundary() {
     assert_section_value(Options {
         condition: "feature/*/start",
@@ -151,102 +186,10 @@ fn simple_globs_do_not_cross_component_boundary() {
 }
 
 #[test]
-fn mixed() {
-    let dir = tempdir().unwrap();
-    let config_path = dir.path().join("a");
-    let hierarchy_branch_path = dir.path().join("hierarchy_branch");
-    let branch_path = dir.path().join("branch");
-    let another_branch_path = dir.path().join("another branch");
-    fs::write(
-        branch_path.as_path(),
-        "
-[core]
-x = branch-override",
-    )
-    .unwrap();
-
-    fs::write(
-        another_branch_path.as_path(),
-        "
-[core]
-z = another-branch-override",
-    )
-    .unwrap();
-
-    fs::write(
-        hierarchy_branch_path.as_path(),
-        "
-[core]
-y = hierarchy-branch-override",
-    )
-    .unwrap();
-
-    fs::write(
-        config_path.as_path(),
-        format!(
-            r#"
-[core]
-x = 1
-y = 1
-z = 1
-[includeIf "onbranch:foo*"]
-path = {}
-[includeIf "onbranch:br/"]
-path = {}
-[includeIf "onbranch:foo*"]
-path = {}"#,
-            escape_backslashes(&branch_path),
-            escape_backslashes(&hierarchy_branch_path),
-            escape_backslashes(&another_branch_path),
-        ),
-    )
-    .unwrap();
-
-    {
-        let branch_name = FullName::try_from(BString::from("refs/heads/foobar")).unwrap();
-        let branch_name = branch_name.as_ref();
-        let options = from_paths::Options {
-            branch_name: Some(branch_name),
-            ..Default::default()
-        };
-
-        let config = git_config::File::from_paths(Some(&config_path), options).unwrap();
-        assert_eq!(
-            config.string("core", None, "x"),
-            Some(cow_str("branch-override")),
-            "branch name match"
-        );
-    }
-
-    {
-        let branch_name = FullName::try_from(BString::from("refs/heads/foo/bar")).unwrap();
-        let branch_name = branch_name.as_ref();
-        let options = from_paths::Options {
-            branch_name: Some(branch_name),
-            ..Default::default()
-        };
-
-        let config = git_config::File::from_paths(Some(&config_path), options).unwrap();
-        assert_eq!(
-            config.string("core", None, "z"),
-            Some(cow_str("1")),
-            "branch name match"
-        );
-    }
-
-    {
-        let branch_name = FullName::try_from(BString::from("refs/heads/br/one")).unwrap();
-        let branch_name = branch_name.as_ref();
-        let options = from_paths::Options {
-            branch_name: Some(branch_name),
-            ..Default::default()
-        };
-
-        let config = git_config::File::from_paths(Some(&config_path), options).unwrap();
-        assert_eq!(
-            config.string("core", None, "y"),
-            Some(cow_str("hierarchy-branch-override")),
-            "hierarchy branch name match"
-        );
-    }
+fn double_star_globs_cross_component_boundaries() {
+    assert_section_value(Options {
+        condition: "feature/**/start",
+        branch_name: "refs/heads/feature/a/b/start",
+        expect: Value::OverrideByInclude,
+    });
 }
