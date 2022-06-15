5a3f6f5ab1ec9f89967deeac7c521ecf72bec84f
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-06-15 13:32:56 +0800

    record a git baseline and prepare for reading it (#427)

---
+++ git-repository/tests/fixtures/make_rev_spec_parse_repos.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -eu -o pipefail
+
+ROOT="$PWD"
+
+function baseline() {
+  local spec=${1:?first argument is the spec to test}
+  {
+    echo "$spec"
+    git rev-parse "$spec" &>/dev/null && echo 0 || echo $?
+  }>> "$ROOT/baseline.git"
+}
+
+# based on https://github.com/git/git/blob/8168d5e9c23ed44ae3d604f392320d66556453c9/t/t1512-rev-parse-disambiguation.sh#L38
+git init --bare blob.prefix
+(
+  cd blob.prefix
+  # Both start with "dead..", under both SHA-1 and SHA-256
+  echo brocdnra | git hash-object -w --stdin
+  echo brigddsv | git hash-object -w --stdin
+  # Both start with "beef.."
+  echo 1agllotbh | git hash-object -w --stdin
+  echo 1bbfctrkc | git hash-object -w --stdin
+
+  baseline "dead"
+  baseline "beef"
+)
+
+
+git init --bare blob.bad
+(
+  cd blob.bad
+  # Both have the prefix "bad0"
+  echo xyzfaowcoh | git hash-object -t bad -w --stdin --literally
+  echo xyzhjpyvwl | git hash-object -t bad -w --stdin --literally
+  baseline "bad0"
+
+  echo 1bbfctrkc | git hash-object -t bad -w --stdin --literally
+  baseline "e328"
+)
+
+
+
--- git-repository/tests/rev_spec/mod.rs
+++ git-repository/tests/rev_spec/mod.rs
@@ -1,26 +1,55 @@
 mod from_bytes {
-    mod ambiguous {
-        use git_repository as git;
-        use git_repository::RevSpec;
+    use git_repository as git;
+    use git_repository::RevSpec;
 
-        fn repo(name: &str) -> crate::Result<git::Repository> {
-            let repo_path = git_testtools::scripted_fixture_repo_read_only("make_rev_parse_disambiguation_repo.sh")?;
-            Ok(git::open(repo_path.join(name))?)
-        }
+    fn parse_spec<'a>(spec: &str, repo: &'a git::Repository) -> Result<RevSpec<'a>, git::rev_spec::parse::Error> {
+        RevSpec::from_bstr(spec, &repo)
+    }
+
+    fn repo(name: &str) -> crate::Result<git::Repository> {
+        let repo_path = git_testtools::scripted_fixture_repo_read_only("make_rev_spec_parse_repos.sh")?;
+        Ok(git::open(repo_path.join(name))?)
+    }
+
+    mod ambiguous {
+        use super::repo;
+        use crate::rev_spec::from_bytes::parse_spec;
 
         #[test]
         fn prefix() {
-            let repo = repo("blob.prefix").unwrap();
-            assert_eq!(
-                RevSpec::from_bstr("dead", &repo).unwrap_err().to_string(),
-                "Found more than one object prefixed with dead\nThe ref partially named 'dead' could not be found"
-            );
-            assert_eq!(
-                RevSpec::from_bstr("beef", &repo).unwrap_err().to_string(),
-                "Found more than one object prefixed with beef\nThe ref partially named 'beef' could not be found"
-            );
+            {
+                let repo = repo("blob.prefix").unwrap();
+                assert_eq!(
+                    parse_spec("dead", &repo).unwrap_err().to_string(),
+                    "Found more than one object prefixed with dead\nThe ref partially named 'dead' could not be found"
+                );
+                assert_eq!(
+                    parse_spec("beef", &repo).unwrap_err().to_string(),
+                    "Found more than one object prefixed with beef\nThe ref partially named 'beef' could not be found"
+                );
+            }
+
+            {
+                let repo = repo("blob.bad").unwrap();
+                assert_eq!(
+                    parse_spec("bad0", &repo).unwrap_err().to_string(),
+                    "Found more than one object prefixed with bad0\nThe ref partially named 'bad0' could not be found",
+                    "git is able to also detect that the object has an invalid type, but we are not because the type doesn't matter here"
+                );
+            }
         }
     }
+
+    #[test]
+    #[ignore]
+    fn bad_objects_are_valid_as_they_are_not_queried() {
+        let repo = repo("blob.bad").unwrap();
+        assert_eq!(
+            parse_spec("e328", &repo).unwrap_err().to_string(),
+            "we are able to return objects even though they are 'bad' when trying to decode them, like git",
+        );
+    }
+
     #[test]
     #[ignore]
     fn find_ref() {}
