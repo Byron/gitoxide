2c8c6e53fd4681289c9fa2308735c779ed4eace5
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-06-20 10:51:26 +0800

    refactor
    Isolate utility methods to know what's actually used.

--- git-config/tests/file/from_paths/includes/conditional/gitdir.rs
+++ git-config/tests/file/from_paths/includes/conditional/gitdir.rs
@@ -1,142 +1,7 @@
-use std::{
-    fs,
-    path::{Path, PathBuf},
-    process::Command,
-};
-
-use bstr::BString;
-use dirs::home_dir;
-use git_config::File;
 use serial_test::serial;
 use tempfile::{tempdir, tempdir_in};
 
 use crate::file::from_paths::includes::conditional::create_symlink;
-use crate::file::{
-    cow_str,
-    from_paths::{escape_backslashes, includes::conditional::options_with_git_dir},
-};
-
-fn assert_section_value(
-    Options {
-        condition,
-        expected,
-        env,
-        config_location,
-    }: Options,
-) {
-    write_config(condition, &env, config_location);
-    git_assert_eq(expected, &env);
-
-    let mut paths = vec![env.git_dir().join("config")];
-    if config_location == ConfigLocation::User {
-        paths.push(env.home_dir().join(".gitconfig"));
-    }
-
-    let config = File::from_paths(paths, options_with_git_dir(&env.git_dir())).unwrap();
-
-    assert_eq!(
-        config.string("section", None, "value"),
-        Some(cow_str(match expected {
-            Value::Original => "base-value",
-            Value::Override => "override-value",
-        })),
-        "git-config disagrees with the expected value",
-    );
-}
-
-fn git_dir(root_dir: &Path, git_dir_suffix: &str) -> PathBuf {
-    let git_dir = root_dir.join(git_dir_suffix).join(".git");
-    fs::create_dir_all(&git_dir).unwrap();
-    git_dir
-}
-
-fn temporary_dir_for_git(subdir_name: impl AsRef<Path>) -> (tempfile::TempDir, PathBuf) {
-    let tmp = tempfile::tempdir().unwrap();
-    let git_dir = tmp.path().join(subdir_name).join(".git");
-    fs::create_dir_all(&git_dir).unwrap();
-    (tmp, git_dir)
-}
-
-fn write_config(condition: &str, env: &EnvOverwrite, overwrite_config_location: ConfigLocation) {
-    let override_config_dir_file = write_override_config(env.repo_dir());
-    write_main_config(condition, override_config_dir_file, env, overwrite_config_location);
-}
-
-fn write_override_config(root_path: &Path) -> PathBuf {
-    let include_path = root_path.join("include.path");
-    fs::create_dir_all(root_path).unwrap();
-    fs::write(
-        include_path.as_path(),
-        "
-[section]
-  value = override-value",
-    )
-    .unwrap();
-    include_path
-}
-
-fn git_assert_eq(expected: Value, env: &EnvOverwrite) {
-    let output = Command::new("git")
-        .args(["config", "--get", "section.value"])
-        .env("HOME", env.home_dir())
-        .env("GIT_DIR", env.git_dir())
-        .current_dir(env.git_dir())
-        .output()
-        .unwrap();
-
-    assert_eq!(output.stderr.len(), 0);
-    let git_output = BString::from(output.stdout.strip_suffix(b"\n").unwrap());
-    assert_eq!(
-        git_output,
-        match expected {
-            Value::Original => "base-value",
-            Value::Override => "override-value",
-        },
-        "git assert equals"
-    );
-}
-
-fn write_main_config(
-    condition: &str,
-    override_config_dir_file: PathBuf,
-    env: &EnvOverwrite,
-    overwrite_config_location: ConfigLocation,
-) {
-    let output = Command::new("git")
-        .args(["init", env.repo_dir().to_str().unwrap()])
-        .output()
-        .unwrap();
-    assert!(output.status.success(), "git init failed: {:?}", output);
-
-    if overwrite_config_location == ConfigLocation::Repo {
-        let output = Command::new("git")
-            .args(["config", "section.value", "base-value"])
-            .env("GIT_DIR", env.git_dir())
-            .output()
-            .unwrap();
-        assert!(output.status.success(), "git config set value failed: {:?}", output);
-    }
-
-    let output = Command::new("git")
-        .args([
-            "config",
-            match overwrite_config_location {
-                ConfigLocation::User => "--global",
-                ConfigLocation::Repo => "--local",
-            },
-            &format!("includeIf.{}.path", condition),
-            &escape_backslashes(override_config_dir_file.as_path()),
-        ])
-        .current_dir(env.git_dir())
-        .env("HOME", env.home_dir())
-        .output()
-        .unwrap();
-    assert!(output.status.success(), "git config set value failed: {:?}", output);
-}
-
-fn home() -> PathBuf {
-    home_dir().unwrap()
-}
 
 #[test]
 #[serial]
@@ -334,10 +199,15 @@ fn dot_path_matching_symlink_with_icase() {
     });
 }
 
-mod types {
-    use crate::file::from_paths::includes::conditional::gitdir::home;
+mod util {
+    use crate::file::cow_str;
+    use crate::file::from_paths::escape_backslashes;
+    use crate::file::from_paths::includes::conditional::options_with_git_dir;
+    use bstr::BString;
     use dirs::home_dir;
+    use std::fs;
     use std::path::{Path, PathBuf};
+    use std::process::Command;
 
     pub struct EnvOverwrite {
         git_dir: PathBuf,
@@ -408,5 +278,129 @@ mod types {
             }
         }
     }
+
+    pub fn assert_section_value(
+        Options {
+            condition,
+            expected,
+            env,
+            config_location,
+        }: Options,
+    ) {
+        write_config(condition, &env, config_location);
+        git_assert_eq(expected, &env);
+
+        let mut paths = vec![env.git_dir().join("config")];
+        if config_location == ConfigLocation::User {
+            paths.push(env.home_dir().join(".gitconfig"));
+        }
+
+        let config = git_config::File::from_paths(paths, options_with_git_dir(&env.git_dir())).unwrap();
+
+        assert_eq!(
+            config.string("section", None, "value"),
+            Some(cow_str(match expected {
+                Value::Original => "base-value",
+                Value::Override => "override-value",
+            })),
+            "git-config disagrees with the expected value",
+        );
+    }
+
+    pub fn git_dir(root_dir: &Path, git_dir_suffix: &str) -> PathBuf {
+        let git_dir = root_dir.join(git_dir_suffix).join(".git");
+        std::fs::create_dir_all(&git_dir).unwrap();
+        git_dir
+    }
+
+    pub fn temporary_dir_for_git(subdir_name: impl AsRef<Path>) -> (tempfile::TempDir, PathBuf) {
+        let tmp = tempfile::tempdir().unwrap();
+        let git_dir = tmp.path().join(subdir_name).join(".git");
+        std::fs::create_dir_all(&git_dir).unwrap();
+        (tmp, git_dir)
+    }
+
+    fn write_config(condition: &str, env: &EnvOverwrite, overwrite_config_location: ConfigLocation) {
+        let override_config_dir_file = write_override_config(env.repo_dir());
+        write_main_config(condition, override_config_dir_file, env, overwrite_config_location);
+    }
+
+    fn write_override_config(root_path: &Path) -> PathBuf {
+        let include_path = root_path.join("include.path");
+        fs::create_dir_all(root_path).unwrap();
+        fs::write(
+            include_path.as_path(),
+            "
+[section]
+  value = override-value",
+        )
+        .unwrap();
+        include_path
+    }
+
+    fn git_assert_eq(expected: Value, env: &EnvOverwrite) {
+        let output = Command::new("git")
+            .args(["config", "--get", "section.value"])
+            .env("HOME", env.home_dir())
+            .env("GIT_DIR", env.git_dir())
+            .current_dir(env.git_dir())
+            .output()
+            .unwrap();
+
+        assert_eq!(output.stderr.len(), 0);
+        let git_output = BString::from(output.stdout.strip_suffix(b"\n").unwrap());
+        assert_eq!(
+            git_output,
+            match expected {
+                Value::Original => "base-value",
+                Value::Override => "override-value",
+            },
+            "git assert equals"
+        );
+    }
+
+    fn write_main_config(
+        condition: &str,
+        override_config_dir_file: PathBuf,
+        env: &EnvOverwrite,
+        overwrite_config_location: ConfigLocation,
+    ) {
+        let output = Command::new("git")
+            .args(["init", env.repo_dir().to_str().unwrap()])
+            .output()
+            .unwrap();
+        assert!(output.status.success(), "git init failed: {:?}", output);
+
+        if overwrite_config_location == ConfigLocation::Repo {
+            let output = Command::new("git")
+                .args(["config", "section.value", "base-value"])
+                .env("GIT_DIR", env.git_dir())
+                .output()
+                .unwrap();
+            assert!(output.status.success(), "git config set value failed: {:?}", output);
+        }
+
+        let output = Command::new("git")
+            .args([
+                "config",
+                match overwrite_config_location {
+                    ConfigLocation::User => "--global",
+                    ConfigLocation::Repo => "--local",
+                },
+                &format!("includeIf.{}.path", condition),
+                &escape_backslashes(override_config_dir_file.as_path()),
+            ])
+            .current_dir(env.git_dir())
+            .env("HOME", env.home_dir())
+            .output()
+            .unwrap();
+        assert!(output.status.success(), "git config set value failed: {:?}", output);
+    }
+
+    pub fn home() -> PathBuf {
+        home_dir().unwrap()
+    }
 }
-pub use types::{ConfigLocation, EnvOverwrite, Options, Value};
+pub use util::{
+    assert_section_value, git_dir, home, temporary_dir_for_git, ConfigLocation, EnvOverwrite, Options, Value,
+};
