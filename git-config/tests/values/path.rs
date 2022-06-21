62e5396ac9221f13437c87f06715c98989981785
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-06-21 22:15:49 +0800

    refactor
    Make clearer what is tested and make some tests more concise.

--- git-config/tests/values/path.rs
+++ git-config/tests/values/path.rs
@@ -1,109 +1,117 @@
-use std::borrow::Cow;
+mod interpolate {
+    use std::borrow::Cow;
+    use std::path::Path;
 
-use git_config::values::{path::interpolate, Path};
+    use git_config::values::Path as InterpolatingPath;
 
-use crate::values::b;
+    use crate::values::b;
+    use git_config::values::path::interpolate::Error;
 
-#[test]
-fn backslash_is_not_special() {
-    let path = &b"C:\\foo\\bar"[..];
-    let actual = Path::from(Cow::Borrowed(path));
-    assert_eq!(&*actual, path);
-    assert!(
-        matches!(&actual.value, Cow::Borrowed(_)),
-        "it does not unnecessarily copy values"
-    );
-}
-
-#[test]
-fn no_interpolation_for_paths_without_tilde_or_prefix() {
-    let path = &b"/foo/bar"[..];
-    let actual = Path::from(Cow::Borrowed(path));
-    assert_eq!(&*actual, path);
-    assert!(
-        matches!(&actual.value, Cow::Borrowed(_)),
-        "it does not unnecessarily copy values"
-    );
-}
-
-#[test]
-fn empty_path_is_error() {
-    assert!(matches!(
-        Path::from(Cow::Borrowed(b(""))).interpolate(None, None),
-        Err(interpolate::Error::Missing { what: "path" })
-    ));
-}
-
-#[test]
-fn prefix_substitutes_git_install_dir() {
-    for git_install_dir in &["/tmp/git", "C:\\git"] {
-        for (val, expected) in &[
-            (&b"%(prefix)/foo/bar"[..], "foo/bar"),
-            (b"%(prefix)/foo\\bar", "foo\\bar"),
-        ] {
-            let expected =
-                &std::path::PathBuf::from(format!("{}{}{}", git_install_dir, std::path::MAIN_SEPARATOR, expected));
-            assert_eq!(
-                &*Path::from(Cow::Borrowed(*val))
-                    .interpolate(std::path::Path::new(git_install_dir).into(), None)
-                    .unwrap(),
-                expected,
-                "prefix interpolation keeps separators as they are"
+    #[test]
+    fn backslash_is_not_special_and_they_are_not_escaping_anything() -> crate::Result {
+        for path in ["C:\\foo\\bar", "/foo/bar"] {
+            let actual = InterpolatingPath::from(Cow::Borrowed(b(path))).interpolate(None, None)?;
+            assert_eq!(actual, Path::new(path));
+            assert!(
+                matches!(actual, Cow::Borrowed(_)),
+                "it does not unnecessarily copy values"
             );
         }
+        Ok(())
     }
-}
 
-#[test]
-fn prefix_substitution_skipped_with_dot_slash() {
-    let path = "./%(prefix)/foo/bar";
-    let git_install_dir = "/tmp/git";
-    assert_eq!(
-        Path::from(Cow::Borrowed(b(path)))
-            .interpolate(std::path::Path::new(git_install_dir).into(), None)
-            .unwrap(),
-        std::path::Path::new(path)
-    );
-}
+    #[test]
+    fn empty_path_is_error() {
+        assert!(matches!(
+            interpolate_without_context(""),
+            Err(Error::Missing { what: "path" })
+        ));
+    }
 
-#[test]
-fn tilde_substitutes_current_user() {
-    let path = &b"~/foo/bar"[..];
-    let home = std::env::current_dir().unwrap();
-    let expected = format!("{}{}foo/bar", home.display(), std::path::MAIN_SEPARATOR);
-    assert_eq!(
-        Path::from(Cow::Borrowed(path))
-            .interpolate(None, Some(&home))
-            .unwrap()
-            .as_ref(),
-        std::path::Path::new(&expected),
-        "note that path separators are not turned into slashes as we work with `std::path::Path`"
-    );
-}
+    #[test]
+    fn prefix_substitutes_git_install_dir() {
+        for git_install_dir in &["/tmp/git", "C:\\git"] {
+            for (val, expected) in &[
+                (&b"%(prefix)/foo/bar"[..], "foo/bar"),
+                (b"%(prefix)/foo\\bar", "foo\\bar"),
+            ] {
+                let expected =
+                    &std::path::PathBuf::from(format!("{}{}{}", git_install_dir, std::path::MAIN_SEPARATOR, expected));
+                assert_eq!(
+                    &*InterpolatingPath::from(Cow::Borrowed(*val))
+                        .interpolate(Path::new(git_install_dir).into(), None)
+                        .unwrap(),
+                    expected,
+                    "prefix interpolation keeps separators as they are"
+                );
+            }
+        }
+    }
 
-#[cfg(windows)]
-#[test]
-fn tilde_with_given_user_is_unsupported_on_windows() {
-    assert!(matches!(
-        Path::from(Cow::Borrowed(&b"~baz/foo/bar"[..])).interpolate(None, None),
-        Err(interpolate::Error::UserInterpolationUnsupported)
-    ));
-}
-
-#[cfg(not(windows))]
-#[test]
-fn tilde_with_given_user() {
-    let user = std::env::var("USER").unwrap();
-    let home = std::env::var("HOME").unwrap();
-    let specific_user_home = format!("~{}", user);
-
-    for path_suffix in &["foo/bar", "foo\\bar", ""] {
-        let path = format!("{}{}{}", specific_user_home, std::path::MAIN_SEPARATOR, path_suffix);
-        let expected = format!("{}{}{}", home, std::path::MAIN_SEPARATOR, path_suffix);
+    #[test]
+    fn prefix_substitution_skipped_with_dot_slash() {
+        let path = "./%(prefix)/foo/bar";
+        let git_install_dir = "/tmp/git";
         assert_eq!(
-            Path::from(Cow::Borrowed(b(&path))).interpolate(None, None).unwrap(),
-            std::path::Path::new(&expected),
-            "it keeps path separators as is"
+            InterpolatingPath::from(Cow::Borrowed(b(path)))
+                .interpolate(Path::new(git_install_dir).into(), None)
+                .unwrap(),
+            Path::new(path)
         );
+    }
+
+    #[test]
+    fn tilde_alone_does_not_interpolate() -> crate::Result {
+        assert_eq!(interpolate_without_context("~")?, Path::new("~"));
+        Ok(())
+    }
+
+    #[test]
+    fn tilde_slash_substitutes_current_user() {
+        let path = &b"~/foo/bar"[..];
+        let home = std::env::current_dir().unwrap();
+        let expected = format!("{}{}foo/bar", home.display(), std::path::MAIN_SEPARATOR);
+        assert_eq!(
+            InterpolatingPath::from(Cow::Borrowed(path))
+                .interpolate(None, Some(&home))
+                .unwrap()
+                .as_ref(),
+            Path::new(&expected),
+            "note that path separators are not turned into slashes as we work with `std::path::Path`"
+        );
+    }
+
+    #[cfg(windows)]
+    #[test]
+    fn tilde_with_given_user_is_unsupported_on_windows() {
+        assert!(matches!(
+            interpolate_without_context("~baz/foo/bar"),
+            Err(Error::UserInterpolationUnsupported)
+        ));
+    }
+
+    #[cfg(not(windows))]
+    #[test]
+    fn tilde_with_given_user() -> crate::Result {
+        let user = std::env::var("USER")?;
+        let home = std::env::var("HOME")?;
+        let specific_user_home = format!("~{}", user);
+
+        for path_suffix in &["foo/bar", "foo\\bar", ""] {
+            let path = format!("{}{}{}", specific_user_home, std::path::MAIN_SEPARATOR, path_suffix);
+            let expected = format!("{}{}{}", home, std::path::MAIN_SEPARATOR, path_suffix);
+            assert_eq!(
+                interpolate_without_context(path)?,
+                Path::new(&expected),
+                "it keeps path separators as is"
+            );
+        }
+        Ok(())
+    }
+
+    fn interpolate_without_context(
+        path: impl AsRef<str>,
+    ) -> Result<Cow<'static, Path>, git_config::values::path::interpolate::Error> {
+        InterpolatingPath::from(Cow::Owned(path.as_ref().to_owned().into())).interpolate(None, None)
     }
 }
