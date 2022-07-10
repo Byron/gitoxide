95fc20a377aeb914d6b527c1d1b8e75d8c42c608
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-07-10 09:39:51 +0800

    more doc adjustments (#331)

--- git-config/src/lib.rs
+++ git-config/src/lib.rs
@@ -33,8 +33,8 @@
 //!
 //! ## Feature Flags
 #![cfg_attr(
-feature = "document-features",
-cfg_attr(doc, doc = ::document_features::document_features!())
+    feature = "document-features",
+    cfg_attr(doc, doc = ::document_features::document_features!())
 )]
 
 pub mod file;
