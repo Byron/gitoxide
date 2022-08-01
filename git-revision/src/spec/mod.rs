6b76c06c1e9e2317f6ee1ff26c3cc57c46ec0b69
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-08-01 09:41:02 +0800

    Better docs for `Spec` and `spec::Kind` (#427)

--- git-revision/src/spec/mod.rs
+++ git-revision/src/spec/mod.rs
@@ -5,17 +5,17 @@ use crate::Spec;
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
 pub enum Kind {
     /// Include commits reachable from this revision, the default when parsing revision `a` for example, i.e. `a` and its ancestors.
-    /// Created by `a`.
+    /// Example: `a`.
     IncludeReachable,
-    /// Exclude commits reachable from this revision, i.e. `a` and its ancestors. Created by `^a`.
+    /// Exclude commits reachable from this revision, i.e. `a` and its ancestors. Example: `^a`.
     ExcludeReachable,
-    /// Every commit that is reachable from `b` but not from `a`. Created by `a..b`.
+    /// Every commit that is reachable from `b` but not from `a`. Example: `a..b`.
     RangeBetween,
-    /// Every commit reachable through either `a` or `b` but no commit that is reachable by both. Created by `a...b`.
+    /// Every commit reachable through either `a` or `b` but no commit that is reachable by both. Example: `a...b`.
     ReachableToMergeBase,
-    /// Include every commit of all parents of `a`, but not `a` itself. Created by `a^@`.
+    /// Include every commit of all parents of `a`, but not `a` itself. Example: `a^@`.
     IncludeReachableFromParents,
-    /// Exclude every commit of all parents of `a`, but not `a` itself. Created by `a^!`.
+    /// Exclude every commit of all parents of `a`, but not `a` itself. Example: `a^!`.
     ExcludeReachableFromParents,
 }
 
--- git-revision/src/types.rs
+++ git-revision/src/types.rs
@@ -5,10 +5,16 @@
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
 #[cfg_attr(feature = "serde1", derive(serde::Serialize, serde::Deserialize))]
 pub enum Spec {
+    /// Include commits reachable from this revision, i.e. `a` and its ancestors.
+    ///
     /// The equivalent to [crate::spec::Kind::IncludeReachable], but with data.
     Include(git_hash::ObjectId),
+    /// Exclude commits reachable from this revision, i.e. `a` and its ancestors. Example: `^a`.
+    ///
     /// The equivalent to [crate::spec::Kind::ExcludeReachable], but with data.
     Exclude(git_hash::ObjectId),
+    /// Every commit that is reachable from `from` to `to`, but not any ancestors of `from`. Example: `from..to`.
+    ///
     /// The equivalent to [crate::spec::Kind::RangeBetween], but with data.
     Range {
         /// The starting point of the range, which is included in the set.
@@ -16,7 +22,8 @@ pub enum Spec {
         /// The end point of the range, which is included in the set.
         to: git_hash::ObjectId,
     },
-
+    /// Every commit reachable through either `theirs` or `ours`, but no commit that is reachable by both. Example: `theirs...ours`.
+    ///
     /// The equivalent to [crate::spec::Kind::ReachableToMergeBase], but with data.
     Merge {
         /// Their side of the merge, which is included in the set.
@@ -24,15 +31,25 @@ pub enum Spec {
         /// Our side of the merge, which is included in the set.
         ours: git_hash::ObjectId,
     },
-
+    /// Include every commit of all parents of `a`, but not `a` itself. Example: `a^@`.
+    ///
     /// The equivalent to [crate::spec::Kind::IncludeReachableFromParents], but with data.
     IncludeOnlyParents(
         /// Include only the parents of this object, but not the object itself.
         git_hash::ObjectId,
     ),
+    /// Exclude every commit of all parents of `a`, but not `a` itself. Example: `a^!`.
+    ///
     /// The equivalent to [crate::spec::Kind::ExcludeReachableFromParents], but with data.
     ExcludeParents(
         /// Exclude the parents of this object, but not the object itself.
         git_hash::ObjectId,
     ),
+}
+
+pub enum Kind {
+    /// Include every commit of all parents of `a`, but not `a` itself. Example: `a^@`.
+    IncludeReachableFromParents,
+    /// Exclude every commit of all parents of `a`, but not `a` itself. Example: `a^!`.
+    ExcludeReachableFromParents,
 }
