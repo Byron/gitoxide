81eef353f8f2add26720e3dd3981ce1b790f996c
Sidney Douw <s.douw@gmx.de>
2022-08-03 10:08:07 +0200

    refactor...
    - write_to now with proper signature
    - no more Writer struct

--- git-index/src/write.rs
+++ git-index/src/write.rs
@@ -1,160 +1,127 @@
 use crate::{extension, State, Version};
-use bstr::ByteVec;
-use std::{
-    collections::{hash_map, HashMap},
-    ops::Range,
-};
+
+pub struct Options {
+    /// The hash kind to use when writing the index file.
+    ///
+    /// It is not always possible to infer the hash kind when reading an index, so this is required.
+    pub hash_kind: git_hash::Kind,
+    pub version: Version,
+}
 
 impl State {
-    pub fn write_to(&self, options: Options) -> Vec<u8> {
-        let mut writer = Writer::new(self, options);
-        writer.generate();
-        writer.data
-    }
-}
+    pub fn write_to(&self, mut out: impl std::io::Write, options: Options) -> std::io::Result<()> {
+        let num_entries = self.entries().len() as u32;
+        header(&mut out, options.version, num_entries)?;
+        entries(&mut out, self)?;
 
-#[derive(Default)]
-pub struct Options {
-    hash_kind: git_hash::Kind,
-}
-
-struct Writer<'a> {
-    state: &'a State,
-    options: Options,
-    data: Vec<u8>,
-    index_table: HashMap<&'static str, Range<usize>>,
-}
-
-impl<'a> Writer<'a> {
-    pub fn new(state: &'a State, options: Options) -> Self {
-        Self {
-            state,
-            options,
-            data: Vec::default(),
-            index_table: Default::default(),
-        }
-    }
-
-    pub fn generate(&mut self) {
-        self.header();
-        self.entries();
-
-        // TODO: Tree extension is always included, I think
-        if let Some(t) = self.state.tree() {
-            self.tree(t)
+        if let Some(t) = self.tree() {
+            tree(&mut out, t)?;
         }
 
-        self.end_of_index();
+        // if num_entries > 0 {
+        //     _end_of_index(&mut out, options.hash_kind)?;
+        // }
+
+        Ok(())
     }
+}
 
-    fn push(&mut self, data: &[u8], key: &'static str) {
-        let start = self.data.len();
-        let end = start + data.len();
+fn header(out: &mut impl std::io::Write, version: Version, num_entries: u32) -> Result<(), std::io::Error> {
+    let signature = b"DIRC";
+    let version = match version {
+        Version::V2 => 2_u32.to_be_bytes(),
+        Version::V3 => 3_u32.to_be_bytes(),
+        Version::V4 => 4_u32.to_be_bytes(),
+    };
+    out.write_all(signature)?;
+    out.write_all(&version)?;
+    out.write_all(&num_entries.to_be_bytes())?;
+    Ok(())
+}
 
-        match self.index_table.entry(key) {
-            hash_map::Entry::Occupied(mut e) => e.get_mut().end = end,
-            hash_map::Entry::Vacant(e) => {
-                e.insert(start..end);
-            }
+fn entries(mut out: impl std::io::Write, state: &State) -> Result<(), std::io::Error> {
+    for e in state.entries() {
+        out.write_all(&e.stat.ctime.secs.to_be_bytes())?;
+        out.write_all(&e.stat.ctime.nsecs.to_be_bytes())?;
+        out.write_all(&e.stat.mtime.secs.to_be_bytes())?;
+        out.write_all(&e.stat.mtime.nsecs.to_be_bytes())?;
+        out.write_all(&e.stat.dev.to_be_bytes())?;
+        out.write_all(&e.stat.ino.to_be_bytes())?;
+        out.write_all(&e.mode.bits().to_be_bytes())?;
+        out.write_all(&e.stat.uid.to_be_bytes())?;
+        out.write_all(&e.stat.gid.to_be_bytes())?;
+        out.write_all(&e.stat.size.to_be_bytes())?;
+        out.write_all(e.id.as_bytes())?;
+        let path = e.path(state);
+        out.write_all(&(e.flags.to_storage().bits() | path.len() as u16).to_be_bytes())?;
+        out.write_all(path)?;
+        out.write_all(b"\0")?;
+
+        // TODO: make this dynamic
+        let size_of_previously_written_bytes = 63 + path.len();
+        let byte_offset = match size_of_previously_written_bytes % 8 {
+            0 => 0,
+            n => 8 - n,
         };
 
-        self.data.push_str(data);
-    }
-
-    fn header(&mut self) {
-        let signature = b"DIRC";
-        let version = match self.state.version() {
-            Version::V2 => 2_u32.to_be_bytes(),
-            Version::V3 => 3_u32.to_be_bytes(),
-            Version::V4 => 4_u32.to_be_bytes(),
-        };
-        let num_entries = self.state.entries().len() as u32;
-
-        self.push(signature, "header");
-        self.push(&version, "header");
-        self.push(&(num_entries).to_be_bytes(), "header");
-    }
-
-    fn entries(&mut self) {
-        for e in self.state.entries() {
-            self.push(&e.stat.ctime.secs.to_be_bytes(), "entries");
-            self.push(&e.stat.ctime.nsecs.to_be_bytes(), "entries");
-            self.push(&e.stat.mtime.secs.to_be_bytes(), "entries");
-            self.push(&e.stat.mtime.nsecs.to_be_bytes(), "entries");
-            self.push(&e.stat.dev.to_be_bytes(), "entries");
-            self.push(&e.stat.ino.to_be_bytes(), "entries");
-            self.push(&e.mode.bits().to_be_bytes(), "entries");
-            self.push(&e.stat.uid.to_be_bytes(), "entries");
-            self.push(&e.stat.gid.to_be_bytes(), "entries");
-            self.push(&e.stat.size.to_be_bytes(), "entries");
-            self.push(e.id.as_bytes(), "entries");
-            //FIXME: correct flag values
-            // probably convert 'in-memory' Flags to at_rest::Flags
-            // self.push(&e.flags.bits().to_be_bytes(), "entries");
-            self.push(b"\x00\x01\x61\x00", "entries");
-
-            println!("{:?}", e.flags.bits());
+        for _i in 0..byte_offset {
+            out.write_all(b"\0")?;
         }
     }
+    Ok(())
+}
 
-    fn tree(&mut self, tree: &extension::Tree) {
-        let signature = b"TREE";
-        let mut size: u32 = 0;
+fn tree(out: &mut impl std::io::Write, tree: &extension::Tree) -> Result<(), std::io::Error> {
+    let signature = b"TREE";
 
-        self.push(signature, "tree");
-        self.push(&size.to_be_bytes(), "tree");
+    // TODO: Can this work without allocating?
+    let mut entries: Vec<u8> = Vec::new();
+    tree_entry(&mut entries, tree)?;
 
-        self.tree_entry(tree);
+    out.write_all(signature)?;
+    out.write_all(&(entries.len() as u32).to_be_bytes())?;
+    out.write_all(&entries)?;
 
-        if let Some(range) = self.index_table.get("tree") {
-            size = (range.end - (range.start + 8)) as u32;
-            self.data[range.start + 4..range.start + 8].copy_from_slice(&size.to_be_bytes());
-        }
+    Ok(())
+}
+
+fn tree_entry(out: &mut impl std::io::Write, tree: &extension::Tree) -> Result<(), std::io::Error> {
+    let num_entries_ascii = tree.num_entries.to_string();
+    let num_children_ascii = tree.children.len().to_string();
+
+    out.write_all(tree.name.as_slice())?;
+    out.write_all(b"\0")?;
+    out.write_all(num_entries_ascii.as_bytes())?;
+    out.write_all(b" ")?;
+    out.write_all(num_children_ascii.as_bytes())?;
+    out.write_all(b"\n")?;
+    out.write_all(tree.id.as_bytes())?;
+
+    for child in &tree.children {
+        tree_entry(out, child)?;
     }
 
-    fn tree_entry(&mut self, tree: &extension::Tree) {
-        let path = [tree.name.as_slice(), b"\0"].concat();
+    Ok(())
+}
 
-        let num_entries_ascii = tree.num_entries.to_string();
-        let num_children_ascii = tree.children.len().to_string();
+// TODO:
+// - offset for the end of the index entries
+// - active extensions and their content length for the hash
+fn _end_of_index(out: &mut impl std::io::Write, hash_kind: git_hash::Kind) -> Result<(), std::io::Error> {
+    let signature = b"EOIE";
+    let extension_size = 4 + hash_kind.len_in_bytes() as u32;
+    let offset: u32 = 0;
 
-        self.push(path.as_slice(), "tree");
-        self.push(num_entries_ascii.as_bytes(), "tree");
-        self.push(b" ", "tree");
-        self.push(num_children_ascii.as_bytes(), "tree");
-        self.push(b"\n", "tree");
-        self.push(tree.id.as_bytes(), "tree");
+    let mut hasher = git_features::hash::hasher(hash_kind);
 
-        for child in &tree.children {
-            self.tree_entry(child);
-        }
-    }
+    hasher.update(b"TREE");
+    hasher.update(&19_u32.to_be_bytes());
+    let hash = hasher.digest();
 
-    fn end_of_index(&mut self) {
-        match self.index_table.get("entries") {
-            Some(range) => {
-                let signature = b"EOIE";
-                let extension_size = 4 + self.options.hash_kind.len_in_bytes() as u32;
-                let offset: u32 = range.end as u32;
+    out.write_all(signature)?;
+    out.write_all(&extension_size.to_be_bytes())?;
+    out.write_all(&offset.to_be_bytes())?;
+    out.write_all(&hash)?;
 
-                let mut hasher = git_features::hash::hasher(self.options.hash_kind);
-
-                match self.index_table.get("tree") {
-                    Some(range) => {
-                        hasher.update(b"TREE");
-                        hasher.update(&self.data[range.start + 4..range.start + 8]);
-                    }
-                    None => {}
-                }
-
-                let hash = hasher.digest();
-
-                self.data.push_str(signature);
-                self.data.push_str(extension_size.to_be_bytes());
-                self.data.push_str(offset.to_be_bytes());
-                self.data.push_str(hash);
-            }
-            None => {}
-        }
-    }
+    Ok(())
 }
