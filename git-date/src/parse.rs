8e6f4a921b6b45945e711aaf5858b7714371fb41
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-08-13 16:23:39 +0800

    refactor

---
+++ git-date/src/parse.rs
@@ -0,0 +1,9 @@
+#[allow(missing_docs)]
+pub fn parse(input: &BStr) -> Option<Time> {
+    // TODO: actual implementation, this is just to not constantly fail
+    if input == "1979-02-26 18:30:00" {
+        Some(Time::new(42, 1800))
+    } else {
+        None
+    }
+}
