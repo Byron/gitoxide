5a9c537ab00c9acfde201a3296a64c96c8c18424
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-08-17 16:54:49 +0800

    test showing that submodules dirs can't be opened right now (#482)

---
+++ git-repository/tests/repository/open.rs
@@ -0,0 +1,45 @@
+mod submodules {
+    use git_repository as git;
+    use std::path::Path;
+
+    #[test]
+    #[ignore]
+    fn by_their_worktree_checkout_and_git_modules_dir() -> crate::Result {
+        let parent_repo = Path::new("with-submodules");
+        let modules = parent_repo.join(".git").join("modules");
+        for module in ["m1", "dir/m1"] {
+            let submodule_m1_workdir = parent_repo.join(module);
+            let submodule_m1_gitdir = modules.join(module);
+
+            for discover_dir in [
+                submodule_m1_workdir.clone(),
+                submodule_m1_workdir.join("subdir"),
+                submodule_m1_gitdir.clone(),
+            ] {
+                let repo = discover_repo(discover_dir)?;
+                assert_eq!(repo.work_dir().expect("non-bare"), submodule_m1_workdir);
+                assert_eq!(repo.git_dir(), submodule_m1_gitdir);
+
+                let repo = git::open_opts(repo.work_dir().expect("non-bare"), git::open::Options::isolated())?;
+                assert_eq!(repo.work_dir().expect("non-bare"), submodule_m1_workdir);
+                assert_eq!(repo.git_dir(), submodule_m1_gitdir);
+            }
+        }
+        Ok(())
+    }
+
+    fn discover_repo(name: impl AsRef<Path>) -> crate::Result<git::Repository> {
+        let dir = git_testtools::scripted_fixture_repo_read_only("make_submodules.sh")?;
+        let repo_dir = dir.join(name);
+        dbg!(&repo_dir);
+        Ok(git::ThreadSafeRepository::discover_opts(
+            repo_dir,
+            Default::default(),
+            git_sec::trust::Mapping {
+                full: crate::restricted(),
+                reduced: crate::restricted(),
+            },
+        )?
+        .to_thread_local())
+    }
+}
