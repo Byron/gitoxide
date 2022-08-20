bfc34320e87f6ccee22674060da43457a4ced136
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-08-20 13:03:35 +0800

    refactor

--- git-repository/examples/new.rs
+++ git-repository/examples/new.rs
@@ -1,35 +1,33 @@
 // cargo run -p git-repository --example new
 
 use git_repository as git;
-use std::fs;
-use std::path::Path;
-
-const NEW_REPO_PATH: &str = "example-new-repo";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = Path::new(NEW_REPO_PATH);
-    if path.exists() {
-        fs::remove_dir_all(path)?;
-    }
-    let repo = git::init(path)?.apply_environment();
+    let tmp = tempfile::TempDir::new()?;
+    let work_dir = tmp.path().join("repo-non-bare");
+    let repo = git::init(work_dir)?;
+
     println!(
-        "Repo: {}",
-        repo.work_dir().as_deref().unwrap_or(repo.git_dir()).display()
+        "Repo: {:?}",
+        repo.work_dir().expect("non-bare repositories have a work-dir")
     );
-    let empty_tree_id = repo.write_object(&git::objs::Tree::empty()).unwrap().detach();
-    let author = git::actor::Signature {
+
+    let empty_tree_id = repo.write_object(git::objs::Tree::empty())?;
+    let author = git::actor::SignatureRef {
         name: "Maria Sanchez".into(),
         email: "maria@example.com".into(),
         time: git_date::Time::now_local_or_utc(),
     };
     let id = repo.commit(
         "HEAD",
-        author.to_ref(),
-        author.to_ref(),
+        author,
+        author,
         "initial commit",
         empty_tree_id,
         git::commit::NO_PARENT_IDS,
     )?;
-    println!("new object id: {:?}", id);
+    println!("new commit id with empty tree: {:?}", id);
+
+    tmp.into_path();
     Ok(())
 }
