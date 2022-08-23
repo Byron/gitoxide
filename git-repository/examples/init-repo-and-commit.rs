05ab87b6fc420656f314c24c616375f13e3b24ed
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-08-23 10:11:58 +0800

    refactor

--- git-repository/examples/init-repo-and-commit.rs
+++ git-repository/examples/init-repo-and-commit.rs
@@ -11,18 +11,14 @@ fn main() -> anyhow::Result<()> {
     // paths may not be UTF-8 encoded and thus can't be forced into a String.
     // gitoxide does not assume encodings that aren't there
     // to match the way git does it as a bare minimum and be just as flexible.
-    let work_dir = std::env::args_os()
+    let git_dir = std::env::args_os()
         .nth(1)
         .context("First argument needs to be the directory to initialize the repository in")?;
-    let repo = git::init(work_dir)?;
+    let repo = git::init_bare(git_dir)?;
 
-    println!(
-        "Repo: {:?}",
-        repo.work_dir().expect("non-bare repositories have a work-dir")
-    );
+    println!("Repo (bare): {:?}", repo.git_dir());
 
     let mut tree = git::objs::Tree::empty();
-    //let empty_tree_id = repo.write_object(git::objs::Tree::empty())?;
     let empty_tree_id = repo.write_object(&tree)?;
 
     let author = git::actor::SignatureRef {
@@ -41,9 +37,7 @@ fn main() -> anyhow::Result<()> {
 
     println!("initial commit id with empty tree: {:?}", initial_commit_id);
 
-    let blob_id = repo
-        .objects
-        .write_buf(git_object::Kind::Blob, "hello world".as_bytes())?;
+    let blob_id = repo.objects.write_buf(git_object::Kind::Blob, b"hello world")?;
 
     let entry = tree::Entry {
         mode: tree::EntryMode::Blob,
