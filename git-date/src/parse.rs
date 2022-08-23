43b6c064bfa407d1a2cabf09aad1cc8334dec651
Svetlin Stefanov <s.m.stefanov@gmail.com>
2022-08-23 22:55:03 +0200

    Draft.

--- git-date/src/parse.rs
+++ git-date/src/parse.rs
@@ -1,4 +1,6 @@
+use crate::time::format::{RFC2822, SHORT};
 use crate::Time;
+use time::{Date, OffsetDateTime};
 
 #[allow(missing_docs)]
 pub fn parse(input: &str) -> Option<Time> {
@@ -6,6 +8,43 @@ pub fn parse(input: &str) -> Option<Time> {
     if input == "1979-02-26 18:30:00" {
         Some(Time::new(42, 1800))
     } else {
-        None
+        return if let Ok(val) = Date::parse(input, SHORT) {
+            let val = val.with_hms(0, 0, 0).expect("date is in range").assume_utc();
+            Some(Time::new(val.unix_timestamp() as u32, val.offset().whole_seconds()))
+        } else if let Ok(val) = OffsetDateTime::parse(input, RFC2822) {
+            Some(Time::new(val.unix_timestamp() as u32, val.offset().whole_seconds()))
+        } else if let Some(val) = relative::parse(input) {
+            Some(Time::new(val.unix_timestamp() as u32, val.offset().whole_seconds()))
+        } else {
+            None
+        };
+    }
+}
+
+mod relative {
+    use std::str::FromStr;
+    use time::{Duration, OffsetDateTime};
+
+    pub(crate) fn parse(input: &str) -> Option<OffsetDateTime> {
+        let split: Vec<&str> = input.split_whitespace().collect();
+        if split.len() != 3 || *split.last().expect("slice has length 3") != "ago" {
+            return None;
+        }
+        let multiplier = i64::from_str(split[0]).ok()?;
+        let period = period_to_seconds(split[1])?;
+        Some(OffsetDateTime::now_utc().checked_sub(Duration::seconds(multiplier * period))?)
+    }
+
+    fn period_to_seconds(period: &str) -> Option<i64> {
+        let period = period.strip_suffix("s").unwrap_or(period);
+        return match period {
+            "second" => Some(1),
+            "minute" => Some(60),
+            "hour" => Some(60 * 60),
+            "day" => Some(24 * 60 * 60),
+            "week" => Some(7 * 24 * 60 * 60),
+            // TODO months & years
+            _ => None,
+        };
     }
 }
--- git-date/tests/time/parse.rs
+++ git-date/tests/time/parse.rs
@@ -1,5 +1,6 @@
 use git_date::time::Sign;
 use git_date::Time;
+use time::OffsetDateTime;
 
 #[test]
 fn special_time_is_ok_for_now() {
@@ -10,5 +11,39 @@ fn special_time_is_ok_for_now() {
             offset_in_seconds: 1800,
             sign: Sign::Plus,
         }
+    );
+}
+
+#[test]
+fn parse() {
+    assert_eq!(
+        git_date::parse("1979-02-26"),
+        Some(Time {
+            seconds_since_unix_epoch: 288835200,
+            offset_in_seconds: 0,
+            sign: Sign::Plus,
+        }),
+        "could not parse with SHORT format"
+    );
+
+    assert_eq!(
+        git_date::parse("Thu, 18 Aug 2022 12:45:06 +0800"),
+        Some(Time {
+            seconds_since_unix_epoch: 1660797906,
+            offset_in_seconds: 28800,
+            sign: Sign::Plus,
+        }),
+        "could not parse with RFC2822 format"
+    );
+
+    let two_weeks_ago = git_date::parse("2 weeks ago").expect("valid time");
+    assert_eq!(Sign::Plus, two_weeks_ago.sign);
+    assert_eq!(0, two_weeks_ago.offset_in_seconds);
+    assert_eq!(
+        OffsetDateTime::from_unix_timestamp(two_weeks_ago.seconds_since_unix_epoch as i64)
+            .expect("valid datetime")
+            .iso_week(),
+        OffsetDateTime::now_utc().iso_week() - 2,
+        "weeks numbers differ"
     );
 }
