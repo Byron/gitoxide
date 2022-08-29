a8d9d123f2bfdebd00a0ca27cee21916ca7e44e4
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-08-29 20:58:32 +0800

    See if not having cargo-run output in program invocation fixes it on macos (#450)

--- git-prompt/tests/prompt.rs
+++ git-prompt/tests/prompt.rs
@@ -5,8 +5,10 @@ mod ask {
     #[cfg(unix)]
     fn username_password() {
         let mut cmd = Command::new(env!("CARGO"));
-        cmd.args(["run", "--example", "credentials"]);
-        let mut p = expectrl::Session::spawn(cmd).unwrap();
+        cmd.args(["build", "--example", "credentials"]);
+        cmd.spawn().unwrap().wait().expect("example builds OK");
+
+        let mut p = expectrl::spawn("../target/debug/examples/credentials").unwrap();
         p.expect("Username: ").unwrap();
         p.send_line(" user with space ").unwrap();
         p.expect("\" user with space\"").unwrap();
