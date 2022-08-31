a0835c5906eee1f7c9270fcbce5842c24c0f66e9
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-08-31 08:59:22 +0800

    Improve the English skills of cargo-smart-release and fix a typo. (#513)

--- cargo-smart-release/src/changelog/write.rs
+++ cargo-smart-release/src/changelog/write.rs
@@ -321,9 +321,10 @@ impl section::Segment {
                 }
                 writeln!(
                     out,
-                    " - {} {} where understood as [conventional](https://www.conventionalcommits.org).",
+                    " - {} {} {} understood as [conventional](https://www.conventionalcommits.org).",
                     conventional_count,
-                    if *conventional_count == 1 { "commit" } else { "commits" }
+                    if *conventional_count == 1 { "commit" } else { "commits" },
+                    if *conventional_count == 1 { "was" } else { "were" }
                 )?;
                 if unique_issues.is_empty() {
                     writeln!(out, " - 0 issues like '(#ID)' where seen in commit messages")?;
