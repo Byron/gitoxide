8e07784d5935efa62fc70085bb81259c038a7330
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-09-05 14:22:36 +0800

    Use repository configuration for calling credential helper (#450)

--- gitoxide-core/src/repository/credential.rs
+++ gitoxide-core/src/repository/credential.rs
@@ -1,24 +1,37 @@
 use git_repository as git;
 use std::convert::TryInto;
 
-pub fn function(_repo: git::Repository, action: git::credentials::program::main::Action) -> anyhow::Result<()> {
-    // TODO: use repo for configuration
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error(transparent)]
+    UrlParse(#[from] git::url::parse::Error),
+    #[error(transparent)]
+    Configuration(#[from] git::config::credential_helpers::Error),
+    #[error(transparent)]
+    Protocol(#[from] git::credentials::protocol::Error),
+}
+
+pub fn function(repo: git::Repository, action: git::credentials::program::main::Action) -> anyhow::Result<()> {
     use git::credentials::program::main::Action::*;
     git::credentials::program::main(
         Some(action.as_str().into()),
         std::io::stdin(),
         std::io::stdout(),
-        |action, context| {
-            git::credentials::helper::Cascade::default()
+        |action, context| -> Result<_, Error> {
+            let (mut cascade, _action, prompt_options) = repo.config_snapshot().credential_helpers(git::url::parse(
+                context.url.as_ref().expect("framework assures URL is present").as_ref(),
+            )?)?;
+            cascade
                 .invoke(
                     match action {
                         Get => git::credentials::helper::Action::Get(context),
                         Erase => git::credentials::helper::Action::Erase(context.to_bstring()),
                         Store => git::credentials::helper::Action::Store(context.to_bstring()),
                     },
-                    git::prompt::Options::default().apply_environment(true, true, true),
+                    prompt_options,
                 )
                 .map(|outcome| outcome.and_then(|outcome| (&outcome.next).try_into().ok()))
+                .map_err(Into::into)
         },
     )
     .map_err(Into::into)
