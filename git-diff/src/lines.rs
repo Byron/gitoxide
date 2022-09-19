e164856ab8d80c13b082a283b4c73b6fb31968f6
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-09-19 21:55:48 +0800

    feat: forward line-diffing capabilities curtesy of the `similar` crate. (#470)
    This is a first and maybe the last step towards providing diffing
    functionality, and it seems like the right choice to keep this in
    similar and contribute there as needed. All algorithms are well
    described and thus shouldn't be git-specific per-se, and `similar`
    is the best the community has to offer.

---
+++ git-diff/src/lines.rs
@@ -0,0 +1,26 @@
+use git_object::bstr::BStr;
+use similar::TextDiff;
+
+/// The crate powering file diffs.
+pub use similar;
+pub use similar::Algorithm;
+
+/// Provide an iterator over the changes needed to turn `old` into `new` with `algorithm`.
+///
+/// See [the `similar` crate documentation][similar::TextDiffConfig::diff_lines()] for information on how to use the iterator.
+pub fn with<'old, 'new, 'bufs>(
+    old: &'old BStr,
+    new: &'new BStr,
+    algorithm: Algorithm,
+) -> TextDiff<'old, 'new, 'bufs, [u8]> {
+    similar::TextDiffConfig::default()
+        .algorithm(algorithm)
+        .diff_lines(old.as_ref(), new.as_ref())
+}
+
+/// Provide an iterator over the changes needed to turn `old` into `new` with Myers algorithm, the default for `git`.
+///
+/// See [the `similar` crate documentation][similar::TextDiffConfig::diff_lines()] for information on how to use the iterator.
+pub fn myers<'old, 'new, 'bufs>(old: &'old BStr, new: &'new BStr) -> TextDiff<'old, 'new, 'bufs, [u8]> {
+    with(old, new, Algorithm::Myers)
+}
