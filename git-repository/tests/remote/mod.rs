8499c3e8afba0767fe9f0ca0e3016fe9f84951e5
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-09-26 08:22:42 +0800

    thanks clippy

--- git-repository/tests/remote/mod.rs
+++ git-repository/tests/remote/mod.rs
@@ -1,8 +1,7 @@
 use std::{borrow::Cow, path::PathBuf};
-use tempfile::TempDir;
 
 use git_repository as git;
-use git_testtools::{scripted_fixture_repo_read_only, scripted_fixture_repo_writable_with_args};
+use git_testtools::scripted_fixture_repo_read_only;
 
 pub(crate) fn repo_path(name: &str) -> PathBuf {
     let dir = scripted_fixture_repo_read_only("make_remote_repos.sh").unwrap();
@@ -13,8 +12,10 @@ pub(crate) fn repo(name: &str) -> git::Repository {
     git::open_opts(repo_path(name), git::open::Options::isolated()).unwrap()
 }
 
-pub(crate) fn repo_rw(name: &str) -> (git::Repository, TempDir) {
-    let dir = scripted_fixture_repo_writable_with_args(
+#[cfg(feature = "blocking-network-client")]
+// TODO: move this to where it's used (fetch)
+pub(crate) fn repo_rw(name: &str) -> (git::Repository, git_testtools::tempfile::TempDir) {
+    let dir = git_testtools::scripted_fixture_repo_writable_with_args(
         "make_remote_repos.sh",
         &[] as &[String],
         git_testtools::Creation::ExecuteScript,
