0fd57c6b491a3c8d0127bc1d1f0eb958437edff9
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-09-27 22:33:04 +0800

    feat: Add `client::http::Transport::new_http()` constructor.
    This goes along with `client::http::connect_http()` to support
    connections to via custom transports which are passed from calling
    crates, without relying on the implementation to be built-in.

--- git-transport/src/client/blocking_io/http/mod.rs
+++ git-transport/src/client/blocking_io/http/mod.rs
@@ -35,33 +35,30 @@ pub struct Transport<H: Http> {
     identity: Option<git_sec::identity::Account>,
 }
 
-fn new<H: Http>(http: H, url: &str, desired_version: Protocol) -> Transport<H> {
-    Transport {
-        url: url.to_owned(),
-        user_agent_header: concat!("User-Agent: git/oxide-", env!("CARGO_PKG_VERSION")),
-        desired_version,
-        actual_version: desired_version,
-        supported_versions: [desired_version],
-        service: None,
-        http,
-        line_provider: None,
-        identity: None,
-    }
-}
-
-#[cfg(all(feature = "http-client", not(feature = "http-client-curl")))]
 impl<H: Http> Transport<H> {
-    /// Create a new instance to communicate to `url` using the given `desired_version` of the `git` protocol.
-    pub fn new(http: H, url: &str, desired_version: Protocol) -> Self {
-        new::<H>(http, url, desired_version)
+    /// Create a new instance with `http` as implementation to communicate to `url` using the given `desired_version` of the `git` protocol.
+    pub fn new_http(http: H, url: &str, desired_version: Protocol) -> Self {
+        Transport {
+            url: url.to_owned(),
+            user_agent_header: concat!("User-Agent: git/oxide-", env!("CARGO_PKG_VERSION")),
+            desired_version,
+            actual_version: desired_version,
+            supported_versions: [desired_version],
+            service: None,
+            http,
+            line_provider: None,
+            identity: None,
+        }
     }
 }
 
 #[cfg(feature = "http-client-curl")]
 impl Transport<Impl> {
     /// Create a new instance to communicate to `url` using the given `desired_version` of the `git` protocol.
+    ///
+    /// Note that the actual implementation depends on feature toggles.
     pub fn new(url: &str, desired_version: Protocol) -> Self {
-        new::<Impl>(Impl::default(), url, desired_version)
+        Self::new_http(Impl::default(), url, desired_version)
     }
 }
 
@@ -298,10 +295,10 @@ impl<H: Http, B: ExtendedBufRead + Unpin> ExtendedBufRead for HeadersThenBody<H,
     }
 }
 
-/// Connect to the given `url` via HTTP/S using the `desired_version` of the `git` protocol.
+/// Connect to the given `url` via HTTP/S using the `desired_version` of the `git` protocol, with `http` as implementation.
 #[cfg(all(feature = "http-client", not(feature = "http-client-curl")))]
-pub fn connect<H: Http>(http: H, url: &str, desired_version: Protocol) -> Result<Transport<H>, Infallible> {
-    Ok(Transport::new(http, url, desired_version))
+pub fn connect_http<H: Http>(http: H, url: &str, desired_version: Protocol) -> Result<Transport<H>, Infallible> {
+    Ok(Transport::new_http(http, url, desired_version))
 }
 
 /// Connect to the given `url` via HTTP/S using the `desired_version` of the `git` protocol.
