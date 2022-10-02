ac17e11ce95ee6ef8fa8d89fd57534be5a8aeda0
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-10-02 16:06:27 +0800

    a sketch of how fetching could work, but it fails with the line-reader for some reason (#450)

--- gitoxide-core/src/repository/fetch.rs
+++ gitoxide-core/src/repository/fetch.rs
@@ -14,7 +14,10 @@ pub const PROGRESS_RANGE: std::ops::RangeInclusive<u8> = 1..=2;
 
 pub(crate) mod function {
     #![allow(unused_variables, unused_mut)]
+
     use super::Options;
+    use crate::OutputFormat;
+    use anyhow::bail;
     use git_repository as git;
 
     pub fn fetch(
@@ -29,7 +32,21 @@ pub(crate) mod function {
             ref_specs,
         }: Options,
     ) -> anyhow::Result<()> {
-        let _remote = crate::repository::remote::by_name_or_url(&repo, remote.as_deref())?;
-        todo!()
+        if format != OutputFormat::Human {
+            bail!("JSON output isn't yet supported for fetching.");
+        }
+        let mut remote = crate::repository::remote::by_name_or_url(&repo, remote.as_deref())?;
+        if !ref_specs.is_empty() {
+            remote.replace_refspecs(ref_specs.iter(), git::remote::Direction::Fetch)?;
+        }
+
+        let res = remote
+            .connect(git::remote::Direction::Fetch, progress)?
+            .prepare_fetch(Default::default())?
+            .with_dry_run(dry_run)
+            .receive(&git::interrupt::IS_INTERRUPTED)?;
+
+        dbg!(res);
+        Ok(())
     }
 }
