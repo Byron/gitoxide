1375368006942bdf246a85466a144b05df5a55ac
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-10-08 16:16:00 +0800

    test another prerequisite

--- git-repository/tests/remote/save.rs
+++ git-repository/tests/remote/save.rs
@@ -4,13 +4,22 @@ mod save_as_to {
     use std::convert::TryInto;
 
     #[test]
-    fn new_anonymous_remote_with_name() {
-        let repo = basic_repo().unwrap();
+    fn anonymous_remotes_cannot_be_save_lacking_a_name() -> crate::Result {
+        let repo = basic_repo()?;
+        let remote = repo.remote_at("https://example.com/path")?;
+        assert!(matches!(
+            remote.save_to(&mut git::config::File::default()).unwrap_err(),
+            git::remote::save::Error::NameMissing { .. }
+        ));
+        Ok(())
+    }
+
+    #[test]
+    fn new_anonymous_remote_with_name() -> crate::Result {
+        let repo = basic_repo()?;
         let mut remote = repo
-            .remote_at("https://example.com/path")
-            .unwrap()
-            .push_url("https://ein.hub/path")
-            .unwrap();
+            .remote_at("https://example.com/path")?
+            .push_url("https://ein.hub/path")?;
         let remote_name = "origin";
         assert!(
             repo.find_remote(remote_name).is_err(),
@@ -18,13 +27,12 @@ mod save_as_to {
         );
         assert_eq!(remote.name(), None);
         let mut config = git::config::File::default();
-        remote
-            .save_as_to(remote_name.try_into().expect("valid name"), &mut config)
-            .unwrap();
+        remote.save_as_to(remote_name.try_into().expect("valid name"), &mut config)?;
         assert_eq!(
             uniformize(config.to_string()),
             "[remote \"origin\"]\n\turl = https://example.com/path\n\tpushurl = https://ein.hub/path\n"
-        )
+        );
+        Ok(())
     }
 
     fn uniformize(input: String) -> String {
