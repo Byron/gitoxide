3821b4fb2d22e7b447ca3c11ae1ba9c6897916cd
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-10-15 17:48:15 +0800

    finally perform actual checkout, but test fails without clear reason (#450)

--- git-repository/src/clone/checkout.rs
+++ git-repository/src/clone/checkout.rs
@@ -26,6 +26,13 @@ pub mod main_worktree {
         WriteIndex(#[from] git_index::file::write::Error),
         #[error(transparent)]
         CheckoutOptions(#[from] crate::config::checkout_options::Error),
+        #[error(transparent)]
+        IndexCheckout(
+            #[from]
+            git_worktree::index::checkout::Error<git_odb::find::existing_object::Error<git_odb::store::find::Error>>,
+        ),
+        #[error("Failed to reopen object database as Arc (only if thread-safety wasn't compiled in)")]
+        OpenArcOdb(#[from] std::io::Error),
     }
 
     /// Modification
@@ -34,14 +41,14 @@ pub mod main_worktree {
         /// on thread per logical core.
         pub fn main_worktree(
             &mut self,
-            _progress: impl crate::Progress,
-            _should_interrupt: &AtomicBool,
+            mut progress: impl crate::Progress,
+            should_interrupt: &AtomicBool,
         ) -> Result<Repository, Error> {
             let repo = self
                 .repo
                 .as_ref()
                 .expect("still present as we never succeeded the worktree checkout yet");
-            let _workdir = repo.work_dir().ok_or_else(|| Error::BareRepository {
+            let workdir = repo.work_dir().ok_or_else(|| Error::BareRepository {
                 git_dir: repo.git_dir().to_owned(),
             })?;
             let root_tree = repo
@@ -57,34 +64,28 @@ pub mod main_worktree {
                     source: err,
                 })?;
             let mut index = git_index::File::from_state(index, repo.index_path());
-            index.write(Default::default())?;
 
             let mut opts = repo.config.checkout_options(repo.git_dir())?;
             opts.destination_is_initially_empty = true;
-            // git_worktree::index::checkout(
-            //     &mut index,
-            //     workdir,
-            //     {
-            //         let objects = repo.objects.into_arc()?;
-            //         move |oid, buf| {
-            //             objects.find_blob(oid, buf).ok();
-            //             if empty_files {
-            //                 // We always want to query the ODB here…
-            //                 objects.find_blob(oid, buf)?;
-            //                 buf.clear();
-            //                 // …but write nothing
-            //                 Ok(git::objs::BlobRef { data: buf })
-            //             } else {
-            //                 objects.find_blob(oid, buf)
-            //             }
-            //         }
-            //     },
-            //     &mut files,
-            //     &mut bytes,
-            //     should_interrupt,
-            //     opts,
-            // );
-            todo!("which branch to use?")
+
+            let mut files = progress.add_child("checkout");
+            let mut bytes = progress.add_child("writing");
+
+            git_worktree::index::checkout(
+                &mut index,
+                workdir,
+                {
+                    let objects = repo.objects.clone().into_arc()?;
+                    move |oid, buf| objects.find_blob(oid, buf)
+                },
+                &mut files,
+                &mut bytes,
+                should_interrupt,
+                opts,
+            )?;
+
+            index.write(Default::default())?;
+            Ok(self.repo.take().expect("still present"))
         }
     }
 }
