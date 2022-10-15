05a666c50e270c30d47fe5bfc3195a4fd1f1aea8
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-10-15 13:18:58 +0800

    read core.checkstat to configure the checkout as well (#450)
    Note that it doesn't check for known or valid values, just like git.
    Given the importance, or lack thereof, of this flag, this is probably
    just right.

--- git-repository/src/config/cache/access.rs
+++ git-repository/src/config/cache/access.rs
@@ -122,7 +122,15 @@ impl Cache {
             overwrite_existing: false,
             keep_going: false,
             trust_ctime: boolean(self, "core.trustCTime", true)?,
-            check_stat: true,
+            check_stat: self.resolved.string("core", None, "checkStat").map_or(true, |v| {
+                if v.as_ref() == "default" {
+                    true
+                } else if v.as_ref() == "minimal" {
+                    false
+                } else {
+                    true
+                }
+            }),
             attribute_globals: Default::default(),
         })
     }
