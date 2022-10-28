ee26ddfe645ca39024795f85bb8e1cab6b6f5863
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-10-28 09:47:24 +0200

    change!: replace `similar` with `imara-diff`.
    The latter clearly has a more complex call signature, but that also
    makes it far more powerful which will pay off with better performance.

--- git-diff/src/lines.rs
+++ git-diff/src/lines.rs
@@ -1,25 +1,15 @@
 use git_object::bstr::BStr;
 /// The crate powering file diffs.
-pub use similar;
-pub use similar::Algorithm;
-use similar::TextDiff;
+pub use imara_diff;
+pub use imara_diff::Algorithm;
 
-/// Provide an iterator over the changes needed to turn `old` into `new` with `algorithm`.
+/// Provide an iterator over the changes needed to turn `old` into `new` with `algorithm` into `sink`.
 ///
-/// See [the `similar` crate documentation][similar::TextDiffConfig::diff_lines()] for information on how to use the iterator.
-pub fn with<'old, 'new, 'bufs>(
-    old: &'old BStr,
-    new: &'new BStr,
-    algorithm: Algorithm,
-) -> TextDiff<'old, 'new, 'bufs, [u8]> {
-    similar::TextDiffConfig::default()
-        .algorithm(algorithm)
-        .diff_lines(old.as_ref(), new.as_ref())
-}
-
-/// Provide an iterator over the changes needed to turn `old` into `new` with Myers algorithm, the default for `git`.
-///
-/// See [the `similar` crate documentation][similar::TextDiffConfig::diff_lines()] for information on how to use the iterator.
-pub fn myers<'old, 'new, 'bufs>(old: &'old BStr, new: &'new BStr) -> TextDiff<'old, 'new, 'bufs, [u8]> {
-    with(old, new, Algorithm::Myers)
+/// See [the `imara-diff` crate documentation][imara_diff] for information on how to implement a [`Sink`][imara_diff::Sink].
+pub fn with<S>(old: &BStr, new: &BStr, algorithm: Algorithm, sink: S) -> S::Out
+where
+    S: imara_diff::Sink,
+{
+    let input = imara_diff::intern::InternedInput::new(old.as_ref(), new.as_ref());
+    imara_diff::diff(algorithm, &input, sink)
 }
