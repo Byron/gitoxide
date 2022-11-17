611a1394b1a7470b9247474ea0c43ef59560f6fe
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-11-17 11:17:20 +0100

    migrate independent parts of ref parsing to generalized handshake.
    V2 ref fetching (invoking a command) is indeed only available in V2
    which in turn is only for fetch.
    
    Maybe the solution here is rather to make ls-refs a general command,
    which might lead to reorganzation. For now, it's OK to keep it there
    I think knowing push is only V1 which is what this is for anyway.

---
+++ git-protocol/src/handshake/refs/mod.rs
@@ -0,0 +1,83 @@
+use super::Ref;
+use bstr::BStr;
+
+mod error {
+    use crate::fetch::refs::parse;
+
+    /// The error returned by [refs()][crate::fetch::refs()].
+    #[derive(Debug, thiserror::Error)]
+    #[allow(missing_docs)]
+    pub enum Error {
+        #[error(transparent)]
+        Io(#[from] std::io::Error),
+        #[error(transparent)]
+        Transport(#[from] git_transport::client::Error),
+        #[error(transparent)]
+        Parse(#[from] parse::Error),
+    }
+}
+pub use error::Error;
+
+///
+pub mod parse {
+    use bstr::BString;
+
+    /// The error returned when parsing References/refs from the server response.
+    #[derive(Debug, thiserror::Error)]
+    #[allow(missing_docs)]
+    pub enum Error {
+        #[error(transparent)]
+        Io(#[from] std::io::Error),
+        #[error(transparent)]
+        Id(#[from] git_hash::decode::Error),
+        #[error("{symref:?} could not be parsed. A symref is expected to look like <NAME>:<target>.")]
+        MalformedSymref { symref: BString },
+        #[error("{0:?} could not be parsed. A V1 ref line should be '<hex-hash> <path>'.")]
+        MalformedV1RefLine(String),
+        #[error(
+            "{0:?} could not be parsed. A V2 ref line should be '<hex-hash> <path>[ (peeled|symref-target):<value>'."
+        )]
+        MalformedV2RefLine(String),
+        #[error("The ref attribute {attribute:?} is unknown. Found in line {line:?}")]
+        UnkownAttribute { attribute: String, line: String },
+        #[error("{message}")]
+        InvariantViolation { message: &'static str },
+    }
+}
+
+impl Ref {
+    /// Provide shared fields referring to the ref itself, namely `(name, target, [peeled])`.
+    /// In case of peeled refs, the tag object itself is returned as it is what the ref directly refers to, and target of the tag is returned
+    /// as `peeled`.
+    /// If `unborn`, the first object id will be the null oid.
+    pub fn unpack(&self) -> (&BStr, Option<&git_hash::oid>, Option<&git_hash::oid>) {
+        match self {
+            Ref::Direct { full_ref_name, object }
+            | Ref::Symbolic {
+                full_ref_name, object, ..
+            } => (full_ref_name.as_ref(), Some(object), None),
+            Ref::Peeled {
+                full_ref_name,
+                tag: object,
+                object: peeled,
+            } => (full_ref_name.as_ref(), Some(object), Some(peeled)),
+            Ref::Unborn {
+                full_ref_name,
+                target: _,
+            } => (full_ref_name.as_ref(), None, None),
+        }
+    }
+}
+
+#[cfg(any(feature = "blocking-client", feature = "async-client"))]
+pub(crate) mod shared;
+
+#[cfg(feature = "async-client")]
+mod async_io;
+#[cfg(feature = "async-client")]
+pub use async_io::{from_v1_refs_received_as_part_of_handshake_and_capabilities, from_v2_refs};
+
+#[cfg(feature = "blocking-client")]
+mod blocking_io;
+#[cfg(feature = "blocking-client")]
+pub use blocking_io::{from_v1_refs_received_as_part_of_handshake_and_capabilities, from_v2_refs};
