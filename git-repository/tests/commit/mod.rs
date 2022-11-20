d4f58a941e3936fb2f11ec66b75156e1b9120fa2
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-11-20 08:00:43 +0100

    stabilize tests (unwrap() -> ?) and improve fixture
    In the fixture, use absolute descriptions like `:/c1` for commit rev-specs to make
    it very clear which commit we mean.

--- git-repository/tests/commit/mod.rs
+++ git-repository/tests/commit/mod.rs
@@ -4,42 +4,32 @@ mod describe {
     use crate::named_repo;
 
     #[test]
-    fn tags_are_sorted_by_date_and_lexicographically() {
-        let repo = named_repo("make_commit_describe_multiple_tags.sh").unwrap();
-        let mut describe = repo.head_commit().unwrap().describe();
+    fn tags_are_sorted_by_date_and_lexicographically() -> crate::Result {
+        let repo = named_repo("make_commit_describe_multiple_tags.sh")?;
+        let mut describe = repo.head_commit()?.describe();
         for filter in &[AnnotatedTags, AllTags, AllRefs] {
             describe = describe.names(*filter);
-            assert_eq!(describe.format().unwrap().to_string(), "v4", "{:?}", filter);
+            assert_eq!(describe.format()?.to_string(), "v4", "{:?}", filter);
         }
+        Ok(())
     }
 
     #[test]
-    fn tags_are_sorted_by_priority() {
-        let repo = named_repo("make_commit_describe_multiple_tags.sh").unwrap();
-        let commit = repo
-            .find_reference("refs/tags/v0")
-            .unwrap()
-            .id()
-            .object()
-            .unwrap()
-            .into_commit();
+    fn tags_are_sorted_by_priority() -> crate::Result {
+        let repo = named_repo("make_commit_describe_multiple_tags.sh")?;
+        let commit = repo.find_reference("refs/tags/v0")?.id().object()?.into_commit();
         let mut describe = commit.describe();
         for filter in &[AnnotatedTags, AllTags, AllRefs] {
             describe = describe.names(*filter);
-            assert_eq!(describe.format().unwrap().to_string(), "v1", "{:?}", filter);
+            assert_eq!(describe.format()?.to_string(), "v1", "{:?}", filter);
         }
+        Ok(())
     }
 
     #[test]
-    fn lightweight_tags_are_sorted_lexicographically() {
-        let repo = named_repo("make_commit_describe_multiple_tags.sh").unwrap();
-        let commit = repo
-            .find_reference("refs/tags/l0")
-            .unwrap()
-            .id()
-            .object()
-            .unwrap()
-            .into_commit();
+    fn lightweight_tags_are_sorted_lexicographically() -> crate::Result {
+        let repo = named_repo("make_commit_describe_multiple_tags.sh")?;
+        let commit = repo.find_reference("refs/tags/l0")?.id().object()?.into_commit();
         let mut describe = commit.describe();
         for filter in &[AnnotatedTags, AllTags, AllRefs] {
             describe = describe.names(*filter);
@@ -47,8 +37,9 @@ mod describe {
                 AnnotatedTags => None,
                 _ => Some("l0"),
             };
-            let actual = describe.try_format().unwrap().map(|f| f.to_string());
+            let actual = describe.try_format()?.map(|f| f.to_string());
             assert_eq!(actual.as_deref(), expected, "{:?}", filter);
         }
+        Ok(())
     }
 }
