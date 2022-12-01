bea689a97a8e42a92af7f77f7d8706cd96c6dc10
Sidney Douw <s.douw@gmx.de>
2022-12-01 14:12:42 +0100

    remove `canonicalize` calls from test…
    …and use CARGO_MANIFEST_DIR instead

--- git-repository/tests/repository/config/worktree.rs
+++ git-repository/tests/repository/config/worktree.rs
@@ -2,14 +2,16 @@ use git_repository::open;
 
 #[test]
 fn with_worktree_configs() -> git_testtools::Result {
-    let dir = git_testtools::scripted_fixture_repo_read_only("make_worktree_repo_with_configs.sh").unwrap();
+    let manifest_dir = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
 
-    let base = open(dir.join("repo")).unwrap();
+    let fixture_dir = git_testtools::scripted_fixture_repo_read_only("make_worktree_repo_with_configs.sh").unwrap();
+
+    let base = open(fixture_dir.join("repo")).unwrap();
     let base_config = base.config_snapshot();
 
-    assert_eq!(base.work_dir(), Some(dir.join("repo").as_path()));
-    assert_eq!(base.git_dir(), dir.join("repo/.git"));
-    assert_eq!(base.common_dir(), dir.join("repo/.git"));
+    assert_eq!(base.work_dir(), Some(fixture_dir.join("repo").as_path()));
+    assert_eq!(base.git_dir(), fixture_dir.join("repo/.git"));
+    assert_eq!(base.common_dir(), fixture_dir.join("repo/.git"));
 
     assert_eq!(
         base_config.string("worktree.setting").expect("exists").to_string(),
@@ -20,13 +22,16 @@ fn with_worktree_configs() -> git_testtools::Result {
         "set in the shared config"
     );
 
-    let wt1 = open(dir.join("wt-1")).unwrap();
+    let wt1 = open(fixture_dir.join("wt-1")).unwrap();
     let wt1_config = wt1.config_snapshot();
-    assert_eq!(wt1.work_dir(), Some(dir.join("wt-1").as_path()));
-    assert_eq!(wt1.git_dir(), dir.join("repo/.git/worktrees/wt-1").canonicalize()?);
+    assert_eq!(wt1.work_dir(), Some(fixture_dir.join("wt-1").as_path()));
+    assert_eq!(
+        wt1.git_dir(),
+        manifest_dir.join(&fixture_dir).join("repo/.git/worktrees/wt-1")
+    );
     assert_eq!(
         wt1.common_dir(),
-        dir.join("repo/.git").canonicalize()?.join("worktrees/wt-1/../..")
+        manifest_dir.join(&fixture_dir).join("repo/.git/worktrees/wt-1/../..")
     );
 
     assert_eq!(
@@ -38,13 +43,16 @@ fn with_worktree_configs() -> git_testtools::Result {
         "set in the shared config"
     );
 
-    let wt2 = open(dir.join("wt-2")).unwrap();
+    let wt2 = open(fixture_dir.join("wt-2")).unwrap();
     let wt2_config = wt2.config_snapshot();
-    assert_eq!(wt2.work_dir(), Some(dir.join("wt-2").as_path()));
-    assert_eq!(wt2.git_dir(), dir.join("repo/.git/worktrees/wt-2").canonicalize()?);
+    assert_eq!(wt2.work_dir(), Some(fixture_dir.join("wt-2").as_path()));
+    assert_eq!(
+        wt2.git_dir(),
+        manifest_dir.join(&fixture_dir).join("repo/.git/worktrees/wt-2")
+    );
     assert_eq!(
         wt2.common_dir(),
-        dir.join("repo/.git").canonicalize()?.join("worktrees/wt-2/../..")
+        manifest_dir.join(&fixture_dir).join("repo/.git/worktrees/wt-2/../..")
     );
 
     assert_eq!(
