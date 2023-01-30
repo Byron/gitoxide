6d38ed4795ea277301f256f8afa0349cd96bf70c
Nathaniel Brough <nathaniel.brough@gmail.com>
2023-01-30 10:34:08 -0800

    fix(git-refspec): Fixes build for parsing fuzz harness

--- git-refspec/fuzz/fuzz_targets/parse.rs
+++ git-refspec/fuzz/fuzz_targets/parse.rs
@@ -2,6 +2,6 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    drop(git_refspec::parse(data.into(), git_refspec::Operation::Push));
-    drop(git_refspec::parse(data.into(), git_refspec::Operation::Fetch));
+    drop(git_refspec::parse(data.into(), git_refspec::parse::Operation::Push));
+    drop(git_refspec::parse(data.into(), git_refspec::parse::Operation::Fetch));
 });
