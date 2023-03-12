a22621d1b92a6155b83a09e68ed1de3a4860e766
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-03-12 19:18:59 +0100

    improve fetchspec handling to be closer to what git does.
    The current implementation was a bit hacky to mimick observed
    behaviour. These improvements make it easier to understand
    and bring it closer to what git actually seems to do.

--- gix/src/remote/connection/fetch/receive_pack.rs
+++ gix/src/remote/connection/fetch/receive_pack.rs
@@ -118,8 +118,7 @@ where
                 con.remote.fetch_tags,
                 &mut arguments,
                 previous_response.as_ref(),
-                (self.shallow != Shallow::NoChange)
-                    .then(|| (&self.shallow, con.remote.refspecs(remote::Direction::Fetch))),
+                (self.shallow != Shallow::NoChange).then_some(&self.shallow),
             ) {
                 Ok(_) if arguments.is_empty() => {
                     gix_protocol::indicate_end_of_interaction(&mut con.transport).await.ok();
