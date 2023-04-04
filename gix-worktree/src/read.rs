16eab8166ea03489c3ac5537365d69626b419bfe
Pascal Kuthe <pascal.kuthe@semimod.de>
2023-04-04 18:13:39 +0200

    refactor gix_worktree::read module

--- gix-worktree/src/read.rs
+++ gix-worktree/src/read.rs
@@ -1,29 +1,21 @@
+//! This module allows creating git blobs from worktree files.
+//! For the most part a blob just contains the raw on-disk data.
+//! However symlinks need to be considered poperly and attributes/config options need to be considered
+
 use crate::fs;
 use gix_object::Blob;
 use gix_path as path;
 use std::borrow::Cow;
 use std::fs::{read_link, File};
-use std::io;
-use std::io::Read;
+use std::io::{self, Read};
 use std::path::Path;
 
 // TODO: tests
-// TODO: module level docs to explain why this would be needed (e.g. symlinks + filters)
 
-/// Error returned by [`blob()`] and related functions.
-#[derive(Debug, thiserror::Error)]
-#[allow(missing_docs)]
-pub enum Error {
-    #[error("Could not convert symlink path to UTF8")]
-    IllformedUtf8,
-    #[error("IO error while reading blob")]
-    Io(#[from] io::Error),
-}
-
-// TODO: what to do about precompose unicode and ignore case_here?
+// TODO: what to do about precompose unicode and ignore_case for symlinks
 
 /// Create a blob from a file or symlink.
-pub fn blob(path: &Path, capabilities: &fs::Capabilities) -> Result<Blob, Error> {
+pub fn blob(path: &Path, capabilities: &fs::Capabilities) -> io::Result<Blob> {
     let mut buf = Vec::new();
     let res = data_with_buf(path, &mut buf, capabilities)?;
     match res {
@@ -33,7 +25,7 @@ pub fn blob(path: &Path, capabilities: &fs::Capabilities) -> Result<Blob, Error>
 }
 
 /// Create a blob from a file or symlink.
-pub fn blob_with_meta(path: &Path, is_symlink: bool, capabilities: &fs::Capabilities) -> Result<Blob, Error> {
+pub fn blob_with_meta(path: &Path, is_symlink: bool, capabilities: &fs::Capabilities) -> io::Result<Blob> {
     let mut buf = Vec::new();
     let res = data_with_buf_and_meta(path, &mut buf, is_symlink, capabilities)?;
     match res {
@@ -52,7 +44,7 @@ pub fn data_with_buf<'a>(
     path: &Path,
     buf: &'a mut Vec<u8>,
     capabilities: &fs::Capabilities,
-) -> Result<Cow<'a, [u8]>, Error> {
+) -> io::Result<Cow<'a, [u8]>> {
     data_with_buf_and_meta(path, buf, path.symlink_metadata()?.is_symlink(), capabilities)
 }
 
@@ -62,15 +54,15 @@ pub fn data_with_buf_and_meta<'a>(
     buf: &'a mut Vec<u8>,
     is_symlink: bool,
     capabilities: &fs::Capabilities,
-) -> Result<Cow<'a, [u8]>, Error> {
+) -> io::Result<Cow<'a, [u8]>> {
     // symlinks are only stored as actual symlinks if the FS supports it otherwise they are just
     // normal files with their content equal to the linked path (so can be read normally)
     //
     if is_symlink && capabilities.symlink {
-        let symlink_path = path::try_into_bstr(read_link(path)?).map_err(|_| Error::IllformedUtf8)?;
-        Ok(Cow::Owned(
-            path::to_unix_separators_on_windows(symlink_path).into_owned().into(),
-        ))
+        // conversion to bstr can never fail because symlinks are only used
+        // on unix (by git) so no reason to use the try version here
+        let symlink_path = path::into_bstr(read_link(path)?);
+        Ok(Cow::Owned(symlink_path.into_owned().into()))
     } else {
         buf.clear();
         File::open(path)?.read_to_end(buf)?;
