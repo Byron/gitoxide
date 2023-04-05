0d340f4fdeff1576460d43ca2210b11f0641c5dd
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-04-05 08:22:25 +0200

    feat: add `xdg_config_home()`, installation_config` and `installation_config_prefix()` functions.

--- gix-path/tests/path.rs
+++ gix-path/tests/path.rs
@@ -5,12 +5,38 @@ mod realpath;
 mod home_dir {
     #[test]
     fn returns_existing_directory() {
-        if let Some(home) = gix_path::home_dir() {
+        if let Some(home) = gix_path::env::home_dir() {
             assert!(
                 home.is_dir(),
                 "the home directory would typically exist, even though on unix we don't test for that."
             );
         }
+    }
+}
+
+mod xdg_config_path {
+    use std::ffi::OsStr;
+
+    #[test]
+    fn prefers_xdg_config_bases() {
+        let actual = gix_path::env::xdg_config("test", &mut |n| {
+            (n == OsStr::new("XDG_CONFIG_HOME")).then(|| "marker".into())
+        })
+        .expect("set");
+        #[cfg(unix)]
+        assert_eq!(actual.to_str(), Some("marker/git/test"));
+        #[cfg(windows)]
+        assert_eq!(actual.to_str(), Some("marker\\git\\test"));
+    }
+
+    #[test]
+    fn falls_back_to_home() {
+        let actual = gix_path::env::xdg_config("test", &mut |n| (n == OsStr::new("HOME")).then(|| "marker".into()))
+            .expect("set");
+        #[cfg(unix)]
+        assert_eq!(actual.to_str(), Some("marker/.config/git/test"));
+        #[cfg(windows)]
+        assert_eq!(actual.to_str(), Some("marker\\.config\\git\\test"));
     }
 }
 mod util;
