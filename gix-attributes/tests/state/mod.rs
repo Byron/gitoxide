424ad62c852112b1104fe853b71de4dfe3a9f915
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-04-08 09:01:27 +0200

    feat!: an API for matching attributes.
    An implementation for `Search<Attributes>` along with various
    breaking modifications of the original module and type layout.

---
+++ gix-attributes/tests/state/mod.rs
@@ -0,0 +1,23 @@
+use gix_attributes::StateRef;
+
+const ILLFORMED_UTF8: &[u8] = b"\xC3\x28\x41";
+
+mod value {
+    use crate::state::ILLFORMED_UTF8;
+    use gix_attributes::state::ValueRef;
+
+    #[test]
+    fn from_bytes() {
+        assert_eq!(ValueRef::from_bytes(ILLFORMED_UTF8).as_bstr(), ILLFORMED_UTF8);
+        assert_eq!(ValueRef::from_bytes("utf8".as_bytes()).as_bstr(), "utf8");
+    }
+}
+
+#[test]
+fn from_value() {
+    assert!(std::str::from_utf8(ILLFORMED_UTF8).is_err());
+    assert!(
+        matches!(StateRef::from_bytes(ILLFORMED_UTF8), StateRef::Value(v) if v.as_bstr() == ILLFORMED_UTF8),
+        "this can round-trip with care"
+    );
+}
