1e73f3c319caec0298cdd8c34f29ff78ff4060ab
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-04-19 17:00:06 +0200

    feat: add `join_bstr_unix_pathsep(base, component)`.
    It's useful to have to avoid certain conversions to happen otherwise.

--- gix-path/tests/convert/mod.rs
+++ gix-path/tests/convert/mod.rs
@@ -19,3 +19,46 @@ fn assure_windows_separators() {
 }
 
 mod normalize;
+
+mod join_bstr_unix_pathsep {
+    use bstr::BStr;
+    use gix_path::join_bstr_unix_pathsep;
+
+    fn b(s: &str) -> &BStr {
+        s.into()
+    }
+
+    #[test]
+    fn typical_with_double_slash_avoidance() {
+        assert_eq!(join_bstr_unix_pathsep(b("base"), "path"), b("base/path"));
+        assert_eq!(
+            join_bstr_unix_pathsep(b("base/"), "path"),
+            b("base/path"),
+            "no double slashes"
+        );
+        assert_eq!(join_bstr_unix_pathsep(b("/base"), "path"), b("/base/path"));
+        assert_eq!(join_bstr_unix_pathsep(b("/base/"), "path"), b("/base/path"));
+    }
+    #[test]
+    fn relative_base_or_path_are_nothing_special() {
+        assert_eq!(join_bstr_unix_pathsep(b("base"), "."), b("base/."));
+        assert_eq!(join_bstr_unix_pathsep(b("base"), ".."), b("base/.."));
+        assert_eq!(join_bstr_unix_pathsep(b("base"), "../dir"), b("base/../dir"));
+    }
+    #[test]
+    fn absolute_path_produces_double_slashes() {
+        assert_eq!(join_bstr_unix_pathsep(b("/base"), "/root"), b("/base//root"));
+        assert_eq!(join_bstr_unix_pathsep(b("base/"), "/root"), b("base//root"));
+    }
+    #[test]
+    fn empty_path_makes_base_end_with_a_slash() {
+        assert_eq!(join_bstr_unix_pathsep(b("base"), ""), b("base/"));
+        assert_eq!(join_bstr_unix_pathsep(b("base/"), ""), b("base/"));
+    }
+    #[test]
+    fn empty_base_creates_absolute_paths() {
+        assert_eq!(join_bstr_unix_pathsep(b(""), ""), b("/"));
+        assert_eq!(join_bstr_unix_pathsep(b(""), "hi"), b("/hi"));
+        assert_eq!(join_bstr_unix_pathsep(b(""), "/hi"), b("//hi"));
+    }
+}
