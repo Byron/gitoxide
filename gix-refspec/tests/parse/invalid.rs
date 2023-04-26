386876fc541be56d1d7173a58ed6c13564f93c64
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-04-26 21:11:38 +0200

    Add clusterfuzz failure which doesn't actually reproduce.
    It looks like it might use an older version of `gix-date` without the fix
    that catches panics. The panic is visible, after all, but it is turned into
    an error.

--- gix-refspec/tests/parse/invalid.rs
+++ gix-refspec/tests/parse/invalid.rs
@@ -51,3 +51,10 @@ fn push_to_empty() {
         Error::PushToEmpty
     ));
 }
+
+#[test]
+fn fuzzed() {
+    let input = include_bytes!("../fixtures/fuzzed/clusterfuzz-testcase-minimized-gix-refspec-parse-4658733962887168");
+    drop(gix_refspec::parse(input.into(), gix_refspec::parse::Operation::Fetch).unwrap_err());
+    drop(gix_refspec::parse(input.into(), gix_refspec::parse::Operation::Push).unwrap_err());
+}
