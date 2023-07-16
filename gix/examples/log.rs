8a6f1e89fa4d736a2c902be55413887e14885957
Tommy Yu <y@metatoaster.com>
2023-07-16 16:17:05 +1200

    `git log` example iterator now properly lazy
    - Unless reverse is required - the git log DAG is unidirectional, the
      whole graph must be read in order for it to be reversed - libgit2 does
      this internally when the `GIT_SORT_REVERSE` is specified.
    
      See: https://github.com/libgit2/libgit2/blob/v1.6.4/src/libgit2/revwalk.c#L659-L669
    - Include some comments that might hopefully help future readers.

--- gix/examples/log.rs
+++ gix/examples/log.rs
@@ -63,18 +63,25 @@ fn run(args: &Args) -> anyhow::Result<()> {
         Sorting::ByCommitTimeNewestFirst
     };
 
-    let mut log_entries = repo
+    let mut log_iter: Box<dyn Iterator<Item = Result<LogEntryInfo, _>>> = Box::new(repo
         .rev_walk([commit.id])
         .sorting(sorting)
         .all()?
         .filter(|info| info.as_ref()
+            // TODO the other implementation can take a sequence of
+            // paths - if so it should apply this check for all paths.
             .map_or(true, |info| args.path.as_ref().map_or(true, |path| {
                 // TODO should make use of the `git2::DiffOptions`
                 // counterpart in gix for a set of files and also to
                 // generate diffs.
+                // should args.path be provided, check that it is in
+                // fact relevant for this commit (it present?)
                 let oid = repo.rev_parse_single(
                     format!("{}:{}", info.id, path).as_str()
                 ).ok();
+                // check via the revspec on the path prefixed by the
+                // tree of the current commit vs. commit's every parents
+                // and see if all matching, if not, include this entry.
                 !info.parent_ids
                     .iter()
                     .all(|id| repo.rev_parse_single(
@@ -86,7 +93,7 @@ fn run(args: &Args) -> anyhow::Result<()> {
             let info = info?;
             let commit = info.object()?;
             let commit_ref = CommitRef::from_bytes(&commit.data)?;
-            Ok(LogEntryInfo {
+            Ok::<_, anyhow::Error>(LogEntryInfo {
                 commit_id: format!("{}", commit.id()),
                 parents: info.parent_ids.iter()
                     // probably could have a better way to display this
@@ -98,26 +105,22 @@ fn run(args: &Args) -> anyhow::Result<()> {
                 message: commit_ref.message.to_string(),
             })
         })
-        // The more memory efficient way is to only collect if reverse;
-        // this example is lazy coding rather than lazy execution...
-        .collect::<anyhow::Result<Vec<_>>>()?;
-
-    args.reverse.then(|| log_entries.reverse());
-
-    // ... as ultimately we will have an iterator here again...
-    let mut log_iter: Box<dyn Iterator<Item = &LogEntryInfo>> = Box::new(
-        log_entries.iter());
-
+    );
+    if args.reverse {
+        let mut results = log_iter.collect::<Vec<_>>();
+        results.reverse();
+        log_iter = Box::new(results.into_iter())
+    }
     if let Some(n) = args.skip {
         log_iter = Box::new(log_iter.skip(n));
     }
     if let Some(n) = args.count {
         log_iter = Box::new(log_iter.take(n));
     }
-
     let mut log_iter = log_iter.peekable();
 
     while let Some(entry) = log_iter.next() {
+        let entry = entry?;
         println!("commit {}", entry.commit_id);
         if entry.parents.len() > 1 {
             println!("Merge: {}", entry.parents.join(" "));
@@ -127,6 +130,7 @@ fn run(args: &Args) -> anyhow::Result<()> {
         for line in entry.message.lines() {
             println!("    {line}");
         }
+        // only include newline if more log entries, mimicking `git log`
         if log_iter.peek().is_some() {
             println!();
         }
