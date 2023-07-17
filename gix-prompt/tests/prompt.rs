41389d690b4302b728c197d4c46372509b2c3911
Niklas Wimmer <mail@nwimmer.me>
2023-07-17 22:04:08 +0200

    evaluate target dir at runtime for gix-prompt tests
    The hardcoded `./target` path fails for environments where Cargo is
    configured to use a different target directory (for example via
    `CARGO_TARGET_DIR`).
    
    Instead of invoking `jq` to extract the relevant information from `cargo
    metadata` it would have also been possible to use a library like
    `serde_json` to perform this operation without shelling out. However,
    because most development machines will have `jq` already installed, I feel
    like there is not enough justification to add a new dependency just for
    this single use-case.

--- gix-prompt/tests/prompt.rs
+++ gix-prompt/tests/prompt.rs
@@ -1,6 +1,37 @@
 mod options;
 
 mod ask {
+    use gix_testtools::bstr::ByteSlice;
+
+    /// Evaluates Cargo's target directory for this project at runtime to adjust for the concrete
+    /// execution environment. This is necessary because certain environment variables and
+    /// configuration options can change its location (e.g. CARGO_TARGET_DIR).
+    fn evaluate_target_dir() -> String {
+        let manifest_proc = std::process::Command::new(env!("CARGO"))
+            .args(["metadata", "--format-version", "1"])
+            .stdout(std::process::Stdio::piped())
+            .spawn()
+            .unwrap();
+
+        let jq_proc = std::process::Command::new("jq")
+            .args(["-r", ".target_directory"]) // -r makes it output raw strings
+            .stdin(manifest_proc.stdout.unwrap())
+            .stdout(std::process::Stdio::piped())
+            .spawn()
+            .expect("jq utility is available in PATH");
+
+        let output = jq_proc
+            .wait_with_output()
+            .expect(".target_directory is a valid search path for manifest format version 1");
+
+        output
+            .stdout
+            .trim()
+            .to_str()
+            .expect("value of target_directory is valid UTF8")
+            .to_owned()
+    }
+
     #[test]
     #[cfg(unix)]
     fn askpass_only() {
@@ -8,7 +39,7 @@ mod ask {
         cmd.args(["build", "--example", "use-askpass", "--example", "askpass"]);
         cmd.spawn().unwrap().wait().expect("example builds OK");
 
-        let mut p = expectrl::spawn("../target/debug/examples/use-askpass").unwrap();
+        let mut p = expectrl::spawn(evaluate_target_dir() + "/debug/examples/use-askpass").unwrap();
         p.expect("Password: ").unwrap();
         p.send_line(" password with space ").unwrap();
         p.expect("\" password with space \"").unwrap();
@@ -22,7 +53,7 @@ mod ask {
         cmd.args(["build", "--example", "credentials"]);
         cmd.spawn().unwrap().wait().expect("example builds OK");
 
-        let mut p = expectrl::spawn("../target/debug/examples/credentials").unwrap();
+        let mut p = expectrl::spawn(evaluate_target_dir() + "/debug/examples/credentials").unwrap();
         p.expect("Username: ").unwrap();
         p.send_line(" user with space ").unwrap();
         p.expect("\" user with space\"").unwrap();
