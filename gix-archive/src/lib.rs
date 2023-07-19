412067c3b7b5b7568d3ab37190987195381d7299
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-07-19 12:40:56 +0200

    remove the `Format` type in favor of an incredibly simple streaming format
    The idea is to only get the vital information in a format that can easily
    be decoded by the API user. This way, all container formats are implemented
    in the calling crate or application.
    
    That way we avoid having to deal with feature toggles for the various utility
    crates that would implement the stream formats.

--- gix-archive/src/lib.rs
+++ gix-archive/src/lib.rs
@@ -1,63 +1,19 @@
 //! The implementation of creating an archive from a git tree, similar to `git archive`.
-#![deny(rust_2018_idioms, missing_docs)]
-#![forbid(unsafe_code)]
+#![deny(rust_2018_idioms, missing_docs, unsafe_code)]
 
-/// The error returned by [`write_to()`].
-#[derive(Debug, thiserror::Error)]
-#[allow(missing_docs)]
-pub enum Error<E>
-where
-    E: std::error::Error + Send + Sync + 'static,
-{
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
-    #[error("Could not find a blob or tree for archival")]
-    Find(#[source] E),
-}
+use gix_object::bstr::BString;
+use std::sync::Arc;
 
-/// The supported container formats for use in [`write_to()`].
-#[derive(Default, PartialEq, Eq, Copy, Clone, Debug)]
-pub enum Format {
-    /// A standard `tar` archive.
-    ///
-    /// Use it as well if a custom container format is desired. The idea is to decode it on a separate thread
-    /// to rewrite the data to the desired format.
-    #[default]
-    Tar,
-    /// A convenience format that will `zip` deflate the `tar` stream.
-    TarGz {
-        /// The compression level to use for the `zlib` compression, ranging from 0 (no compression) to 9 (best compression).
-        compression_level: u8,
-    },
-    /// Use the zip` container format, instead of `tar`, provided for convenience.
-    Zip {
-        /// The compression level to use for the `zlib` compression, ranging from 0 (no compression) to 9 (best compression).
-        compression_level: u8,
-    },
-}
-
-/// Options for configuring [`write_to()`].
-#[derive(Clone, Debug)]
-pub struct Options {
-    /// The archive's format.
-    pub format: Format,
-    /// Given a `path`, originating in the git tree, to place into the archive, put `<prefix>/path` in front of it.
-    pub tree_prefix: Option<String>,
-    /// The modification time for all entries in the archive.
-    ///
-    /// Defaults to the current time. The caller may set this to the commit time if available.
-    pub modification_time: std::time::SystemTime,
-}
-
-impl Default for Options {
-    fn default() -> Self {
-        Options {
-            format: Default::default(),
-            tree_prefix: None,
-            modification_time: std::time::SystemTime::now(),
-        }
-    }
-}
+///
+pub mod stream;
 
 mod write;
 pub use write::write_to;
+
+/// A stream of entries that is produced from an underlying reader.
+pub struct Stream {
+    read: gix_features::io::pipe::Reader,
+    err: Arc<parking_lot::Mutex<Option<stream::Error>>>,
+    /// `None` if currently held by an entry.
+    path_buf: Option<BString>,
+}
--- gix-archive/src/write.rs
+++ gix-archive/src/write.rs
@@ -1,7 +1,8 @@
-use crate::{Error, Options};
+use crate::Stream;
 use gix_object::bstr::BStr;
 
-/// Use `find` to traverse `tree` and fetch the contained blobs to write to `out` configured according to `opts`.
+/// Use `find` to traverse `tree` and fetch the contained blobs to return as [`Stream`], which makes them queryable
+/// on demand with support for streaming each entry.
 /// `pipeline` is used to convert blobs to their worktree representation, and `attributes` is used to read
 /// the `export-ignore` attribute. If set on a directory or blob, it won't be added to the archive.
 ///
@@ -9,23 +10,28 @@ use gix_object::bstr::BStr;
 ///
 /// For per-file progress, integrate progress handling into `find` as it is called for trees and blobs.
 /// `find` should also be used for interrupt handling, as it can return an error once per file.
-/// For progress on bytes-written, integrate progress reporting into `out`.
+/// For progress on bytes-written, integrate progress reporting when consuming the stream.
+/// Further it's possible to drop the returned [`Stream`] to halt all operation.
+///
+/// ### Threaded Operation
+///
+/// This function spawns a thread that will access the tree data in the background, synchronized through
+/// `Stream` so that it will not be faster than the consumer, with at most one file in flight at any time.
 ///
 /// ### Limitations
 ///
 /// * `export-subst` is not support, as it requires the entire formatting engine of `git log`.
-pub fn write_to<W, Find, E>(
-    _tree: &gix_hash::oid,
+pub fn write_to<W, Find, E1, E2>(
+    _tree: gix_hash::ObjectId,
     mut _find: Find,
     _pipeline: &mut gix_filter::Pipeline,
-    _attributes: impl FnOnce(&BStr, &mut gix_attributes::search::Outcome),
-    mut _out: W,
-    _opts: Options,
-) -> Result<(), Error<E>>
+    _attributes: impl FnMut(&BStr, &mut gix_attributes::search::Outcome) -> Result<(), E2> + Send,
+) -> Stream
 where
     W: std::io::Write,
-    Find: for<'a> FnMut(&gix_hash::oid, &'a mut Vec<u8>) -> Result<gix_object::Data<'a>, E>,
-    E: std::error::Error + Send + Sync + 'static,
+    Find: for<'a> FnMut(&gix_hash::oid, &'a mut Vec<u8>) -> Result<gix_object::Data<'a>, E1> + Send,
+    E1: std::error::Error + Send + Sync + 'static,
+    E2: std::error::Error + Send + Sync + 'static,
 {
-    Ok(())
+    todo!()
 }
