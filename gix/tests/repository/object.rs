d4a8f8cf6d8b059978719ea314fc8a4bfe26c60d
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-07-19 08:22:58 +0200

    feat: Add `Id::header()` and `Id::try_header()` as syblings to `::object()` and `::try_object()`.
    With the new header related functions one can obtain information about an object more quickly.

--- gix/tests/repository/object.rs
+++ gix/tests/repository/object.rs
@@ -129,6 +129,7 @@ mod find {
             for commit_id in repo.head()?.peeled()?.id().expect("born").ancestors().all()? {
                 let commit = commit_id?;
                 assert_eq!(commit.id().object()?.kind, gix_object::Kind::Commit);
+                assert_eq!(commit.id().header()?.kind(), gix_object::Kind::Commit);
                 if round == 2 {
                     assert_eq!(
                         commit.id().object()?.kind,
@@ -138,6 +139,10 @@ mod find {
                 }
                 assert_eq!(
                     commit.id().try_object()?.expect("exists").kind,
+                    gix_object::Kind::Commit,
+                );
+                assert_eq!(
+                    commit.id().try_header()?.expect("exists").kind(),
                     gix_object::Kind::Commit,
                 );
             }
