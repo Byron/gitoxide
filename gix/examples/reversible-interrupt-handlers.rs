46225c2bc399e6db5a56b522c978e0d1fac163df
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-08-09 15:43:19 +0200

    feat!: improve `interrupt::init_handler()` to be usable from multiple threads
    Previously it was geared towards applications which would initialize handlers
    only from the main thread.
    
    Now the API supports multiple threads.

--- gix/examples/reversible-interrupt-handlers.rs
+++ gix/examples/reversible-interrupt-handlers.rs
@@ -1,6 +1,6 @@
 fn main() -> anyhow::Result<()> {
     {
-        let _deregister_on_drop = gix::interrupt::init_handler(|| {})?.auto_deregister();
+        let _deregister_on_drop = gix::interrupt::init_handler(1, || {})?.auto_deregister();
     }
     eprintln!("About to emit the first term signal, which acts just like a normal one");
     signal_hook::low_level::raise(signal_hook::consts::SIGTERM)?;
