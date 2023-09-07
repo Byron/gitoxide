799a5152c7ca444a8240a022e049c14b0b61d22d
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-09-07 21:04:02 +0200

    simplify test-suite

--- gix/tests/gix.rs
+++ gix/tests/gix.rs
@@ -1,30 +1,16 @@
-#[cfg(not(feature = "revparse-regex"))]
 mod util;
-#[cfg(not(feature = "revparse-regex"))]
 use util::*;
 
-#[cfg(not(feature = "revparse-regex"))]
 mod clone;
-#[cfg(not(feature = "revparse-regex"))]
 mod commit;
-#[cfg(not(feature = "revparse-regex"))]
 mod config;
-#[cfg(not(feature = "revparse-regex"))]
 mod head;
-#[cfg(not(feature = "revparse-regex"))]
 mod id;
-#[cfg(not(feature = "revparse-regex"))]
 mod init;
-#[cfg(not(feature = "revparse-regex"))]
 mod object;
-#[cfg(not(feature = "revparse-regex"))]
 mod reference;
-#[cfg(not(feature = "revparse-regex"))]
 mod remote;
-#[cfg(not(feature = "revparse-regex"))]
 mod repository;
-#[cfg(not(feature = "revparse-regex"))]
 #[cfg(feature = "revision")]
 mod revision;
-#[cfg(not(feature = "revparse-regex"))]
 mod submodule;
