ee92af46d419980887afaa08d35fed83aef49c0c
Wyatt Hauben <whauben11@gmail.com>
2023-09-26 16:58:46 -0400

    Created FetchHead struct to encode FETCH_HEAD data.

--- gix-fetchhead/src/lib.rs
+++ gix-fetchhead/src/lib.rs
@@ -1,2 +1,16 @@
 #![deny(rust_2018_idioms)]
 #![forbid(unsafe_code)]
+
+/// Represents parsed data from the FETCH_HEAD file.
+#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct FetchHead {
+
+    //Head of fetched repository
+    pub head: gix_hash::ObjectId,
+
+    //Branch name
+    pub branch: &'static str,
+
+    //Remote url
+    pub remote: gix_url::Url,
+}
