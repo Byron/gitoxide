a5300378899d205ce96f53581b98d3149e1e8c66
Niklas Wimmer <mail@nwimmer.me>
2023-09-29 18:12:59 +0200

    Improve output of the gix-url baseline test

--- gix-url/tests/fixtures/make_baseline.sh
+++ gix-url/tests/fixtures/make_baseline.sh
@@ -8,11 +8,12 @@ tests_unix=()
 # urls only intended for testing on Windows
 tests_windows=()
 
-# The contents and structure of this loop are a adaption
+# The contents and structure of this loop are an adaption
 # from git's own test suite (t/t5500-fetch-pack.sh).
 # Please do not change this loop and instead add additional
 # test cases at the bottom of this file.
 for path in "repo" "re:po" "re/po"; do
+  # normal urls
   for protocol in "ssh+git" "git+ssh" "git" "ssh"; do
     for host in "host" "user@host" "user@[::1]" "user@::1"; do
       for port_separator in "" ":"; do
@@ -25,6 +26,7 @@ for path in "repo" "re:po" "re/po"; do
       tests+=("$protocol://$host:22/$path")
     done
   done
+  # file protocol urls
   for protocol in "file"; do
     tests_unix+=("$protocol://$host/$path")
 
@@ -34,14 +36,14 @@ for path in "repo" "re:po" "re/po"; do
     tests_unix+=("$protocol://$host/~$path")
     tests_windows+=("$protocol://$host/~$path")
   done
+  # local paths
   for host in "nohost" "nohost:12" "[::1]" "[::1]:23" "[" "[:aa"; do
     tests+=("./$host:$path")
     tests+=("./$protocol:$host/~$path")
   done
-  protocol="ssh"
+  # SCP like urls
   for host in "host" "[::1]"; do
     tests+=("$host:$path")
-
     tests+=("$host:/~$path")
   done
 done
