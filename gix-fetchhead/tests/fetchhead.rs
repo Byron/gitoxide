a17c0ae1b75a30f0279a7ee79ac13a5163ad86f3
Wyatt Hauben <whauben11@gmail.com>
2023-10-13 17:28:42 -0400

    fixed testing working dir issue.

--- gix-fetchhead/tests/fetchhead.rs
+++ gix-fetchhead/tests/fetchhead.rs
@@ -1,3 +1,9 @@
+use std::path::{Path, PathBuf};
+
 pub use gix_testtools::Result;
+
+fn fixture_path(path: impl AsRef<Path>) -> PathBuf {
+    PathBuf::from("tests").join("fixtures").join(path.as_ref())
+}
 
 mod parse;
--- gix-fetchhead/tests/parse/mod.rs
+++ gix-fetchhead/tests/parse/mod.rs
@@ -1,18 +1,17 @@
-use std::{/*fs::File, io::Read,*/ env, path::PathBuf};
+use std::{fs::File, io::Read, env};
+use crate::fixture_path;
+
 
 #[test]
 fn git_repo_test() -> crate::Result {
-    println!("TESTING");
-    //let path = gix_testtools::fixture_path("git");
-    //let path = gix_testtools::fixture_path_standalone("git");
-    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    path.push("fixtures/git");
+    let path = fixture_path("git");
     println!("{path:?}");
-    // let mut source = File::open(&path)?;
-    // let mut s = String::new();
-    // match source.read_to_string(&mut s) {
-    //     Err(why) => panic!("couldn't read {path:?}: {why}"),
-    //     Ok(_) => print!("{path:?} contains:\n{s}"),
-    // }
+    println!("{:?}", env::current_dir());
+    let mut source = File::open(&path)?;
+    let mut s = String::new();
+    match source.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {path:?}: {why}"),
+        Ok(_) => print!("{path:?} contains:\n{s}"),
+    }
     Ok(())
 }
