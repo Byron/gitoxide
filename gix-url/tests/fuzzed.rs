d78f445449e534bbd858a8db1a16ad837417a78f
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-10-30 13:31:19 +0100

    Add new test-case to reproduce gix-url failure in debug mode only (#1082)

--- gix-url/tests/fuzzed.rs
+++ gix-url/tests/fuzzed.rs
@@ -3,6 +3,23 @@ use std::path::Path;
 use std::time::Duration;
 
 #[test]
+#[cfg_attr(debug_assertions, ignore = "fails due to integer overflow")]
+fn abort_unfixed_in_debug_mode() {
+    let name = "very-long-abort";
+    let base = Path::new("tests").join("fixtures").join("fuzzed");
+    let location = base.join(Path::new(name).with_extension("url"));
+    let url = std::fs::read(&location).unwrap();
+    let start = std::time::Instant::now();
+    gix_url::parse(url.as_bstr()).ok();
+    assert!(
+        start.elapsed() < Duration::from_millis(100),
+        "URL at '{}' parsed too slowly, took {:.00}s",
+        location.display(),
+        start.elapsed().as_secs_f32()
+    );
+}
+
+#[test]
 fn fuzzed() {
     for name in [
         "very-long6",
