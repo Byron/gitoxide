be1fa2f6370501c6b8bbe9bb89f6ef5507b5f425
Urgau <urgau@numericable.fr>
2023-11-19 14:21:21 +0100

    Remove dead code from non-existent "lean-cli" feature

--- src/shared.rs
+++ src/shared.rs
@@ -270,28 +270,6 @@ pub fn setup_line_renderer_range(
     )
 }
 
-#[cfg(all(feature = "lean-cli", not(feature = "pretty-cli")))]
-pub fn from_env<T: argh::TopLevelCommand>() -> T {
-    static VERSION: &str = concat!(env!("CARGO_PKG_NAME"), " ", env!("CARGO_PKG_VERSION"));
-    let strings: Vec<String> = std::env::args().collect();
-    let strs: Vec<&str> = strings.iter().map(|s| s.as_str()).collect();
-    T::from_args(&[strs[0]], &strs[1..]).unwrap_or_else(|early_exit| {
-        // This allows us to make subcommands mandatory,
-        // and trigger a helpful message unless --version is specified
-        if let Some(arg) = std::env::args().nth(1) {
-            if arg == "--version" {
-                println!("{}", VERSION);
-                std::process::exit(0);
-            }
-        }
-        println!("{}", early_exit.output);
-        std::process::exit(match early_exit.status {
-            Ok(()) => 0,
-            Err(()) => 1,
-        })
-    })
-}
-
 mod clap {
     use std::{ffi::OsStr, str::FromStr};
 
