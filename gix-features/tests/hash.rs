5c2d615548dd27f23586e4fe5cfa8c6507635694
Alexander Kjäll <alexander.kjall@gmail.com>
2023-11-27 18:16:18 +0100

    test size_of_sha1 reports wrong size on i386

--- gix-features/tests/hash.rs
+++ gix-features/tests/hash.rs
@@ -6,7 +6,13 @@ fn size_of_sha1() {
     assert_eq!(std::mem::size_of::<Sha1>(), 96)
 }
 
-#[cfg(feature = "fast-sha1")]
+#[cfg(all(feature = "fast-sha1", target_arch = "x86"))]
+#[test]
+fn size_of_sha1() {
+    assert_eq!(std::mem::size_of::<Sha1>(), 96)
+}
+
+#[cfg(all(feature = "fast-sha1", not(target_arch = "x86")))]
 #[test]
 fn size_of_sha1() {
     assert_eq!(std::mem::size_of::<Sha1>(), 104)
