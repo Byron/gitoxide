1d45f056263ba9c69612abc81fdd3783f95de4ca
Alexander Kjäll <alexander.kjall@gmail.com>
2023-11-27 20:47:40 +0100

    review feedback, use an 'if cfg' construct instead

--- gix-features/tests/hash.rs
+++ gix-features/tests/hash.rs
@@ -6,14 +6,8 @@ fn size_of_sha1() {
     assert_eq!(std::mem::size_of::<Sha1>(), 96)
 }
 
-#[cfg(all(feature = "fast-sha1", target_arch = "x86"))]
+#[cfg(feature = "fast-sha1")]
 #[test]
 fn size_of_sha1() {
-    assert_eq!(std::mem::size_of::<Sha1>(), 96)
-}
-
-#[cfg(all(feature = "fast-sha1", not(target_arch = "x86")))]
-#[test]
-fn size_of_sha1() {
-    assert_eq!(std::mem::size_of::<Sha1>(), 104)
+    assert_eq!(std::mem::size_of::<Sha1>(), if cfg!(target_arch = "x86") { 96 } else { 104 })
 }
