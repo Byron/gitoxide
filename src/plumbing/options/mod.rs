cf51a4de2d06eb28435ef8e386131710003b6928
Sebastian Thiel <sebastian.thiel@icloud.com>
2023-12-05 09:04:14 +0100

    feat: `gix rev parse --format` to provide different versions of the same content.
    This only applies to blobs, but allows to obtain different versions of the same blob
    like:
    
    * what's stored in Git
    * what would be checked out to the worktree
    * what would be diffed

--- src/plumbing/options/mod.rs
+++ src/plumbing/options/mod.rs
@@ -614,6 +614,19 @@ pub mod revision {
             #[default]
             Pretty,
         }
+
+        #[derive(Default, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, clap::ValueEnum)]
+        pub enum BlobFormat {
+            /// The version stored in the Git Object Database.
+            #[default]
+            Git,
+            /// The version that would be checked out into the worktree, including filters.
+            Worktree,
+            /// The version that would be diffed (Worktree + Text-Conversion)
+            Diff,
+            /// The version that would be diffed if there is a text-conversion, or the one stored in Git otherwise.
+            DiffOrGit,
+        }
     }
     #[derive(Debug, clap::Subcommand)]
     #[clap(visible_alias = "rev", visible_alias = "r")]
@@ -645,8 +658,12 @@ pub mod revision {
             /// Show the first resulting object similar to how `git cat-file` would, but don't show the resolved spec.
             #[clap(short = 'c', long, conflicts_with = "explain")]
             cat_file: bool,
-            #[clap(short = 't', long)]
-            tree_mode: Option<resolve::TreeMode>,
+            /// How to display blobs.
+            #[clap(short = 'b', long, default_value = "git")]
+            blob_format: resolve::BlobFormat,
+            /// How to display trees as obtained with `@:dirname` or `@^{tree}`.
+            #[clap(short = 't', long, default_value = "pretty")]
+            tree_mode: resolve::TreeMode,
             /// rev-specs like `@`, `@~1` or `HEAD^2`.
             #[clap(required = true)]
             specs: Vec<std::ffi::OsString>,
