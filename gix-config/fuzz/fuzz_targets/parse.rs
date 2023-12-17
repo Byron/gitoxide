8053710e2940ced8ab8a7805de3bb94408352a9e
Nathaniel Brough <nathaniel.brough@gmail.com>
2023-12-16 18:02:21 -0800

    feat: Fuzz more of the gix-config api

--- gix-config/fuzz/fuzz_targets/parse.rs
+++ gix-config/fuzz/fuzz_targets/parse.rs
@@ -1,8 +1,19 @@
 #![no_main]
 
+use arbitrary::Arbitrary;
+use gix_config::{parse::Events, File};
 use libfuzzer_sys::fuzz_target;
+use std::hint::black_box;
 
-fuzz_target!(|data: &[u8]| {
-    // Don't name this _; Rust may optimize it out.
-    let _a = gix_config::parse::from_bytes(data, &mut |_e| ());
+#[derive(Debug, Arbitrary, Clone)]
+struct Ctx<'a> {
+    parse_from_bytes: &'a [u8],
+    parse_file: &'a str,
+    parse_events: &'a str,
+}
+
+fuzz_target!(|ctx: Ctx| {
+    _ = black_box(gix_config::parse::from_bytes(ctx.parse_from_bytes, &mut |_e| ()));
+    _ = black_box(File::try_from(ctx.parse_file));
+    _ = black_box(Events::try_from(ctx.parse_events));
 });
