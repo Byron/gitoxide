d83b4e0b16ba7cf28145184d76cef5f89143f641
Nathaniel Brough <nathaniel.brough@gmail.com>
2023-12-22 17:23:30 -0800

    Fuzz more of gix-ref log

--- gix-ref/fuzz/fuzz_targets/fuzz_log.rs
+++ gix-ref/fuzz/fuzz_targets/fuzz_log.rs
@@ -1,17 +1,34 @@
 #![no_main]
 
 use anyhow::Result;
+use arbitrary::Arbitrary;
 use gix_ref::file::log;
 use libfuzzer_sys::fuzz_target;
 use std::hint::black_box;
 
-fn fuzz(line: &[u8]) -> Result<()> {
-    let line = log::LineRef::from_bytes(line)?;
+#[derive(Arbitrary, Debug)]
+struct Ctx<'a> {
+    line_ref: &'a [u8],
+    multi_line_reverse: &'a [u8],
+    multi_line_forward: &'a [u8],
+}
+
+fn fuzz(ctx: Ctx) -> Result<()> {
+    let line = log::LineRef::from_bytes(ctx.line_ref)?;
     _ = black_box(line.previous_oid());
     _ = black_box(line.new_oid());
+
+    let mut buf = [0u8; 1024];
+    let read = std::io::Cursor::new(ctx.multi_line_reverse);
+    let mut iter = gix_ref::file::log::iter::reverse(read, &mut buf)?;
+    _ = black_box(iter.map(|x| black_box(x)).count());
+
+    let mut iter = gix_ref::file::log::iter::forward(ctx.multi_line_forward);
+    _ = black_box(iter.map(|x| black_box(x)).count());
+
     Ok(())
 }
 
-fuzz_target!(|ctx: &[u8]| {
+fuzz_target!(|ctx: Ctx| {
     _ = black_box(fuzz(ctx));
 });
