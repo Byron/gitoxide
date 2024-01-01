85476a2d2648266a0be9b1888dd4bdc09a317335
Sebastian Thiel <sebastian.thiel@icloud.com>
2024-01-01 10:09:53 +0100

    optimize fuzzer allocations for slightly better performance

--- gix-config-value/fuzz/fuzz_targets/fuzz_value.rs
+++ gix-config-value/fuzz/fuzz_targets/fuzz_value.rs
@@ -4,14 +4,14 @@ extern crate libfuzzer_sys;
 
 use anyhow::Result;
 use arbitrary::Arbitrary;
-use bstr::{BStr, BString};
+use bstr::BStr;
 use gix_config_value::{
     color::{Attribute, Name},
     path::interpolate::Context,
     Boolean, Color, Integer, Path,
 };
 use libfuzzer_sys::fuzz_target;
-use std::{borrow::Cow, hint::black_box, str::FromStr};
+use std::{borrow::Cow, fmt::Write, hint::black_box, str::FromStr};
 
 #[derive(Debug, Arbitrary)]
 struct Ctx<'a> {
@@ -29,16 +29,17 @@ fn fuzz(ctx: Ctx) -> Result<()> {
 
     _ = black_box(Color::try_from(BStr::new(ctx.color_str)))?;
 
+    let mut buf = String::with_capacity(128);
     let a = Attribute::from_str(ctx.attribute_str)?;
-    _ = black_box(format!("{a}"));
+    _ = black_box(write!(&mut buf, "{a}"));
 
     let name = Name::from_str(ctx.name_str)?;
-    _ = black_box(format!("{name}"));
+    _ = black_box(write!(&mut buf, "{name}"));
 
     let i = Integer::try_from(BStr::new(ctx.integer_str))?;
     _ = black_box(i.to_decimal());
 
-    let p = Path::try_from(Cow::Owned(BString::from(ctx.path_str)))?;
+    let p = Path::from(Cow::Borrowed(BStr::new(ctx.path_str)));
     _ = black_box(p.interpolate(Context::default()));
 
     Ok(())
