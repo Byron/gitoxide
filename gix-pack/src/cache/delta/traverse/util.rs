def27ae510ac97d19fb97f2cfe737bd2a4b6be81
Martin von Zweigbergk <martinvonz@google.com>
2024-01-02 22:25:59 -0800

    make ItemSliceSync::get_mut() check that the index is in range
    This removes some of the unsafety from
    ItemSliceSync::get_mut(). There's still the unsafety that the caller
    needs to make sure a single index is not used concurrently.

--- gix-pack/src/cache/delta/traverse/util.rs
+++ gix-pack/src/cache/delta/traverse/util.rs
@@ -5,6 +5,7 @@ where
     T: Send,
 {
     items: *mut T,
+    len: usize,
     phantom: PhantomData<&'a T>,
 }
 
@@ -15,13 +16,18 @@ where
     pub fn new(items: &'a mut [T]) -> Self {
         ItemSliceSync {
             items: items.as_mut_ptr(),
+            len: items.len(),
             phantom: PhantomData,
         }
     }
 
-    /// SAFETY: The index must point into the slice and must not be reused concurrently.
+    // SAFETY: The index must not be reused concurrently
     #[allow(unsafe_code)]
     pub unsafe fn get_mut(&self, index: usize) -> &'a mut T {
+        if index >= self.len {
+            panic!("index out of bounds: the len is {} but the index is {index}", self.len);
+        }
+        // SAFETY: The index is within the slice
         // SAFETY: The children array is alive by the 'a lifetime.
         unsafe { &mut *self.items.add(index) }
     }
