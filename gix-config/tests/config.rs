6bb407fe2bdc98a3a34172932642f1eff8c4c576
Sebastian Thiel <sebastian.thiel@icloud.com>
2024-01-05 11:03:09 +0100

    add test for memory usage in worse-case scenario

--- gix-config/tests/config.rs
+++ gix-config/tests/config.rs
@@ -1,6 +1,7 @@
 pub use gix_testtools::Result;
 
 mod file;
+mod mem;
 mod parse;
 mod source;
 mod value;
---
+++ gix-config/tests/mem.rs
@@ -0,0 +1,33 @@
+use bytesize::ByteSize;
+use cap::Cap;
+use std::alloc;
+use std::time::Instant;
+
+#[global_allocator]
+static ALLOCATOR: Cap<alloc::System> = Cap::new(alloc::System, usize::max_value());
+
+#[test]
+fn usage() {
+    let before = ALLOCATOR.allocated();
+    let start = Instant::now();
+    let config = gix_config::File::from_bytes_no_includes(
+        include_bytes!("fixtures/fuzzed/mem-amplification.config"),
+        gix_config::file::Metadata::from(gix_config::Source::User),
+        Default::default(),
+    )
+    .unwrap();
+    let after = ALLOCATOR.allocated();
+    let used = after - before;
+    let elapsed = start.elapsed().as_secs_f32();
+    eprintln!(
+        "used mem: {}B for {} sections, took {elapsed:.02}s [total-mem: {total}, peak-mem: {peak}]",
+        ByteSize(used as u64),
+        config.sections().count(),
+        total = ByteSize(ALLOCATOR.total_allocated() as u64),
+        peak = ByteSize(ALLOCATOR.max_allocated() as u64),
+    );
+    assert!(
+        used < 60 * 1024 * 1024,
+        "we should now start using more memory than anticipated, to keep mem-amplification low"
+    );
+}
