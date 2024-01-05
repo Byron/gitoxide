096fd1215bacfaa8c798c2746716eecb1b655ac6
Bittrance <bittrance@gmail.com>
2024-01-05 10:50:54 +0100

    Key trait docs.

--- gix-config/src/traits.rs
+++ gix-config/src/traits.rs
@@ -1,11 +1,11 @@
 use bstr::BStr;
 
-/// TODO
+/// Parsed elements of a Git config key, like `remote.origin.url` or `core.bare`.
 pub trait Key {
-    /// TODO
+    /// The name of the section key, like `url` in `remote.origin.url`.
     fn name(&self) -> &str;
-    /// TODO
+    /// The name of the section, like `core` in `core.bare`.
     fn section_name(&self) -> &str;
-    /// TODO
+    /// The name of the sub-section, like `origin` in `remote.origin.url`.
     fn subsection_name(&self) -> Option<&BStr>;
 }
