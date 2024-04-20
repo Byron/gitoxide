474bf0dc6efae8c939b963a47c8139cf5710a617
Eliah Kagan <degeneracypressure@gmail.com>
2024-04-20 14:54:33 -0400

    Make the script more robust, and don't require `ex`

--- gix-worktree/tests/fixtures/make_traverse_dotdot_trees.sh
+++ gix-worktree/tests/fixtures/make_traverse_dotdot_trees.sh
@@ -3,27 +3,35 @@
 set -eu
 
 repo="$1"
+target_bin='.cargo/bin'
+
 git init -- "$repo"
 cd -- "$repo"
 
-cat >ls.tmp <<'EOF'
+cat >payload <<'EOF'
 #!/bin/sh
 printf 'Vulnerable!\n'
 date >~/vulnerable
 exec /bin/ls "$@"
 EOF
-chmod +x ls.tmp
+chmod +x payload
 
 upward='..'
 for subdir in .a .b .c .d .e .f .g .h .i .j; do
-    upward="..@$upward"
-    cp -- ls.tmp "$subdir@$upward@.cargo@bin@ls"
+    upward="../$upward"
+    target="$subdir/$upward/$target_bin/ls"
+    standin="$(printf '%s' "$target" | tr / @)"
+
     mkdir -- "$subdir"
     touch -- "$subdir/.keep"
+    cp -- payload "$standin"
+    git add -- "$subdir/.keep" "$standin"
+
+    standin_pattern="$(printf '%s' "$standin" | sed 's|\.|\\\.|g')"
+    cp .git/index old_index
+    sed "s|$standin_pattern|$target|g" old_index >.git/index
 done
 
-rm ls.tmp
-git add .
-ex -s -c '%s/@\.\./\/../g' -c '%s/@\.cargo@bin@ls/\/.cargo\/bin\/ls/g' -c 'x' .git/index
 git commit -m 'Initial commit'
+rm payload old_index
 git show --stat
