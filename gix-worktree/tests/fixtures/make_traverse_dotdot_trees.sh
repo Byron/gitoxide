0d15e5c561a3aaf2eac439b022b95e5b0ebdde51
Eliah Kagan <degeneracypressure@gmail.com>
2024-04-22 20:30:16 -0400

    No need to actually create the directories
    Because committing the staged paths creates the necessary Git tree
    objects irrespective of what directories exist or are otherwise
    represented.
    
    In addition to simplifying the proof-of-concept repository, this
    also makes it so its entries are properly ordered in its Git object
    database, so `git fsck` does not report errors about that, and
    exits reporting success (though of course still warns about the
    presence of `..` components).

--- gix-worktree/tests/fixtures/make_traverse_dotdot_trees.sh
+++ gix-worktree/tests/fixtures/make_traverse_dotdot_trees.sh
@@ -22,10 +22,8 @@ for subdir in .a .b .c .d .e .f .g .h .i .j; do
     target="$subdir/$upward/$target_bin/ls"
     standin="$(printf '%s' "$target" | tr / @)"
 
-    mkdir -- "$subdir"
-    touch -- "$subdir/.keep"
     cp -- payload "$standin"
-    git add -- "$subdir/.keep" "$standin"
+    git add -- "$standin"
 
     standin_pattern="$(printf '%s' "$standin" | sed 's|\.|\\\.|g')"
     cp .git/index old_index
