89ee1806c91692af9266fb9fad6dd1b235292dad
Eliah Kagan <degeneracypressure@gmail.com>
2024-05-01 04:23:50 -0400

    Reword to be more portable and self-documenting
    This requires xxd now, but it honors its /bin/sh hashbang line, no
    longer assuming printf understands \xNN in a format string.

--- gix-worktree/tests/fixtures/make_traverse_dotgit_slashes.sh
+++ gix-worktree/tests/fixtures/make_traverse_dotgit_slashes.sh
@@ -12,23 +12,22 @@ date >vulnerable
 EOF
 }
 
-# Initialize the repository.
 repo="$1"
 git init -- "$repo"
 cd -- "$repo"
-branch="$(git symbolic-ref --short HEAD)"
 
-# Create the blob of the payload.
 blob_hash="$(emit_payload | git hash-object -w --stdin)"
-escaped_blob_hash="$(printf '%s' "$blob_hash" | sed 's/../\\x&/g')"
+printf '%s' "$blob_hash" | xxd -r -p >blob-hash-bytes
 
-# Create the top-level tree object referencing the blob with the stange name.
 tree_hash="$(
-    printf '%s %s\0'"$escaped_blob_hash" "$filemode" "$filename" |
+    printf '%s %s\0' "$filemode" "$filename" |
+    cat - blob-hash-bytes |
     git hash-object -t tree -w --stdin
 )"
 
-# Commit the tree as an initial commit, setting the default branch to it.
+rm blob-hash-bytes
+
 commit_hash="$(git commit-tree -m 'Initial commit' "$tree_hash")"
+branch="$(git symbolic-ref --short HEAD)"
 git branch -f -- "$branch" "$commit_hash"
 git show
