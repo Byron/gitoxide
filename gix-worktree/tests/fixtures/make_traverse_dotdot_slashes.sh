fe8c2c939db69ffce855059e2b16be50efcc05e6
Eliah Kagan <degeneracypressure@gmail.com>
2024-05-20 21:28:16 -0400

    Adjust make_traverse_dotdot_slashes.sh for environment
    These are changes that do not significantly affect behavior but use
    the set of tools that should be availalble in testing environments,
    as well as refactorings that are useful to do not before really
    making this usable as a fixture.
    
    - Use bash shebang, enable pipefail.
    - Don't require xxd.
    - Don't create an extra temporary file.
    - Shorten, simplify, and clarify some logic.

--- gix-worktree/tests/fixtures/make_traverse_dotdot_slashes.sh
+++ gix-worktree/tests/fixtures/make_traverse_dotdot_slashes.sh
@@ -1,27 +1,27 @@
-#!/bin/sh
-set -eu
+#!/bin/bash
+set -eu -o pipefail
 
 readonly filename='../outside'
 readonly filemode=100644
 
 emit_payload() {
-    printf 'A file outside the working tree, somehow.\n'
+    echo 'A file outside the working tree, somehow.'
 }
 
 repo="$1"
 git init -- "$repo"
 cd -- "$repo"
 
-blob_hash="$(emit_payload | git hash-object -w --stdin)"
-printf '%s' "$blob_hash" | xxd -r -p >blob-hash-bytes
-
-tree_hash="$(
-    printf '%s %s\0' "$filemode" "$filename" |
-    cat - blob-hash-bytes |
-    git hash-object -t tree -w --stdin --literally
+blob_hash_escaped="$(
+    emit_payload |
+    git hash-object -w --stdin |
+    sed 's/../\\x&/g'
 )"
 
-rm blob-hash-bytes
+tree_hash="$(
+    printf "%s %s\\0$blob_hash_escaped" "$filemode" "$filename" |
+    git hash-object -t tree -w --stdin --literally
+)"
 
 commit_hash="$(git commit-tree -m 'Initial commit' "$tree_hash")"
 branch="$(git symbolic-ref --short HEAD)"
