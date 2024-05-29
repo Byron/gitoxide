8644d0ff8111233a8ffcf722393a98eb83578177
Sebastian Thiel <sebastian.thiel@icloud.com>
2024-05-29 07:06:01 +0200

    avoid condition that is always false in `is_collision_error`

--- gix-fs/src/symlink.rs
+++ gix-fs/src/symlink.rs
@@ -1,17 +1,17 @@
 use std::{io, io::ErrorKind::AlreadyExists, path::Path};
 
-#[cfg(not(windows))]
 /// Create a new symlink at `link` which points to `original`.
 ///
 /// Note that `original` doesn't have to exist.
+#[cfg(not(windows))]
 pub fn create(original: &Path, link: &Path) -> io::Result<()> {
     std::os::unix::fs::symlink(original, link)
 }
 
-#[cfg(not(windows))]
 /// Remove a symlink.
 ///
 /// Note that on only on windows this is special.
+#[cfg(not(windows))]
 pub fn remove(path: &Path) -> io::Result<()> {
     std::fs::remove_file(path)
 }
@@ -31,12 +31,12 @@ pub fn remove(path: &Path) -> io::Result<()> {
     }
 }
 
-#[cfg(windows)]
 /// Create a new symlink at `link` which points to `original`.
 ///
 /// Note that if a symlink target (the `original`) isn't present on disk, it's assumed to be a
 /// file, creating a dangling file symlink. This is similar to a dangling symlink on Unix,
 /// which doesn't have to care about the target type though.
+#[cfg(windows)]
 pub fn create(original: &Path, link: &Path) -> io::Result<()> {
     use std::os::windows::fs::{symlink_dir, symlink_file};
     // TODO: figure out if links to links count as files or whatever they point at
@@ -53,20 +53,20 @@ pub fn create(original: &Path, link: &Path) -> io::Result<()> {
     }
 }
 
-#[cfg(not(windows))]
 /// Return true if `err` indicates that a file collision happened, i.e. a symlink couldn't be created as the `link`
 /// already exists as filesystem object.
+#[cfg(not(windows))]
 pub fn is_collision_error(err: &std::io::Error) -> bool {
     // TODO: use ::IsDirectory as well when stabilized instead of raw_os_error(), and ::FileSystemLoop respectively
     err.kind() == AlreadyExists
-            || err.raw_os_error() == Some(if cfg!(windows) { 5 } else { 21 })
+            || err.raw_os_error() == Some(21)
             || err.raw_os_error() == Some(62) // no-follow on symlnk on mac-os
             || err.raw_os_error() == Some(40) // no-follow on symlnk on ubuntu
 }
 
-#[cfg(windows)]
 /// Return true if `err` indicates that a file collision happened, i.e. a symlink couldn't be created as the `link`
 /// already exists as filesystem object.
+#[cfg(windows)]
 pub fn is_collision_error(err: &std::io::Error) -> bool {
     err.kind() == AlreadyExists || err.kind() == std::io::ErrorKind::PermissionDenied
 }
