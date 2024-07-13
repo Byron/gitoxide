7c8ff669fec7df6cac7f3e5d73e1bb2719d49ec6
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-07-13 10:17:10 +0200

    Start exploring gix APIs in gix-blame

--- gix-blame/tests/blame.rs
+++ gix-blame/tests/blame.rs
@@ -1,4 +1,58 @@
+use std::path::PathBuf;
+
+use gix_odb::pack::FindExt;
+use gix_ref::{file::ReferenceExt, store::WriteReflog};
+
 #[test]
 fn it_works() {
-    let _worktree = gix_testtools::scripted_fixture_read_only("make_blame_repo.sh").unwrap();
+    // TODO
+    // At a high level, what we want to do is the following:
+    //
+    // - get the commit that belongs to a commit id
+    // - walk through parents
+    //   - for each parent, do a diff and mark lines that don’t have a suspect (this is the term
+    //     used in `libgit2`) yet, but that have been changed in this commit
+    //
+    // The algorithm in `libgit2` works by going through parents and keeping a linked list of blame
+    // suspects. It can be visualized as follows:
+    //
+    // <---------------------------------------->
+    // <---------------><----------------------->
+    // <---><----------><----------------------->
+    // <---><----------><-------><-----><------->
+    // <---><---><-----><-------><-----><------->
+    // <---><---><-----><-------><-----><-><-><->
+
+    let worktree = fixture_path();
+
+    let store = gix_ref::file::Store::at(
+        worktree.join(".git"),
+        gix_ref::store::init::Options {
+            write_reflog: WriteReflog::Disable,
+            ..Default::default()
+        },
+    );
+    let odb = odb_at("");
+
+    let mut reference = gix_ref::file::Store::find(&store, "HEAD").unwrap();
+
+    let head = reference.peel_to_id_in_place(&store, &odb).unwrap();
+
+    let mut buffer = Vec::new();
+
+    let (commit, _) = odb.find_commit(&head, &mut buffer).unwrap();
+
+    let last_parent = commit.parents().last().unwrap();
+
+    let mut buffer = Vec::new();
+
+    let (last_parent, _) = odb.find_commit(&last_parent, &mut buffer).unwrap();
+}
+
+fn odb_at(name: &str) -> gix_odb::Handle {
+    gix_odb::at(fixture_path().join(name).join(".git/objects")).unwrap()
+}
+
+fn fixture_path() -> PathBuf {
+    gix_testtools::scripted_fixture_read_only("make_blame_repo.sh").unwrap()
 }
