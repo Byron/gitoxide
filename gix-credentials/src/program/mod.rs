1772f88882824b92fd233bc2148c4ad123740096
Sebastian Thiel <sebastian.thiel@icloud.com>
2024-07-25 19:00:53 +0200

    fix: avoid `as_ref()` call as `bstr` could break downstream with new impls (#1466)
    As `bstr` adds a new implementation of `AsRef`, the `as_ref()` call becomes
    ambiguous which will then break `gitoxide`.
    
    Now `as_ref()` is avoided in favor of a method that can't fail.

--- gix-credentials/src/program/mod.rs
+++ gix-credentials/src/program/mod.rs
@@ -80,7 +80,7 @@ impl Program {
                 args.insert_str(0, "credential-");
                 args.insert_str(0, " ");
                 args.insert_str(0, git_program.to_string_lossy().as_ref());
-                gix_command::prepare(gix_path::from_bstr(args.as_ref()).into_owned())
+                gix_command::prepare(gix_path::from_bstr(args.as_bstr()).into_owned())
                     .arg(action.as_arg(true))
                     .with_shell_allow_argument_splitting()
                     .into()
