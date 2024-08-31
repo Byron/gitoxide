b11f7db7d16c06c040619cf0fb5feab85734bc06
Eliah Kagan <degeneracypressure@gmail.com>
2024-08-31 06:44:33 -0400

    Run `cargo fmt`

--- gix-path/src/env/git/tests.rs
+++ gix-path/src/env/git/tests.rs
@@ -560,7 +560,8 @@ mod exe_info {
     fn first_file_from_config_with_origin() {
         let macos =
             "file:/Applications/Xcode.app/Contents/Developer/usr/share/git-core/gitconfig\0credential.helper\0file:/Users/byron/.gitconfig\0push.default\0";
-        let win_msys = "file:C:/git-sdk-64/etc/gitconfig\0core.symlinks\0file:C:/git-sdk-64/etc/gitconfig\0core.autocrlf\0";
+        let win_msys =
+            "file:C:/git-sdk-64/etc/gitconfig\0core.symlinks\0file:C:/git-sdk-64/etc/gitconfig\0core.autocrlf\0";
         let win_cmd =
             "file:C:/Program Files/Git/etc/gitconfig\0diff.astextplain.textconv\0file:C:/Program Files/Git/etc/gitconfig\0filter.lfs.clean\0";
         let win_msys_old =
